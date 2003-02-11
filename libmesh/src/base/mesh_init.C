6c82799df6b794005d2d185c488bda9e63db32d6
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2003-02-11 22:59:35 +0000

    configure for mpi

--- libmesh/src/base/mesh_init.C
+++ libmesh/src/base/mesh_init.C
@@ -1,4 +1,4 @@
-// $Id: mesh_init.C,v 1.1 2003-02-10 03:55:51 benkirk Exp $
+// $Id: mesh_init.C,v 1.2 2003-02-11 22:59:35 benkirk Exp $
 
 // The Next Great Finite Element Library.
 // Copyright (C) 2002  Benjamin S. Kirk, John W. Peterson
@@ -24,25 +24,34 @@
 #include "mesh_init.h"
 
 
-
-#ifdef HAVE_PETSC
-
-#ifndef USE_COMPLEX_NUMBERS
+#if defined(HAVE_PETSC)
+#  if  !defined(USE_COMPLEX_NUMBERS)
 /**
  * Petsc include files.  PETSc with complex numbers 
  * is actually C++.
  */
-namespace Petsc {
-extern "C" {
-#include <petsc.h>
-}
+namespace Petsc
+{
+  extern "C"
+  {
+#   include <petsc.h>
+  }
 }
 using namespace Petsc;
 // for easy switching between Petsc 2.1.0/2.1.1
 // typedef Scalar PetscScalar;
-#else
-#include <petsc.h>
-#endif 
+#  else
+#    include <petsc.h>
+#  endif
+#elif defined(HAVE_MPI)
+namespace Mpi
+{
+  extern "C"
+  {
+#    include <mpi.h>
+  }
+}
+using namespace Mpi;
 #endif
 
 
@@ -51,12 +60,15 @@ using namespace Petsc;
 void libMesh::init (int argc, char** argv)
 {
 
-#ifdef HAVE_PETSC
+#if defined(HAVE_PETSC)
 
   PetscInitialize (&argc, &argv, NULL, NULL);
+
+#elif defined(HAVE_MPI)
+
+  MPI_Init (&argc, &argv);
   
 #endif
-
   
 };
 
@@ -65,9 +77,13 @@ void libMesh::init (int argc, char** argv)
 void libMesh::close ()
 {
 
-#ifdef HAVE_PETSC
+#if defined(HAVE_PETSC)
 
   PetscFinalize ();
+
+#elif defined(HAVE_MPI)
+
+  MPI_Finalize();
   
 #endif
   
