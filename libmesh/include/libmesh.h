676a60a1c0f4515cdc86e3089c01b9ff3702174d
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2003-04-08 12:46:07 +0000

    fixed doc bug

--- libmesh/include/libmesh.h
+++ libmesh/include/libmesh.h
@@ -1,4 +1,4 @@
-// $Id: libmesh.h,v 1.8 2003-04-08 03:21:35 benkirk Exp $
+// $Id: libmesh.h,v 1.9 2003-04-08 12:46:07 benkirk Exp $
 
 // The Next Great Finite Element Library.
 // Copyright (C) 2002  Benjamin S. Kirk, John W. Peterson
@@ -139,7 +139,7 @@ public:
 
 
   /**
-   * \f$ \zero=0. \f$.
+   * \f$ zero=0. \f$.
    */
   static const Number zero;
 
