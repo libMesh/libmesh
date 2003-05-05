06242249f1f6d11312859710ff67d281f36ec7c6
no_author <no_author@no_author>
2003-05-05 15:30:37 +0000

    This commit was manufactured by cvs2svn to create branch 'libmesh-devel'.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@259 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ branches/libmesh-devel/libmesh/src/mesh/mesh_gmv_support.C
@@ -0,0 +1,661 @@
+// $Id: mesh_gmv_support.C,v 1.16 2003-03-27 18:30:05 spetersen Exp $
+
+// The Next Great Finite Element Library.
+// Copyright (C) 2002  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+// C++ includes
+#include <iomanip>
+#include <fstream>
+#include <stdio.h>
+
+// Local includes
+#include "equation_systems_base.h"
+#include "mesh_base.h"
+#include "elem.h"
+
+
+
+void MeshBase::write_gmv (const std::string& name,
+			  EquationSystemsBase& es,
+			  const bool write_partitioning)
+{
+  std::ofstream out(name.c_str());
+
+  write_gmv (out, es, write_partitioning);
+}
+
+
+
+void MeshBase::write_gmv (std::ostream& out,
+			  EquationSystemsBase& es,
+			  const bool write_partitioning)
+{
+  std::vector<Number> soln;
+  std::vector<std::string> names;
+  
+  es.build_variable_names  (names);
+  es.build_solution_vector (soln);
+
+  if (processor_id() == 0)
+    write_gmv (out, &soln, &names, write_partitioning);
+}
+
+
+
+void MeshBase::write_gmv (const std::string& name,
+			  const std::vector<Number>* v,
+			  const std::vector<std::string>* solution_names,
+			  const bool write_partitioning)
+{
+  std::ofstream out(name.c_str());
+
+  write_gmv (out, v, solution_names, write_partitioning);
+}
+
+
+
+
+void MeshBase::write_gmv(std::ostream& out,
+			 const std::vector<Number>*  v,
+			 const std::vector<std::string>* solution_names,
+			 const bool write_partitioning)
+{
+  assert (out);
+
+  // Begin interfacing with the GMV data file
+  {
+    // write the nodes
+    
+    out << "gmvinput ascii" << std::endl << std::endl;
+    out << "nodes " << n_nodes() << std::endl;
+    for (unsigned int v=0; v<n_nodes(); v++)
+      out << point(v)(0) << " ";
+         
+    out << std::endl;
+    
+    for (unsigned int v=0; v<n_nodes(); v++)
+      out << point(v)(1) << " ";
+    
+    out << std::endl;
+    
+    for (unsigned int v=0; v<n_nodes(); v++)
+      out << point(v)(2) << " ";
+     
+    out << std::endl << std::endl;
+  }
+
+
+  
+  {
+    // write the connectivity
+    
+    out << "cells " << n_active_sub_elem() << std::endl;
+
+    const_active_elem_iterator       it (elements_begin());
+    const const_active_elem_iterator end(elements_end());
+    
+    switch (_dim)
+      {
+      case 1:
+	{
+	  for ( ; it != end; ++it)
+	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
+	      {
+		out << "line 2" << std::endl;
+		std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		for (unsigned int i=0; i<conn.size(); i++)
+		  out << conn[i] << " ";
+		
+		out << std::endl;
+	      }
+	  
+	  break;
+	}
+	
+      case 2:
+	{
+	  for ( ; it != end; ++it)
+	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
+	      {
+		if (((*it)->type() == QUAD4) ||
+		    ((*it)->type() == QUAD8) ||
+		    ((*it)->type() == QUAD9)
+#ifdef ENABLE_INFINITE_ELEMENTS
+		    || ((*it)->type() == INFQUAD4)
+		    || ((*it)->type() == INFQUAD6)
+#endif
+		    )
+		  {
+		    out << "quad 4" << std::endl;
+		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		    for (unsigned int i=0; i<conn.size(); i++)
+		      out << conn[i] << " ";
+		  }
+		  else if (((*it)->type() == TRI3) ||
+			   ((*it)->type() == TRI6))
+		    {
+		      out << "tri 3" << std::endl;
+		      std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		      for (unsigned int i=0; i<3; i++)
+			out << conn[i] << " ";
+		    }
+		else
+		  {
+		    error();
+		  }
+		
+		out << std::endl;
+	      }
+	  
+	  break;
+	}
+	
+	
+      case 3:
+	{
+	  for ( ; it != end; ++it)
+	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
+	      {
+
+#ifndef  ENABLE_INFINITE_ELEMENTS
+		if (((*it)->type() == HEX8)   ||    
+		    ((*it)->type() == HEX27))
+		  {
+		    out << "phex8 8" << std::endl;
+		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		    for (unsigned int i=0; i<conn.size(); i++)
+		      out << conn[i] << " ";
+		  }
+		
+		else if ((*it)->type() == HEX20)
+		  {
+		    out << "phex20 20" << std::endl;
+		    out << (*it)->node(0)+1  << " "
+			<< (*it)->node(1)+1  << " "
+			<< (*it)->node(2)+1  << " "
+			<< (*it)->node(3)+1  << " "
+			<< (*it)->node(4)+1  << " "
+			<< (*it)->node(5)+1  << " "
+			<< (*it)->node(6)+1  << " "
+			<< (*it)->node(7)+1  << " "
+			<< (*it)->node(8)+1  << " "
+			<< (*it)->node(9)+1  << " "
+			<< (*it)->node(10)+1 << " "
+			<< (*it)->node(11)+1 << " "
+			<< (*it)->node(16)+1 << " "
+			<< (*it)->node(17)+1 << " "
+			<< (*it)->node(18)+1 << " "
+			<< (*it)->node(19)+1 << " "
+			<< (*it)->node(12)+1 << " "
+			<< (*it)->node(13)+1 << " "
+			<< (*it)->node(14)+1 << " "
+			<< (*it)->node(15)+1 << " ";
+		  }
+#else
+		/*
+		 * In case of infinite elements, HEX20
+		 * should be handled just like the
+		 * INFHEX16, since these connect to each other
+		 */
+		if (((*it)->type() == HEX8)     ||
+		    ((*it)->type() == HEX27)    ||
+		    ((*it)->type() == INFHEX8)  ||
+		    ((*it)->type() == INFHEX16) ||
+		    ((*it)->type() == INFHEX18) ||
+		    ((*it)->type() == HEX20))
+		  {
+		    out << "phex8 8" << std::endl;
+		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		    for (unsigned int i=0; i<conn.size(); i++)
+		      out << conn[i] << " ";
+		  }
+#endif
+		
+		else if (((*it)->type() == TET4)  ||
+			 ((*it)->type() == TET10))
+		  {
+		    out << "tet 4" << std::endl;
+		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		    out << conn[0] << " "
+			<< conn[2] << " "
+			<< conn[1] << " "
+			<< conn[4] << " ";
+		    }
+
+		else if ((*it)->type() == PRISM6)
+		  {
+		    /**
+		     * Note that here PRISM6 is treted as
+		     * a degenerated phex8.
+		     */
+		    out << "phex8 8" << std::endl;
+		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+		    for (unsigned int i=0; i<conn.size(); i++)
+		      out << conn[i] << " ";
+		  }
+		
+		else
+		  {
+		    error();
+		  }
+		
+		out << std::endl;
+	      }
+	  
+	  break;
+	}
+	
+      default:
+	error();
+      }
+    
+    out << std::endl;
+  }
+  
+
+  
+  // optionally write the partition information
+  if (write_partitioning)
+    {
+      out << "material "
+	  << n_processors()
+	  << " 0"<< std::endl;
+
+      for (unsigned int sbd=0; sbd<n_processors(); sbd++)
+	out << "sbd_" << sbd << std::endl;
+      
+      const_active_elem_iterator       it (elements_begin());
+      const const_active_elem_iterator end(elements_end());
+
+      for ( ; it != end; ++it)
+	for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
+	    out << (*it)->processor_id()+1 << std::endl;
+      
+      out << std::endl;
+    }
+
+
+  
+  // optionally write the data
+  if ((solution_names != NULL) &&
+      (v != NULL))
+    {      
+      const unsigned int n_vars = solution_names->size();
+      
+      assert (v->size() == n_nodes()*n_vars);
+
+      out << "variable" << std::endl;
+
+
+      for (unsigned int c=0; c<n_vars; c++)
+	{
+
+#ifdef USE_COMPLEX_NUMBERS
+
+	  // in case of complex data, write _tree_ data sets
+	  // for each component
+
+	  // this is the real part
+	  out << "r_" << (*solution_names)[c] << " 1" << std::endl;
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    out << std::setprecision(10) << (*v)[n*n_vars + c].real() << " ";
+	  
+	  out << std::endl << std::endl;
+
+
+	  // this is the imaginary part
+	  out << "i_" << (*solution_names)[c] << " 1" << std::endl;
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    out << std::setprecision(10) << (*v)[n*n_vars + c].imag() << " ";
+	  
+	  out << std::endl << std::endl;
+
+	  // this is the magnitude
+	  out << "a_" << (*solution_names)[c] << " 1" << std::endl;
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    out << std::setprecision(10)
+		<< std::abs((*v)[n*n_vars + c]) << " ";
+
+	  out << std::endl << std::endl;
+
+#else
+
+	  out << (*solution_names)[c] << " 1" << std::endl;
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    out << std::setprecision(10) << (*v)[n*n_vars + c] << " ";
+	  
+	  out << std::endl << std::endl;
+
+#endif
+
+	}
+      
+      out << "endvars" << std::endl;
+    }
+
+  
+  // end of the file
+  out << std::endl << "endgmv" << std::endl;
+}
+
+
+
+
+void MeshBase::write_gmv_binary (const std::string& name,
+				 EquationSystemsBase& es,
+				 const bool write_partitioning)
+{
+  std::ofstream out(name.c_str());
+
+  write_gmv_binary (out, es, write_partitioning);
+}
+
+
+
+void MeshBase::write_gmv_binary (std::ostream& out,
+				 EquationSystemsBase& es,
+				 const bool write_partitioning)
+{
+  std::vector<Number> soln;
+  std::vector<std::string> names;
+
+  es.build_variable_names  (names);
+  es.build_solution_vector (soln);
+  
+  if (processor_id() == 0)
+    write_gmv_binary (out, &soln, &names, write_partitioning);
+}
+
+
+
+void MeshBase::write_gmv_binary (const std::string& name,
+				 const std::vector<Number>* v,
+				 const std::vector<std::string>* solution_names,
+				 const bool write_partitioning)
+{
+  std::ofstream out (name.c_str(), std::ios::out|std::ios::binary);
+  
+  write_gmv_binary (out, v, solution_names, write_partitioning);
+}
+
+
+
+
+void MeshBase::write_gmv_binary(std::ostream& out,
+				const std::vector<Number>* v,
+				const std::vector<std::string>* solution_names,
+				const bool write_partitioning)
+{
+  assert (out);
+
+  char buf[80];
+
+  // Begin interfacing with the GMV data file
+  {
+    // write the nodes
+    strcpy(buf, "gmvinput");
+    out.write(buf, strlen(buf));
+	      
+    strcpy(buf, "ieeei4r4");
+    out.write(buf, strlen(buf));
+
+  }
+
+
+  
+  // write the nodes
+  {
+    strcpy(buf, "nodes   ");
+    out.write(buf, strlen(buf));
+
+    unsigned int tempint = n_nodes();
+    
+    memcpy(buf, &tempint, sizeof(unsigned int));
+
+    out.write(buf, sizeof(unsigned int));
+
+    // write the x coordinate
+    float *temp = new float[n_nodes()];
+    for (unsigned int v=0; v<n_nodes(); v++)
+      temp[v] = static_cast<float>(point(v)(0));
+    out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+    for (unsigned int v=0; v<n_nodes(); v++)
+      temp[v] = static_cast<float>(point(v)(1));
+    out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+    for (unsigned int v=0; v<n_nodes(); v++)
+      temp[v] = static_cast<float>(point(v)(2));
+    out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+    delete [] temp;
+  }
+
+
+  
+  // write the connectivity
+  {
+    strcpy(buf, "cells   ");
+    out.write(buf, strlen(buf));
+
+    unsigned int tempint = n_active_sub_elem();
+    
+    memcpy(buf, &tempint, sizeof(unsigned int));
+    
+    out.write(buf, sizeof(unsigned int));
+
+    const_active_elem_iterator       it (elements_begin());
+    const const_active_elem_iterator end(elements_end());
+
+    switch (_dim)
+      {
+
+      case 1:
+       
+	for ( ; it != end; ++it)
+	  for(unsigned se = 0; se < (*it)->n_sub_elem(); ++se)
+	    {
+	      strcpy(buf, "line    ");
+	      out.write(buf, strlen(buf));
+	      
+	      tempint = 2;
+	      memcpy(buf, &tempint, sizeof(unsigned int));
+	      out.write(buf, sizeof(unsigned int));
+	      
+	      std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+	      
+	      out.write(reinterpret_cast<char*>(&conn[0]), sizeof(unsigned int)*tempint);
+	    }
+	
+	break;
+	
+      case 2:
+       
+      for ( ; it != end; ++it)
+	for(unsigned se = 0; se < (*it)->n_sub_elem(); ++se)
+	  {
+	    strcpy(buf, "quad    ");
+	    out.write(buf, strlen(buf));
+	    
+	    tempint = 4;
+	    memcpy(buf, &tempint, sizeof(unsigned int));
+	    out.write(buf, sizeof(unsigned int));
+		
+	    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+	    
+	    out.write(reinterpret_cast<char*>(&conn[0]), sizeof(unsigned int)*tempint);
+	  }
+	
+	break;
+	
+      case 3:
+       
+      for ( ; it != end; ++it)
+	for(unsigned se = 0; se < (*it)->n_sub_elem(); ++se)
+	  {
+	    strcpy(buf, "phex8   ");
+	    out.write(buf, strlen(buf));
+	    
+	    tempint = 8;
+	    memcpy(buf, &tempint, sizeof(unsigned int));
+	    out.write(buf, sizeof(unsigned int));
+	    
+	    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
+	    
+	    out.write(reinterpret_cast<char*>(&conn[0]), sizeof(unsigned int)*tempint);
+	  }
+      
+	break;
+	
+      default:
+	error();
+	
+      }
+  }
+  
+  
+  
+  // optionally write the partition information
+  if (write_partitioning)
+    {
+      strcpy(buf, "material");
+      out.write(buf, strlen(buf));
+      
+      unsigned int tmpint = n_processors();
+      memcpy(buf, &tmpint, sizeof(unsigned int));
+      out.write(buf, sizeof(unsigned int));
+
+      tmpint = 0; // IDs are cell based
+      memcpy(buf, &tmpint, sizeof(unsigned int));
+      out.write(buf, sizeof(unsigned int));
+
+
+      for (unsigned int sbd=0; sbd<n_processors(); sbd++)
+	{
+	  sprintf(buf, "sbd_%d", sbd);
+	  out.write(buf, 8);
+	}
+
+      unsigned int* sbd_id = new unsigned int[n_active_sub_elem()];
+      
+      unsigned int n=0;
+      
+      const_active_elem_iterator       it (elements_begin());
+      const const_active_elem_iterator end(elements_end());
+
+      for ( ; it != end; ++it)
+	for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
+	  sbd_id[n++] = (*it)->processor_id()+1;
+      
+      
+      out.write(reinterpret_cast<char *>(sbd_id), sizeof(unsigned int)*n_active_sub_elem());
+
+      delete [] sbd_id;
+    }
+
+
+  
+  // optionally write the data
+  if ((solution_names != NULL) &&
+      (v != NULL))
+    {
+      strcpy(buf, "variable");
+      out.write(buf, strlen(buf));
+      
+      float *temp = new float[n_nodes()];
+
+      const unsigned int n_vars = solution_names->size();
+      
+      for (unsigned int c=0; c<n_vars; c++)
+	{
+
+#ifdef USE_COMPLEX_NUMBERS
+	  // for complex data, write two datasets
+
+
+	  // Real part
+	  strcpy(buf, "r_");
+	  out.write(buf, 2);
+	  strcpy(buf, (*solution_names)[c].c_str());
+	  out.write(buf, 6);
+	  
+	  unsigned int tempint = 1; // always do nodal data
+	  memcpy(buf, &tempint, sizeof(unsigned int));
+	  out.write(buf, sizeof(unsigned int));
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    temp[n] = static_cast<float>( (*v)[n*n_vars + c].real() );
+	  
+	  out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+
+	  // imaginary part
+	  strcpy(buf, "i_");
+	  out.write(buf, 2);
+	  strcpy(buf, (*solution_names)[c].c_str());
+	  out.write(buf, 6);
+	  
+	  memcpy(buf, &tempint, sizeof(unsigned int));
+	  out.write(buf, sizeof(unsigned int));
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    temp[n] = static_cast<float>( (*v)[n*n_vars + c].imag() );
+	  
+	  out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+
+#else
+
+
+	  strcpy(buf, (*solution_names)[c].c_str());
+	  out.write(buf, 8);
+	  
+	  unsigned int tempint = 1; // always do nodal data
+	  memcpy(buf, &tempint, sizeof(unsigned int));
+	  out.write(buf, sizeof(unsigned int));
+	  
+	  for (unsigned int n=0; n<n_nodes(); n++)
+	    temp[n] = static_cast<float>((*v)[n*n_vars + c]);
+	  
+	  out.write(reinterpret_cast<char *>(temp), sizeof(float)*n_nodes());
+
+
+#endif
+
+	  
+	}
+    
+      delete [] temp;
+      
+      strcpy(buf, "endvars ");
+      out.write(buf, strlen(buf));
+
+    }
+
+  // end the file
+  strcpy(buf, "endgmv  ");
+  out.write(buf, strlen(buf));
+}
+
+
+
+
---
+++ branches/libmesh-devel/libmesh/src/numerics/laspack_vector.C
@@ -0,0 +1,313 @@
+// $Id: laspack_vector.C,v 1.12 2003-04-29 21:39:36 benkirk Exp $
+
+// The Next Great Finite Element Library.
+// Copyright (C) 2002  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+// C++ includes
+#include <math.h>
+
+// Local Includes
+#include "laspack_vector.h"
+#include "laspack_matrix.h"
+
+
+#ifdef HAVE_LASPACK
+
+
+
+// template <typename T>
+// void LaspackVector<T> ::init (const NumericVector<T>& v, const bool fast)
+// {
+//   error();
+  
+//   init (v.local_size(), v.size(), fast);
+
+//   vec = dynamic_cast<const LaspackVector<T>&>(v).vec;
+// }
+
+
+
+template <typename T>
+Real LaspackVector<T>::l1_norm () const
+{
+  assert(this->closed());
+  
+  return static_cast<Real>(l1Norm_V(const_cast<QVector*>(&_vec)));
+}
+
+
+
+template <typename T>
+Real LaspackVector<T>::l2_norm () const
+{
+  assert(this->closed());
+  
+  return static_cast<Real>(l2Norm_V(const_cast<QVector*>(&_vec)));
+}
+
+
+
+template <typename T>
+Real LaspackVector<T>::linfty_norm () const
+{
+  assert(this->closed());
+  
+  return static_cast<Real>(MaxNorm_V(const_cast<QVector*>(&_vec)));
+}
+
+
+
+
+template <typename T>
+NumericVector<T>& LaspackVector<T>::operator += (const NumericVector<T>& v)
+{
+  assert(this->closed());
+  
+  add(1., v);
+  
+  return *this;
+}
+
+
+
+
+template <typename T>
+NumericVector<T>& LaspackVector<T>::operator -= (const NumericVector<T>& v)
+{
+  assert(this->closed());
+  
+  add(-1., v);
+  
+  return *this;
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::add (const T v)
+{
+  for (unsigned int i=0; i<size(); i++)
+    add (i, v);
+}
+
+
+
+
+template <typename T>
+void LaspackVector<T>::add (const NumericVector<T>& v)
+{
+  add (1., v);
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::add (const T a, const NumericVector<T>& v_in)
+{
+  const LaspackVector& v = dynamic_cast<const LaspackVector&>(v_in);
+  
+  assert(size() == v.size());
+
+  for (unsigned int i=0; i<v.size(); i++)
+    add (i, a*v(i));
+}
+
+
+
+template <typename T> 
+void LaspackVector<T>::add_vector (const NumericVector<T> &vec_in,
+				   SparseMatrix<T> &mat_in)
+{
+  // Convert from input types
+  const LaspackVector<T> &vec = dynamic_cast<const LaspackVector<T>&>(vec_in);
+  LaspackMatrix<T>       &mat = dynamic_cast<LaspackMatrix<T>&>      (mat_in);
+
+  // += mat*vec
+  AddAsgn_VV (&_vec, Mul_QV(&mat._QMat, const_cast<QVector*>(&vec._vec)));
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::scale (const T factor)
+{
+  assert (this->initialized());
+  
+  Mul_SV (factor, &_vec);
+}
+
+
+
+template <typename T>
+NumericVector<T>& 
+LaspackVector<T>::operator = (const T s)
+{
+  assert (this->initialized());
+
+  V_SetAllCmp (&_vec, s);
+  
+  return *this;
+}
+
+
+
+template <typename T>
+NumericVector<T>&
+LaspackVector<T>::operator = (const NumericVector<T>& v_in)
+{
+  const LaspackVector& v = dynamic_cast<const LaspackVector&>(v_in);
+
+  *this = v;
+
+  return *this;
+}
+
+
+
+template <typename T>
+LaspackVector<T>&
+LaspackVector<T>::operator = (const LaspackVector<T>& v)
+{
+  _is_closed      = v._is_closed;
+  _is_initialized = v._is_initialized;
+
+  if (v.size() != 0)    
+    Asgn_VV (const_cast<QVector*>(&_vec),
+		      const_cast<QVector*>(&v._vec)
+		      );
+  return *this;
+}
+
+
+
+template <typename T>
+NumericVector<T>&
+LaspackVector<T>::operator = (const std::vector<T>& v)
+{
+  /**
+   * Case 1:  The vector is the same size of
+   * The global vector.  Only add the local components.
+   */
+  if (size() == v.size())      
+    for (unsigned int i=0; i<v.size(); i++)
+      set (i, v[i]);
+  
+  else
+    error();
+
+  return *this;
+}
+
+
+template <typename T>
+void LaspackVector<T>::localize (NumericVector<T>& v_local_in) const
+{
+  LaspackVector& v_local =
+    dynamic_cast<LaspackVector&>(v_local_in);
+
+  v_local = *this;
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::localize (NumericVector<T>& v_local_in,
+				 const std::vector<unsigned int>& send_list) const
+{
+  LaspackVector& v_local =
+    dynamic_cast<LaspackVector&>(v_local_in);
+
+  assert (send_list.size() == v_local.size());
+
+  v_local = *this;
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::localize (std::vector<T>& v_local) const
+
+{
+  v_local.resize(size());
+
+  for (unsigned int i=0; i<v_local.size(); i++)
+    v_local[i] = (*this)(i);  
+}
+
+
+
+template <typename T>
+void LaspackVector<T>::localize_to_one (std::vector<T>& v_local,
+					const unsigned int pid) const
+{
+  assert (pid == 0);
+
+  localize (v_local);
+}
+
+
+// Full specialization for Real datatypes
+#ifdef USE_REAL_NUMBERS
+
+template <> 
+inline
+Real LaspackVector<Real>::min () const
+{
+  assert (this->initialized());
+
+  Real min = 1.e30;
+
+  for (unsigned int i=0; i<size(); i++)
+    min = std::min (min, (*this)(i));
+
+  return min;
+}
+
+
+#endif
+
+
+
+// Full specialization for Complex datatypes
+#ifdef USE_COMPLEX_NUMBERS
+
+template <> 
+inline
+Real LaspackVector<Complex>::min () const
+{
+  assert (this->initialized());
+
+  Real min = 1.e30;
+
+  for (unsigned int i=0; i<size(); i++)
+    min = std::min (min, (*this)(i).real() );
+
+  return min;
+}
+
+#endif
+
+
+
+//------------------------------------------------------------------
+// Explicit instantiations
+template class LaspackVector<Number>;
+ 
+
+#endif // #ifdef HAVE_LASPACK
