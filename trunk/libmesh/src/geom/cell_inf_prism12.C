fe8f940a4784fbc5fd934f7e9bc915e0bf9269eb
Daniel Dreyer <d.dreyer@tu-harburg.de>
2003-07-12 16:33:18 +0000

    fixed the ::build_side() and ::side() for infinite cells.  the BoundaryMesh now works with infinite elements, too... yields cool pic's
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@386 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/geom/cell_inf_prism12.C
+++ trunk/libmesh/src/geom/cell_inf_prism12.C
@@ -1,4 +1,4 @@
-// $Id: cell_inf_prism12.C,v 1.15 2003-03-11 00:47:42 ddreyer Exp $
+// $Id: cell_inf_prism12.C,v 1.16 2003-07-12 16:33:18 ddreyer Exp $
 
 // The Next Great Finite Element Library.
 // Copyright (C) 2002  Benjamin S. Kirk, John W. Peterson
@@ -61,8 +61,8 @@ AutoPtr<Elem> InfPrism12::build_side (const unsigned int i) const
 	
 	face->set_node(0) = this->get_node(0);
 	face->set_node(1) = this->get_node(1);
-	face->set_node(2) = this->get_node(4);
-	face->set_node(3) = this->get_node(3);
+	face->set_node(2) = this->get_node(3);
+	face->set_node(3) = this->get_node(4);
 	face->set_node(4) = this->get_node(6);
 	face->set_node(5) = this->get_node(9);
 	
@@ -75,8 +75,8 @@ AutoPtr<Elem> InfPrism12::build_side (const unsigned int i) const
 
 	face->set_node(0) = this->get_node(1);
 	face->set_node(1) = this->get_node(2);
-	face->set_node(2) = this->get_node(5);
-	face->set_node(3) = this->get_node(4);
+	face->set_node(2) = this->get_node(4);
+	face->set_node(3) = this->get_node(5);
 	face->set_node(4) = this->get_node(7);
 	face->set_node(5) = this->get_node(10);
 
@@ -89,8 +89,8 @@ AutoPtr<Elem> InfPrism12::build_side (const unsigned int i) const
 
 	face->set_node(0) = this->get_node(2);
 	face->set_node(1) = this->get_node(0);
-	face->set_node(2) = this->get_node(3);
-	face->set_node(3) = this->get_node(5);
+	face->set_node(2) = this->get_node(5);
+	face->set_node(3) = this->get_node(3);
 	face->set_node(4) = this->get_node(8);
 	face->set_node(5) = this->get_node(11);
 	
