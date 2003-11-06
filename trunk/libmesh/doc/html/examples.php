637c0539ab45d7bb9d41b6b787323f0fd940a245
John Peterson <jwpeterson@gmail.com>
2003-11-06 21:32:08 +0000

    Checked in updated code for the examples title page.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@520 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/examples.php
+++ trunk/libmesh/doc/html/examples.php
@@ -11,23 +11,33 @@
 <?php make_navigation("examples",$root)?>
 
 <div class="content">
-<h1>This is the starting point for examples</h1>
-<ul>
-  <li>    Foo </li>
-  <li>    Bar </li>
-</ul>
+<h1>A Series of Example Programs</h1>
+The following series of example programs have been
+designed to get you started on the right foot.
+For the most part they are arranged in order of
+increasing complexity, and could be attempted in
+that order.  Click the links below, or use the
+menu on the left to navigate the examples.
+<br>
+<br>
 
-<h2>Sub-Heading 1</h2>
-<ul>
-  <li> Foo </li>
-  <li> Bar </li>
-</ul>
+<h2><a href="ex1.php">Example 1</a> -- Creation of a Mesh Object</h2>
+This is the first example program.  It simply demonstrates
+how to create a mesh object.  A mesh is read from file,
+information is printed to the screen, and the mesh is then
+written.
 
-<h2>Sub-Heading 2 </h2>
-<ul>
-  <li> Foo </li>
-  <li> Bar </li>
-</ul>
+<br>
+<br>
+
+<h2><a href="ex2.php">Example 2</a> -- Using PETSc to Solve a Simple System</h2>
+The second example program demonstrates how to
+create an equation system for a simple scalar system.  This
+example will also introduce some of the issues involved with using Petsc
+in your application.
+
+<br>
+<br>
 
 </div>
 
@@ -40,3 +50,10 @@
 
 </body>
 </html>
+
+
+<?php if (0) { ?>
+# Local Variables:
+# mode: html
+# End:
+<?php } ?>
