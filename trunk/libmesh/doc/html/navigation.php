cf03cc515fbff3cf505a55d4869c95e6bee99f4d
John Peterson <jwpeterson@gmail.com>
2003-11-07 03:13:42 +0000

    Added download and mailing list links to the home page.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@524 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/navigation.php
+++ trunk/libmesh/doc/html/navigation.php
@@ -26,7 +26,8 @@
     <?php echo "<a class=\"L1\" href = \"", $root, "publications.php\">Publications</a><BR>"; ?>
     <?php echo "<a class=\"L1\" href = \"", $root, "developers.php\">Developers</a><BR>"; ?>
     <?php echo "<a class=\"L1\" href = \"", $root, "doxygen/index.html\">Class Docs</a><BR>"; ?>
-
+    <?php echo "<a class=\"L1\" href = \"", $root, "http://sourceforge.net/project/showfiles.php?group_id=71130\">Download</a><BR>" ?>
+    <?php echo "<a class=\"L1\" href = \"", $root, "http://sourceforge.net/mail/?group_id=71130\">Mailing Lists</a><BR>" ?>
 </div>
 <?php } ?>
 
