b1610121ae27700eea33a0eaf0a19d8b5f958476
John Peterson <jwpeterson@gmail.com>
2003-11-07 22:14:19 +0000

    Added the developers page with pictures.  More to come.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@528 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/developers.php
+++ trunk/libmesh/doc/html/developers.php
@@ -10,20 +10,61 @@
 <body>
 <?php make_navigation("developers",$root)?>
 
+<?php function dev_table_entry($pic, $name, $homepage, $titles, $institutions) { ?>
+<table>
+<tr>
+  <td>
+    <?php echo "<img src=\"$pic\">"; ?>
+  </td>
+  <td>
+  <?php echo "Name: <a href=\"$homepage\">$name</a> <br>"; ?>
+  <?php echo "Title(s): $titles <br>"; ?>
+  <?php echo "Institution(s): $institutions"; ?>
+  </td>
+</tr>
+</table>
+<?php } ?>
+
 <div class="content">
 <h1>libMesh Developers</h1>
-<ul>
-  <li> foo
-</ul>
-</div>
+
+<?php
+dev_table_entry("images/benkirk.jpg",
+                "Benjamin S. Kirk",
+                "http://www.cfdlab.ae.utexas.edu/~benkirk",
+                "PhD Student, libMesh Project Manager",
+	        "University of Texas at Austin");
+?>
 
 <br>
-<br>
-<br>
-<!--
-<div id="navBeta">
+
+<?php
+dev_table_entry("images/jwpeterson.jpg",
+                "John W. Peterson",
+                "http://www.cfdlab.ae.utexas.edu/~peterson",
+		"PhD Student, libMesh Developer",
+		"University of Texas at Austin");
+?>
+
+<?php
+dev_table_entry("images/mikeando.jpg",
+                "Michael L. Anderson",
+                "http://www.cfdlab.ae.utexas.edu/~michaela",
+		"Post-doctoral Fellow, libMesh Developer",
+		"University of Texas at Austin, University of Western Australia");
+?>
+
+
+<?php
+dev_table_entry("images/bbarth.jpg",
+                "William L. Barth",
+                "http://www.cfdlab.ae.utexas.edu/~bbarth",
+		"PhD Student, libMesh Developer",
+		"University of Texas at Austin");
+?>
+
 </div>
--->
+
 
 <?php make_footer() ?>
 
