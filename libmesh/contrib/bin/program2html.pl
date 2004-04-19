ea54711076995c3d6c5aa2caf62eb2490306614b
John W. Peterson <jwpeterson@users.sourceforge.net>
2004-04-19 17:08:20 +0000

    Fixed bug with program2html.pl which prevented some include files from showing up in the docs.

--- libmesh/contrib/bin/program2html.pl
+++ libmesh/contrib/bin/program2html.pl
@@ -5,7 +5,8 @@
 # print "<h1> The commented program</h1>\n";
 
 # ignore comments at the start of the program. this includes CVS
-# tags and copyright notices.
+# tags and copyright notices.  It also specifically skips lines
+# beginning with C-style comments.
 $_ = <>;
 while ( m!^/[/\*]!  ||  m/^$/ ) {
     $_ = <>;
@@ -17,15 +18,28 @@ $comment_mode = 0;
 $program_mode = 1;
 $state =  $comment_mode;
 
+# Assume every program starts off in comment mode
 print "<div class = \"comment\">\n";
 
 do {
-    # substitute special characters
-    # s/&/&amp;/g;
-#     s/</&lt;/g;
-#     s/>/&gt;/g;
-#     s/\t/        /g;
+    # Since we are starting out in comment mode and our comments
+    # sometimes contain html, we don't necessarily want to replace
+    # the special characters.  For example, we use <h1> tags in
+    # the title and don't want it replaced by &lt;h1&gt;.
 
+    # Replace the ampersand character with &amp;
+    # s/&/&amp;/g;
+
+    # Replace "less than" and "greater than"
+    # symbols with &lt; &gt;, respectively
+    #s/</&lt;/g;
+    #s/>/&gt;/g;
+
+    # Replace tab characters with eight spaces
+    # s/\t/        /g;
+
+
+    # Switch into comment mode if a line begins with //
     if (($state == $program_mode) && m!^\s*//!)
     {     
 	$state = $comment_mode;
@@ -36,6 +50,8 @@ do {
 	# Begin a new comment division
 	print "<div class = \"comment\">\n";
     }
+
+
     
     # if in comment mode and no comment line: toggle state.
     # don't do so, if only a blank line
@@ -51,13 +67,23 @@ do {
 	# Also start a <pre> block
 	print "<pre>\n";
 
-	# substitute special characters
-	s/&/&amp;/g;
+	# substitute special characters.  Note: don't replace
+	# the ampersand character!  You might already be using
+	# it for special characters like &lt; &rt;, etc!
+	# s/&/&amp;/g;
 	s/</&lt;/g;
 	s/>/&gt;/g;
 	s/\t/        /g;
     }
-    
+
+    # If we are still in program mode, replace special code characters
+    # i.e. less-than and greater-than symbols by their html representations.
+    if ($state == $program_mode)
+    {
+	s/</&lt;/g;
+	s/>/&gt;/g;
+    }
+
     if ($state == $comment_mode) 
     {
 	# in comment mode: first skip leading whitespace and
@@ -84,6 +110,9 @@ do {
 	print "        $_";
     }
 } while (<>);
+
+
+
 
 
 # Close off the last code section
