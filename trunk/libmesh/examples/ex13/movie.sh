7f45f5ae3341c89fcfe235642c0c46e5d06df51d
John Peterson <jwpeterson@gmail.com>
2004-07-13 20:10:45 +0000

    Added option for non-glx gmv.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@767 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/examples/ex13/movie.sh
+++ trunk/libmesh/examples/ex13/movie.sh
@@ -3,8 +3,13 @@
 # mode of gmv.
 # Set window size: -w 639 100 640 480
 
+# Change this line if you are using the Mesa-enabled
+# version of gmv.
+gmv=gmv;
+#gmv=linuxMesa;
+
 for i in *.gmv.*; do
-    gmv -m -a gmv_attributes.attr  -i $i -s $i.rgb;
+    $gmv -m -a gmv_attributes.attr  -i $i -s $i.rgb;
 done
 
 # for i in *.gmv.*.rgb; do
