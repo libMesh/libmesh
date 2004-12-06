fc2838c71035115ccad4d02a365916a93554d090
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2004-12-06 04:52:41 +0000

    improved the Parameters class

--- libmesh/include/utils/parameters.h
+++ libmesh/include/utils/parameters.h
@@ -1,4 +1,4 @@
-// $Id: parameters.h,v 1.1 2004-11-19 13:57:52 benkirk Exp $
+// $Id: parameters.h,v 1.2 2004-12-06 04:52:41 benkirk Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2004  Benjamin S. Kirk, John W. Peterson
@@ -23,11 +23,13 @@
 #define __parameters_h__
 
 // C++ includes
+#include <typeinfo>
 #include <string>
 #include <map>
 
 // Local includes
 #include "libmesh_common.h"
+#include "reference_counted_object.h"
 
 
 
@@ -39,7 +41,7 @@
  *
  * \author Benjamin S. Kirk
  * \date 2004
- * \version $Revision: 1.1 $
+ * \version $Revision: 1.2 $
  */
 
 // ------------------------------------------------------------
@@ -48,61 +50,322 @@ class Parameters
 {
 public:
 
+  /**
+   * Default constructor.  Does nothing.
+   */
+  Parameters () {};
+
+  /**
+   * Copy constructor.
+   */
+  Parameters (const Parameters&);
+
+  /**
+   * Destructor.  Clears any allocated memory.
+   */
+  ~Parameters ();
+
+  /**
+   * Assignment operator.
+   */
+  Parameters& operator= (const Parameters&);
+  
+  /**
+   * @returns \p true if a parameter of type \p T
+   * with a specified name exists, \p false otherwise.
+   */
   template <typename T>
   bool have_parameter (const std::string&) const;
 
+  /**
+   * @returns a constant reference to the specified parameter
+   * value.  Requires, of course, that the parameter exists.
+   */
   template <typename T>
   const T& get (const std::string&) const;
-  
+
+  /**
+   * @returns a writeable reference to the specified parameter.
+   * This method will create the parameter if it does not exist,
+   * so it can be used to define parameters which will later be
+   * accessed with the \p get() member.
+   */
   template <typename T>
   T& set (const std::string&);
 
-  void clear () { error(); }
+  /**
+   * Clears internal data structures & frees any allocated memory.
+   */
+  void clear ();
+
+  /**
+   * Prints the contents to the specified stream.
+   */
+  void print (std::ostream& os=std::cout) const;
+
   
 private:
 
-  mutable std::map<std::string, int>         _int;
-  mutable std::map<std::string, Real>        _real;
-  mutable std::map<std::string, Complex>     _complex;
-  mutable std::map<std::string, std::string> _string;
+  /**
+   * Abstract definition of a parameter value.
+   */
+  class Value
+  {
+  public:
+
+    /**
+     * Destructor.
+     */
+    virtual ~Value() {};
+
+    /**
+     * String identifying the type of parameter stored.
+     * Must be reimplemented in derived classes.
+     */
+    virtual std::string type () const = 0;
+
+    /**
+     * Prints the parameter value to the specified stream.
+     * Must be reimplemented in derived classes.
+     */
+    virtual void print(std::ostream&) const = 0;
+
+    /**
+     * Clone this value.  Useful in copy-construction.
+     * Must be reimplemented in derived classes.
+     */
+    virtual Value* clone () const = 0;
+  };
+
+
+  /**
+   * Concrete definition of a parameter value
+   * for a specified type.
+   */
+  template <typename T>
+  struct Parameter : public Value,
+                     public ReferenceCountedObject<Parameter<T> >
+  {
+  public:
+
+    /**
+     * @returns a read-only reference to the parameter value.
+     */
+    const T& get () const { return _value; }
+    
+    /**
+     * @returns a writeable reference to the parameter value.
+     */
+    T& set () { return _value; }
+
+    /**
+     * String identifying the type of parameter stored.
+     */
+    virtual std::string type () const;
+
+    /**
+     * Prints the parameter value to the specified stream.
+     */
+    virtual void print(std::ostream&) const;
+
+    /**
+     * Clone this value.  Useful in copy-construction.
+     */
+    virtual Value* clone () const;
+    
+  private:
+
+    /**
+     * Stored parameter value.
+     */
+    T _value;
+  };
+
+  /**
+   * Data structure to map names with values.
+   */
+  std::map<std::string, Value*> _values;
+
+  /**
+   * Parameter map iterator.
+   */
+  typedef std::map<std::string, Value*>::iterator iterator;
+
+  /**
+   * Constant parameter map iterator.
+   */
+  typedef std::map<std::string, Value*>::const_iterator const_iterator;
 };
 
 
 
 // ------------------------------------------------------------
-// Parameters class inline methods
-template <>
+// Parameters::Parameter<> class inline methods
+template <typename T>
 inline
-bool Parameters::have_parameter<int>(const std::string& name) const
+std::string Parameters::Parameter<T>::type () const
 {
-  return _int.count(name);
+  return typeid(T).name();
 }
 
 
 
-template <>
+template <typename T>
 inline
-const int& Parameters::get (const std::string& name) const
+void Parameters::Parameter<T>::print (std::ostream& os) const
 {
-  if (!this->have_parameter<int>(name))
+  os << _value;
+}
+
+
+
+template <typename T>
+inline
+Parameters::Value* Parameters::Parameter<T>::clone () const
+{
+  Parameters::Parameter<T>
+    *copy = new Parameters::Parameter<T>;
+
+  assert (copy != NULL);
+  
+  copy->_value = _value;
+
+  return copy;
+}
+
+
+// ------------------------------------------------------------
+// Parameters class inline methods
+inline
+void Parameters::clear () // since this is inline we must define it
+{                         // before its first use (for some compilers)
+  while (!_values.empty())
     {
-      std::cerr << "ERROR: no integer parameter named "
-		<< name << std::endl;
+      Parameters::iterator it = _values.begin();
+      
+      delete it->second;      
+      it->second = NULL;
+      
+      _values.erase(it);
+    }
+}
+
+
+
+inline
+Parameters& Parameters::operator= (const Parameters& rhs)
+{
+  this->clear();
+  
+  for (Parameters::const_iterator it = rhs._values.begin();
+       it != rhs._values.end(); ++it)
+    _values[it->first] = it->second->clone();
+  
+  return *this;
+}
+
+
+
+inline
+Parameters::Parameters (const Parameters& p)
+{
+  *this = p;
+}
+
+
+
+inline
+Parameters::~Parameters ()
+{
+  this->clear ();
+}
+
+
+
+inline
+void Parameters::print (std::ostream& os) const
+{
+  Parameters::const_iterator it = _values.begin();
+
+  os << "Name\t Type\t Value\n"
+     << "---------------------\n";
+  while (it != _values.end())
+    {
+      os << " "   << it->first
+	 << "\t " << it->second->type()
+	 << "\t ";   it->second->print(os);      
+      os << '\n';
+
+      ++it;
+    }
+}
+
+
+
+inline
+std::ostream& operator << (std::ostream& os, const Parameters& p)
+{
+  p.print(os);
+  return os;
+}
+
+
+
+template <typename T>
+inline
+bool Parameters::have_parameter (const std::string& name) const
+{
+  Parameters::const_iterator it = _values.find(name);
+
+  if (it != _values.end())
+    if (dynamic_cast<const Parameter<T>*>(it->second) != NULL)
+      return true;
+
+  return false;
+}
+
+
+
+template <typename T>
+inline
+const T& Parameters::get (const std::string& name) const
+{
+  if (!this->have_parameter<T>(name))
+    {
+      std::cerr << "ERROR: no "
+		<< typeid(T).name()
+		<< " parameter named "
+		<< name << ":" << std::endl
+		<< *this;
+      
       error();
     }
+
+  Parameters::const_iterator it = _values.find(name);
+
+  assert (it != _values.end());
+  assert (it->second != NULL);
+  assert (dynamic_cast<const Parameter<T>*>(it->second) != NULL);
   
-  return _int[name];
+  return dynamic_cast<Parameter<T>*>(it->second)->get();
 }
 
 
 
-template <>
+template <typename T>
 inline
-int& Parameters::set (const std::string& name)
+T& Parameters::set (const std::string& name)
 {
-  return _int[name];
+  Parameter<T>* param = NULL;
+  
+  if (!this->have_parameter<T>(name))
+    _values[name] = new Parameter<T>;
+
+  param = dynamic_cast<Parameter<T>*>(_values[name]);
+
+  assert (param != NULL);
+  
+  return param->set();
 }
-
-
 
 #endif // #define __parameters_h__
