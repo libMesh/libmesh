57f02a019e56259769e3ac4452725fc3eee4fcc5
Roy H. Stogner <roystgnr@users.sourceforge.net>
2005-01-13 21:22:43 +0000

    Added small tensor classes (for function hessians, primarily)

---
+++ libmesh/include/numerics/tensor_value.h
@@ -0,0 +1,154 @@
+// $Id: tensor_value.h,v 1.1 2005-01-13 21:22:43 roystgnr Exp $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2004  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __tensor_value_h__
+#define __tensor_value_h__
+
+// C++ includes
+
+// Local includes
+#include "type_tensor.h"
+
+
+
+
+/**
+ * This class defines a tensor in DIM dimensional Real or Complex
+ * space.  The typedef RealTensorValue always defines a real-valued tensor,
+ * and NumberTensorValue defines a real or complex-valued tensor depending
+ * on how the library was configured.
+ * 
+ * \author Roy H. Stogner 2004
+ */
+template <typename T>
+class TensorValue : public TypeTensor<T>
+{
+public:
+
+  /**
+   * Constructor.  By default sets all entries to 0.
+   */
+  TensorValue  (const T xx=0.,
+		const T xy=0.,
+		const T xz=0.,
+		const T yx=0.,
+		const T yy=0.,
+		const T yz=0.,
+		const T zx=0.,
+		const T zy=0.,
+		const T zz=0.);
+  
+  /**
+   * Copy-constructor.
+   */
+  TensorValue (const TensorValue<T>& p);
+
+  /**
+   * Copy-constructor.
+   */
+  TensorValue (const TypeTensor<T>& p);
+
+
+#ifdef USE_COMPLEX_NUMBERS
+  /**
+   * Constructor that takes two \p TypeVecor<Real>
+   * representing the real and imaginary part as
+   * arguments.
+   */
+  TensorValue (const TypeTensor<Real>& p_re,
+	       const TypeTensor<Real>& p_im);
+#endif
+
+  
+private:
+
+  
+};
+
+
+
+/**
+ * Useful typedefs to allow transparent switching
+ * between Real and Complex data types.
+ */
+typedef TensorValue<Real>   RealTensorValue;
+typedef TensorValue<Number> NumberTensorValue;
+typedef RealTensorValue     RealTensor;
+typedef NumberTensorValue   Tensor;
+
+
+
+//------------------------------------------------------
+// Inline functions
+template <typename T>
+inline
+TensorValue<T>::TensorValue (const T xx,
+			     const T xy,
+			     const T xz,
+			     const T yx,
+			     const T yy,
+			     const T yz,
+			     const T zx,
+			     const T zy,
+			     const T zz) :
+  TypeTensor<T> (xx,xy,xz,yx,yy,yz,zx,zy,zz)
+{
+}
+
+
+
+template <typename T>
+inline
+TensorValue<T>::TensorValue (const TensorValue<T>& p) :
+  TypeTensor<T> (p)
+{
+}
+
+
+
+template <typename T>
+inline
+TensorValue<T>::TensorValue (const TypeTensor<T>& p) :
+  TypeTensor<T> (p)
+{
+}
+
+
+#ifdef USE_COMPLEX_NUMBERS
+template <typename T>
+inline
+TensorValue<T>::TensorValue (const TypeTensor<Real>& p_re,
+			     const TypeTensor<Real>& p_im) :
+  TypeTensor<T> (Complex (p_re(0,0), p_im(0,0)),
+		 Complex (p_re(0,1), p_im(0,1)),
+		 Complex (p_re(0,2), p_im(0,2)),
+		 Complex (p_re(1,0), p_im(1,0)),
+		 Complex (p_re(1,1), p_im(1,1)),
+		 Complex (p_re(1,2), p_im(1,2)),
+		 Complex (p_re(2,0), p_im(2,0)),
+		 Complex (p_re(2,1), p_im(2,1)),
+		 Complex (p_re(2,2), p_im(2,2)))
+{
+}
+#endif
+
+
+#endif // #define __tensor_value_h__
---
+++ libmesh/src/numerics/type_tensor.C
@@ -0,0 +1,158 @@
+// $Id: type_tensor.C,v 1.1 2005-01-13 21:22:30 roystgnr Exp $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2004  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+
+// C++ includes
+#include <iostream>
+#include <iomanip> // for std::setw, std::setiosflags
+
+// Local includes
+#include "type_tensor.h"
+
+
+
+
+// ------------------------------------------------------------
+// TypeTensor<T> class member funcions
+
+
+template <typename T>
+void TypeTensor<T>::print(std::ostream& os) const
+{
+#if DIM == 1
+  
+  os << "x=" << (*this)(0) << std::endl;
+  
+#endif
+#if DIM == 2
+  
+  os << "(xx,xy)=("
+     << std::setw(8) << (*this)(0,0) << ", "
+     << std::setw(8) << (*this)(0,1) << ")"
+     << std::endl;
+  os << "(yx,yy)=("
+     << std::setw(8) << (*this)(1,0) << ", "
+     << std::setw(8) << (*this)(1,1) << ")"
+     << std::endl;
+
+#endif
+#if DIM == 3
+  
+  os <<  "(xx,xy,xz)=("
+     << std::setw(8) << (*this)(0,0) << ", "
+     << std::setw(8) << (*this)(0,1) << ", "
+     << std::setw(8) << (*this)(0,2) << ")"
+     << std::endl;
+  os <<  "(yx,yy,yz)=("
+     << std::setw(8) << (*this)(1,0) << ", "
+     << std::setw(8) << (*this)(1,1) << ", "
+     << std::setw(8) << (*this)(1,2) << ")"
+     << std::endl;
+  os <<  "(zx,zy,zz)=("
+     << std::setw(8) << (*this)(2,0) << ", "
+     << std::setw(8) << (*this)(2,1) << ", "
+     << std::setw(8) << (*this)(2,2) << ")"
+     << std::endl;
+#endif
+}
+
+
+
+
+
+template <typename T>
+void TypeTensor<T>::write_unformatted (std::ostream &out,
+				       const bool newline) const
+{
+  assert (out);
+
+  out << std::setiosflags(std::ios::showpoint)
+      << (*this)(0,0) << " "
+      << (*this)(0,1) << " "
+      << (*this)(0,2) << " ";
+  if (newline)
+    out << '\n';      
+
+  out << std::setiosflags(std::ios::showpoint)
+      << (*this)(1,0) << " "
+      << (*this)(1,1) << " "
+      << (*this)(1,2) << " ";
+  if (newline)
+    out << '\n';      
+
+  out << std::setiosflags(std::ios::showpoint)
+      << (*this)(2,0) << " "
+      << (*this)(2,1) << " "
+      << (*this)(2,2) << " ";
+  if (newline)
+    out << '\n';      
+}
+
+
+
+template <>
+bool TypeTensor<Real>::operator < (const TypeTensor<Real>& rhs) const
+{
+  for (unsigned int i=0; i<DIM; i++)
+    for (unsigned int j=0; j<DIM; j++)
+      {
+        if ((*this)(i,j) < rhs(i,j))
+          return true;
+        if ((*this)(i,j) > rhs(i,j))
+          return false;
+      }
+  return false;
+}
+
+
+
+#ifdef USE_COMPLEX_NUMBERS
+template <>
+bool TypeTensor<Complex>::operator < (const TypeTensor<Complex>& rhs) const
+{
+  for (unsigned int i=0; i<DIM; i++)
+    for (unsigned int j=0; j<DIM; j++)
+      {
+        if ((*this)(i,j).real() < rhs(i,j).real())
+          return true;
+        if ((*this)(i,j).real() > rhs(i,j).real())
+          return false;
+        if ((*this)(i,j).imag() < rhs(i,j).imag())
+          return true;
+        if ((*this)(i,j).imag() > rhs(i,j).imag())
+          return false;
+      }
+  return false;
+}
+
+
+
+#endif
+
+
+
+// ------------------------------------------------------------
+// Explicit instantiations
+template class TypeTensor<Real>;
+
+#ifdef USE_COMPLEX_NUMBERS
+template class TypeTensor<Complex>;
+#endif
