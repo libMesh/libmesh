14e056cab647a8a3c2e543f92e28c214778d2542
Steffen Petersen <steffenp@stanford.edu>
2005-03-07 12:41:41 +0000

    Changes for complex numbers.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@998 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/fe/fe.C
+++ trunk/libmesh/src/fe/fe.C
@@ -1,4 +1,4 @@
-// $Id: fe.C,v 1.33 2005-03-01 15:50:25 roystgnr Exp $
+// $Id: fe.C,v 1.34 2005-03-07 12:41:36 spetersen Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -559,7 +559,7 @@ void FE<Dim,T>::compute_C0_constraints (DofConstraints &constraints,
 	        {
 	          const unsigned int j = my_side_dofs[js];
 	          const unsigned int my_dof_g = child_dof_indices[j];
-		  const Real their_dof_value = Ue[is](js);
+		  const Number their_dof_value = Ue[is](js);
 		  if (their_dof_g == my_dof_g)
 		    {
 		      assert(std::abs(their_dof_value-1.) < 1.e-5);
@@ -708,7 +708,7 @@ void FE<Dim,T>::compute_C1_constraints (DofConstraints &constraints,
 	        {
 	          const unsigned int j = my_side_dofs[js];
 	          const unsigned int my_dof_g = child_dof_indices[j];
-		  const Real their_dof_value = Ue[is](js);
+		  const Number their_dof_value = Ue[is](js);
 		  if (their_dof_g == my_dof_g)
 		    {
 		      assert(std::abs(their_dof_value-1.) < 1.e-5);
