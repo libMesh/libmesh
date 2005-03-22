e6caf41664afb48fea1014bbc39eaf2c01daa195
John W. Peterson <jwpeterson@users.sourceforge.net>
2005-03-22 22:01:55 +0000

    Improvements to variant_filter_iterator.  Tested on several compilers including xlC, icc, and gcc 2.95.3 and later.  May not work on SUNPRO yet, based on past experience.  It is now possible to construct const_iterators from regular iterators in the same way that this technique is used in the STL.

--- libmesh/include/base/variant_filter_iterator.h
+++ libmesh/include/base/variant_filter_iterator.h
@@ -1,7 +1,7 @@
-// $Id: variant_filter_iterator.h,v 1.8 2005-02-22 22:17:30 jwpeterson Exp $
+// $Id: variant_filter_iterator.h,v 1.9 2005-03-22 22:01:46 jwpeterson Exp $
 
 // The libMesh Finite Element Library.
-// Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
+// Copyright (C) 2002-2004  Benjamin S. Kirk, John W. Peterson
   
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -26,6 +26,11 @@
 #include <iostream>
 #include <algorithm> // for std::swap
 #include <cstdlib>   // for std::abort()
+#include <assert.h>
+
+#if defined(__GNUC__) && (__GNUC__ < 3)  && !defined(__INTEL_COMPILER)
+#include <typeinfo>
+#endif
 
 /**
  * Original Authors: Corwin Joy          * Michael Gradman
@@ -40,32 +45,47 @@
  *
  * @author John W. Peterson, 2004.
  */
-template<class Type, class Predicate>
+template<class Predicate, class Type, class ReferenceType = Type&, class PointerType = Type*>
 #if defined(__GNUC__) && (__GNUC__ < 3)  && !defined(__INTEL_COMPILER)
 class variant_filter_iterator : public std::forward_iterator<std::forward_iterator_tag, Type>
 #else
 class variant_filter_iterator : public std::iterator<std::forward_iterator_tag,  Type>
 #endif
 {
-#ifdef __SUNPRO_CC // make these public in this case
- public:           // otherwise we'll need *lots* of friends
-#else
- private:
-#endif
-
+public:
   /**
-   * Abstract base class for the iterator type.
+   * Shortcut name for the fully-qualified typename.
+   */
+  typedef variant_filter_iterator<Predicate, Type, ReferenceType, PointerType> Iterator;
+
+
+  
+public:
+  /**
+   * Abstract base class for the iterator type.  Ideally these mixin classes would be protected,
+   * but due to the fact that different templated versions of the same class (which are not related
+   * by inheritance) need to be able to see each other's IterBase and PredBase members.  Thus, the
+   * mixin classes are in the public interface.
    */
   struct IterBase
   {
     virtual ~IterBase() {}
     virtual  IterBase* clone() const = 0 ;
-    virtual Type& operator*() const = 0;    // <-- CUSTOM INTERFACE METHOD
+    virtual ReferenceType operator*() const = 0;    // <-- CUSTOM INTERFACE METHOD
     virtual void operator++() = 0;          // <-- CUSTOM INTERFACE METHOD
     virtual bool equal(const IterBase *other) const = 0;
+
+    // Similar to clone function above, but returns a pointer to a copy of a different type.
+    // typedef typename variant_filter_iterator<Predicate, Type, const Type&, const Type*>::IterBase const_IterBase;
+    typedef typename variant_filter_iterator<Predicate, Type const, Type const & , Type const*>::IterBase const_IterBase;
+    virtual const_IterBase* const_clone() const = 0;
   };
 
 
+
+
+
+  
   
   /**
    * Abstract base class for the predicate.
@@ -75,9 +95,18 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
     virtual ~PredBase() {}
     virtual PredBase* clone() const = 0 ;
     virtual bool operator()(const IterBase* in) const = 0;
+
+    // Similar to clone function above, but returns a pointer to a copy of a different type.
+    // typedef typename variant_filter_iterator<Predicate, Type, const Type&, const Type*>::PredBase const_PredBase;
+    typedef typename variant_filter_iterator<Predicate, Type const,  Type const &,  Type const *>::PredBase const_PredBase;
+    virtual const_PredBase* const_clone() const = 0;
   };
 
 
+
+
+
+  
   
   /**
    * The actual iterator object is held as a template parameter here.
@@ -85,11 +114,24 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
   template<typename IterType>
   struct Iter : IterBase
   {
+    
     /**
      * Constructor
      */
     Iter (const IterType& v) :
-      iter_data (v) {}
+      iter_data (v)
+    {
+      // std::cout << "In Iter<IterType>::Iter(const IterType& v)" << std::endl;
+    }
+    
+
+    /**
+     * Copy Constructor.
+     */
+    Iter (const Iter& other) :
+      iter_data(other.iter_data)
+    {}
+
 
     /**
      * Destructor
@@ -114,9 +156,27 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
     }
 
     /**
+     * Returns a copy of this object as a pointer to a
+     * different type of object.
+     */
+    virtual typename IterBase::const_IterBase* const_clone() const
+    {
+      /**
+       * Important typedef for const_iterators.  Notice the weird syntax!  Does it compile everywhere?
+       */
+      // typedef typename variant_filter_iterator<Predicate, Type, const Type&, const Type*>::template Iter<IterType> const_Iter;
+      typedef typename variant_filter_iterator<Predicate, Type const, Type const &,  Type const *>::template Iter<IterType> const_Iter;
+
+      typename IterBase::const_IterBase* copy =
+	new const_Iter(iter_data);
+
+      return copy;
+    }
+
+    /**
      * Custom interface method.
      */
-    virtual Type& operator*() const   // <-- CUSTOM INTERFACE METHOD
+    virtual ReferenceType operator*() const   // <-- CUSTOM INTERFACE METHOD
     {
       return *iter_data;
     }
@@ -148,7 +208,7 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
       if (p == NULL)
 	{
 	  std::cerr << "Dynamic cast failed in Iter::equal(...)" << std::endl;
-	  std::abort();
+	  abort();
 	}
       
       return (iter_data == p->iter_data);
@@ -160,6 +220,7 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
     IterType iter_data;
   };
 
+  
 
   
   /**
@@ -196,6 +257,29 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
       
       return copy;
     }
+
+
+    /**
+     * The redefinition of the const_clone function for the Pred class.
+     * Notice the strange typename syntax required.  Will it compile everywhere?
+     */
+    virtual typename PredBase::const_PredBase* const_clone() const
+    {
+      /**
+       * Important typedef for const_iterators.  Notice the weird syntax!  Does it compile everywhere?
+       */
+      //      typedef typename variant_filter_iterator<Predicate, Type, const Type&, const Type*>::template Pred<IterType, PredType> const_Pred;
+      typedef typename variant_filter_iterator<Predicate, Type const, Type const &,  Type const *>::template Pred<IterType, PredType> const_Pred;
+
+
+      typename PredBase::const_PredBase* copy =
+	new const_Pred(pred_data);
+
+      return copy;
+    }
+
+
+    
     
     /**
      * Re-implementation of op()
@@ -218,7 +302,7 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
 	{
 	  std::cerr << "Dynamic cast failed in Pred::op()" << std::endl;
 	  std::cerr << "typeid(IterType).name()=" << typeid(IterType).name() << std::endl;
-	  std::abort();
+	  abort();
 	}
       
       // Return result of op() for the user's predicate.
@@ -232,8 +316,14 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
   };
 
 
-  
+
+public:
   /**
+   * Ideally this private member data should have protected access.  However, if we want
+   * a const_iterator to be constructable from an non-const one, templated versions of the
+   * same class (not related by inheritance) will need to know about these private members.
+   * Thus, they have public access.
+   *
    * Polymorphic pointer to the object.  Don't confuse
    * with the data pointer located in the \p Iter!
    */
@@ -255,22 +345,16 @@ class variant_filter_iterator : public std::iterator<std::forward_iterator_tag, 
 
   
 public:
-
-  /**
-   * The iterator this class provides.
-   */
-  typedef variant_filter_iterator<Type, Predicate> Iterator;
-
   /**
    * Templated Constructor.  Allows you to construct the iterator
    * and predicate from any types.  Also advances the data pointer
    * to the first entry which satisfies the predicate.
    */
-  template<typename IterType, class PredType>
+  template<typename PredType, typename IterType>
   variant_filter_iterator (const IterType& d,
 			   const IterType& e,
 			   const PredType& p ):
-    data ( new Iter<IterType>(d) ),
+    data ( new Iter<IterType>(d) ), // note: uses default IterBase copy constructor
     end  ( new Iter<IterType>(e) ),
     pred ( new Pred<IterType,PredType>(p) )
   {
@@ -293,11 +377,54 @@ public:
     data (rhs.data != NULL ? rhs.data->clone() : NULL),
     end  (rhs.end  != NULL ? rhs.end->clone()  : NULL),
     pred (rhs.pred != NULL ? rhs.pred->clone() : NULL) {}
+
+//   /**
+//    * Copy construct from another (similar) variant_filter_iterator
+//    * Predicate and Type are the same, but the ReferenceType and
+//    * PointerType are different.  Note that OtherReferenceType and
+//    * OtherPointerType are simply intended to be const versions of
+//    * ReferenceType and PointerType.  This function is implemented in
+//    * terms of the const_clone() member functions of the private data.
+//    */
+//   template <class OtherReferenceType, class OtherPointerType>
+//   variant_filter_iterator (const variant_filter_iterator<Predicate, Type, OtherReferenceType, OtherPointerType>& rhs)
+//     : data (rhs.data != NULL ? rhs.data->const_clone() : NULL),
+//       end  (rhs.end  != NULL ? rhs.end->const_clone()  : NULL),
+//       pred (rhs.pred != NULL ? rhs.pred->const_clone() : NULL)
+//   {
+//     // std::cout << "Called templated copy constructor for variant_filter_iterator" << std::endl;
+//   }
+
+
+  /**
+   * Copy construct from another (similar) variant_filter_iterator.
+   * The Predicate is the same, but the Type, ReferenceType and
+   * PointerType are different.  Example:
+   * You are iterating over a std::vector<int*> with std::vector<int*>::iterator
+   * Then, you have:
+   * Type=int* ,  ReferenceType=int*& , PointerType=int**
+   * On the other hand, when you iterate using std::vector<int*>::const_iterator
+   * you have:
+   * Type=int * const, ReferenceType=int * const & , PointerType=int * const *
+   */
+  template <class OtherType, class OtherReferenceType, class OtherPointerType>
+  variant_filter_iterator (const variant_filter_iterator<Predicate, OtherType, OtherReferenceType, OtherPointerType>& rhs)
+    : data (rhs.data != NULL ? rhs.data->const_clone() : NULL),
+      end  (rhs.end  != NULL ? rhs.end->const_clone()  : NULL),
+      pred (rhs.pred != NULL ? rhs.pred->const_clone() : NULL)
+  {
+    // std::cout << "Called templated copy constructor for variant_filter_iterator" << std::endl;
+  }
+
+
+
+
+  
   
   /**
    * Destructor
    */
-  ~variant_filter_iterator()
+  virtual ~variant_filter_iterator()
   {
     delete data; data = NULL;
     delete end;  end  = NULL;
@@ -307,7 +434,7 @@ public:
   /**
    * unary op*() forwards on to \p Iter::op*()
    */
-  Type& operator*() const
+  ReferenceType operator*() const
   {
     return **data;
   }
@@ -316,7 +443,7 @@ public:
   /**
    * op->()
    */
-  Type* operator->() const
+  PointerType operator->() const
   {
     return (&**this);
   }
@@ -400,10 +527,10 @@ private:
 
 //---------------------------------------------------------------------------
 // op==
-template<class Type, class Predicate>
+template<class Predicate, class Type, class ReferenceType, class PointerType>
 inline
-bool operator==(const variant_filter_iterator<Type, Predicate>& x,
-		const variant_filter_iterator<Type, Predicate>& y)
+bool operator==(const variant_filter_iterator<Predicate, Type, ReferenceType, PointerType>& x,
+		const variant_filter_iterator<Predicate, Type, ReferenceType, PointerType>& y)
 {
   return x.equal(y);
 }
@@ -411,10 +538,10 @@ bool operator==(const variant_filter_iterator<Type, Predicate>& x,
 
 
 // op!=
-template<class Type, class Predicate>
+template<class Predicate, class Type, class ReferenceType, class PointerType>
 inline
-bool operator!=(const variant_filter_iterator<Type, Predicate>& x,
-		const variant_filter_iterator<Type, Predicate>& y)
+bool operator!=(const variant_filter_iterator<Predicate, Type, ReferenceType, PointerType>& x,
+		const variant_filter_iterator<Predicate, Type, ReferenceType, PointerType>& y)
 {
   return !(x == y);
 }
