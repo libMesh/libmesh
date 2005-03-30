a7c26c2a66ecb536a3c6614f076f94e6113223b7
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2005-03-30 15:21:14 +0000

    updated gmsh support from Martin.  Thanks.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1019 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/mesh/gmsh_io.C
+++ trunk/libmesh/src/mesh/gmsh_io.C
@@ -1,4 +1,4 @@
-// $Id: gmsh_io.C,v 1.8 2005-02-22 22:17:39 jwpeterson Exp $
+// $Id: gmsh_io.C,v 1.9 2005-03-30 15:21:14 benkirk Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -17,7 +17,7 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
+// This file was massively overhauled and extended by Martin Lüthi, mluthi@tnoo.net
 
 // C++ includes
 #include <fstream>
@@ -26,6 +26,237 @@
 #include "libmesh_config.h"
 #include "gmsh_io.h"
 #include "elem.h"
+
+// anonymous namespace to hold local data
+namespace
+{
+  /**
+   * Defines mapping from libMesh element types to Gmsh element types.
+   */
+  struct elementDefinition {
+    std::string label;
+    std::vector<unsigned int> nodes;
+    ElemType type;
+    unsigned int exptype;
+  };
+
+
+  // maps from a libMesh element type to the proper
+  // Gmsh elementDefinition.  Placing the data structure
+  // here in this anonymous namespace gives us the
+  // benefits of a global variable without the nasty
+  // side-effects
+  std::map<ElemType, elementDefinition> eletypes_exp;
+  std::map<unsigned int, elementDefinition> eletypes_imp;
+
+
+
+  // ------------------------------------------------------------
+  // helper function to initialize the eletypes map
+  void init_eletypes ()
+  {
+    if (eletypes_exp.empty() && eletypes_imp.empty())
+      {
+	// This should happen only once.  The first time this method
+	// is called the eletypes data struture will be empty, and
+	// we will fill it.  Any subsequent calls will find an initialized
+	// eletypes map and will do nothing.
+
+	//==============================
+	// setup the element definitions
+	elementDefinition eledef;
+
+	// use "swap trick" from Scott Meyer's "Effective STL" to initialize
+	// eledef.nodes vector
+	
+	// POINT (only Gmsh)
+	{
+	  eledef.exptype = 15;
+	  eledef.nodes.clear();
+	
+          // import only
+          eletypes_imp[15] = eledef;
+	}
+
+	// EDGE2
+	{
+	  eledef.type    = EDGE2;
+	  eledef.exptype = 1;
+	  eledef.nodes.clear();
+	
+	  eletypes_exp[EDGE2] = eledef;
+          eletypes_imp[1]     = eledef;
+	}
+  
+	// EDGE3
+	{
+	  eledef.type    = EDGE3;
+	  eledef.exptype = 8;
+	  eledef.nodes.clear();
+	  
+	  eletypes_exp[EDGE3] = eledef;
+          eletypes_imp[8]     = eledef;
+	}
+      
+	// TRI3
+	{
+          eledef.type    = TRI3;
+	  eledef.exptype = 2;
+	  eledef.nodes.clear();
+	  
+	  eletypes_exp[TRI3] = eledef;
+          eletypes_imp[2] = eledef;
+	}
+      
+	// TRI6
+	{
+          eledef.type    = TRI6;
+	  eledef.exptype = 9;
+	  eledef.nodes.clear();
+
+	  eletypes_exp[TRI6] = eledef;
+          eletypes_imp[9]    = eledef;
+	}
+      
+	// QUAD4
+	{
+          eledef.type    = QUAD4;
+	  eledef.exptype = 3;
+	  eledef.nodes.clear();
+
+	  eletypes_exp[QUAD4] = eledef;
+          eletypes_imp[3]     = eledef;
+	}
+      
+	// QUAD8
+        // TODO: what should be done with this on writing?
+	{
+          eledef.type    = QUAD8;
+	  eledef.exptype = 100;
+	  const unsigned int nodes[] = {1,2,3,4,5,6,7,8};
+	  const unsigned int nnodes = sizeof(nodes)/sizeof(nodes[0]);
+	  std::vector<unsigned int>(nodes, nodes+nnodes).swap(eledef.nodes);
+
+	  eletypes_exp[QUAD8] = eledef;
+          eletypes_imp[10]    = eledef;
+	}
+      
+	// QUAD9
+	{
+          eledef.type    = QUAD9;
+	  eledef.exptype = 10;
+	  eledef.nodes.clear();
+
+	  eletypes_exp[QUAD9] = eledef;
+          eletypes_imp[10]    = eledef;
+	}
+      
+	// HEX8
+	{
+          eledef.type    = HEX8;
+	  eledef.exptype = 5;
+	  eledef.nodes.clear();
+
+	  eletypes_exp[HEX8] = eledef;
+          eletypes_imp[5]    = eledef;
+	}
+      
+	// HEX20
+        // TODO: what should be done with this on writing?
+	{
+          eledef.type    = HEX20;
+	  eledef.exptype = 101;
+	  const unsigned int nodes[] = {1,2,3,4,5,6,7,8,9,10,11,16,17,18,19,12,13,14,15,16};
+	  const unsigned int nnodes = sizeof(nodes)/sizeof(nodes[0]);
+	  std::vector<unsigned int>(nodes, nodes+nnodes).swap(eledef.nodes);
+
+	  eletypes_exp[HEX20] = eledef;
+          eletypes_imp[12]    = eledef;
+	}
+      
+	// HEX27
+	{
+          eledef.type    = HEX27;
+	  eledef.exptype = 12;
+          // export
+          const unsigned int nodes_exp[] = {0,1,2,3,4,5,6,7,8,11,12,9,13,10,14,15,16,19,17,18,20,21,24,22,23,25,26};
+          unsigned int nnodes = sizeof(nodes_exp)/sizeof(nodes_exp[0]);
+	  std::vector<unsigned int>(nodes_exp, nodes_exp+nnodes).swap(eledef.nodes);
+	  eletypes_exp[HEX27] = eledef;
+
+          // import 
+          const unsigned int nodes_imp[] = {1,2,3,4,5,6,7,8,9,12,14,10,11,13,15,16,17,19,20,18,21,22,24,25,23,26,27};
+          std::vector<unsigned int>(nodes_imp, nodes_imp+nnodes).swap(eledef.nodes);
+          eletypes_imp[12]    = eledef;
+	}
+      
+	// TET4
+	{
+          eledef.type    = TET4;
+	  eledef.exptype = 4;
+	  eledef.nodes.clear();
+
+          eletypes_exp[TET4] = eledef;
+          eletypes_imp[4]    = eledef;
+	}
+      
+	// TET10
+	{
+          eledef.type    = TET10;
+	  eledef.exptype = 11;
+          // export
+          const unsigned int nodes_exp[] = {0,1,2,3,4,5,6,7,9,8};
+          unsigned int nnodes = sizeof(nodes_exp)/sizeof(nodes_exp[0]);
+	  std::vector<unsigned int>(nodes_exp, nodes_exp+nnodes).swap(eledef.nodes);
+	  eletypes_exp[TET10] = eledef;
+
+          // import 
+          const unsigned int nodes_imp[] = {1,2,3,4,5,6,7,8,10,9};
+          std::vector<unsigned int>(nodes_imp, nodes_imp+nnodes).swap(eledef.nodes);
+          eletypes_imp[11]    = eledef;
+	}
+      
+	// PRISM6
+	{
+          eledef.type    = PRISM6;
+	  eledef.exptype = 6;
+	  eledef.nodes.clear();
+	  
+	  eletypes_exp[PRISM6] = eledef;
+          eletypes_imp[6]      = eledef;
+	}
+      
+	// PRISM15
+        // TODO: what should be done with this on writing?
+	{
+          eledef.type    = PRISM15;
+	  eledef.exptype = 103;
+	  eledef.nodes.clear();
+	  
+	  eletypes_exp[PRISM15] = eledef;
+          eletypes_imp[13] = eledef;
+	}
+
+	// PRISM18
+	{
+          eledef.type    = PRISM18;
+	  eledef.exptype = 13;
+          // export
+          const unsigned int nodes_exp[] = {0,1,2,3,4,5,6,8,9,7,10,11,12,14,13,15,17,16};
+          unsigned int nnodes = sizeof(nodes_exp)/sizeof(nodes_exp[0]);
+	  std::vector<unsigned int>(nodes_exp, nodes_exp+nnodes).swap(eledef.nodes);
+	  eletypes_exp[PRISM18] = eledef;
+
+          // import 
+          const unsigned int nodes_imp[] = {1,2,3,4,5,6,7,10,8,9,11,12,13,15,14,16,18,17};
+          std::vector<unsigned int>(nodes_imp, nodes_imp+nnodes).swap(eledef.nodes);
+          eletypes_imp[13]      = eledef;
+	}
+	//==============================      
+      }
+  }
+  
+} // end anonymous namespace
 
 
 // ------------------------------------------------------------
@@ -38,14 +269,153 @@ void GmshIO::read (const std::string& name)
 }
 
 
-
-
 void GmshIO::read_stream(std::istream& in)
 {
 
   assert(in.good());
-  // Not yet implemented
-  error();
+
+  // initialize the map with element types
+  init_eletypes();
+
+  // clear any data in the mesh
+  MeshBase& mesh = MeshInput<MeshBase>::mesh();
+  mesh.clear();
+
+  // some variables
+  const int  bufLen = 256;
+  char       buf[bufLen+1];
+  int        format=0, size=0;
+  Real       version = 1.0;
+  
+  // map to hold the node numbers for translation
+  // note the the nodes can be non-consecutive
+  std::map<unsigned int, unsigned int> nodetrans;
+  
+  {
+    while (!in.eof()) {
+      in >> buf;
+
+      if (!strncmp(buf,"$MeshFormat",11))
+        {
+          in >> version >> format >> size;
+          if(version != 2.0){
+            std::cerr << "Error: Wrong msh file version " << version << "\n";
+            error();
+          }
+          if(format){
+            std::cerr << "Error: Unknown data format for mesh\n";
+            error();
+          }
+        }
+
+      // read the node block
+      else if (!strncmp(buf,"$NOD",4) ||
+          !strncmp(buf,"$NOE",4) ||
+          !strncmp(buf,"$Nodes",6) 
+          )
+        {
+          unsigned int numNodes = 0;
+          in >> numNodes;
+          mesh.reserve_nodes (numNodes);
+
+          // read in the nodal coordinates and form points.
+          Real x, y, z;
+          unsigned int id;
+        
+          // add the nodal coordinates to the mesh
+          for (unsigned int i=0; i<numNodes; ++i)      
+            {
+              in >> id >> x >> y >> z;
+              mesh.add_point (Point(x, y, z));
+              nodetrans[id] = i;
+            }
+          // read the $ENDNOD delimiter
+          in >> buf;
+        }
+
+      // read the element block
+      else if (!strncmp(buf,"$ELM",4) ||
+               !strncmp(buf,"$Elements",9)
+               )
+        {
+          unsigned int numElem = 0;
+          in >> numElem;
+          mesh.reserve_elem (numElem);
+
+          // read the elements
+          for (unsigned int iel=0; iel<numElem; ++iel)      
+            {
+              unsigned int id, type, physical, elementary, partition = 1, numNodes, numTags;
+              if(version <= 1.0)
+                {
+                  in >> id >> type >> physical >> elementary >> numNodes;
+                }
+              else
+                {
+                  in >> id >> type >> numTags;
+                  elementary = physical = partition = 1;
+                  for(unsigned int j = 0; j < numTags; j++)
+                    {
+                      int tag;
+                      in >> tag;
+                      if(j == 0)
+                        physical = tag;
+                      else if(j == 1)
+                        elementary = tag;
+                      else if(j == 2)
+                        partition = tag;
+                      // ignore any other tags for now
+                    }
+                }
+
+              // consult the import element table which element to build
+              const elementDefinition& eletype = eletypes_imp[type];
+    
+              // add the elements to the mesh
+              Elem* elem = mesh.add_elem(Elem::build(eletype.type).release());
+
+              // check number of nodes. We cannot do that for version 2.0
+              if (version <= 1.0) 
+                {
+                  if (elem->n_nodes() != numNodes)
+                    {
+                      std::cerr << "Number of nodes for element " << id
+                                << " of type " << eletypes_imp[type].type
+                                << " (Gmsh type " << type  
+                                << ") does not match Libmesh definition. "
+                                << "I expected " << elem->n_nodes()
+                                << " nodes, but got " << numNodes << "\n";
+                      error();
+                    }
+                }
+
+              // add node pointers to the elements
+              int nod = 0;
+              // if there is a node translation table, use it
+              if (eletype.nodes.size() > 0)
+                for (unsigned int i=0; i<numNodes; i++)
+                  {
+                    in >> nod;
+                    elem->set_node(i) = mesh.node_ptr(nodetrans[eletype.nodes[nod]]);
+                  }
+              else
+                {
+                  for (unsigned int i=0; i<numNodes; i++)
+                    {
+                      in >> nod;
+                      elem->set_node(i) = mesh.node_ptr(nodetrans[nod]);
+                    }
+                }
+            }
+          // read the $ENDELM delimiter
+          in >> buf;
+        }
+    
+    }
+
+  }
+  //mesh.reserve_elem  (numElem);
+
 }
 
 
@@ -67,6 +437,10 @@ void GmshIO::write_stream (std::ostream& out)
   // Be sure that the stream is valid.
   assert (out.good());
   
+  // initialize the map with element types
+  init_eletypes();
+
+
   // Get a const reference to the mesh
   const MeshBase& mesh = MeshOutput<MeshBase>::mesh();
   
@@ -91,221 +465,59 @@ void GmshIO::write_stream (std::ostream& out)
 	  << mesh.node(v)(2) << '\n';
     out << "$EndNodes\n";
   }
-
-
   
   {
     // write the connectivity
-    
     out << "$Elements\n";
-    out << mesh.n_active_sub_elem() << '\n';
-
-
-//     const_active_elem_iterator       it (mesh.elements_begin());
-//     const const_active_elem_iterator end(mesh.elements_end());
+    out << mesh.n_active_elem() << '\n';
 
     MeshBase::const_element_iterator       it  = mesh.active_elements_begin();
     const MeshBase::const_element_iterator end = mesh.active_elements_end(); 
     
-    switch (mesh.mesh_dimension())
+    // the element number
+    for ( ; it != end; ++it)
       {
+        const Elem* elem = *it;
 
-	// 1D Meshes
-      case 1:
-	{
-// 	  for ( ; it != end; ++it)
-// 	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
-// 	      {
-// 		out << "line 2\n";
-// 		std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
-// 		for (unsigned int i=0; i<conn.size(); i++)
-// 		  out << conn[i] << " ";
-		
-// 		out << '\n';
-// 	      }
-	  error();  // Not yet implemented
-	  break;
-	}
+	// Make sure we have a valid entry for
+	// the current element type.
+	assert (eletypes_exp.count(elem->type()));
 
+        // consult the import element table 
+        const elementDefinition& eletype = eletypes_exp[elem->type()];
 
-
+	// The element mapper better not require any more nodes
+	// than are present in the current element!
+	assert (eletype.nodes.size() <= elem->n_nodes());
 	
-	// 2D Meshes
-      case 2:
-	{
-	  // The same temporary storage will be used for each element
-	  std::vector<unsigned int> conn;
+        // elements ids are 1 based in Gmsh
+        out << elem->id()+1 << " ";
 
-	  unsigned int ctr=1;
-	  for ( ; it != end; ++it)
-	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
-	      {
-		// Write element number
-		out << ctr++ << " ";
+        // element type
+        std::cout << elem->type() << "\n";
+        out << eletype.exptype;
 
-		// Quadrilateral elements
-		if (((*it)->type() == QUAD4) ||
-		    ((*it)->type() == QUAD8) ||
-		    ((*it)->type() == QUAD9)
-#ifdef ENABLE_INFINITE_ELEMENTS
-		    || ((*it)->type() == INFQUAD4)
-		    || ((*it)->type() == INFQUAD6)
-#endif
-		    )
-		  {
-		    out << "3 ";
-		    
-		    // Write the number of tags 
-		    out << "0 ";
+        // write the number of tags and
+        // tag1 (physical entity), and tag2 (geometric entity)
+        out << " 3 1 1 ";
 
-		    // Write connectivity
-		    (*it)->connectivity(se, TECPLOT, conn);
-		    for (unsigned int i=0; i<conn.size(); i++)
-		      out << conn[i] << " ";
+        // write the partition the element belongs to
+        out << elem->processor_id()+1 << " ";
 
-		  }
-
-		// Triangular elements
-		else if (((*it)->type() == TRI3) ||
-			 ((*it)->type() == TRI6))
-		  {
-		    out << "2 ";
-		    
-		    // Write the number of tags 
-		    out << "0 ";
-		    
-		    // Write connectivity (last entry of conn is skipped!)
-		    (*it)->connectivity(se, TECPLOT, conn);
-		    for (unsigned int i=0; i<3; i++)
-		      out << conn[i] << " ";
-		  }
-		
-		else
-		  {
-		    error();
-		  }
-
-		// Go to next line
-		out << '\n';
-	      }
-	  
-	  break;
-	}
-	
-	
-      case 3:
-	{
-// 	  for ( ; it != end; ++it)
-// 	    for (unsigned int se=0; se<(*it)->n_sub_elem(); se++)
-// 	      {
-
-// #ifndef  ENABLE_INFINITE_ELEMENTS
-// 		if (((*it)->type() == HEX8)   ||    
-// 		    ((*it)->type() == HEX27))
-// 		  {
-// 		    out << "phex8 8\n";
-// 		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
-// 		    for (unsigned int i=0; i<conn.size(); i++)
-// 		      out << conn[i] << " ";
-// 		  }
-		
-// 		else if ((*it)->type() == HEX20)
-// 		  {
-// 		    out << "phex20 20\n";
-// 		    out << (*it)->node(0)+1  << " "
-// 			<< (*it)->node(1)+1  << " "
-// 			<< (*it)->node(2)+1  << " "
-// 			<< (*it)->node(3)+1  << " "
-// 			<< (*it)->node(4)+1  << " "
-// 			<< (*it)->node(5)+1  << " "
-// 			<< (*it)->node(6)+1  << " "
-// 			<< (*it)->node(7)+1  << " "
-// 			<< (*it)->node(8)+1  << " "
-// 			<< (*it)->node(9)+1  << " "
-// 			<< (*it)->node(10)+1 << " "
-// 			<< (*it)->node(11)+1 << " "
-// 			<< (*it)->node(16)+1 << " "
-// 			<< (*it)->node(17)+1 << " "
-// 			<< (*it)->node(18)+1 << " "
-// 			<< (*it)->node(19)+1 << " "
-// 			<< (*it)->node(12)+1 << " "
-// 			<< (*it)->node(13)+1 << " "
-// 			<< (*it)->node(14)+1 << " "
-// 			<< (*it)->node(15)+1 << " ";
-// 		  }
-// #else
-// 		/*
-// 		 * In case of infinite elements, HEX20
-// 		 * should be handled just like the
-// 		 * INFHEX16, since these connect to each other
-// 		 */
-// 		if (((*it)->type() == HEX8)     ||
-// 		    ((*it)->type() == HEX27)    ||
-// 		    ((*it)->type() == INFHEX8)  ||
-// 		    ((*it)->type() == INFHEX16) ||
-// 		    ((*it)->type() == INFHEX18) ||
-// 		    ((*it)->type() == HEX20))
-// 		  {
-// 		    out << "phex8 8\n";
-// 		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
-// 		    for (unsigned int i=0; i<conn.size(); i++)
-// 		      out << conn[i] << " ";
-// 		  }
-// #endif
-		
-// 		else if (((*it)->type() == TET4)  ||
-// 			 ((*it)->type() == TET10))
-// 		  {
-// 		    out << "tet 4\n";
-// 		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
-// 		    out << conn[0] << " "
-// 			<< conn[2] << " "
-// 			<< conn[1] << " "
-// 			<< conn[4] << " ";
-// 		    }
-// #ifndef  ENABLE_INFINITE_ELEMENTS
-// 		else if (((*it)->type() == PRISM6)  ||
-// 			 ((*it)->type() == PRISM15) ||
-// 			 ((*it)->type() == PRISM18))
-// #else
-// 		else if (((*it)->type() == PRISM6)     ||
-// 			 ((*it)->type() == PRISM15)    ||
-// 			 ((*it)->type() == PRISM18)    ||
-// 			 ((*it)->type() == INFPRISM6)  ||
-// 			 ((*it)->type() == INFPRISM12))
-// #endif
-// 		  {
-// 		    /**
-// 		     * Note that the prisms are treated as
-// 		     * degenerated phex8's.
-// 		     */
-// 		    out << "phex8 8\n";
-// 		    std::vector<unsigned int> conn = (*it)->tecplot_connectivity(se);
-// 		    for (unsigned int i=0; i<conn.size(); i++)
-// 		      out << conn[i] << " ";
-// 		  }
-		
-// 		else
-// 		  {
-// 		    error();
-// 		  }
-		
-// 		out << '\n';
-// 	      }
-	  
-	  error(); // Not yet implemented
-	  break;
-	}
-	
-      default:
-	error();
-      }
-    
+        // if there is a node translation table, use it
+        if (eletype.nodes.size() > 0)
+          for (unsigned int i=0; i < elem->n_nodes(); i++)
+            out << elem->node(eletype.nodes[i])+1 << " ";   // gmsh is 1-based 
+        // otherwise keep the same node order
+        else
+          for (unsigned int i=0; i < elem->n_nodes(); i++)
+            out << elem->node(i)+1 << " ";                 // gmsh is 1-based 
+        out << "\n";
+      } // element loop
+    out << "$EndElements\n";
   }
-
-  
   // end of the file
-  out << '\n' << "$EndElements\n";
+
 }
 
 
