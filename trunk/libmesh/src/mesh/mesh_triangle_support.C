18b83aab777f1703e787ad9f2bc51be10a7c56bb
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2005-05-06 20:58:41 +0000

    added standard header
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1054 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/mesh/mesh_triangle_support.C
+++ trunk/libmesh/src/mesh/mesh_triangle_support.C
@@ -1,3 +1,24 @@
+// $Id: mesh_triangle_support.C,v 1.5 2005-05-06 20:58:41 benkirk Exp $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
 #include "mesh_triangle_support.h"
 #include "mesh.h"
 #include "elem.h"
