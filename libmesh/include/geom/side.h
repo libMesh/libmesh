5bdba806e84073db9c77ef7511cdeff9af6bc4d5
Roy H. Stogner <roystgnr@users.sourceforge.net>
2005-05-11 18:19:46 +0000

    Added SideEdge proxy class

--- libmesh/include/geom/side.h
+++ libmesh/include/geom/side.h
@@ -1,4 +1,4 @@
-// $Id: side.h,v 1.3 2005-02-22 22:17:33 jwpeterson Exp $
+// $Id: side.h,v 1.4 2005-05-11 18:19:46 roystgnr Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -43,8 +43,8 @@ class Node;
  * does not store any.
  *
  * \author  Benjamin S. Kirk
- * \date    $Date: 2005-02-22 22:17:33 $
- * \version $Revision: 1.3 $
+ * \date    $Date: 2005-05-11 18:19:46 $
+ * \version $Revision: 1.4 $
  */
 
 // ------------------------------------------------------------
@@ -129,6 +129,105 @@ class Side : public SideType
    * The side on the parent element
    */
   const unsigned int _side_number;
+};
+
+
+
+/**
+ * This defines the \p SideEdge class.  Like \p Side, \p SideEdge is basically
+ * a proxy (or stand-in replacement) class, this time for an element's edge.
+ * It acts like a standard \p Elem, but allocates no additional memory for
+ * storing connectivity.  Instead, its nodes are mapped directly from the
+ * parent element (the element for which the side is created).  Similarly, you
+ * cannot access the neighbors of a side since it does not store any.
+ *
+ * \author  Roy H. Stogner
+ * \date    $Date: 2005-05-11 18:19:46 $
+ * \version $Revision: 1.4 $
+ */
+
+// ------------------------------------------------------------
+//SideEdge class definition
+template <class EdgeType, class ParentType>
+class SideEdge : public EdgeType
+{
+ public:
+
+  /**
+   * Constructor.  Creates a side from an element.
+   */ 
+  SideEdge (const Elem* parent,
+	    const unsigned int edge) :
+    EdgeType(0,0,parent), // Allocate no storage for nodes or neighbors!
+    _edge_number(edge)
+  {
+    assert (parent != NULL);
+    assert (_edge_number < this->parent()->n_edges());
+    assert (this->dim() == 1);
+  }
+
+  /**
+   * @returns the \p Point associated with local \p Node \p i.
+   */
+  virtual const Point & point (const unsigned int i) const
+  {
+    assert (i < this->n_nodes());    
+    return this->parent()->point (ParentType::edge_nodes_map[_edge_number][i]);
+  }
+ 
+  /**
+   * @returns the \p Point associated with local \p Node \p i
+   * as a writeable reference.
+   */
+  virtual Point & point (const unsigned int i)
+  {
+    assert (i < this->n_nodes());
+    //TODO:[BSK] figure out how to get rid of this hideous const_cast
+    return const_cast<Elem*>(this->parent())->point (ParentType::edge_nodes_map[_edge_number][i]);
+  }
+  
+  /**
+   * @returns the global id number of local \p Node \p i.
+   */
+  virtual unsigned int node (const unsigned int i) const
+  {
+    assert (i < this->n_nodes());
+    return this->parent()->node (ParentType::edge_nodes_map[_edge_number][i]);  
+  }
+
+  /**
+   * @returns the pointer to local \p Node \p i.
+   */
+  virtual Node* get_node (const unsigned int i) const
+  {
+    assert (i < this->n_nodes());    
+    return this->parent()->get_node (ParentType::edge_nodes_map[_edge_number][i]);
+  }
+
+  /**
+   * @returns the pointer to local \p Node \p i as a writeable reference.
+   */
+  virtual Node* & set_node (const unsigned int i)
+  {
+    assert (i < this->n_nodes());    
+    //TODO:[BSK] figure out how to get rid of this hideous const_cast
+    return const_cast<Elem*>(this->parent())->set_node (ParentType::edge_nodes_map[_edge_number][i]);
+  }
+
+  /**
+   * @returns 0. Sides effectively do not have sides, so
+   * don't even ask!
+   */
+  virtual unsigned int n_sides () const { return 0; }
+
+  
+ private:
+
+  
+  /**
+   * The side on the parent element
+   */
+  const unsigned int _edge_number;
 };
 
 
