60cd01b0baa0c16e5017f552f8a76a20ee36b958
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2005-06-12 14:20:17 +0000

    Utility to convert a string to an enumerated value

---
+++ libmesh/include/utils/string_to_enum.h
@@ -0,0 +1,52 @@
+// $Id: string_to_enum.h,v 1.1 2005-06-12 14:20:15 benkirk Exp $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __string_to_enum_h__
+#define __string_to_enum_h__
+
+
+
+// C++ includes
+#include <string>
+
+// Local includes
+
+
+
+// ------------------------------------------------------------
+// Utility::string_to_enum<> definition
+
+namespace Utility {
+  
+  /**
+   * Takes the string \p s and returns the matching
+   * enumeration of type \p T.
+   */
+  template <typename T>
+  T string_to_enum (const std::string& s);
+}
+
+
+#endif // #define __string_to_enum_h__
+
+
+
+
