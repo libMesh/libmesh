bc043fe1955f449bca3ad4495695a3119a056e19
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2006-06-25 03:53:21 +0000

    updated documentation

--- libmesh/include/base/libmesh_documentation.h
+++ libmesh/include/base/libmesh_documentation.h
@@ -27,7 +27,7 @@
   <a href="http://sourceforge.net"> SourceForge </a>
   for <a href="http://sourceforge.net/projects/libmesh"> hosting the project</a>. 
   You can find out what is currently happening in the development branch
-  by checking out the <a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/libmesh"> CVS Logs</a>
+  by checking out the <a href="http://libmesh.cvs.sourceforge.net/libmesh/libmesh"> CVS Logs</a>
   online. 
   
   A major goal of the library is to provide support for adaptive mesh
