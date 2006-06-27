03a390b3315a30d8db0e68416444388fc6807e94
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2006-06-27 19:36:47 +0000

    added more external links
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1569 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/externalsoftware.php
+++ trunk/libmesh/doc/html/externalsoftware.php
@@ -14,10 +14,12 @@
 <code>libMesh</code> interfaces to a number of high-quality software packages to provide certain functionality.  This page provides a list of packages and a description of their use in the library.
 
 <ul>
- <li>
- <a href="http://www-unix.mcs.anl.gov/petsc/petsc-2">PETSc</a>
- </li>
- 
+ <li> <a href="http://www-unix.mcs.anl.gov/petsc/petsc-2">PETSc</a> </li>
+ <li> <a href="http://www.grycap.upv.es/slepc">SLEPc</a> </li>
+ <li> <a href="http://www.cs.cmu.edu/~quake/triangle.html">Triangle</a> </li>
+ <li> <a href="http://tetgen.berlios.de">Tetgen</a> </li>
+ <li> <a href="http://www.tecplot.com">Tecplot</a> </li>
+ <li> <a href="http://getpot.sourceforge.net">GetPot</a> </li>
 </ul>
 
 
