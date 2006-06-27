d067940086ebd86d51865054b4b868486f98eb66
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2006-06-27 02:35:13 +0000

    contain doxygen files under same style as main page
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1567 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/doc/html/externalsoftware.php
@@ -0,0 +1,43 @@
+<?php $root=""; ?>
+<?php require($root."navigation.php"); ?>
+
+<html>
+<head>
+  <title>Packages used by libMesh</title>
+  <?php load_style($root); ?>
+</head>
+
+<body>
+<?php make_navigation("externalsoftware",$root)?>
+
+<div class="content">
+<code>libMesh</code> interfaces to a number of high-quality software packages to provide certain functionality.  This page provides a list of packages and a description of their use in the library.
+
+<ul>
+ <li>
+ <a href="http://www-unix.mcs.anl.gov/petsc/petsc-2">PETSc</a>
+ </li>
+ 
+</ul>
+
+
+
+
+<br>
+<br>
+<br>
+<!--
+<div id="navBeta">
+</div>
+-->
+
+<?php make_footer() ?>
+
+</body>
+</html>
+
+<?php if (0) { ?>
+# Local Variables:
+# mode: html
+# End:
+<?php } ?>
--- trunk/libmesh/doc/html/presentations.php
+++ trunk/libmesh/doc/html/presentations.php
@@ -13,6 +13,7 @@
 <div class="content">
 <h1>Presentations</h1>
 <ul>
+
   <li> <h2>Ben's Presentations</h2> </li>
   <ul>
     <li> <a href="http://cfdlab.ae.utexas.edu/~benkirk/adv_flows_project.pdf">Adavnced Flows Class Project</a> (natural convection, 2002)</li>
@@ -21,14 +22,31 @@
     <li> <a href="http://cfdlab.ae.utexas.edu/~benkirk/applications_uwa03.pdf">AMR Applications</a> (from the June 2003 UWA Shortcourse)</li>
     <li> <a href="http://cfdlab.ae.utexas.edu/~benkirk/seminar/talk.pdf">Finite Elements - Introduction and Applications</a> (April 2004 NASA/JSC technical seminar)</li>
   </ul>
-  <li> <a href="http://usnccm.sandia.gov/mslist/upload/IncomprssblCFD/2230_Final_abstract.pdf">USNCCM VII</a> conference abstract. </li>
+  
+  <li> <h2>USNCCM VII</h2> </li>
+  <ul>
+    <li> <a href="http://usnccm.sandia.gov/mslist/upload/IncomprssblCFD/2230_Final_abstract.pdf">Abstract </a> </li>
+    <li> <a href="http://www.cfdlab.ae.utexas.edu/~peterson/usnccm.pdf">Presentation.</a> </li>
+  </ul>
 
-    <li>  <a href="http://www.cfdlab.ae.utexas.edu/~peterson/usnccm.pdf">USNCCM VII</a> Presentation.</li>
+  <li> <h2>USNCCM VIII</h2> </li>
+  <ul>
+    <li> <a href="http://www.cfdlab.ae.utexas.edu/~benkirk/carter-USNCCMVIII.pdf">Compressible Flow Studies Using Parallel Adaptive Mesh Refinement</a> </li>
+    <li> <a href="http://www.cfdlab.ae.utexas.edu/~benkirk/carey-USNCCMVIII.pdf"> Algorithms for Compressible Flows with Adaptive Mesh Refinement</a> </li>
+    <li> <a href="http://www.cfdlab.ae.utexas.edu/~peterson/USNCCM8.pdf">         Adaptive Finite Element Simulations of Thermosolutal Convection in Porous Media</a> </li>
+  </ul>
+  
+  <li> <h2>Finite Element Rodeo</h2> </li>
+  <ul>
+    <li> <a href="http://www.cfdlab.ae.utexas.edu/~peterson/Rodeo_2006.pdf">A Stabilized h-Adaptive Continuation Method for Double-Diffusive Convection in Porous Media</a> </li>
+  </ul>
 
-    <li>  <a href="http://www.cfdlab.ae.utexas.edu/~peterson/scraper.pdf">Scraper Flow</a> Presentation with I. Schoegl.</li>
-    <li> <a href="http://www.challenge.nm.org/FinalReports/08.pdf">Albuquerque Academy</a> Presentation on Stress Analysis in a Torus.</li>
-  <li> ICIAM </li>
-  <li>  <a href="http://www.saviac.org/74th_Symposium/abstracts/U040.htm">SAVIAC</a> conference abstract.</li>
+  
+  <!--   add some blank space -->
+  <h2> </h2> q
+  <li> <a href="http://www.cfdlab.ae.utexas.edu/~peterson/scraper.pdf">Scraper Flow</a> Presentation with I. Schoegl.</li>
+  <li> <a href="http://www.challenge.nm.org/FinalReports/08.pdf">Albuquerque Academy</a> Presentation on Stress Analysis in a Torus.</li>
+  <li> <a href="http://www.saviac.org/74th_Symposium/abstracts/U040.htm">SAVIAC</a> conference abstract.</li>
 
 </ul>
 </div>
