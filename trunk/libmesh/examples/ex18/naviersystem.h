117b4e83924d0a6f1ae5cbe31362e8b18f11124f
Roy Stogner <roystgnr@ices.utexas.edu>
2006-06-29 20:49:42 +0000

    Break ex18 into (relatively) physics-independent and physics-specific parts
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1587 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/examples/ex18/naviersystem.h
@@ -0,0 +1,54 @@
+/* $Id: naviersystem.h,v 1.1 2006-06-29 20:49:42 roystgnr Exp $ */
+
+/* The Next Great Finite Element Library. */
+/* Copyright (C) 2003  Benjamin S. Kirk */
+
+/* This library is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
+
+/* This library is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+// DiffSystem framework files
+#include "fem_system.h"
+
+// The Navier-Stokes system class.
+// FEMSystem, TimeSolver and  NewtonSolver will handle most tasks,
+// but we must specify element residuals
+class NavierSystem : public FEMSystem
+{
+public:
+  // Constructor
+  NavierSystem(EquationSystems& es,
+               const std::string& name,
+               const unsigned int number)
+  : FEMSystem(es, name, number), Reynolds(10.) {}
+
+  // System initialization
+  virtual void init_data ();
+
+  // Element residual and jacobian calculations
+  // Time dependent parts
+  virtual bool element_time_derivative (bool request_jacobian);
+
+  // Constraint parts
+  virtual bool element_constraint (bool request_jacobian);
+  virtual bool side_constraint (bool request_jacobian);
+
+  // Finite elements for the velocity and pressure on element interiors
+  FEBase *fe_velocity, *fe_pressure;
+
+  // Finite element for the velocity on element sides
+  FEBase *fe_side_vel;
+
+  // The Reynolds number to solve for
+  Real Reynolds;
+};
