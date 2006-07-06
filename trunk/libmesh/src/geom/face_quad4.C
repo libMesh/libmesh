5c768ec6388d7b28e5ab8418b7c4054b214cf781
John Peterson <jwpeterson@gmail.com>
2006-07-06 21:21:49 +0000

    Added specialized volume computation for Quad4. Also added an approximate volume computation for non-planar Quad4 (living in R^3).  The analyical expression for the area is too complicated to evaluate and the quadrature (2x2 rule) results in 4 to 5 digits of accuracy in most cases.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1595 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/geom/face_quad4.C
+++ trunk/libmesh/src/geom/face_quad4.C
@@ -1,4 +1,4 @@
-// $Id: face_quad4.C,v 1.24 2006-06-20 20:18:48 jwpeterson Exp $
+// $Id: face_quad4.C,v 1.25 2006-07-06 21:21:49 jwpeterson Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -222,17 +222,63 @@ void Quad4::connectivity(const unsigned int sf,
 
 
 
-// Real Quad4::volume () const
-// {
-//   // This is the Varignon parallelogram formula,
-//   // http://softsurfer.com/Archive/algorithm_0101/algorithm_0101.htm#Quadrilaterals
+Real Quad4::volume () const
+{
+  // The A,B,C,D naming scheme here corresponds exactly to the
+  // libmesh counter-clockwise numbering scheme.
   
-//   // First Diagonal vector
-//   Point v20 ( *(this->get_node(2)) - *(this->get_node(0)) );
-
-//   // Second Diagonal vector
-//   Point v31 ( *(this->get_node(3)) - *(this->get_node(1)) );
+  //        3           2        D           C
+  // QUAD4: o-----------o	 o-----------o
+  //        |           |	 |           |
+  //        |           |	 |           |
+  //        |           | 	 |           |
+  //        |           |	 |           |
+  //        |           |	 |           |
+  //        o-----------o	 o-----------o
+  //        0           1	 A           B
   
-//   return 0.5 * (v20.cross(v31)).size() ;
+  // Vector pointing from A to C
+  Point AC ( this->point(2) - this->point(0) );
+  
+  // Vector pointing from A to B
+  Point AB ( this->point(1) - this->point(0) );
 
-// }
+  // Vector pointing from A to D
+  Point AD ( this->point(3) - this->point(0) );
+
+  // The diagonal vector minus the side vectors
+  Point AC_AB_AD (AC - AB - AD);
+    
+  // Check for quick return for planar QUAD4.  This will
+  // be the most common case, occuring for all purely 2D meshes.
+  if (AC_AB_AD == Point(0.,0.,0.))
+    return AB.cross(AD).size();
+  
+  else
+    {
+      // Use 2x2 quadrature to approximate the surface area.  (The
+      // true integral is too difficult to compute analytically.)  The
+      // accuracy here is exactly the same as would be obtained via a
+      // call to Elem::volume(), however it is a bit more optimized to
+      // do it this way.  The technique used is to integrate the magnitude
+      // of the normal vector over the whole area.  See for example,
+      //
+      // Y. Zhang, C. Bajaj, G. Xu. Surface Smoothing and Quality
+      // Improvement of Quadrilateral/Hexahedral Meshes with Geometric
+      // Flow. The special issue of the Journal Communications in
+      // Numerical Methods in Engineering (CNME), submitted as an
+      // invited paper, 2006.
+      // http://www.ices.utexas.edu/~jessica/paper/quadhexgf/quadhex_geomflow_CNM.pdf
+      
+      // 4-point rule
+      const Real q[2] = {0.5 - std::sqrt(3.) / 6.,
+			 0.5 + std::sqrt(3.) / 6.};
+
+      Real vol=0.;
+      for (unsigned int i=0; i<2; ++i)
+	for (unsigned int j=0; j<2; ++j)
+	  vol += (AB + q[i]*AC_AB_AD).cross(AD + q[j]*AC_AB_AD).size();
+      
+      return 0.25*vol;
+    }
+}
