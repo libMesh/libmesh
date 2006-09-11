ef935ae42a518f367cb36482e410b2b19c409fd7
Roy Stogner <roystgnr@ices.utexas.edu>
2006-09-11 17:17:42 +0000

    Reverting broken/incomplete patch
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@1659 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/include/fe/fe_base.h
+++ trunk/libmesh/include/fe/fe_base.h
@@ -1,4 +1,4 @@
-// $Id: fe_base.h,v 1.21 2006-09-11 16:49:38 roystgnr Exp $
+// $Id: fe_base.h,v 1.22 2006-09-11 17:17:42 roystgnr Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -136,21 +136,17 @@ public:
     
   /**
    * Reinitializes all the physical element-dependent data based on
-   * the \p side of the element \p elem.  The \p tolerance paremeter
-   * is passed to the involved call to \p inverse_map().
+   * the \p side of the element \p elem.
    */
   virtual void reinit (const Elem* elem,
-		       const unsigned int side,
-		       const Real tolerance = TOLERANCE) = 0;
+		       const unsigned int side) = 0;
   
   /**
    * Reinitializes all the physical element-dependent data based on
-   * the \p edge of the element \p elem.  The \p tolerance paremeter
-   * is passed to the involved call to \p inverse_map().
+   * the \p edge of the element \p elem.
    */
   virtual void edge_reinit (const Elem* elem,
-		            const unsigned int edge,
-			    const Real tolerance = TOLERANCE) = 0;
+		            const unsigned int edge) = 0;
   
   /**
    * @returns true if the point p is located on the reference element
