32e2178ca29fb95e112b547ec7723152aff28004
Roy H. Stogner <roystgnr@users.sourceforge.net>
2007-05-04 23:54:46 +0000

    Constraining ancestor elements is sometimes necessary after all...

--- libmesh/src/fe/fe_lagrange.C
+++ libmesh/src/fe/fe_lagrange.C
@@ -1,4 +1,4 @@
-// $Id: fe_lagrange.C,v 1.31 2006-11-09 15:12:49 roystgnr Exp $
+// $Id: fe_lagrange.C,v 1.32 2007-05-04 23:54:46 roystgnr Exp $
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
@@ -672,8 +672,8 @@ void FE<Dim,T>::compute_constraints (DofConstraints &constraints,
   if (Dim == 1)
     return;
 
-  // Only constrain active elements
-  if (!elem->active())
+  // Only constrain active and ancestor elements
+  if (elem->subactive())
     return;
 
   assert (elem != NULL);
