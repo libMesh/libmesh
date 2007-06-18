87abaed5015a06468dbe0e60656fce6488be8335
John W. Peterson <jwpeterson@users.sourceforge.net>
2007-06-18 19:47:47 +0000

    Adding more publications.

--- libmesh/doc/html/publications.php
+++ libmesh/doc/html/publications.php
@@ -38,6 +38,22 @@ PhD Dissertation</a>. </li>";
 "<li> John Peterson's <a href=\"http://www.cfdlab.ae.utexas.edu/~peterson/masters.pdf\">
 Masters Report</a>. </li>";
 ?>
+
+<li>
+J.W. Peterson, G.F. Carey, D.J. Knezevic, and B.T. Murray, <i>Adaptive
+  finite element methodology for tumor angiogenesis modeling.</i>
+  <a href="http://dx.doi.org/10.1002/nme.1802">
+  IJNME</a>, vol. 69, no. 6, pp. 1212--1238, 2007.
+</li>
+
+<li>
+B. Kirk, J. W. Peterson, R. H. Stogner, and G. F. Carey,
+<code>libMesh</code><i>: A
+  C++ Library for Parallel Adaptive Mesh Refinement/Coarsening Simulations.</i>
+<a href="http://dx.doi.org/10.1007/s00366-006-0049-3">Engineering with Computers</a>,
+vol. 22, no. 3--4, pp. 237--254, 2006.
+</li>
+
 </ul>
 
 <h1>Publications and Preprints Using LibMesh</h1>
@@ -54,7 +70,8 @@ IJNMF</a> Volume 46, Issue 12, Pages 1211 - 1236, 2004.
 <li>
 Michael Schindler, Peter Talkner, and Peter Hanggi,					     
 <i>Computing stationary free-surface shapes in microfluidics.</i>
-<a href="http://arxiv.org/pdf/physics/0511217">Physics Preprint arXiv</a>
+<a href="http://arxiv.org/pdf/physics/0511217">Physics Preprint arXiv</a> and
+<a href="http://scitation.aip.org/getabs/servlet/GetabsServlet?prog=normal&id=PHFLE6000018000010103303000001&idtype=cvips&gifs=yes">Physics of Fluids</a> 2006.
 </li>
 
 <li>
@@ -69,7 +86,26 @@ Jose Camata, Alvaro Coutinho, and Graham Carey,
 <i>Numerical Evaluation of the LCD Method Implemented in the LibMesh Library.</i>
 <a href="http://www.inf.ufes.br/~avalli/papers/2005/CIL-0692.pdf.gz">CILAMCE</a> 2005.
 </li>
+
+
+<li>
+Matthew Anderson and Jung-Han Kimn,
+<i>A Numerical Approach to Space-Time Finite Elements for the Wave Equation.</i>
+<a href="http://arxiv.org/pdf/gr-qc/0601099">arXiv.org</a>, 2006.
+</li>
+
+
+<li>
+M.P. Luthi,
+<i>A Full Ice Stream Model for Jakobshavn Isbrae.</i>
+<a href="http://www.cosis.net/abstracts/EGU2007/02503/EGU2007-J-02503.pdf?PHPSESSID=f00857a95461bcae8e598e9edf0f1ba8">Geophysical Research Abstracts</a>, Vol 9., 2007.
+</li>
+
+
+
 </ul>
+
+
 
 
 <h1>Misc. Publications</h1>
