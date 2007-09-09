505aba0c50398e7a9f97e71a1d5e7e005f656b8a
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2007-09-09 19:42:07 +0000

    updated documentation
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2078 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/index.php
+++ trunk/libmesh/doc/html/index.php
@@ -1,12 +1,12 @@
-<!-- $Id: index.php,v 1.20 2007-07-01 00:43:38 benkirk Exp $ -->
+<!-- $Id: index.php,v 1.21 2007-09-09 19:42:07 benkirk Exp $ -->
 <?php $root=""; ?>
 <?php require($root."navigation.php"); ?>
 
 <HTML>
 <head>
-  <title>libMesh - A C++ Finite Element Library</title>
-  <META name="description" content="A parallel adaptive C++ framework for the simulation of partial differential equations.">
-  <META name="keywords" content="science math numerical analysis, science math numerical analysis software, numerical analysis research group, partial differential equations, applied partial differential equations, elliptic parabolic hyperbolic partial differential equations, finite element analysis modeling software, system of linear equation solver, simultaneous linear equation solver, computational fluid dynamics software">
+  <title>libMesh - C++ Finite Element Library</title>
+  <META name="description" content="A parallel adaptive C++ finite element library for simulating partial differential equations.">
+  <META name="keywords" content="finite element, finite element method, finite element modeling, finite element modelling, finite element software, finite element methods, science math numerical analysis, science math numerical analysis software, numerical analysis research group, partial differential equations, applied partial differential equations, elliptic parabolic hyperbolic partial differential equations, finite element analysis modeling software, system of linear equation solver, simultaneous linear equation solver, computational fluid dynamics software">
   <META name="robots" content="index, follow">
   <?php load_style($root); ?>
 </head>
@@ -25,7 +25,7 @@
 // This php script makes the words wrap nicely around the image which
 // we have split up into several sections.  This apparently only works
 // for Gecko-enabled browsers, e.g. Firefox.
-if (NW_IS_GECKO)
+if (NW_IS_GECKO || NW_IS_MAC)
 {
   // You can get these numbers using the 'file' command on most linux systems.
   $pic_widths  = array( 417, 417, 417, 417, 417, 400, 345, 270, 205, 145, 80 );
@@ -43,6 +43,14 @@ if (NW_IS_GECKO)
 	  echo "<img class=\"slant\" width=$pic_widths[$i] height=$pic_heights[$i] src=\"images/libmesh_mesh_layer_10.png\">";
 	}
     }
+
+//  $pic_widths  = array( 56, 122, 180, 245, 310, 373, 410 );
+//  $pic_heights = array( 13,  13,  12,  12,  12,  13, 128 );
+//
+//  for ($i=0; $i<7; $i++)
+//    {
+//      echo "<img class=\"slant_right\" width=$pic_widths[$i] height=$pic_heights[$i] src=\"images/libmesh_mesh_layer_rhs_0$i.png\">";
+//    }
 }
 
 // Do not do slanting text
@@ -59,26 +67,36 @@ else
     }
 }
 ?>
-The <code>libMesh</code> library is a C++ framework for the numerical
-simulation of partial differential equations on serial and parallel
-platforms. Development began in March 2002 with the intent of
-providing a friendly interface to a number of high-quality software
-packages that are publicly available. Currently the library supports
-1D, 2D, and 3D steady and transient finite element and finite volume simulations.
+The <code>libMesh</code> library provides a framework for the
+numerical simulation of partial differential equations using arbitrary
+unstructured discretizations on serial and parallel platforms. A major
+goal of the library is to provide support for adaptive mesh refinement
+(AMR) computations in parallel while allowing a research scientist to
+focus on the physics they are modeling.
+
+<!-- Development began in March 2002 with the intent of providing a -->
+<!-- friendly interface to a number of high-quality software packages that -->
+<!-- are publicly available. -->
+
+<br>
+<br>
+<code>libMesh</code> currently supports 1D, 2D, and 3D steady and transient finite element simulations.
+The library makes use of high-quality, existing software whenever possible.
 <a href="http://www-unix.mcs.anl.gov/petsc/petsc-2">PETSc</a> is
-currently used for the solution of linear systems on both serial and
+used for the solution of linear systems on both serial and
 parallel platforms, and
-<a href="http://www.tu-dresden.de/mwism/skalicky/laspack/laspack.html">LASPack</a>
+<a href="http://www.mgnet.org/mgnet/Codes/laspack/html/laspack.html">LASPack</a>
 is included with the library to provide linear solver support on serial machines.
 An optional interface to <a href="http://www.grycap.upv.es/slepc">SLEPc</a> is also
 provided for solving both standard and generalized eigenvalue problems.
+A complete list of external applications used in the library may be found <a href="externalsoftware.php">here</a>.
 
 
 <br>
 <br>
-The <code>libMesh</code> library is primarily developed at The
+The <code>libMesh</code> library has been actively developed at The
 University of Texas at Austin in the 
-<a href="http://www.cfdlab.ae.utexas.edu">CFDLab</a>.  Major 
+<a href="http://www.cfdlab.ae.utexas.edu">CFDLab</a> since March 2002.  Major 
 contributions have come from developers at the Technische 
 Universit&auml;t Hamburg-Harburg
 <a href="http://www.mub.tu-harburg.de/index_e.html">Institute of Modelling and Computation</a>, 
@@ -94,17 +112,6 @@ development branch by checking out the
 <a href="http://libmesh.cvs.sourceforge.net/libmesh/libmesh">CVS
 Logs</a> online, and you can see many people are downloading the library
 on the <a href="http://sourceforge.net/project/stats/?group_id=71130&ugn=libmesh">statistics</a> page.
-
-<br>
-<br>
-A major goal of the library is to provide support for adaptive mesh
-refinement (AMR) computations in parallel while allowing a research
-scientist to focus on the physics they are modeling.
-The library makes use of high-quality, existing software whenever possible.
-A complete list of external applications used in the library may be found <a href="externalsoftware.php">here</a>.
-
-<br>
-<br>
 
 </div>
 <br>
