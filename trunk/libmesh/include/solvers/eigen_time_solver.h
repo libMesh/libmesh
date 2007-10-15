0ad6a7c5f7702ae6a500cb9589c0fb7d7c46b172
John Peterson <jwpeterson@gmail.com>
2007-10-15 20:33:02 +0000

    First check-in of the generalized EigenTimeSolver based on Steffen's Slepc solver.  This code is largely untested and probably shouldn't be used yet unless you know what you're doing.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2173 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/include/solvers/eigen_time_solver.h
@@ -0,0 +1,207 @@
+// $Id: eigen_time_solver.h,v 1.1 2007-10-15 20:33:01 jwpeterson Exp $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2005  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __eigen_time_solver_h__
+#define __eigen_time_solver_h__
+
+// C++ includes
+
+// Local includes
+#include "time_solver.h"
+
+// Forward declarations
+template <typename T> class EigenSolver;
+
+
+/**
+ * The name of this class is confusing...it's meant to refer to the
+ * base class (TimeSolver) while still telling one that it's for solving
+ * (generalized) EigenValue problems that arise from finite element
+ * discretizations.  For a time-dependent problem du/dt=F(u), with a
+ * steady solution 0=F(u_0), we look at the time evolution of a small
+ * perturbation, p=u-u_0, for which the (linearized) governing equation is
+ *
+ * dp/dt = F'(u_0)p
+ *
+ * where F'(u_0) is the Jacobian.  The generalized eigenvalue problem arises
+ * by considering perturbations of the general form p = exp(lambda*t)x, which
+ * leads to
+ *
+ *  Ax = lambda*Bx
+ *
+ * where A is the (discretized by FEM) Jacobian matrix and B is the
+ * (discretized by FEM) mass matrix.
+ *
+ * The EigenSystem class (by Steffen Petersen) is related but does not
+ * fall under the FEMSystem paradigm invented by Roy Stogner.  The EigenSolver
+ * class (also by Steffen) is meant to provide a generic "linear solver"
+ * interface for EigenValue software.  The only current concrete implementation
+ * is a SLEPc-based eigensolver class, which we make use of here as well.
+ *
+ * @author John W. Peterson 2007
+ */
+
+
+class EigenTimeSolver : public TimeSolver
+{
+public:
+  /**
+   * The type of system
+   */
+  typedef DifferentiableSystem sys_type;
+
+  /**
+   * The parent class
+   */
+  typedef TimeSolver Parent;
+  
+  /**
+   * Constructor. Requires a reference to the system
+   * to be solved.
+   */
+  EigenTimeSolver (sys_type& s);
+  
+  /**
+   * Destructor.
+   */
+  virtual ~EigenTimeSolver ();
+
+  /**
+   * The initialization function.  This method is used to
+   * initialize internal data structures before a simulation begins.
+   */
+  virtual void init ();
+
+  /**
+   * The reinitialization function.  This method is used after
+   * changes in the mesh
+   */
+  virtual void reinit ();
+
+  /**
+   * Implements the assembly of both matrices A and B, and calls
+   * the EigenSolver to compute the eigenvalues.
+   */
+  virtual void solve ();
+
+  /**
+   * It doesn't make sense to advance the timestep, so we shouldn't call this.
+   */
+  virtual void advance_timestep () { }
+
+  /**
+   * error convergence order against deltat is
+   * not applicable to an eigenvalue problem.
+   */
+  virtual Real error_order() const { return 0.; }
+
+  /**
+   * Forms either the spatial (Jacobian) or mass matrix part of the
+   * operator, depending on which is requested.
+   */
+  virtual bool element_residual (bool get_jacobian);
+
+  /**
+   * Forms the jacobian of the boundary terms.
+   */
+  virtual bool side_residual (bool get_jacobian);
+
+  /**
+   * Nominally computes the size of the difference between
+   * successive solution iterates ||u^{n+1} - u^{n}|| in some norm,
+   * but for this class just returns 0.
+   */
+  Real du (unsigned char) { return 0.; }
+
+  /**
+   * The EigenSolver object.  This is what actually 
+   * makes the calls to SLEPc.
+   */
+  AutoPtr<EigenSolver<Number> > eigen_solver;
+
+  /**
+   * The linear solver tolerance to be used when solving the
+   * eigenvalue problem. FIXME: need more info...
+   */
+  Real tol;
+
+  /**
+   * The maximum number of iterations allowed to solve the problem.
+   */
+  unsigned int maxits;
+
+  /**
+   * The number of eigenvectors/values to be computed.
+   */
+  unsigned int n_eigenpairs_to_compute;    
+
+  /**
+   * The number of basis vectors to use in the computation.  According
+   * to ex16, the number of basis vectors must be >= the number of
+   * eigenpairs requested, and ncv >= 2*nev is recommended.
+   * Increasing this number, even by a little bit, can _greatly_
+   * reduce the number of (EigenSolver) iterations required to compute
+   * the desired number of eigenpairs, but the _cost per iteration_
+   * goes up drastically as well.
+   */
+  unsigned int n_basis_vectors_to_use;
+
+  /**
+   * After a solve, holds the number of eigenpairs successfully
+   * converged.
+   */
+  unsigned int n_converged_eigenpairs;
+
+  /**
+   * After a solve, holds the number of iterations required to converge
+   * the requested number of eigenpairs.
+   */
+  unsigned int n_iterations_reqd;
+  
+private:
+  
+  enum NowAssembling {
+    /**
+     * The matrix associated with the spatial part of the operator.
+     */
+    Matrix_A,
+
+    /**
+     * The matrix associated with the time derivative (mass matrix).
+     */
+    Matrix_B,
+
+    /**
+     * The enum is in an invalid state.
+     */
+    Invalid_Matrix
+  };
+
+  /**
+   * Flag which controls the internals of element_residual() and side_residual().
+   */
+  NowAssembling now_assembling;
+  
+};
+
+
+
+#endif // #define __eigen_time_solver_h__
---
+++ trunk/libmesh/src/solvers/eigen_time_solver.C
@@ -0,0 +1,178 @@
+#include "diff_solver.h"
+#include "diff_system.h"
+#include "eigen_time_solver.h"
+#include "eigen_solver.h"
+#include "sparse_matrix.h"
+
+// Constructor
+EigenTimeSolver::EigenTimeSolver (sys_type& s)
+  : Parent(s),
+    eigen_solver     (EigenSolver<Number>::build()),
+    tol(pow(TOLERANCE, 5./3.)),
+    maxits(1000),
+    n_eigenpairs_to_compute(5),
+    n_basis_vectors_to_use(3*n_eigenpairs_to_compute),
+    n_converged_eigenpairs(0),    
+    n_iterations_reqd(0)
+{
+  untested();
+  eigen_solver->set_eigenproblem_type(GHEP);//or GNHEP
+  eigen_solver->set_position_of_spectrum(LARGEST_MAGNITUDE);
+}
+
+EigenTimeSolver::~EigenTimeSolver ()
+{
+}
+
+void EigenTimeSolver::reinit ()
+{
+  // empty...
+}
+  
+void EigenTimeSolver::init ()
+{
+  // FIXME: Add matrix "B" to _system?
+  _system.add_matrix("B");
+}
+
+void EigenTimeSolver::solve ()
+{
+  // The standard implementation is basically to call:
+  // _diff_solver->solve();
+  // which internally assembles (when necessary) matrices and vectors
+  // and calls linear solver software while also doing Newton steps (see newton_solver.C)
+  //
+  // The element_residual and side_residual functions below control
+  // what happens in the interior of the element assembly loops.
+  // We have a system reference, so it's possible to call _system.assembly()
+  // ourselves if we want to...
+  //
+  // Interestingly, for the EigenSolver we don't need residuals...just Jacobians.
+  // The Jacobian should therefore always be requested, and always return
+  // jacobian_computed as being true.
+
+  // The basic plan of attack is:
+  // .) Construct the Jacobian using _system.assembly(true,true) as we
+  //    would for a steady system.  Use a flag in this class to
+  //    control behavior in element_residual and side_residual
+  // .) Swap _system.matrix to matrix "B" (be sure to add this extra matrix during init)
+  // .) Call _system.assembly(true,true) again, using the flag in element_residual
+  //    and side_residual to only get the mass matrix terms.
+  // .) Send A and B to Steffen's EigenSolver interface.
+
+  // Assemble the spatial part (matrix A) of the operator
+  if (!this->quiet)
+    std::cout << "Assembling matrix A." << std::endl;
+  _system.matrix =   &( _system.get_matrix ("System Matrix") );
+  this->now_assembling = Matrix_A;
+  _system.assembly(true, true);
+  //_system.matrix->print_matlab("matrix_A.m");
+  
+  // Point the system's matrix at B, call assembly again.
+  if (!this->quiet)
+    std::cout << "Assembling matrix B." << std::endl;
+  _system.matrix =   &( _system.get_matrix ("B") );
+  this->now_assembling = Matrix_B;
+  _system.assembly(true, true);
+  //_system.matrix->print_matlab("matrix_B.m");
+  
+  // Send matrices A, B to Steffen's SlepcEigenSolver interface
+  //here();
+  if (!this->quiet)
+    std::cout << "Calling the EigenSolver." << std::endl;
+  std::pair<unsigned int, unsigned int> solve_data =
+    eigen_solver->solve_generalized (_system.get_matrix ("System Matrix"),
+				     _system.get_matrix ("B"),
+				     n_eigenpairs_to_compute,
+				     n_basis_vectors_to_use,
+				     tol,
+				     maxits);
+  
+  this->n_converged_eigenpairs = solve_data.first;
+  this->n_iterations_reqd      = solve_data.second;
+}
+
+
+
+bool EigenTimeSolver::element_residual(bool request_jacobian)
+{
+  // The EigenTimeSolver always computes jacobians!
+  assert (request_jacobian);
+  
+  // Assemble the operator for the spatial part.
+  if (now_assembling == Matrix_A)
+    {
+      bool jacobian_computed =
+	_system.element_time_derivative(request_jacobian);
+
+      // The user shouldn't compute a jacobian unless requested
+      assert(request_jacobian || !jacobian_computed);
+
+      bool jacobian_computed2 =
+	_system.element_constraint(jacobian_computed);
+
+      // The user shouldn't compute a jacobian unless requested
+      assert (jacobian_computed || !jacobian_computed2);
+
+      return jacobian_computed && jacobian_computed2;
+  
+    }
+
+  // Assemble the mass matrix operator
+  else if (now_assembling == Matrix_B)
+    {
+      bool mass_jacobian_computed =
+	_system.mass_residual(request_jacobian);
+
+      // Scale Jacobian by -1?
+      //_system.elem_jacobian *= -1.0;
+
+      return mass_jacobian_computed;
+    }
+
+  else
+    {
+      error();
+      return false;
+    }
+}
+
+
+
+bool EigenTimeSolver::side_residual(bool request_jacobian)
+{
+  // The EigenTimeSolver always requests jacobians?
+  //assert (request_jacobian);
+
+  // Assemble the operator for the spatial part.
+  if (now_assembling == Matrix_A)
+    {
+      bool jacobian_computed =
+	_system.side_time_derivative(request_jacobian);
+
+      // The user shouldn't compute a jacobian unless requested
+      assert (request_jacobian || !jacobian_computed);
+
+      bool jacobian_computed2 =
+	_system.side_constraint(jacobian_computed);
+
+      // The user shouldn't compute a jacobian unless requested
+      assert (jacobian_computed || !jacobian_computed2);
+  
+      return jacobian_computed && jacobian_computed2;
+  
+    }
+
+  // There is no "side" equivalent for the mass matrix
+  else if (now_assembling == Matrix_B)
+    {
+      return false;//?
+      //return true;
+    }
+
+  else
+    {
+      error();
+      return false;
+    }
+}
