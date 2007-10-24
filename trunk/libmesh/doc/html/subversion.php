223340c6d645ef5d7a89452652bcbd6ce78292b8
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2007-10-24 16:15:06 +0000

    updated documentation
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2231 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/doc/html/subversion.php
+++ trunk/libmesh/doc/html/subversion.php
@@ -14,28 +14,21 @@
 <h1>Subversion Repository Information</h1>
 You may  access the SVN source tree for the latest code. You can get access
 to the SVN repository via:
-<br>
-
 <div class="fragment">
   <pre>svn checkout https://libmesh.svn.sourceforge.net/svnroot/libmesh/trunk/libmesh </pre>
 </div>
-
-<br>
 If you would like to contribute to the project you will need a SourceForge
 developer account, or you can contribute patches. To create a patch from a
 modified SVN tree simply do:
-<br>
 <div class="fragment">
   <pre>svn diff &gt; patch </pre>
 </div>
-
-<br>
 in the top-level directory. You can then submit the file <code>patch</code>.
 
 
 <h2>Repository Organization</h2>
-  The base repository for the <code>libMesh</code> project is <pre>https://libmesh.svn.sourceforge.net/svnroot/libmesh </pre>
-  which holds the active development branch and previously tagged versions.  The top-level repostory is organized as follows:
+  The base repository for the <code>libMesh</code> project is <code>https://libmesh.svn.sourceforge.net/svnroot/libmesh </code>
+  which holds the active development branch and previously tagged versions.  The top-level repository is organized as follows:
 <div class="fragment">
   <pre>
   ./branches --> Branches for major feature additions.
@@ -44,8 +37,6 @@ in the top-level directory. You can then submit the file <code>patch</code>.
   </pre>
 </div>
 
-<br>  
-  
 
 <h2>Tagged Versions</h2>
 <h3>Creating a new Tag</h3>
@@ -56,21 +47,21 @@ We often create tagged versions of the repository.  These tags often correspond 
              https://libmesh.svn.sourceforge.net/svnroot/libmesh/tags/libmesh-x.y.z
   </pre>
 </div>
-<br>
 will create a tagged version <code>libmesh-x.y.z</code>.
 
-<h3>Cheking out a Specified Tag</h3>
+<h3>Checking out a Specified Tag</h3>
 It is equally simple to check out a tagged version of the code:
 <div class="fragment">
   <pre>
   $ svn checkout https://libmesh.svn.sourceforge.net/svnroot/libmesh/tags/libmesh-x.y.z/libmesh libmesh-x.y.z
   </pre>
 </div>
-<br>
 will checkout the tagged version <code>libmesh-x.y.z</code> and place it in the local directory <code>libmesh-x.y.z</code>
 </div>
 <br>
 
+<h2>Reference</h2>
+For a comprehensive discussion of subversion, its features, and examples of its usage read <a href="http://svnbook.red-bean.com"><i>Version Control with</i> Subversion</a>.
 <?php make_footer() ?>
 
 </body>
