d6e716a8616926378c510cebc4b7e467946435d2
Roy Stogner <roystgnr@ices.utexas.edu>
2007-11-03 19:07:19 +0000

    Another bugfix in the sanity check
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2309 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/partitioning/partitioner.C
+++ trunk/libmesh/src/partitioning/partitioner.C
@@ -115,6 +115,6 @@ void Partitioner::_set_node_processor_ids(MeshBase& mesh)
   node_it  = mesh.nodes_begin();
   // Make sure we hit all the nodes
   for ( ; node_it != node_end; ++node_it)
-    assert((*node_it)->processor_id() != DofObject::invalid_id);
+    assert((*node_it)->processor_id() != DofObject::invalid_processor_id);
 #endif
 }
