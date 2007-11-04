3ae22307f5e39790b32abbcd265e790c4b23b7a5
Roy Stogner <roystgnr@ices.utexas.edu>
2007-11-04 03:05:18 +0000

    Added static global remote_elem pointer for use with ParallelMesh
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2321 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/src/geom/remote_elem.C
@@ -0,0 +1,27 @@
+// $Id: remote_elem.C 2233 2007-10-24 16:58:42Z benkirk $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2007  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+// Local includes
+#include "remote_elem.h"
+
+// Pointer to singleton Remote Element (to be created in
+// libMesh::init()
+const RemoteElem* remote_elem;
