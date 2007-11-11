1d33e2d6e4b5f4c4633546d4577e6fed8500afa6
Derek Gaston <friedmud@users.sourceforge.net>
2007-11-11 02:26:50 +0000

    Initial cppUnit Commit

---
+++ libmesh_tests/cppunit/driver.C
@@ -0,0 +1,11 @@
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int main( int argc, char **argv)
+{
+  CppUnit::TextUi::TestRunner runner;
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+  runner.addTest( registry.makeTest() );
+  runner.run();
+  return 0;
+}
