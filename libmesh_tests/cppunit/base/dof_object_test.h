2a33605540b4e949eb6246eb03ef691b106286fe
Derek Gaston <friedmud@users.sourceforge.net>
2007-11-12 01:14:38 +0000

    more dof_object tests

--- libmesh_tests/cppunit/base/dof_object_test.h
+++ libmesh_tests/cppunit/base/dof_object_test.h
@@ -5,7 +5,12 @@
 #include <cppunit/TestCase.h>
 
 #define DOFOBJECTTEST \
-  CPPUNIT_TEST( testValidId );
+  CPPUNIT_TEST( testSetId ); \
+  CPPUNIT_TEST( testValidId ); \
+  CPPUNIT_TEST( testInvalidateId ); \
+  CPPUNIT_TEST( testSetProcId ); \
+  CPPUNIT_TEST( testValidProcId ); \
+  CPPUNIT_TEST( testInvalidateProcId );
 
 template <class DerivedClass>
 class DofObjectTest {
@@ -19,6 +24,14 @@ public:
     instance=derived_instance;
   }
 
+  void testSetId()
+  {
+    DofObject aobject(*instance);
+
+    aobject.set_id(1);
+    CPPUNIT_ASSERT_EQUAL( (unsigned int)1 , aobject.id() );
+  }
+  
   void testValidId()
   {
     DofObject aobject(*instance);
@@ -28,6 +41,45 @@ public:
 
     aobject.set_id(DofObject::invalid_id);
     CPPUNIT_ASSERT( !aobject.valid_id() );
+  }
+
+  void testInvalidateId()
+  {
+    DofObject aobject(*instance);
+
+    aobject.set_id(1);
+    aobject.invalidate_id();
+
+    CPPUNIT_ASSERT( !aobject.valid_id() );
+  }
+
+  void testSetProcId()
+  {
+    DofObject aobject(*instance);
+
+    aobject.processor_id(libMesh::processor_id());
+    CPPUNIT_ASSERT_EQUAL( (short unsigned int)libMesh::processor_id() , aobject.processor_id() );
+  }
+
+  void testValidProcId()
+  {
+    DofObject aobject(*instance);
+
+    aobject.processor_id(libMesh::processor_id());
+    CPPUNIT_ASSERT(aobject.valid_processor_id());
+    
+    aobject.processor_id(DofObject::invalid_processor_id);
+    CPPUNIT_ASSERT(!aobject.valid_processor_id());
+  }
+
+  void testInvalidateProcId()
+  {
+    DofObject aobject(*instance);
+
+    aobject.processor_id(libMesh::processor_id());
+    aobject.invalidate_processor_id();
+
+    CPPUNIT_ASSERT( !aobject.valid_processor_id() );
   }
 };
 
