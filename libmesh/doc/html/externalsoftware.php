57f6da7744b3347779c07a9c1d903ad5348c9b5f
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2007-12-02 17:57:32 +0000

    more package descriptions

--- libmesh/doc/html/externalsoftware.php
+++ libmesh/doc/html/externalsoftware.php
@@ -21,8 +21,7 @@
 
 <h3>SLEPc</h3> The Scalable Library for Eigenvalue Computations <a href="http://www.grycap.upv.es/slepc">(SLEPc)</a> is a library for the solution of large scale sparse eigenvalue problems on parallel computers. It is an extension of PETSc and can be used for either standard or generalized eigenproblems, with real or complex arithmetic.
 
-<h3>MPI</h3> The <a href="http://www-unix.mcs.anl.gov/mpi">Message Passing Interface</a> is a standard for parallel programming using the message passing model.  PETSc requires MPI for its functionality.  <code>libMesh</code> makes use of MPI to when running in parallel for certain operations. 
-
+<h3>MPI</h3> The <a href="http://www-unix.mcs.anl.gov/mpi">Message Passing Interface</a> is a standard for parallel programming using the message passing model. PETSc requires MPI for its functionality.  <code>libMesh</code> makes use of MPI to when running in parallel for certain operations, including its <code>ParallelMesh</code> distributed-memory, fully unstructured mesh implementation. 
 
 <h3>BLAS</h3> The <a href="http://www.netlib.org/blas">Basic Linear Algebra Subprograms</a> are routines that provide standard building blocks for performing basic vector and matrix operations. The Level 1 BLAS perform scalar, vector and vector-vector operations, the Level 2 BLAS perform matrix-vector operations, and the Level 3 BLAS perform matrix-matrix operations. High-performance implementations of the BLAS are generally provided by computer hardware manufacturers for a particular architecture.  PETSc makes extensive use of the BLAS hence a high-performance BLAS implementation is key to achieving high performance from the PETSc linear solvers.
 
@@ -30,15 +29,30 @@
 <a href="http://www.netlib.org/lapack">LAPACK</a> s written in Fortran77 and provides routines for solving systems of simultaneous linear equations, least-squares solutions of linear systems of equations, eigenvalue problems, and singular value problems. The associated matrix factorizations (LU, Cholesky, QR, SVD, Schur, generalized Schur) are also provided, as are related computations such as reordering of the Schur factorizations and estimating condition numbers. Dense and banded matrices are handled, but not general sparse matrices.  PETSc makes use of LAPACK in several computational kernels within its linear solver framework.
 
 <hr>
-<h2>LASPACK - Serial Linear Solvers</h2>
+<h2>LASPack - Serial Linear Solvers</h2>
+ <a href="http://www.tu-freiberg.de/urz/anwendungen/sprodukte/soft/LASPACK/laspack.html">LASPack</a> is an object-oriented package of iterative methods, multigrid solvers, and auxiliary routines for the iterative solution of linear systems. It does not run in parallel. There are data structures for vectors, general and square matrices, and preconditioners; a large number of accessing and manipulating these objects is available.
 
+<!--
+Main features:
+<ul>
+  <li>The primary aim of LASPack is the implementation of efficient iterative methods for the solution of systems of linear equations. All routines and data structures are optimized for effective usage of resources especially with regard to large sparse matrices. The package can be accessed from an application through a straightforward interface defined in the form of procedure calls.</li>
+  <li>Besides the obligatory Jacobi, successive over-relaxation, Chebyshev, and conjugate gradient solvers, LASPack contains selected state-of-the-art algorithms which are commonly used for large sparse systems:
+  <ul>
+   <li>CG-like methods for non-symmetric systems: CGN, GMRES, BiCG, QMR, CGS, and BiCGStab,</li>
+   <li>multilevel methods such as the multigrid and conjugate gradient methods preconditioned by multigrid and BPX preconditioners</li>
+  </ul>
+  All the above solvers are applicable not only to the positive definite or non-symmetric matrices, but are also adopted for singular systems (e.g. arising from discretization of Neumann boundary value problems).</li>
+  <li>The implementation is based on an object-oriented approach (although it is programmed in C). Vectors and matrices are defined as new data types in connection with the corresponding supporting routines. The basic operations are implemented in such a way that they allow the programming of linear algebra algorithms in a natural way.</li>
+  <li>LASPack is extensible in a simple manner. An access to the internal representation of vectors and matrices is not necessary and is avoided, as required of the object-oriented programming. This allows an improvement of algorithms or a modification of data structures with no adjustment of application programs using the package</li>
+</ul>
+-->
 
 
 <hr>
 <h2>Mesh Generation</h2>
 <h3>Triangle</h3> <a href="http://www.cs.cmu.edu/~quake/triangle.html">Triangle</a>
 <h3>Tetgen</h3> <a href="http://tetgen.berlios.de">Tetgen</a>
-<h3>EXODUS II</h3> EXODUS II, available via the <a href="http://endo.sandia.gov/SEACAS/Documentation/SEACAS.html">Sandia Engineering Analysis Code Access System</a>, is a model developed to store and retrieve data for finite element analyses. It is used for preprocessing (problem definition), postprocessing (results visualization), as well as code to code data transfer. An EXODUS II data file is a random access, machine independent, binary file that is written and read via C, C++, or Fortran library routines which comprise the Application Programming Interface. <code>libMesh</doc> can detect a valid EXODUS II installation and use it to read EXODUS II mesh files.  
+<h3>EXODUS II</h3> EXODUS II, available via <a href="http://sourceforge.net/projects/exodusii">sourceforge</a>, is a model developed to store and retrieve data for finite element analyses. It is used for preprocessing (problem definition), postprocessing (results visualization), as well as code to code data transfer. An EXODUS II data file is a random access, machine independent, binary file that is written and read via C, C++, or Fortran library routines which comprise the Application Programming Interface. <code>libMesh</code> contains source code for the EXODUS II library in <code>./contrib/exodusii</code> and can use it to read EXODUS II mesh files.  Additional information may be found on the <a href="http://endo.sandia.gov/SEACAS/Documentation/SEACAS.html">Sandia Engineering Analysis Code Access System</a>
 
 
 
@@ -53,19 +67,19 @@
 
 <hr>
 <h2>Utilities</h2>
-<h3>XDR</h3>
-<h3>GetPot</h3> <a href="http://getpot.sourceforge.net">GetPot</a>
 
+<h3>XDR</h3> The <a href="http://www.faqs.org/rfcs/rfc1014.html">XDR: External Data Representation Standard</a> is a standard for the description and encoding of data.  It is useful for transferring data between different computer architectures, and as such provides a very simple, portable approach for writing platform independent binary files. <code>libMesh</code> uses the <a href="doxygen/classXdr.php">Xdr</a> class to provide a uniform interface to input/output operations for files in either XDR binary or ASCII text formats.
+
+<h3>GetPot</h3> <a href="http://getpot.sourceforge.net">GetPot</a> is a powerful command line and configuration file parsing for C++, Python, Ruby and Java. This tool provides many features, such as separate treatment for options, variables, and flags, unrecognized object detection, prefixes and much more. <code>libMesh</code> uses GetPot to parse command line options upon initialization and for input file parsing in some of the <a href="examples.php">examples</a>.
+
+<h3>libHilbert</h3> <a href="http://flame.cs.dal.ca/~chamilto/hilbert">libHilbert</a> is library written by <a href="http://flame.cs.dal.ca/~chamilto">Chris Hamilton</a> for producing compact Hilbert indices for multidimensional data.  <code>libMesh</code> uses <code>libHilbert</code> to assign unique, global identifiers for nodes an elements which are independent of a particular domain decomposition.
 
 
 
 <br>
 <br>
 <br>
-<!--
-<div id="navBeta">
-</div>
--->
+
 
 <?php make_footer() ?>
 
