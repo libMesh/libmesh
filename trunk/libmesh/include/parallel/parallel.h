9cf8e4f9a765e08499dc34fb2fbebaa6d2589a57
Roy Stogner <roystgnr@ices.utexas.edu>
2008-01-24 20:42:29 +0000

    Fixes for std::complex<> use
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2611 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/include/parallel/parallel.h
+++ trunk/libmesh/include/parallel/parallel.h
@@ -139,9 +139,6 @@ namespace Parallel
   template <typename T>
   inline void sum(T &r);
 
-  template <typename T>
-  inline void sum(std::complex<T> &r);
-
   //-------------------------------------------------------------------
   /**
    * Take a vector of local variables and replace each entry with the sum of
@@ -149,9 +146,6 @@ namespace Parallel
    */
   template <typename T>
   inline void sum(std::vector<T> &r);
-
-  template <typename T>
-  inline void sum(std::vector<std::complex<T> > &r);
 
   //-------------------------------------------------------------------
   /**
@@ -287,6 +281,10 @@ namespace Parallel
   template <typename T>
   inline void broadcast(std::vector<T> &data, const unsigned int root_id=0);
 
+  // gcc appears to need an additional declaration to make sure it
+  // uses the right definition below
+  template <typename T>
+  inline void broadcast(std::vector<std::complex<T> > &data, const unsigned int root_id=0);
 
 
   //-----------------------------------------------------------------------
@@ -325,7 +323,6 @@ namespace Parallel
 
   template<>
   inline MPI_Datatype datatype<long double>() { return MPI_LONG_DOUBLE; }
-
 
   template <typename T>
   inline bool verify(const T &r)
@@ -469,14 +466,13 @@ namespace Parallel
   {
     if (libMesh::n_processors() > 1)
       {
-	T temp;
-	MPI_Allreduce (&r,
-		       &temp,
+	T temp = r;
+	MPI_Allreduce (&temp,
+		       &r,
 		       1,
 		       datatype<T>(),
 		       MPI_SUM,
 		       libMesh::COMM_WORLD);
-	r = temp;
       }
   }
 
@@ -486,14 +482,13 @@ namespace Parallel
   {
     if (libMesh::n_processors() > 1 && !r.empty())
       {
-	std::vector<T> temp(r.size());
-	MPI_Allreduce (&r[0],
-		       &temp[0],
+	std::vector<T> temp(r);
+	MPI_Allreduce (&temp[0],
+		       &r[0],
 		       r.size(),
 		       datatype<T>(),
 		       MPI_SUM,
 		       libMesh::COMM_WORLD);
-	r = temp;
       }
   }
 
@@ -503,17 +498,13 @@ namespace Parallel
   {
     if (libMesh::n_processors() > 1)
       {
-	T tempinput[2], tempoutput[2];
-	tempinput[0] = r.real();
-	tempinput[1] = r.imag();
-	MPI_Allreduce (&tempinput,
-		       &tempoutput,
+	std::complex<T> temp(r);
+	MPI_Allreduce (&temp,
+		       &r,
 		       2,
 		       datatype<T>(),
 		       MPI_SUM,
 		       libMesh::COMM_WORLD);
-	r.real() = tempoutput[0];
-	r.imag() = tempoutput[1];
       }
   }
 
@@ -523,32 +514,13 @@ namespace Parallel
   {
     if (libMesh::n_processors() > 1 && !r.empty())
       {
-	std::vector<T> temprealinput(r.size()),
-	  tempimaginput(r.size()),
-	  temprealoutput(r.size()),
-	  tempimagoutput(r.size());
-	for (unsigned int i=0; i != r.size(); ++i)
-	  {
-	    temprealinput[i] = r[i].real();
-	    tempimaginput[i] = r[i].imag();
-	  }
-	MPI_Allreduce (&temprealinput[0],
-		       &temprealoutput[0],
-		       r.size(),
+	std::vector<std::complex<T> > temp(r);
+	MPI_Allreduce (&temp[0],
+		       &r[0],
+		       r.size() * 2,
 		       datatype<T>(),
 		       MPI_SUM,
 		       libMesh::COMM_WORLD);
-	MPI_Allreduce (&tempimaginput[0],
-		       &tempimagoutput[0],
-		       r.size(),
-		       datatype<T>(),
-		       MPI_SUM,
-		       libMesh::COMM_WORLD);
-	for (unsigned int i=0; i != r.size(); ++i)
-	  {
-	    r[i].real() = temprealoutput[i];
-	    r[i].imag() = tempimagoutput[i];
-	  }
       }
   }
 
@@ -564,6 +536,26 @@ namespace Parallel
     const int ierr =	  
       MPI_Send (buf.empty() ? NULL : &buf[0],
 		buf.size(),
+		datatype<T>(),
+		dest_processor_id,
+		tag,
+		libMesh::COMM_WORLD);    
+    assert (ierr == MPI_SUCCESS);
+    
+    STOP_LOG("send()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void send (const unsigned int dest_processor_id,
+		    std::vector<std::complex<T> > &buf,
+		    const int tag)
+  {
+    START_LOG("send()", "Parallel");
+    
+    const int ierr =	  
+      MPI_Send (buf.empty() ? NULL : &buf[0],
+		buf.size() * 2,
 		datatype<T>(),
 		dest_processor_id,
 		tag,
@@ -597,6 +589,28 @@ namespace Parallel
   }
 
 
+  template <typename T>
+  inline void isend (const unsigned int dest_processor_id,
+		     std::vector<std::complex<T> > &buf,
+		     request &r,
+		     const int tag)
+  {
+    START_LOG("isend()", "Parallel");
+    
+    const int ierr =	  
+      MPI_Isend (buf.empty() ? NULL : &buf[0],
+		 buf.size() * 2,
+		 datatype<T>(),
+		 dest_processor_id,
+		 tag,
+		 libMesh::COMM_WORLD,
+		 &r);    
+    assert (ierr == MPI_SUCCESS);
+    
+    STOP_LOG("isend()", "Parallel");
+  }
+
+
 
   template <typename T>
   inline void recv (const unsigned int src_processor_id,
@@ -608,6 +622,27 @@ namespace Parallel
     const int ierr =	  
       MPI_Recv (buf.empty() ? NULL : &buf[0],
 		buf.size(),
+		datatype<T>(),
+		src_processor_id,
+		tag,
+		libMesh::COMM_WORLD,
+		MPI_STATUS_IGNORE);    
+    assert (ierr == MPI_SUCCESS);
+    
+    STOP_LOG("recv()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void recv (const unsigned int src_processor_id,
+		    std::vector<std::complex<T> > &buf,
+		    const int tag)
+  {
+    START_LOG("recv()", "Parallel");
+    
+    const int ierr =	  
+      MPI_Recv (buf.empty() ? NULL : &buf[0],
+		buf.size() * 2,
 		datatype<T>(),
 		src_processor_id,
 		tag,
@@ -631,6 +666,28 @@ namespace Parallel
     const int ierr =	  
       MPI_Irecv (buf.empty() ? NULL : &buf[0],
 		 buf.size(),
+		 datatype<T>(),
+		 src_processor_id,
+		 tag,
+		 libMesh::COMM_WORLD,
+		 &r);    
+    assert (ierr == MPI_SUCCESS);
+    
+    STOP_LOG("irecv()", "Parallel");
+  }
+
+ 
+  template <typename T>
+  inline void irecv (const unsigned int src_processor_id,
+		     std::vector<std::complex<T> > &buf,
+		     request &r,
+		     const int tag)
+  {
+    START_LOG("irecv()", "Parallel");
+    
+    const int ierr =	  
+      MPI_Irecv (buf.empty() ? NULL : &buf[0],
+		 buf.size() * 2,
 		 datatype<T>(),
 		 src_processor_id,
 		 tag,
@@ -685,6 +742,26 @@ namespace Parallel
   }
 
 
+  template <typename T>
+  inline void send_receive(const unsigned int dest_processor_id,
+                           std::complex<T> &send,
+			   const unsigned int source_processor_id,
+                           std::complex<T> &recv)
+  {
+    START_LOG("send_receive()", "Parallel");
+
+    MPI_Status status;
+    MPI_Sendrecv(&send, 2, datatype<T>(),
+		 dest_processor_id, 0,
+		 &recv, 2, datatype<T>(),
+		 source_processor_id, 0,
+		 libMesh::COMM_WORLD,
+		 &status);
+
+    STOP_LOG("send_receive()", "Parallel");
+  }
+
+
 
   template <typename T>
   inline void send_receive(const unsigned int dest_processor_id,
@@ -709,6 +786,37 @@ namespace Parallel
     MPI_Sendrecv(sendsize ? &send[0] : NULL, sendsize, datatype<T>(),
 		 dest_processor_id, 0,
 		 recvsize ? &recv[0] : NULL, recvsize, datatype<T>(),
+		 source_processor_id, 0,
+		 libMesh::COMM_WORLD,
+		 &status);
+    
+    STOP_LOG("send_receive()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void send_receive(const unsigned int dest_processor_id,
+                           std::vector<std::complex<T> > &send,
+			   const unsigned int source_processor_id,
+                           std::vector<std::complex<T> > &recv)
+  {
+    START_LOG("send_receive()", "Parallel");
+
+    // Trade buffer sizes first
+    unsigned int sendsize = send.size(), recvsize;
+    MPI_Status status;
+    MPI_Sendrecv(&sendsize, 1, datatype<unsigned int>(),
+		 dest_processor_id, 0,
+		 &recvsize, 1, datatype<unsigned int>(),
+		 source_processor_id, 0,
+		 libMesh::COMM_WORLD,
+		 &status);
+
+    recv.resize(recvsize);
+
+    MPI_Sendrecv(sendsize ? &send[0] : NULL, sendsize * 2, datatype<T>(),
+		 dest_processor_id, 0,
+		 recvsize ? &recv[0] : NULL, recvsize * 2, datatype<T>(),
 		 source_processor_id, 0,
 		 libMesh::COMM_WORLD,
 		 &status);
@@ -860,43 +968,19 @@ namespace Parallel
   {
     assert(root_id < libMesh::n_processors());
 
-    std::vector<T> temprealoutput, tempimagoutput;
-
     if (libMesh::processor_id() == root_id)
-      {
-        temprealoutput.resize(libMesh::n_processors());
-        tempimagoutput.resize(libMesh::n_processors());
-        recv.resize(libMesh::n_processors());
-      }
+      recv.resize(libMesh::n_processors());
     
     if (libMesh::n_processors() > 1)
       {
-	T realinput = send.real(),
-	  imaginput = send.imag();
-
-	MPI_Gather(&realinput,
-		   1,
+	MPI_Gather(&send,
+		   2,
 		   datatype<T>(),
-		   temprealoutput.empty() ? NULL : &temprealoutput[0],
-		   1,
+		   recv.empty() ? NULL : &recv[0],
+		   2,
 		   datatype<T>(),
 		   root_id,
 		   libMesh::COMM_WORLD);
-
-	MPI_Gather(&imaginput,
-		   1,
-		   datatype<T>(),
-		   tempimagoutput.empty() ? NULL : &tempimagoutput[0],
-		   1,
-		   datatype<T>(),
-		   root_id,
-		   libMesh::COMM_WORLD);
-
-	for (unsigned int i=0; i != recv.size(); ++i)
-	  {
-	    recv[i].real() = temprealoutput[i];
-	    recv[i].imag() = tempimagoutput[i];
-	  }
       }
     else
       recv[0] = send;
@@ -972,10 +1056,66 @@ namespace Parallel
     // and get the data from the remote processors
     const int ierr =
       MPI_Gatherv (r_src.empty() ? NULL : &r_src[0], mysize, datatype<T>(),
-		   r.empty() ? NULL :  &r[0], &sendlengths[0], &displacements[0], datatype<T>(),
+		   r.empty() ? NULL :  &r[0], &sendlengths[0],
+		   &displacements[0], datatype<T>(),
 		   root_id,
 		   libMesh::COMM_WORLD);
 
+    assert (ierr == MPI_SUCCESS);
+
+    STOP_LOG("gather()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void gather(const unsigned int root_id,
+		     std::vector<std::complex<T> > &r)
+  {
+    if (libMesh::n_processors() == 1)
+      {
+	assert (libMesh::processor_id()==root_id);
+	return;
+      }
+    
+    START_LOG("gather()", "Parallel");
+    
+    std::vector<int>
+      sendlengths  (libMesh::n_processors(), 0),
+      displacements(libMesh::n_processors(), 0);
+
+    const int mysize = r.size() * 2;
+    Parallel::allgather(mysize, sendlengths);
+
+    // Find the total size of the final array and
+    // set up the displacement offsets for each processor.
+    unsigned int globalsize = 0; 
+    for (unsigned int i=0; i != libMesh::n_processors(); ++i)
+      {
+	displacements[i] = globalsize;
+	globalsize += sendlengths[i];
+      }
+
+    // Check for quick return
+    if (globalsize == 0)
+      {
+	STOP_LOG("gather()", "Parallel");
+	return;
+      }
+
+    // Make temporary buffers for the input/output data
+    std::vector<std::complex<T> > r_src(r);
+
+    // now resize r to hold the global data
+    // on the receiving processor
+    if (root_id == libMesh::processor_id())
+      r.resize(globalsize);
+
+    // and get the data from the remote processors
+    const int ierr =
+      MPI_Gatherv (r_src.empty() ? NULL : &r_src[0], mysize, datatype<T>(),
+		   r.empty() ? NULL : &r[0], &sendlengths[0],
+		   &displacements[0], datatype<T>(),
+		   root_id, libMesh::COMM_WORLD);
     assert (ierr == MPI_SUCCESS);
 
     STOP_LOG("gather()", "Parallel");
@@ -1009,34 +1149,17 @@ namespace Parallel
   inline void allgather(std::complex<T> send,
 			std::vector<std::complex<T> > &recv)
   {
-    assert(recv.size() == libMesh::n_processors());
+    //assert(recv.size() == libMesh::n_processors());
+    recv.resize(libMesh::n_processors());
 
     if (libMesh::n_processors() > 1)
       {
-	std::vector<T> temprealoutput(recv.size()),
-	  tempimagoutput(recv.size());
-	T realinput = send.real(),
-	  imaginput = send.imag();
-
-	MPI_Allgather (&realinput,
-		       1,
+	MPI_Allgather (&send,
+		       2,
 		       datatype<T>(),
-		       &temprealoutput[0],
-		       1, 
+		       &recv[0],
+		       2, 
 		       libMesh::COMM_WORLD);
-
-	MPI_Allgather (&imaginput,
-		       1,
-		       datatype<T>(),
-		       &tempimagoutput[0],
-		       1, 
-		       libMesh::COMM_WORLD);
-
-	for (unsigned int i=0; i != recv.size(); ++i)
-	  {
-	    recv[i].real() = temprealoutput[i];
-	    recv[i].imag() = tempimagoutput[i];
-	  }
       }
     else
       recv[0] = send;
@@ -1107,9 +1230,59 @@ namespace Parallel
     // Pass NULL if our vector is empty.
     const int ierr =
       MPI_Allgatherv (r_src.empty() ? NULL : &r_src[0], mysize, datatype<T>(),
-		      r.empty()     ? NULL : &r[0],     &sendlengths[0], &displacements[0], datatype<T>(),
-		      libMesh::COMM_WORLD);
+		      r.empty()     ? NULL : &r[0],     &sendlengths[0],
+		      &displacements[0], datatype<T>(), libMesh::COMM_WORLD);
 
+    assert (ierr == MPI_SUCCESS);
+
+    STOP_LOG("allgather()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void allgather(std::vector<std::complex<T> > &r)
+  {
+    if (libMesh::n_processors() == 1)
+      return;
+
+    START_LOG("allgather()", "Parallel");
+
+    std::vector<int>
+      sendlengths  (libMesh::n_processors(), 0),
+      displacements(libMesh::n_processors(), 0);
+
+    const int mysize = r.size() * 2;
+    Parallel::allgather(mysize, sendlengths);
+
+    // Find the total size of the final array and
+    // set up the displacement offsets for each processor.
+    unsigned int globalsize = 0; 
+    for (unsigned int i=0; i != libMesh::n_processors(); ++i)
+      {
+	displacements[i] = globalsize;
+	globalsize += sendlengths[i];
+      }
+
+    // Check for quick return
+    if (globalsize == 0)
+      {
+	STOP_LOG("allgather()", "Parallel");
+	return;
+      }
+
+    // copy the input buffer
+    std::vector<std::complex<T> > r_src(r);
+
+    // now resize it to hold the global data
+    r.resize(globalsize);
+
+    // and get the data from the remote processors.
+    // Pass NULL if our vector is empty.
+    const int ierr =
+      MPI_Allgatherv (r_src.empty() ? NULL : &r_src[0], mysize, datatype<T>(),
+		      r.empty()     ? NULL : &r[0],     &sendlengths[0],
+		      &displacements[0], datatype<T>(),
+		      libMesh::COMM_WORLD);
     assert (ierr == MPI_SUCCESS);
 
     STOP_LOG("allgather()", "Parallel");
@@ -1132,6 +1305,26 @@ namespace Parallel
     const int ierr =
       MPI_Bcast (&data, 1, datatype<T>(), root_id, libMesh::COMM_WORLD);
 
+    assert (ierr == MPI_SUCCESS);
+
+    STOP_LOG("broadcast()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void broadcast (std::complex<T> &data, const unsigned int root_id)
+  {
+    if (libMesh::n_processors() == 1)
+      {
+	assert (libMesh::processor_id() == root_id);
+	return;
+      }
+    
+    START_LOG("broadcast()", "Parallel");
+
+    // Spread data to remote processors.
+    const int ierr =
+      MPI_Bcast (&data, 2, datatype<T>(), root_id, libMesh::COMM_WORLD);
     assert (ierr == MPI_SUCCESS);
 
     STOP_LOG("broadcast()", "Parallel");
@@ -1184,8 +1377,32 @@ namespace Parallel
     // and get the data from the remote processors.
     // Pass NULL if our vector is empty.
     const int ierr =
-      MPI_Bcast (data.empty() ? NULL : &data[0], data.size(), datatype<T>(), root_id, libMesh::COMM_WORLD);
+      MPI_Bcast (data.empty() ? NULL : &data[0], data.size(), datatype<T>(),
+		 root_id, libMesh::COMM_WORLD);
 
+    assert (ierr == MPI_SUCCESS);
+
+    STOP_LOG("broadcast()", "Parallel");
+  }
+
+
+  template <typename T>
+  inline void broadcast (std::vector<std::complex<T> > &data,
+			 const unsigned int root_id)
+  {
+    if (libMesh::n_processors() == 1)
+      {
+	assert (libMesh::processor_id() == root_id);
+	return;
+      }
+
+    START_LOG("broadcast()", "Parallel");
+
+    // and get the data from the remote processors.
+    // Pass NULL if our vector is empty.
+    const int ierr =
+      MPI_Bcast (data.empty() ? NULL : &data[0], data.size() * 2, datatype<T>(),
+		 root_id, libMesh::COMM_WORLD);
     assert (ierr == MPI_SUCCESS);
 
     STOP_LOG("broadcast()", "Parallel");
