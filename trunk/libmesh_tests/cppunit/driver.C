b9f0188f8ec36b73d374d8b06c8b13b3975d33fb
Roy Stogner <roystgnr@ices.utexas.edu>
2008-03-01 20:21:04 +0000

    Change from libMesh::init()/close() to LibMeshInit
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2722 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh_tests/cppunit/driver.C
+++ trunk/libmesh_tests/cppunit/driver.C
@@ -8,14 +8,11 @@ int main( int argc, char **argv)
   // Initialize the library.  This is necessary because the library
   // may depend on a number of other libraries (i.e. MPI  and Petsc)
   // that require initialization before use. 
-  libMesh::init(argc, argv);
-  {
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest( registry.makeTest() );
-    runner.run();
+  LibMeshInit init(argc, argv);
+  CppUnit::TextUi::TestRunner runner;
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+  runner.addTest( registry.makeTest() );
+  runner.run();
 
-  }
-
-  return libMesh::close();
+  return 0;
 }
