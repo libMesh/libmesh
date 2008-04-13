5593a811f36871dcbb6804bafab04db72e637010
Roy Stogner <roystgnr@ices.utexas.edu>
2008-04-13 17:23:02 +0000

    Try for more robust unordered_map, unordered_set includes
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@2791 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/include/mesh/mesh_refinement.h
+++ trunk/libmesh/include/mesh/mesh_refinement.h
@@ -32,8 +32,10 @@
 #include <vector>
 #include <list>
 
-#if   defined(HAVE_UNORDERED_MAP)
+#if   defined(HAVE_TR1_UNORDERED_MAP)
 # include <tr1/unordered_map>
+#elif defined(HAVE_UNORDERED_MAP)
+# include <unordered_map>
 #elif defined(HAVE_HASH_MAP)
 # include <hash_map>
 #elif defined(HAVE_EXT_HASH_MAP)
@@ -616,7 +618,7 @@ private:
    * For efficiency we will use a hashed multimap if it is
    * available, otherwise a regular multimap.
    */
-#if   defined(HAVE_UNORDERED_MAP)
+#if   defined(HAVE_UNORDERED_MAP) || defined(HAVE_TR1_UNORDERED_MAP)
     typedef std::tr1::unordered_multimap<unsigned int, Node*> map_type;
 #elif defined(HAVE_HASH_MAP)    
     typedef std::hash_multimap<unsigned int, Node*> map_type;    
--- trunk/libmesh/src/mesh/unstructured_mesh.C
+++ trunk/libmesh/src/mesh/unstructured_mesh.C
@@ -51,12 +51,16 @@
 #include "legacy_xdr_io.h"
 #include "vtk_io.h"
 
-#if   defined(HAVE_UNORDERED_MAP)
+#if   defined(HAVE_TR1_UNORDERED_MAP)
 # include <tr1/unordered_map>
+#elif defined(HAVE_UNORDERED_MAP)
+# include <unordered_map>
 #elif defined(HAVE_HASH_MAP)
 # include <hash_map>
 #elif defined(HAVE_EXT_HASH_MAP)
 # include <ext/hash_map>
+#else
+# include <map>
 #endif
 
 
@@ -212,7 +216,7 @@ void UnstructuredMesh::find_neighbors(bool reset_remote_elements)
     typedef std::pair<Elem*, unsigned char> val_type;
     typedef std::pair<key_type, val_type>   key_val_pair;
     
-#if   defined(HAVE_UNORDERED_MAP)
+#if   defined(HAVE_UNORDERED_MAP) || defined(HAVE_TR1_UNORDERED_MAP)
     typedef std::tr1::unordered_multimap<key_type, val_type> map_type;    
 #elif defined(HAVE_HASH_MAP)    
     typedef std::hash_multimap<key_type, val_type> map_type;    
@@ -324,7 +328,7 @@ void UnstructuredMesh::find_neighbors(bool reset_remote_elements)
 		
 		// use the lower bound as a hint for
 		// where to put it.
-#if defined(HAVE_UNORDERED_MAP) || defined(HAVE_HASH_MAP) || defined(HAVE_EXT_HASH_MAP)
+#if defined(HAVE_UNORDERED_MAP) || defined(HAVE_TR1_UNORDERED_MAP) || defined(HAVE_HASH_MAP) || defined(HAVE_EXT_HASH_MAP)
 		side_to_elem_map.insert (kvp);
 #else
 		side_to_elem_map.insert (bounds.first,kvp);
