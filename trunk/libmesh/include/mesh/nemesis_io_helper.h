f06c333295ae5737597b1dcd7cf541a771983d41
John Peterson <jwpeterson@gmail.com>
2008-09-16 22:15:08 +0000

    Added Nemesis IO helper class.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@3037 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/include/mesh/nemesis_io_helper.h
@@ -0,0 +1,285 @@
+// $Id$
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2007  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef __nemesis_io_helper_h__
+#define __nemesis_io_helper_h__
+
+#include "libmesh_config.h"
+
+#if defined(HAVE_NEMESIS_API) && defined(HAVE_EXODUS_API)
+
+#include <vector>
+#include "exodusII_io_helper.h"
+
+// The Nemesis API header file.  Should already be
+// correctly extern C'd but it doesn't hurt :)
+namespace Nemesis {
+  extern "C" {
+#include "ne_nemesisI.h"
+  }
+}
+
+
+/**
+ * This is the \p Nemesis_IO_Helper class.  Think of it as
+ * a big struct with storage for all the stuff one might
+ * want to pull from a Nemesis file.  Also contains an
+ * ExodusII_IO_Helper object, since Nemesis is based on
+ * the same file format.
+ *
+ * @author Johw W. Peterson, 2008.
+ */
+class Nemesis_IO_Helper
+{
+public:
+  /**
+   * Constructor.
+   */
+  Nemesis_IO_Helper(bool verbose);
+
+  /**
+   * Destructor.
+   */
+  ~Nemesis_IO_Helper();
+  
+  // Member functions.  These just allocate memory for you and call the Nemesis
+  // routines of the same name.  They also handle error checking for the Nemesis
+  // return value.  Be careful calling these at random, some depend on others
+  // being called first...
+  void get_init_global();
+  void get_ss_param_global();
+  void get_ns_param_global();
+  void get_eb_info_global();
+  void get_init_info();
+  void get_loadbal_param();
+  void get_elem_map();
+  void get_node_map();
+  void get_cmap_params();
+  void get_node_cmap();
+  void get_elem_cmap();
+  
+  // Member data
+
+  /**
+   * Instance of the Exodus IO Helper.  We call
+   * the Exodus API through this object.
+   */
+  ExodusII_IO_Helper ex2helper;
+
+  /**
+   * All (?) Nemesis functions return an int.  If it's negative that signals an error!
+   * Internally, we use the ExodusII_IO_Helper::check_err() function to check for errors.
+   */
+  int nemesis_err_flag;
+    
+  /**
+   * Global initial information.  The names are self-explanatory
+   * for the most part.  Used with Nemesis::ne_get_init_global().
+   */
+  int num_nodes_global;
+  int num_elems_global;
+  int num_elem_blks_global;
+  int num_node_sets_global;
+  int num_side_sets_global;
+
+  /**
+   * The number of processors for which the NEMESIS I file was created.
+   * To be used with Nemesis::ne_get_init_info().
+   */
+  int num_proc;
+
+  /**
+   * The number of processors for which the NEMESIS I file stores information.
+   * This is generally equal to 1 (1 CPU/file) at least for the splitting Derek gave us.
+   * To be used with Nemesis::ne_get_init_info().   
+   */
+  int num_proc_in_file;
+
+  /**
+   * The type of file to be written. Either 's', for a scalar
+   * load-balance file, or 'p' for a parallel file.
+   * To be used with Nemesis::ne_get_init_info().   
+   */
+  char ftype;
+  
+  /**
+   * Containers for reading global sideset (boundary conditions) information.  Each vector will
+   * eventually have num_side_sets_global entries, and be used in calls to
+   * Nemesis::ne_get_ss_param_global().
+   *
+   * It's an error to call ne_get_ss_param_global when num_side_sets_global==0
+   */
+  std::vector<int> global_sideset_ids;
+  std::vector<int> num_global_side_counts;
+  std::vector<int> num_global_side_df_counts;
+
+
+  /**
+   * Containers for reading global nodeset information.  One vector entry per nodeset.
+   * Each vector will eventually have num_node_sets_global entries, and
+   * will be used in calls to Nemesis::ne_get_ns_param_global().
+   *
+   * It's an error to call ne_get_ns_param_global when num_node_sets_global==0
+   */
+  std::vector<int> global_nodeset_ids;
+  std::vector<int> num_global_node_counts;
+  std::vector<int> num_global_node_df_counts;
+
+
+  /**
+   * Read the global element block IDs and counts.  These vectors will
+   * eventually have num_elem_blks_global entries.  To be used with
+   * Nemesis::ne_get_eb_info_global().
+   */
+  std::vector<int> global_elem_blk_ids;
+  std::vector<int> global_elem_blk_cnts;
+  
+
+  /**
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+
+  /**
+   * The number of FEM nodes contained in FEM elements wholly owned by the current processor.
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_internal_nodes;
+
+  /**
+   * The number of FEM nodes local to a processor but residing in an
+   * element which also has FEM nodes on other processors.
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_border_nodes;
+  
+  /**
+   * The number of FEM nodes that reside on another processor but
+   * whose element partially resides on the current processor.
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_external_nodes;
+  
+  /**
+   * The number of internal FEM elements. Elements local to this processor.
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_internal_elems;
+
+  /**
+   * The number of border FEM elements. Elements local to this
+   * processor but whose FEM nodes reside on other processors as well.
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_border_elems;
+  
+  /**
+   * The number of nodal communication maps for this processor. (One
+   * per neighboring proc?)
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_node_cmaps;
+  
+  /**
+   * The number of elemental communication maps for this
+   * processor. (One per neighboring proc?)
+   * To be used with the Nemesis::ne_get_loadbal_param() routine.
+   */
+  int num_elem_cmaps;
+
+
+  
+  /**
+   * Vector which stores internal element IDs.  Will have length
+   * num_internal_elems.
+   * To be used with Nemesis::ne_get_elem_map().
+   */
+  std::vector<int> elem_mapi;
+  
+  /**
+   * Vector which stores border element IDs.  Will have length
+   * num_border_elems.
+   * To be used with Nemesis::ne_get_elem_map().
+   */
+  std::vector<int> elem_mapb;
+
+
+  
+  /**
+   * Vector which stores internal node IDs.  Will have length
+   * num_internal_nodes.
+   * To be used with Nemesis::ne_get_node_map().
+   */
+  std::vector<int> node_mapi;
+  
+  /**
+   * Vector which stores border node IDs.  Will have length
+   * num_border_nodes.
+   * To be used with Nemesis::ne_get_node_map().
+   */
+  std::vector<int> node_mapb;
+
+  /**
+   * Vector which stores external node IDs.  Will have length
+   * num_external_nodes.
+   * To be used with Nemesis::ne_get_node_map().
+   */
+  std::vector<int> node_mape;
+  
+  
+  /**
+   * Vectors for storing the communication map parameters.
+   * Each will eventually have length num_node_cmaps OR
+   * num_elem_cmaps as appropriate.
+   * For use with Nemesis::ne_get_cmap_params().
+   */
+  std::vector<int> node_cmap_ids;
+  std::vector<int> node_cmap_node_cnts;
+  std::vector<int> elem_cmap_ids;
+  std::vector<int> elem_cmap_elem_cnts;
+
+
+  /**
+   * 2 vectors of vectors for storing the node communication IDs for this processor.
+   * There will be num_node_cmaps rows, row i will have node_cmap_node_cnts[i] entries.
+   * To be used with Nemesis::ne_get_node_cmap().
+   *
+   * Remark: node_cmap_proc_ids is a vector, all entries of which are = node_cmap_ids[i]
+   * Not sure what the point of that is...
+   */
+  std::vector<std::vector<int> > node_cmap_node_ids;
+  std::vector<std::vector<int> > node_cmap_proc_ids;
+
+
+  /**
+   * 3 vectors of vectors for storing element communication IDs for this processor.
+   * There will be num_elem_cmaps rows, row i will have elem_cmap_elem_cnts[i] entries.
+   * To be used with Nemesis::ne_get_elem_cmap().
+   */
+  std::vector<std::vector<int> > elem_cmap_elem_ids;
+  std::vector<std::vector<int> > elem_cmap_side_ids;
+  std::vector<std::vector<int> > elem_cmap_proc_ids;
+
+
+private:
+  bool _verbose;
+  
+};
+
+#endif // #if defined(HAVE_NEMESIS_API) && defined(HAVE_EXODUS_API)
+#endif // #ifndef __nemesis_io_helper_h__
---
+++ trunk/libmesh/src/mesh/nemesis_io_helper.C
@@ -0,0 +1,389 @@
+// $Id$
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2007  Benjamin S. Kirk, John W. Peterson
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+#include "nemesis_io_helper.h"
+
+#if defined(HAVE_NEMESIS_API) && defined(HAVE_EXODUS_API)
+
+
+// Initialize the various integer members to zero.  We can check
+// these later to see if they've been properly initialized...
+Nemesis_IO_Helper::Nemesis_IO_Helper(bool verbose=false) :
+  ex2helper(verbose),
+  nemesis_err_flag(0),
+  num_nodes_global(0),
+  num_elems_global(0),
+  num_elem_blks_global(0),
+  num_node_sets_global(0),
+  num_side_sets_global(0),
+  num_proc(0),
+  num_proc_in_file(0),
+  ftype('\0'),
+  num_internal_nodes(0),
+  num_border_nodes(0),
+  num_external_nodes(0),
+  num_internal_elems(0),
+  num_border_elems(0),
+  num_node_cmaps(0),
+  num_elem_cmaps(0),
+  _verbose(verbose)
+{
+  // Warn about using untested code!
+  untested();
+}
+
+
+Nemesis_IO_Helper::~Nemesis_IO_Helper()
+{
+}
+
+
+
+void Nemesis_IO_Helper::get_init_global()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_init_global(ex2helper.ex_id,
+				&num_nodes_global,
+				&num_elems_global,
+				&num_elem_blks_global,
+				&num_node_sets_global,
+				&num_side_sets_global);
+  ex2helper.check_err(nemesis_err_flag, "Error reading initial global data!");
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_nodes_global=" << num_nodes_global << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_elems_global=" << num_elems_global << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_elem_blks_global=" << num_elem_blks_global << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_node_sets_global=" << num_node_sets_global << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_side_sets_global=" << num_side_sets_global << std::endl;
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_ss_param_global()
+{
+  if (num_side_sets_global > 0)
+    {
+      nemesis_err_flag =
+	Nemesis::ne_get_ss_param_global(ex2helper.ex_id,
+					&global_sideset_ids[0],
+					&num_global_side_counts[0],
+					&num_global_side_df_counts[0]);
+      ex2helper.check_err(nemesis_err_flag, "Error reading global sideset parameters!");
+
+      if (_verbose)
+	{
+	  std::cout << "[" << libMesh::processor_id() << "] " << "Global Sideset IDs, Side Counts, and DF counts:" << std::endl;
+	  for (unsigned int bn=0; bn<global_sideset_ids.size(); ++bn)
+	    {
+	      std::cout << "  [" << libMesh::processor_id() << "] "
+			<< "global_sideset_ids["<<bn<<"]=" << global_sideset_ids[bn]
+			<< ", num_global_side_counts["<<bn<<"]=" << num_global_side_counts[bn]
+			<< ", num_global_side_df_counts["<<bn<<"]=" << num_global_side_df_counts[bn]
+			<< std::endl;
+	    }
+	}
+    }
+}
+
+
+
+
+void Nemesis_IO_Helper::get_ns_param_global()
+{
+  if (num_node_sets_global > 0)
+    {
+      nemesis_err_flag =
+	Nemesis::ne_get_ns_param_global(ex2helper.ex_id,
+					&global_nodeset_ids[0],
+					&num_global_node_counts[0],
+					&num_global_node_df_counts[0]);
+      ex2helper.check_err(nemesis_err_flag, "Error reading global nodeset parameters!");
+
+      if (_verbose)
+	{
+	  std::cout << "[" << libMesh::processor_id() << "] " << "Global Nodeset IDs, Node Counts, and DF counts:" << std::endl;
+	  for (unsigned int bn=0; bn<global_nodeset_ids.size(); ++bn)
+	    {
+	      std::cout << "  [" << libMesh::processor_id() << "] "
+			<< "global_nodeset_ids["<<bn<<"]=" << global_nodeset_ids[bn]
+			<< ", num_global_node_counts["<<bn<<"]=" << num_global_node_counts[bn]
+			<< ", num_global_node_df_counts["<<bn<<"]=" << num_global_node_df_counts[bn]
+			<< std::endl;
+	    }
+	}
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_eb_info_global()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_eb_info_global(ex2helper.ex_id,
+				   &global_elem_blk_ids[0],
+				   &global_elem_blk_cnts[0]);			  
+  ex2helper.check_err(nemesis_err_flag, "Error reading global element block info!");
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] " << "Global Element Block IDs and Counts:" << std::endl;
+      for (unsigned int bn=0; bn<global_elem_blk_ids.size(); ++bn)
+	{
+	  std::cout << "  [" << libMesh::processor_id() << "] "
+		    << "global_elem_blk_ids["<<bn<<"]=" << global_elem_blk_ids[bn]
+		    << ", global_elem_blk_cnts["<<bn<<"]=" << global_elem_blk_cnts[bn]
+		    << std::endl;
+	}
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_init_info()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_init_info(ex2helper.ex_id,
+			      &num_proc,
+			      &num_proc_in_file,
+			      &ftype);
+  ex2helper.check_err(nemesis_err_flag, "Error reading initial info!");
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_proc=" << num_proc << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_proc_in_file=" << num_proc_in_file << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "ftype=" << ftype << std::endl;
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_loadbal_param()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_loadbal_param(ex2helper.ex_id,
+				  &num_internal_nodes,
+				  &num_border_nodes,
+				  &num_external_nodes,
+				  &num_internal_elems,
+				  &num_border_elems,
+				  &num_node_cmaps,
+				  &num_elem_cmaps,
+				  libMesh::processor_id() // The ID of the processor for which info is to be read
+				  );
+  ex2helper.check_err(nemesis_err_flag, "Error reading load balance parameters!");
+	
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_internal_nodes=" << num_internal_nodes << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_border_nodes=" << num_border_nodes << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_external_nodes=" << num_external_nodes << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_internal_elems=" << num_internal_elems << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_border_elems=" << num_border_elems << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_node_cmaps=" << num_node_cmaps << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "num_elem_cmaps=" << num_elem_cmaps << std::endl;
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_elem_map()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_elem_map(ex2helper.ex_id,
+			     &elem_mapi[0],
+			     &elem_mapb[0],
+			     libMesh::processor_id()
+			     );
+  ex2helper.check_err(nemesis_err_flag, "Error reading element maps!");
+
+
+  if (_verbose)
+    {
+      // These are not contiguous ranges....
+      //std::cout << "[" << libMesh::processor_id() << "] " << "first interior elem id=" << elem_mapi[0] << std::endl;
+      //std::cout << "[" << libMesh::processor_id() << "] " << "last interior elem id=" << elem_mapi.back() << std::endl;
+      //std::cout << "[" << libMesh::processor_id() << "] " << "first boundary elem id=" << elem_mapb[0] << std::endl;
+      //std::cout << "[" << libMesh::processor_id() << "] " << "last boundary elem id=" << elem_mapb.back() << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] elem_mapi[i] = ";
+      for (unsigned int i=0; i< static_cast<unsigned int>(std::min(10, num_internal_elems-1)); ++i)
+	std::cout << elem_mapi[i] << ", ";
+      std::cout << "... " << elem_mapi.back() << std::endl;
+
+      std::cout << "[" << libMesh::processor_id() << "] elem_mapb[i] = ";
+      for (unsigned int i=0; i< static_cast<unsigned int>(std::min(10, num_internal_elems-1)); ++i)
+	std::cout << elem_mapb[i] << ", ";
+      std::cout << "... " << elem_mapb.back() << std::endl;
+    }
+}
+
+
+
+
+void Nemesis_IO_Helper::get_node_map()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_node_map(ex2helper.ex_id,
+			     &node_mapi[0],
+			     &node_mapb[0],
+			     &node_mape[0], 
+			     libMesh::processor_id()
+			     );
+  ex2helper.check_err(nemesis_err_flag, "Error reading node maps!");
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] " << "first interior node id=" << node_mapi[0] << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "last interior node id=" << node_mapi.back() << std::endl;
+
+      std::cout << "[" << libMesh::processor_id() << "] " << "first boundary node id=" << node_mapb[0] << std::endl;
+      std::cout << "[" << libMesh::processor_id() << "] " << "last boundary node id=" << node_mapb.back() << std::endl;
+
+      // The number of external nodes is sometimes zero, don't try to access
+      // node_mape.back() in this case!
+      if (num_external_nodes > 0)
+	{
+	  std::cout << "[" << libMesh::processor_id() << "] " << "first external node id=" << node_mape[0] << std::endl;
+	  std::cout << "[" << libMesh::processor_id() << "] " << "last external node id=" << node_mape.back() << std::endl;
+	}
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_cmap_params()
+{
+  nemesis_err_flag =
+    Nemesis::ne_get_cmap_params(ex2helper.ex_id,
+				&node_cmap_ids[0],
+				&node_cmap_node_cnts[0],
+				&elem_cmap_ids[0],
+				&elem_cmap_elem_cnts[0],
+				libMesh::processor_id());
+  ex2helper.check_err(nemesis_err_flag, "Error reading cmap parameters!");
+
+
+  if (_verbose)
+    {
+      std::cout << "[" << libMesh::processor_id() << "] ";
+      for (unsigned int i=0; i<node_cmap_ids.size(); ++i)
+	std::cout << "node_cmap_ids["<<i<<"]=" << node_cmap_ids[i] << " ";
+      std::cout << std::endl;
+	
+      std::cout << "[" << libMesh::processor_id() << "] ";
+      for (unsigned int i=0; i<node_cmap_node_cnts.size(); ++i)
+	std::cout << "node_cmap_node_cnts["<<i<<"]=" << node_cmap_node_cnts[i] << " ";
+      std::cout << std::endl;
+
+      std::cout << "[" << libMesh::processor_id() << "] ";
+      for (unsigned int i=0; i<elem_cmap_ids.size(); ++i)
+	std::cout << "elem_cmap_ids["<<i<<"]=" << elem_cmap_ids[i] << " ";
+      std::cout << std::endl;
+
+      std::cout << "[" << libMesh::processor_id() << "] ";
+      for (unsigned int i=0; i<elem_cmap_elem_cnts.size(); ++i)
+	std::cout << "elem_cmap_elem_cnts["<<i<<"]=" << elem_cmap_elem_cnts[i] << " ";
+      std::cout << std::endl;
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_node_cmap()
+{
+  for (unsigned int i=0; i<node_cmap_node_ids.size(); ++i)
+    {
+      node_cmap_node_ids[i].resize(node_cmap_node_cnts[i]);
+      node_cmap_proc_ids[i].resize(node_cmap_node_cnts[i]);
+
+      nemesis_err_flag =
+	Nemesis::ne_get_node_cmap(ex2helper.ex_id,
+				  node_cmap_ids[i],
+				  &node_cmap_node_ids[i][0],
+				  &node_cmap_proc_ids[i][0],
+				  libMesh::processor_id());
+      ex2helper.check_err(nemesis_err_flag, "Error reading node cmap node and processor ids!");
+
+      if (_verbose)
+	{
+	  std::cout << "[" << libMesh::processor_id() << "] node_cmap_node_ids["<<i<<"]=";
+	  for (unsigned int j=0; j<node_cmap_node_ids[i].size(); ++j)
+	    std::cout << node_cmap_node_ids[i][j] << " ";
+	  std::cout << std::endl;
+
+	  // This is basically a vector, all entries of which are = node_cmap_ids[i]
+	  // Not sure if it's always guaranteed to be that or what...
+	  std::cout << "[" << libMesh::processor_id() << "] node_cmap_proc_ids["<<i<<"]=";
+	  for (unsigned int j=0; j<node_cmap_proc_ids[i].size(); ++j)
+	    std::cout << node_cmap_proc_ids[i][j] << " ";
+	  std::cout << std::endl;
+	}
+    }
+}
+
+
+
+void Nemesis_IO_Helper::get_elem_cmap()
+{
+  for (unsigned int i=0; i<elem_cmap_elem_ids.size(); ++i)
+    {
+      elem_cmap_elem_ids[i].resize(elem_cmap_elem_cnts[i]);
+      elem_cmap_side_ids[i].resize(elem_cmap_elem_cnts[i]);
+      elem_cmap_proc_ids[i].resize(elem_cmap_elem_cnts[i]);
+
+      nemesis_err_flag =
+	Nemesis::ne_get_elem_cmap(ex2helper.ex_id,
+				  elem_cmap_ids[i],
+				  &elem_cmap_elem_ids[i][0],
+				  &elem_cmap_side_ids[i][0],
+				  &elem_cmap_proc_ids[i][0],
+				  libMesh::processor_id());
+      ex2helper.check_err(nemesis_err_flag, "Error reading elem cmap elem, side, and processor ids!");
+
+
+      if (_verbose)
+	{
+	  std::cout << "[" << libMesh::processor_id() << "] elem_cmap_elem_ids["<<i<<"]=";
+	  for (unsigned int j=0; j<elem_cmap_elem_ids[i].size(); ++j)
+	    std::cout << elem_cmap_elem_ids[i][j] << " ";
+	  std::cout << std::endl;
+
+	  // These must be the (local) side IDs (in the ExodusII face numbering scheme)
+	  // of the sides shared across processors.
+	  std::cout << "[" << libMesh::processor_id() << "] elem_cmap_side_ids["<<i<<"]=";
+	  for (unsigned int j=0; j<elem_cmap_side_ids[i].size(); ++j)
+	    std::cout << elem_cmap_side_ids[i][j] << " ";
+	  std::cout << std::endl;
+
+	  // This is basically a vector, all entries of which are = elem_cmap_ids[i]
+	  // Not sure if it's always guaranteed to be that or what...
+	  std::cout << "[" << libMesh::processor_id() << "] elem_cmap_proc_ids["<<i<<"]=";
+	  for (unsigned int j=0; j<elem_cmap_proc_ids[i].size(); ++j)
+	    std::cout << elem_cmap_proc_ids[i][j] << " ";
+	  std::cout << std::endl;
+	}
+    }
+}
+
+#endif // #if defined(HAVE_NEMESIS_API) && defined(HAVE_EXODUS_API)
