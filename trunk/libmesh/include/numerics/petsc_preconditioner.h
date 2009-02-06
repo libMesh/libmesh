2da637f8450588cd33e54b2c3390be6a992b78cb
Derek Gaston <friedmud@gmail.com>
2009-02-06 21:59:25 +0000

    preconditioner base and petsc preconditioner classes
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@3276 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/include/numerics/petsc_preconditioner.h
@@ -0,0 +1,133 @@
+// $Id: petsc_preconditioner.h 3158 2009-02-02 17:44:57Z drgasto $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __petsc_preconditioner_h__
+#define __petsc_preconditioner_h__
+
+#include "libmesh_config.h"
+
+#ifdef LIBMESH_HAVE_PETSC
+
+// C++ includes
+
+// Local includes
+#include "preconditioner.h"
+#include "libmesh_common.h"
+#include "enum_solver_package.h"
+#include "enum_preconditioner_type.h"
+#include "reference_counted_object.h"
+#include "libmesh.h"
+
+// Petsc includes
+#include "petscpc.h"
+
+// forward declarations
+template <typename T> class AutoPtr;
+template <typename T> class SparseMatrix;
+template <typename T> class NumericVector;
+template <typename T> class ShellMatrix;
+
+
+/**
+ * This class provides an interface to the suite of preconditioners available
+ * from Petsc.
+ * 
+ * @author Derek Gaston, 2009
+ */
+
+template <typename T>
+class PetscPreconditioner : public Preconditioner<T>
+{
+public:
+  
+  /**
+   *  Constructor. Initializes PetscPreconditioner data structures
+   */
+  PetscPreconditioner ();
+    
+  /**
+   * Destructor.
+   */
+  virtual ~PetscPreconditioner ();  
+
+  /**
+   * Computes the preconditioned vector "y" based on input "x".
+   * Usually by solving Py=x to get the action of P^-1 x.
+   */
+  virtual void apply(const NumericVector<T> & x, NumericVector<T> & y);
+  
+  /**
+   * Release all memory and clear data structures.
+   */
+  virtual void clear () {}
+
+  /**
+   * Initialize data structures if not done so already.
+   */
+  virtual void init ();
+
+  /**
+   * Returns the actual Petsc PC struct.  Useful for more advanced
+   * purposes
+   */
+  PC pc() { return _pc; }
+  
+  /**
+   * Tells PETSC to use the user-specified preconditioner
+   */
+  static void set_petsc_preconditioner_type (const PreconditionerType & preconditioner_type, PC & pc);
+  
+protected:
+  
+  /**
+   * Preconditioner context
+   */
+  PC _pc;
+
+  /**
+   * Petsc Matrix that's been pulled out of the _matrix object.
+   * This happens during init...
+   */
+  Mat _mat;
+};
+
+
+
+
+/*----------------------- inline functions ----------------------------------*/
+template <typename T>
+inline
+PetscPreconditioner<T>::PetscPreconditioner () :
+  Preconditioner<T>()
+{
+}
+
+
+
+template <typename T>
+inline
+PetscPreconditioner<T>::~PetscPreconditioner ()
+{
+  this->clear ();
+}
+
+#endif // #ifdef LIBMESH_HAVE_PETSC
+#endif // #ifdef __petsc_linear_solver_h__
---
+++ trunk/libmesh/src/numerics/preconditioner.C
@@ -0,0 +1,84 @@
+// $Id: preconditioner.C 3158 2009-02-02 17:44:57Z drgasto $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+// C++ includes
+
+// Local Includes
+#include "auto_ptr.h"
+#include "preconditioner.h"
+#include "petsc_preconditioner.h"
+
+//------------------------------------------------------------------
+// Preconditioner members
+template <typename T>
+AutoPtr<Preconditioner<T> >
+Preconditioner<T>::build(const SolverPackage solver_package)
+{
+  // Build the appropriate solver
+  switch (solver_package)
+    {
+
+/*
+#ifdef LIBMESH_HAVE_LASPACK
+    case LASPACK_SOLVERS:
+      {
+	AutoPtr<Preconditioner<T> > ap(new LaspackPreconditioner<T>);
+	return ap;
+      }
+#endif
+*/
+
+#ifdef LIBMESH_HAVE_PETSC
+    case PETSC_SOLVERS:
+      {
+	AutoPtr<Preconditioner<T> > ap(new PetscPreconditioner<T>);
+	return ap;
+      }
+#endif
+
+/*
+#ifdef LIBMESH_HAVE_TRILINOS
+    case TRILINOS_SOLVERS:
+      {
+	AutoPtr<Preconditioner<T> > ap(new AztecPreconditioner<T>);
+	return ap;
+      }
+#endif
+*/
+    default:
+      std::cerr << "ERROR:  Unrecognized solver package: "
+		<< solver_package
+		<< std::endl;
+      libmesh_error();
+    }
+    
+  AutoPtr<Preconditioner<T> > ap(NULL);
+  return ap;    
+}
+
+
+
+//------------------------------------------------------------------
+// Explicit instantiations
+template class Preconditioner<Number>;
+
+
+
