ac3a668b37ed620d6b0e17ea6529ebf7c73f2f51
David Knezevic <dknezevic@seas.harvard.edu>
2009-04-30 19:07:19 +0000

    Added support for SCALAR variable type
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@3372 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/src/fe/fe_scalar.C
@@ -0,0 +1,107 @@
+// $Id: fe_lagrange.C 3244 2009-01-30 23:42:03Z roystgnr $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+// Local includes
+#include "dof_map.h"
+#include "fe.h"
+#include "fe_macro.h"
+#include "elem.h"
+
+
+
+
+// ------------------------------------------------------------
+// SCALAR-specific implementations
+
+template <unsigned int Dim, FEFamily T>
+void FE<Dim,T>::nodal_soln(const Elem*,
+			   const Order,
+			   const std::vector<Number>&,
+			   std::vector<Number>&)
+{
+  // Do nothing, not relevant to SCALAR
+  return;
+}
+
+template <unsigned int Dim, FEFamily T>
+unsigned int FE<Dim,T>::n_dofs(const ElemType, const Order o)
+{
+  // The Order indicates the number of SCALAR dofs
+  return o;
+}
+
+template <unsigned int Dim, FEFamily T>
+unsigned int FE<Dim,T>::n_dofs_at_node(const ElemType,
+				       const Order,
+				       const unsigned int)
+{
+  // SCALARs have no dofs at nodes
+
+  return 0;
+}
+
+template <unsigned int Dim, FEFamily T>
+unsigned int FE<Dim,T>::n_dofs_per_elem(const ElemType,
+					const Order)
+{
+  // SCALARs have no dofs per element
+
+  return 0;
+}
+
+template <unsigned int Dim, FEFamily T>
+FEContinuity FE<Dim,T>::get_continuity() const
+{
+  // This doesn't really make sense for a SCALAR...
+  return C_ZERO;
+}
+
+
+
+template <unsigned int Dim, FEFamily T>
+bool FE<Dim,T>::is_hierarchic() const
+{
+  return false;
+}
+
+#ifdef LIBMESH_ENABLE_AMR
+template <unsigned int Dim, FEFamily T>
+void FE<Dim,T>::compute_constraints (DofConstraints &constraints,
+				     DofMap &dof_map,
+				     const unsigned int variable_number,
+				     const Elem* elem)
+{
+  return;
+}
+#endif // #ifdef LIBMESH_ENABLE_AMR
+
+template <unsigned int Dim, FEFamily T>
+bool FE<Dim,T>::shapes_need_reinit() const
+{
+  return false;
+}
+
+//--------------------------------------------------------------
+// Explicit instantiation of member functions
+INSTANTIATE_MBRF(0,SCALAR);
+INSTANTIATE_MBRF(1,SCALAR);
+INSTANTIATE_MBRF(2,SCALAR);
+INSTANTIATE_MBRF(3,SCALAR);
