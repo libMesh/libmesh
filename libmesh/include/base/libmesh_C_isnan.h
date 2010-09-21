4b934f6b29efa4b2300136104b872f87ee0678e2
Roy H. Stogner <roystgnr@users.sourceforge.net>
2010-09-21 00:00:30 +0000

    Using a C file to grab isnan() since strict C++ standard compilers don't have it.  Might replace this with the x!=x trick later...

---
+++ libmesh/include/base/libmesh_C_isnan.h
@@ -0,0 +1,30 @@
+
+// $Id: libmesh.h 3919 2010-08-24 21:36:44Z roystgnr $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __libmesh_C_isnan_h__
+#define __libmesh_C_isnan_h__
+
+int libmesh_C_isnan_float(float a);
+int libmesh_C_isnan_double(double a);
+int libmesh_C_isnan_longdouble(long double a);
+
+#endif // #define __libmesh_C_isnan_h__
---
+++ libmesh/src/base/libmesh_isnan.c
@@ -0,0 +1,40 @@
+// $Id: libmesh.C 3968 2010-09-18 21:47:27Z roystgnr $
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+  
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+  
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ 
+
+// C++ includes
+#include <math.h>
+#include "libmesh_C_isnan.h"
+
+#ifdef isnan
+#warning isnan() method detected!
+
+int libmesh_C_isnan_float(float a) { return isnan(a); }
+int libmesh_C_isnan_double(double a) { return isnan(a); }
+int libmesh_C_isnan_longdouble(long double a) { return isnan(a); }
+
+#else
+#warning No isnan() method detected!
+
+int libmesh_C_isnan_float(float a) { return false; }
+int libmesh_C_isnan_double(double a) { return false; }
+int libmesh_C_isnan_longdouble(long double a) { return false; }
+
+#endif
