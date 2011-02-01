29c8dbb3e60293a6b4105d8fae9036183492097c
Roy H. Stogner <roystgnr@users.sourceforge.net>
2011-02-01 20:49:15 +0000

    Added adjoint-based-refinement example

---
+++ libmesh/examples/ex26/L-shaped.h
@@ -0,0 +1,93 @@
+#include "enum_fe_family.h"
+#include "fem_system.h"
+#include "system.h"
+
+#include "trilinos_epetra_matrix.h"
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+// FEMSystem, TimeSolver and  NewtonSolver will handle most tasks,
+// but we must specify element residuals
+class LaplaceSystem : public FEMSystem
+{
+public:
+  // Constructor
+  LaplaceSystem(EquationSystems& es,
+               const std::string& name,
+               const unsigned int number)
+  : FEMSystem(es, name, number),
+    _fe_family("LAGRANGE"), _fe_order(1),
+    _analytic_jacobians(true) { qoi.resize(2); }
+
+  std::string & fe_family() { return _fe_family;  }
+  unsigned int & fe_order() { return _fe_order;  }
+  bool & analytic_jacobians() { return _analytic_jacobians; }
+
+  // Postprocessing function which we are going to override for this application
+
+  virtual void postprocess(void);
+
+  Real &get_QoI_value(std::string type, unsigned int QoI_index)
+    {
+      if(type == "exact")
+	{
+	  return exact_QoI[QoI_index];
+	}
+      else
+	{
+	  return computed_QoI[QoI_index];
+	}
+    }
+
+  protected:
+  // System initialization
+  virtual void init_data ();
+
+  // Context initialization
+  virtual void init_context (DiffContext &context);
+
+  // Element residual and jacobian calculations
+  // Time dependent parts
+  virtual bool element_time_derivative (bool request_jacobian,
+					DiffContext &context);
+
+  // Constraint parts
+  virtual bool side_constraint (bool request_jacobian,
+				DiffContext &context);
+
+  // Overloading the postprocess function
+
+  virtual void element_postprocess(DiffContext &context);  
+
+  virtual void side_postprocess(DiffContext &context);
+
+  // Overloading the qoi function on elements
+
+  virtual void element_qoi_derivative
+    (DiffContext &context,
+     const QoISet & qois);  
+
+  // Overloading the qoi function on sides
+
+  virtual void side_qoi_derivative
+    (DiffContext &context,
+     const QoISet & qois);  
+
+  Number exact_solution (const Point&);
+
+  // Variables to hold the computed QoIs
+  
+  Real computed_QoI[2];
+
+  // Variables to read in the exact QoIs from l-shaped.in
+ 
+  Real exact_QoI[2];
+  
+  // The FE type to use
+  std::string _fe_family;
+  unsigned int _fe_order;
+
+  // Calculate Jacobians analytically or not?
+  bool _analytic_jacobians;
+};
---
+++ libmesh/examples/ex26/femparameters.h
@@ -0,0 +1,73 @@
+#ifndef __fem_parameters_h__
+#define __fem_parameters_h__
+
+#include <limits>
+
+#include "libmesh_common.h"
+#include "dof_map.h"
+#include "enum_norm_type.h"
+#include "getpot.h"
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+class FEMParameters
+{
+public:
+    FEMParameters() :  
+      dimension(2),       
+      elementorder(2),
+      domainfile("lshaped.xda"),
+      coarserefinements(0),            
+      solver_quiet(true),
+      require_residual_reduction(true),
+      min_step_length(1e-5),
+      max_linear_iterations(200000), max_nonlinear_iterations(20),
+      relative_step_tolerance(1.e-7), relative_residual_tolerance(1.e-10),
+      initial_linear_tolerance(1.e-3), minimum_linear_tolerance(TOLERANCE*TOLERANCE),
+      linear_tolerance_multiplier(1.e-3),
+      nelem_target(30000), global_tolerance(0.0),
+      refine_fraction(0.3), coarsen_fraction(0.3), coarsen_threshold(10),
+      refine_uniformly(false),
+      max_adaptivesteps(1),      
+      indicator_type("kelly"),
+      fe_family(1, "LAGRANGE"), fe_order(1, 1),	
+      analytic_jacobians(true), verify_analytic_jacobians(0.0),
+      print_solution_norms(false), print_solutions(false),
+      print_residual_norms(false), print_residuals(false),
+      print_jacobian_norms(false), print_jacobians(false) {}
+
+    void read(GetPot &input);
+   
+    unsigned int dimension;  
+    Real elementorder;    
+    std::string domainfile;
+    unsigned int coarserefinements;
+            
+    bool solver_quiet, require_residual_reduction;
+    Real min_step_length;
+    unsigned int max_linear_iterations, max_nonlinear_iterations;
+    Real relative_step_tolerance, relative_residual_tolerance,
+	 initial_linear_tolerance, minimum_linear_tolerance,
+	 linear_tolerance_multiplier;
+
+    unsigned int nelem_target;
+    Real global_tolerance;
+    Real refine_fraction, coarsen_fraction, coarsen_threshold;
+    bool refine_uniformly;
+    unsigned int max_adaptivesteps;
+    
+    std::string indicator_type;    
+
+    std::vector<std::string> fe_family;
+    std::vector<unsigned int> fe_order;
+
+    bool analytic_jacobians;
+    Real verify_analytic_jacobians;
+
+    bool print_solution_norms, print_solutions,
+         print_residual_norms, print_residuals,
+         print_jacobian_norms, print_jacobians;
+};
+
+#endif // __fem_parameters_h__
