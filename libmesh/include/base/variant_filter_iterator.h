3e928dd4ebe70199a0d5b29345419f6330bd608d
Roy H. Stogner <roystgnr@users.sourceforge.net>
2011-03-14 04:26:09 +0000

    Specifying libMesh namespace; we can't guarantee a using statement will precede this file.

--- libmesh/include/base/variant_filter_iterator.h
+++ libmesh/include/base/variant_filter_iterator.h
@@ -200,10 +200,10 @@ public:
     {
 #if defined(__SUNPRO_CC) || (defined(__GNUC__) && (__GNUC__ < 3)  && !defined(__INTEL_COMPILER))
       const variant_filter_iterator::Iter<IterType>* p = 
-	libmesh_cast_ptr<const variant_filter_iterator::Iter<IterType>*>(other);
+	libMesh::libmesh_cast_ptr<const variant_filter_iterator::Iter<IterType>*>(other);
 #else      
       const Iter<IterType>* p = 
-	libmesh_cast_ptr<const Iter<IterType>*>(other);      
+	libMesh::libmesh_cast_ptr<const Iter<IterType>*>(other);      
 #endif
       
       return (iter_data == p->iter_data);
@@ -286,10 +286,10 @@ public:
       // Attempt downcast
 #if defined(__SUNPRO_CC) || (defined(__GNUC__) && (__GNUC__ < 3)  && !defined(__INTEL_COMPILER))
       const variant_filter_iterator::Iter<IterType>* p =
-	libmesh_cast_ptr<const variant_filter_iterator::Iter<IterType>* >(in);
+	libMesh::libmesh_cast_ptr<const variant_filter_iterator::Iter<IterType>* >(in);
 #else
       const Iter<IterType>* p =
-	libmesh_cast_ptr<const Iter<IterType>* >(in);
+	libMesh::libmesh_cast_ptr<const Iter<IterType>* >(in);
 #endif
       
       // Return result of op() for the user's predicate.
