5a2c2657183b8e56dbff499dc47863ef16d33435
John Peterson <jwpeterson@gmail.com>
2011-05-16 16:42:33 +0000

    Bounds checking in ExodusII_IO_Helper::Conversion.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@4488 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/mesh/exodusII_io_helper.C
+++ trunk/libmesh/src/mesh/exodusII_io_helper.C
@@ -1387,9 +1387,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
   
   // dummy return value, we won't get here
   const Conversion conv(tri3_node_map,
+			sizeof(tri3_node_map),
 			tri3_node_map, // inverse node map same as forward node map
+			sizeof(tri3_node_map),
 			tri_edge_map,
+			sizeof(tri_edge_map),
 			tri_inverse_edge_map,
+			sizeof(tri_inverse_edge_map),
 			TRI3,
 			"TRI3");
   return conv;  
@@ -1405,9 +1409,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case QUAD4:
       {
 	const Conversion conv(quad4_node_map,
+			      sizeof(quad4_node_map),
 			      quad4_node_map, // inverse node map same as forward node map
+			      sizeof(quad4_node_map),
 			      quad_edge_map,
+			      sizeof(quad_edge_map),
 			      quad_inverse_edge_map,
+			      sizeof(quad_inverse_edge_map),
 			      QUAD4,
 			      "QUAD4");
 	return conv;
@@ -1416,9 +1424,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case QUAD8:
       {
 	const Conversion conv(quad8_node_map,
+			      sizeof(quad8_node_map),
 			      quad8_node_map, // inverse node map same as forward node map
+			      sizeof(quad8_node_map),
 			      quad_edge_map,
+			      sizeof(quad_edge_map),
 			      quad_inverse_edge_map,
+			      sizeof(quad_inverse_edge_map),
 			      QUAD8,
 			      "QUAD8");
 	return conv;
@@ -1427,9 +1439,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case QUAD9:
       {
 	const Conversion conv(quad9_node_map,
+			      sizeof(quad9_node_map),
 			      quad9_node_map, // inverse node map same as forward node map
+			      sizeof(quad9_node_map),
 			      quad_edge_map,
+			      sizeof(quad_edge_map),
 			      quad_inverse_edge_map,
+			      sizeof(quad_inverse_edge_map),
 			      QUAD9,
 			      "QUAD9");
 	return conv;
@@ -1438,9 +1454,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case TRI3:
       {
 	const Conversion conv(tri3_node_map,
+			      sizeof(tri3_node_map),
 			      tri3_node_map, // inverse node map same as forward node map
+			      sizeof(tri3_node_map),
 			      tri_edge_map,
+			      sizeof(tri_edge_map),
 			      tri_inverse_edge_map,
+			      sizeof(tri_inverse_edge_map),
 			      TRI3,
 			      "TRI3");
 	return conv;
@@ -1449,9 +1469,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case TRI6:
       {
 	const Conversion conv(tri6_node_map,
+			      sizeof(tri6_node_map),
 			      tri6_node_map, // inverse node map same as forward node map
+			      sizeof(tri6_node_map),
 			      tri_edge_map,
+			      sizeof(tri_edge_map),
 			      tri_inverse_edge_map,
+			      sizeof(tri_inverse_edge_map),
 			      TRI6,
 			      "TRI6");
 	return conv;
@@ -1460,9 +1484,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case HEX8:
       {
 	const Conversion conv(hex8_node_map,
+			      sizeof(hex8_node_map),
 			      hex8_node_map, // inverse node map same as forward node map
+			      sizeof(hex8_node_map),
 			      hex_face_map,
+			      sizeof(hex_face_map),
 			      hex_inverse_face_map,
+			      sizeof(hex_inverse_face_map),
 			      HEX8,
 			      "HEX8");
 	return conv;
@@ -1471,9 +1499,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case HEX20:
       {
 	const Conversion conv(hex20_node_map,
+			      sizeof(hex20_node_map),
 			      hex20_node_map, // inverse node map same as forward node map
+			      sizeof(hex20_node_map),
 			      hex_face_map,
+			      sizeof(hex_face_map),
 			      hex_inverse_face_map,
+			      sizeof(hex_inverse_face_map),
 			      HEX20,
 			      "HEX20");
 	return conv;
@@ -1482,9 +1514,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case HEX27:
       {
 	const Conversion conv(hex27_node_map,
+			      sizeof(hex27_node_map),
 			      hex27_inverse_node_map, // different inverse node map for Hex27!
+			      sizeof(hex27_inverse_node_map),
 			      hex27_face_map,
+			      sizeof(hex27_face_map),
 			      hex27_inverse_face_map,
+			      sizeof(hex27_inverse_face_map),
 			      HEX27,
 			      "HEX27");
 	return conv;
@@ -1493,9 +1529,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case TET4:
       {
 	const Conversion conv(tet4_node_map,
+			      sizeof(tet4_node_map),
 			      tet4_node_map, // inverse node map same as forward node map
+			      sizeof(tet4_node_map),
 			      tet_face_map,
+			      sizeof(tet_face_map),
 			      tet_inverse_face_map,
+			      sizeof(tet_inverse_face_map),
 			      TET4,
 			      "TETRA4");
 	return conv;
@@ -1504,9 +1544,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case TET10:
       {
 	const Conversion conv(tet10_node_map,
+			      sizeof(tet10_node_map),
 			      tet10_node_map, // inverse node map same as forward node map
+			      sizeof(tet10_node_map),
 			      tet_face_map,
+			      sizeof(tet_face_map),
 			      tet_inverse_face_map,
+			      sizeof(tet_inverse_face_map),
 			      TET10,
 			      "TETRA10");
 	return conv;
@@ -1515,9 +1559,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case PRISM6:
       {
 	const Conversion conv(prism6_node_map,
+			      sizeof(prism6_node_map),
 			      prism6_node_map, // inverse node map same as forward node map
+			      sizeof(prism6_node_map),
 			      prism_face_map,
+			      sizeof(prism_face_map),
 			      prism_inverse_face_map,
+			      sizeof(prism_inverse_face_map),
 			      PRISM6,
 			      "WEDGE");
 	return conv;
@@ -1526,9 +1574,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case PRISM15:
       {
 	const Conversion conv(prism15_node_map,
+			      sizeof(prism15_node_map),
 			      prism15_node_map, // inverse node map same as forward node map
+			      sizeof(prism15_node_map),
 			      prism_face_map,
+			      sizeof(prism_face_map),
 			      prism_inverse_face_map,
+			      sizeof(prism_inverse_face_map),
 			      PRISM15,
 			      "WEDGE15");
 	return conv;
@@ -1537,9 +1589,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case PRISM18:
       {
 	const Conversion conv(prism18_node_map,
+			      sizeof(prism18_node_map),
 			      prism18_node_map, // inverse node map same as forward node map
+			      sizeof(prism18_node_map),
 			      prism_face_map,
+			      sizeof(prism_face_map),
 			      prism_inverse_face_map,
+			      sizeof(prism_inverse_face_map),
 			      PRISM18,
 			      "WEDGE18");
 	return conv;
@@ -1548,9 +1604,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     case PYRAMID5:
       {
 	const Conversion conv(pyramid5_node_map,
+			      sizeof(pyramid5_node_map),
 			      pyramid5_node_map, // inverse node map same as forward node map
+			      sizeof(pyramid5_node_map),
 			      pyramid_face_map,
+			      sizeof(pyramid_face_map),
 			      pyramid_inverse_face_map,
+			      sizeof(pyramid_inverse_face_map),
 			      PYRAMID5,
 			      "PYRAMID5");
 	return conv;
@@ -1564,9 +1624,13 @@ ExodusII_IO_Helper::Conversion ExodusII_IO_Helper::ElementMaps::assign_conversio
     
   // dummy return value, we will never get here
   const Conversion conv(tri3_node_map,
+			sizeof(tri3_node_map),
 			tri3_node_map, // inverse node map same as forward node map
+			sizeof(tri3_node_map),
 			tri_edge_map,
+			sizeof(tri_edge_map),
 			tri_inverse_edge_map,
+			sizeof(tri_inverse_edge_map),
 			TRI3,
 			"TRI3");
   return conv;  
