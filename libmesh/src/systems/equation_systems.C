2cce28301e021aa0432ce3399015cf1b569dbc2d
Roy H. Stogner <roystgnr@users.sourceforge.net>
2011-05-19 14:26:32 +0000

    Fix for --disable-amr

--- libmesh/src/systems/equation_systems.C
+++ libmesh/src/systems/equation_systems.C
@@ -297,12 +297,14 @@ void EquationSystems::allgather ()
       DofMap &dof_map = sys.get_dof_map();
       dof_map.distribute_dofs(_mesh);
 
+#ifdef LIBMESH_ENABLE_AMR
       // The user probably won't need constraint equations or the
       // send_list after an allgather, but let's keep it in consistent
       // shape just in case.
       dof_map.create_dof_constraints(_mesh);
       sys.user_constrain();
       dof_map.process_constraints();
+#endif
       dof_map.prepare_send_list();
     }
 }
