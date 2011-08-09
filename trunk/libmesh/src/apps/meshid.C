2a95a3b1de5e76bf7b92984ebdebc19fafd0ec37
Cody Permann <codypermann@gmail.com>
2011-08-09 16:46:10 +0000

    Utility to renumber blocks/sidesets/nodesets in exodus meshes
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@4792 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/apps/meshid.C
+++ trunk/libmesh/src/apps/meshid.C
@@ -1,32 +1,38 @@
 // Open the mesh named in command line arguments,
-// update elems in oldid to newid
+// update ids of one or more of the following enties
+// as perscribed on the command line:
+// blocks, sidesets and nodesets
 
 #include <iostream>
 
-#include "libmesh.h"
-
-#include "mesh.h"
-#include "elem.h"
+#include "exodusII.h"
+#include "exodusII_int.h"
 #include "getpot.h"
-#include "boundary_info.h"
+
+#define BLOCKS  0x4
+#define SIDES   0x2
+#define NODES   0x1
+
+void handle_error(int error)
+{
+  std::cout << "An error occured while working with the netCDF API" << std::endl;
+
+  exit(1);
+}
 
 void usage_error(const char *progname)
 {
   std::cout << "Usage: " << progname
-            << " --input inputmesh --output outputmesh --oldid --newid" 
+            << " --input inputmesh --oldid <n> --newid <n> [--nodesetonly | --sidesetonly | --blockonly]" 
             << std::endl;
-
   exit(1);
 }
 
 int main(int argc, char** argv)
 {
-  LibMeshInit init(argc, argv);
-
   GetPot cl(argc, argv);
-
-  Mesh mesh;
-
+  
+  // Command line parsing
   if(!cl.search("--input"))
   {
     std::cerr << "No --input argument found!" << std::endl;
@@ -34,64 +40,94 @@ int main(int argc, char** argv)
   } 
   const char* meshname = cl.next("");
 
-  mesh.read(meshname);
-  std::cout << "Loaded mesh " << meshname << std::endl;
-
   if(!cl.search("--oldid"))
   {
     std::cerr << "No --oldid argument found!" << std::endl;
     usage_error(argv[0]);
   }
-  unsigned int oldid = cl.next(0);
+  long oldid = cl.next(0);
 
   if(!cl.search("--newid"))
   {
     std::cerr << "No --newid argument found!" << std::endl;
     usage_error(argv[0]);
   }
-  unsigned int newid = cl.next(0);
+  long newid = cl.next(0);
 
-  MeshBase::element_iterator           el = mesh.elements_begin();
-  const MeshBase::element_iterator end_el = mesh.elements_end();
-  
-  for (; el != end_el; ++el)
+  unsigned char flags = 0;
+  if (cl.search("--nodesetonly"))
+    flags |= NODES;
+  if (cl.search("--sidesetonly"))
+    flags |= SIDES;
+  if (cl.search("--blockonly"))
+    flags |= BLOCKS;
+  if (!flags)
+    flags = NODES | SIDES | BLOCKS; // ALL ON
+
+  // flags are exclusive
+  if (flags != NODES && flags != SIDES && flags != BLOCKS  && flags != (NODES | SIDES | BLOCKS))
   {
-    Elem *elem = *el;
-
-    // Update the elements in old subdomain to the new subdomain
-    if (elem->subdomain_id() == oldid)
-      elem->subdomain_id() = newid;
-
-    // Update sides that match this id
-    unsigned int n_sides = elem->n_sides();
-    for (unsigned int s=0; s != n_sides; ++s)
-    {
-      std::vector<short int> boundary_ids = mesh.boundary_info->boundary_ids(elem, s);
-      if (std::find(boundary_ids.begin(), boundary_ids.end(), oldid) != boundary_ids.end())
-      {
-        mesh.boundary_info->remove_side(elem, s, oldid);
-        mesh.boundary_info->add_side(elem, s, newid);
-      }
-    }
+    std::cerr << "Only one of the following options may be active [--nodesetonly | --sidesetonly | --blockonly]!" << std::endl;
+    usage_error(argv[0]);
   }
-  // Finally update all the nodesets
-  mesh.boundary_info->clear_boundary_node_ids();
-  mesh.boundary_info->build_node_list_from_side_list();
-//  mesh.prepare_for_use();
+  
+  // Processing
+  std::string var_name, dim_name;
+  int status;
+  int nc_id, var_id, dim_id, var_len;
+  size_t dim_len;
 
-  std::string outputname;
-  if(cl.search("--output"))
+  status = nc_open (meshname, NC_WRITE, &nc_id);
+  if (status != NC_NOERR) handle_error(status);
+
+  for (unsigned char mask = 1<<2; mask; mask>>=1)
   {
-    outputname = cl.next("");
-  } 
-  else
-  { 
-    outputname = "new.";
-    outputname += meshname;
-  } 
+    switch (flags & mask)
+    {
+    case BLOCKS:
+      dim_name = DIM_NUM_EL_BLK;
+      var_name = VAR_ID_EL_BLK;
+      break;
+    case SIDES:
+      dim_name = DIM_NUM_SS;
+      var_name = VAR_SS_IDS;
+      break;
+    case NODES:
+      dim_name = DIM_NUM_NS;
+      var_name = VAR_NS_IDS;
+      break;
+    default:
+      continue;
+    }
 
-  mesh.write(outputname.c_str());
-  std::cout << "Wrote mesh " << outputname << std::endl;
+    // Get the length of the "variable" in question - stored in a dimension field
+    status = nc_inq_dimid (nc_id, dim_name.c_str(), &dim_id);
+    if (status != NC_NOERR) handle_error(status);
 
-  return 0;
+    status = nc_inq_dimlen (nc_id, dim_id, &dim_len);
+    if (status != NC_NOERR) handle_error(status);
+    
+    // Now get the variable values themselves
+    std::vector<long> var_vals(dim_len);
+    
+    status = nc_inq_varid (nc_id, var_name.c_str(), &var_id);
+    if (status != NC_NOERR) handle_error(status);
+
+    status = nc_get_var_long (nc_id, var_id, &var_vals[0]);
+    if (status != NC_NOERR) handle_error(status);
+
+    // Update the variable value specified on the command line
+    for (unsigned int i=0; i<dim_len; ++i)
+      if (var_vals[i] == oldid)
+        var_vals[i] = newid;
+
+    // Save that value back to the NetCDF database
+    status = nc_put_var_long (nc_id, var_id, &var_vals[0]);
+    if (status != NC_NOERR) handle_error(status);
+  }
+
+  // Write out the dataset
+  status = nc_close(nc_id);
+  
+  exit(0);
 }
