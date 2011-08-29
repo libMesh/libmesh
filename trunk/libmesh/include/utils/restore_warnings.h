bf8529e02cddb10cc9a8f7c16bda7e54d9dccb29
John Peterson <jwpeterson@gmail.com>
2011-08-29 14:51:25 +0000

    Enable SVN keyword expansion in all files.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@4823 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/include/utils/restore_warnings.h
+++ trunk/libmesh/include/utils/restore_warnings.h
@@ -1,5 +1,5 @@
 
-// $Id: libmesh.h 4219 2011-03-02 20:38:14Z roystgnr $
+// $Id$
 
 // The libMesh Finite Element Library.
 // Copyright (C) 2002-2008 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
