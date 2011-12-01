3ba0ef428e39e9732b4da4fea1f8e171dbac716d
Roy Stogner <roystgnr@ices.utexas.edu>
2011-12-01 22:53:37 +0000

    Starting to accomodate subdirectory-sorted examples
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@4992 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/contrib/bin/create_example_docs.sh
+++ trunk/libmesh/contrib/bin/create_example_docs.sh
@@ -9,11 +9,12 @@ if [ "x$LIBMESH_DIR" = x ]; then
 fi
 
 cd $LIBMESH_DIR/examples/
-exampleslist=`ls -d ex*/ | sed 's#/##g'`
+exampleslist=`ls -d ex*/ */ex*/ | sed 's#/$##g'`
 echo $exampleslist
 cd ../contrib/bin/
 
 for exdir in $exampleslist; do
+    mybasename=`basename $exdir`
     echo "Processing example $exdir";
-    ./ex2html.sh $LIBMESH_DIR/examples/$exdir/$exdir.C "make --no-print-directory -C $LIBMESH_DIR/examples/$exdir run"
+    ./ex2html.sh $LIBMESH_DIR/examples/$exdir/$mybasename.C "make --no-print-directory -C $LIBMESH_DIR/examples/$exdir run"
 done
