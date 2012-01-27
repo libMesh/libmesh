0a56345637023dc73dca3b477d157141d1a8038b
Roy Stogner <roystgnr@ices.utexas.edu>
2012-01-27 00:07:33 +0000

    Making meshdiff slilghtly more informative in unusual cases
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5039 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/apps/meshdiff.C
+++ trunk/libmesh/src/apps/meshdiff.C
@@ -42,11 +42,11 @@ int main(int argc, char** argv)
 
   mesh1.read(argv[1]);
   std::cout << "Loaded coarse mesh " << argv[1] << std::endl;
-  es1.read(argv[2], libMeshEnums::READ);
+  es1.read(argv[2]);
   std::cout << "Loaded coarse solution " << argv[2] << std::endl;
   mesh2.read(argv[3]);
   std::cout << "Loaded fine mesh " << argv[3] << std::endl;
-  es2.read(argv[4], libMeshEnums::READ);
+  es2.read(argv[4]);
   std::cout << "Loaded fine solution " << argv[4] << std::endl;
 
   ExactSolution exact_sol(es1);
@@ -58,27 +58,70 @@ int main(int argc, char** argv)
   for (unsigned int i = 0; i != es1.n_systems(); ++i)
     if (es2.has_system(es1.get_system(i).name()))
       sysnames.push_back(es1.get_system(i).name());
+    else
+      std::cout << "Coarse solution system "
+                << es1.get_system(i).name()
+		<< " not found in fine solution!" << std::endl;
+
+  for (unsigned int i = 0; i != es2.n_systems(); ++i)
+    if (!es1.has_system(es2.get_system(i).name()))
+      std::cout << "Fine solution system "
+                << es2.get_system(i).name()
+		<< " not found in coarse solution!" << std::endl;
+
+  if (!es1.n_systems() && !es2.n_systems())
+    std::cout << "No systems found in fine or coarse solution!" 
+              << std::endl;
 
   for (unsigned int i = 0; i != sysnames.size(); ++i)
     {
-      std::string sysname = sysnames[i];
-      System &sys1 = es1.get_system(sysname);
-      System &sys2 = es2.get_system(sysname);
+      const std::string sysname = sysnames[i];
+      const System &sys1 = es1.get_system(sysname);
+      const System &sys2 = es2.get_system(sysname);
 
       for (unsigned int j = 0; j != sys1.n_vars(); ++j)
         {
-          std::string varname = sys1.variable_name(j);
+          const std::string varname = sys1.variable_name(j);
 
           if (!sys2.has_variable(varname))
-            continue;
+            {
+              std::cout << "Fine solution system " << sysname 
+                        << " variable " << varname
+		        << " not found in coarse solution!" << std::endl;
+              continue;
+            }
+          const unsigned int j2 = sys2.variable_number(varname);
 
           exact_sol.compute_error(sysname, varname);
 
           std::cout << "Errors in system " << sysname << ", variable " << varname << ":" << std::endl;
-          std::cout << "L2 error: " << exact_sol.l2_error(sysname, varname) << std::endl;
-          std::cout << "H1 error: " << exact_sol.h1_error(sysname, varname) << std::endl;
-          std::cout << "H2 error: " << exact_sol.h2_error(sysname, varname) << std::endl;
+          std::cout << "L2 error: " << exact_sol.l2_error(sysname, varname) 
+                    << ", fine norm: " << sys1.calculate_norm(*sys1.solution, j, L2)
+                    << ", coarse norm: " << sys2.calculate_norm(*sys2.solution, j2, L2) << std::endl;
+          std::cout << "H1 error: " << exact_sol.h1_error(sysname, varname)
+                    << ", fine norm: " << sys1.calculate_norm(*sys1.solution, j, H1)
+                    << ", coarse norm: " << sys2.calculate_norm(*sys2.solution, j2, H1) << std::endl;
+          std::cout << "H2 error: " << exact_sol.h2_error(sysname, varname)
+                    << ", fine norm: " << sys1.calculate_norm(*sys1.solution, j, H2)
+                    << ", coarse norm: " << sys2.calculate_norm(*sys2.solution, j2, H2) << std::endl;
         }
+
+      for (unsigned int j = 0; j != sys2.n_vars(); ++j)
+        {
+          const std::string varname = sys2.variable_name(j);
+
+          if (!sys1.has_variable(varname))
+            {
+              std::cout << "Coarse solution system " << sysname 
+                        << " variable " << varname
+		        << " not found in fine solution!" << std::endl;
+              continue;
+            }
+        }
+
+      if (!sys1.n_vars() && !sys2.n_vars())
+        std::cout << "No variables found in fine or coarse solution system "
+                  << sysname << '!' << std::endl;
     }
 
   return 0;
