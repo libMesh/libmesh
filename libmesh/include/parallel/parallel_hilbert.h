3d3a03ea623b1d8b4a20eee6339e3d76f90e2537
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-01-30 22:06:28 +0000

    Fix for misparse

--- libmesh/include/parallel/parallel_hilbert.h
+++ libmesh/include/parallel/parallel_hilbert.h
@@ -41,7 +41,7 @@ namespace Parallel {
   class StandardType<Hilbert::HilbertIndices> : public DataType
   {
   public:
-    inline StandardType(const Hilbert::HilbertIndices *=NULL) {
+    inline StandardType(const Hilbert::HilbertIndices* =NULL) {
       // _static_type never gets freed, but it only gets committed once
       // so it's not a *huge* memory leak...
       static DataType _static_type;
