a0d6e9c1b027fbb27b12e2b21e2697813e9a783c
Cody Permann <codypermann@gmail.com>
2012-01-30 20:09:43 +0000

    fparser fixes
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5047 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/contrib/fparser/fpoptimizer/optimize_main.cc
+++ trunk/libmesh/contrib/fparser/fpoptimizer/optimize_main.cc
@@ -55,6 +55,58 @@ void FunctionParserBase<Value_t>::Optimize()
     //PrintByteCode(std::cout);
 }
 
+// The following is an explicit definition that appears to be require on Apple platforms - it
+// is an exact copy of the function above specialized
+#ifdef __APPLE__
+template<>
+void FunctionParserBase<double>::Optimize()
+{
+  using namespace FPoptimizer_CodeTree;
+
+    CopyOnWrite();
+
+    //PrintByteCode(std::cout);
+    /*std::fprintf(stderr,
+        "O:refCount:%u mVarCount:%u mfuncPtrs:%u mFuncParsers:%u mByteCode:%u mImmed:%u\n",
+        mData->mReferenceCounter,
+        mData->mVariablesAmount,
+        (unsigned)mData->mFuncPtrs.size(),
+        (unsigned)mData->mFuncParsers.size(),
+        (unsigned)mData->mByteCode.size(),
+        (unsigned)mData->mImmed.size()
+    );*/
+
+    CodeTree<double> tree;
+    tree.GenerateFrom(*mData);
+
+    FPoptimizer_Optimize::ApplyGrammars(tree);
+
+    std::vector<unsigned> byteCode;
+    std::vector<double> immed;
+    size_t stacktop_max = 0;
+    tree.SynthesizeByteCode(byteCode, immed, stacktop_max);
+
+    /*std::cout << std::flush;
+    std::cerr << std::flush;
+    fprintf(stderr, "Estimated stacktop %u\n", (unsigned)stacktop_max);
+    fflush(stderr);*/
+
+    if(mData->mStackSize != stacktop_max)
+    {
+        mData->mStackSize = unsigned(stacktop_max); // Note: Ignoring GCC warning here.
+#if !defined(FP_USE_THREAD_SAFE_EVAL) && \
+    !defined(FP_USE_THREAD_SAFE_EVAL_WITH_ALLOCA)
+        mData->mStack.resize(stacktop_max);
+#endif
+    }
+
+    mData->mByteCode.swap(byteCode);
+    mData->mImmed.swap(immed);
+
+    //PrintByteCode(std::cout);
+}
+#endif
+
 #ifdef FP_SUPPORT_MPFR_FLOAT_TYPE
 template<>
 void FunctionParserBase<MpfrFloat>::Optimize()
