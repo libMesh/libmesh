8aecda6ebfff82798430232698bf49fcf179b675
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2012-03-01 20:10:44 +0000

    use pkg-config to test installed headers and examples
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5240 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- branches/libmesh.automake/contrib/bin/test_installed_examples.sh
+++ branches/libmesh.automake/contrib/bin/test_installed_examples.sh
@@ -2,73 +2,61 @@
 set -e
 #env
 
-## Terminal commands to goto specific columns
-#rescol=65;
-#
-## Terminal commands for setting the color
-#gotocolumn="\033["$rescol"G";
-#white="\033[01;37m";
-#green="\033[01;32m";
-#red="\033[01;31m";
-##grey="\033[00;37m";
-#colorreset="\033[m"; # Terminal command to reset to terminal default
-#
-##echo "CXX=$CXX"
-##echo "INCLUDEDIR_BASE=$INCLUDEDIR"
-#
-#include_I_path=$libmesh_optional_INCLUDES
-#
-#for dir in $INCLUDEDIR/* ; do
-#   #echo $dir
-#   if (test -d $INCLUDEDIR); then
-#     include_I_path="-I$dir $include_I_path"
-#   fi
-#done
-#
-##echo "libmesh_optional_INCLUDES=$libmesh_optional_INCLUDES"
-##echo "include_I_path=$include_I_path"
+# Terminal commands to goto specific columns
+rescol=65;
+
+# Terminal commands for setting the color
+gotocolumn="\033["$rescol"G";
+white="\033[01;37m";
+green="\033[01;32m";
+red="\033[01;31m";
+#grey="\033[00;37m";
+colorreset="\033[m"; # Terminal command to reset to terminal default
 
 
 echo "Testing examples in $examples_install_path"
 
+installed_CXXFLAGS=`pkg-config libmesh --cflags`
+installed_LIBS=`pkg-config libmesh --libs`
+
+#echo "installed_CXXFLAGS=$installed_CXXFLAGS"
+#echo "installed_LIBS=$installed_LIBS"
+
 returnval=0
-exit $returnval
 
-for header_to_test in $HEADERS_TO_TEST ; do
-    echo -n "Testing Header $header_to_test ... "
+app_to_link=`mktemp --tmpdir ex.XXXXXXXXXX`
+errlog=$app_to_link.log
 
-    header_name=`basename $header_to_test`
-    app_file=`mktemp -t $header_name.XXXXXXXXXX`
-    source_file=$app_file.cxx
-    object_file=$app_file.o
-    errlog=$app_file.log
+#echo " "
+#echo "$app_to_link"
+#echo "$errlog"
+#echo " "
+
+cd $examples_install_path
+for exdir in */* ; do
+
+    echo -n "Testing example installation $exdir ... "
+    cd $examples_install_path/$exdir
     
-    echo "#include \"$header_name\"" >> $source_file
-    echo "int foo () { return 0; }" >> $source_file
-
-    #echo $CXX $include_I_path $source_file -o $app_file
-    if $CXX $include_I_path $source_file -c -o $object_file >$errlog 2>&1 ; then
+    if $CXX $installed_CXXFLAGS *.C -o $app_to_link $installed_LIBS > $errlog 2>&1 ; then
+	 
  	echo -e $gotocolumn $white"["$green"   OK   "$white"]";
-	echo -e -n $colorreset;    
+	echo -e -n $colorreset;
+	
     else
  	echo -e $gotocolumn $white"["$red" FAILED "$white"]";
 	echo -e -n $colorreset;
-	echo "Source file:"
-	cat $source_file
-	echo ""
-	echo "Command line:"
-	echo $CXX $include_I_path $source_file -c -o $object_file
+	echo "Compile line:"
+	echo $CXX $installed_CXXFLAGS *.C -o $app_to_link $installed_LIBS
 	echo ""
 	echo "Output:"
 	cat $errlog
 	echo ""
-	returnval=1
+	returnval=1	
     fi
 
-    #echo -e -n $colorreset;    
-    #cat $source_file
-    rm -f $source_file $app_file $object_file $errlog
+    
 done
 
+rm -f $app_to_link $errlog
 exit $returnval
-
--- branches/libmesh.automake/contrib/bin/test_installed_headers.sh
+++ branches/libmesh.automake/contrib/bin/test_installed_headers.sh
@@ -14,19 +14,8 @@ red="\033[01;31m";
 colorreset="\033[m"; # Terminal command to reset to terminal default
 
 #echo "CXX=$CXX"
-#echo "INCLUDEDIR_BASE=$INCLUDEDIR"
 
-include_I_path=$libmesh_optional_INCLUDES
-
-for dir in $INCLUDEDIR/* ; do
-   #echo $dir
-   if (test -d $INCLUDEDIR); then
-     include_I_path="-I$dir $include_I_path"
-   fi
-done
-
-#echo "libmesh_optional_INCLUDES=$libmesh_optional_INCLUDES"
-#echo "include_I_path=$include_I_path"
+installed_CXXFLAGS=`pkg-config libmesh --cflags`
 
 
 returnval=0
@@ -42,8 +31,8 @@ for header_to_test in $HEADERS_TO_TEST ; do
     echo "#include \"$header_name\"" >> $source_file
     echo "int foo () { return 0; }" >> $source_file
 
-    #echo $CXX $include_I_path $source_file -o $app_file
-    if $CXX $include_I_path $source_file -c -o $object_file >$errlog 2>&1 ; then
+    #echo $CXX $installed_CXXFLAGS $source_file -o $app_file
+    if $CXX $installed_CXXFLAGS $source_file -c -o $object_file >$errlog 2>&1 ; then
  	echo -e $gotocolumn $white"["$green"   OK   "$white"]";
 	echo -e -n $colorreset;    
     else
@@ -53,7 +42,7 @@ for header_to_test in $HEADERS_TO_TEST ; do
 	cat $source_file
 	echo ""
 	echo "Command line:"
-	echo $CXX $include_I_path $source_file -c -o $object_file
+	echo $CXX $installed_CXXFLAGS $source_file -c -o $object_file
 	echo ""
 	echo "Output:"
 	cat $errlog
@@ -68,55 +57,3 @@ done
 
 exit $returnval
 
-
-
-
-
-
-
-# # Note: script was /bin/sh but I don't think -n, -e are POSIX
-# # arguments to echo
-
-# if [ "x$LIBMESH_DIR" = x ]; then
-#   export LIBMESH_DIR=../..
-# fi
-
-# headers_to_test=`ls $LIBMESH_DIR/include/*/*.h`
-
-# if test $# -ge 1; then
-#     headers_to_test=$*
-# fi
-
-
-# # Errors during the tests will be printed here
-# errlog=test_headers.log
-
-# returnval=0
-
-# for i in $headers_to_test; do
-#     header_name=`basename $i`
-#     source_name=TestHeader_$header_name.C # Use .C here, take advantage of our make rule
-#     source_file=$LIBMESH_DIR/contrib/bin/$source_name
-#     app_name=TestHeader_$header_name.o
-#     app_file=$LIBMESH_DIR/contrib/bin/$app_name
-
-#     rm -f $source_file $app_file
-    
-#     echo "#include \"$header_name\"" >> $source_file
-#     echo "int main () { return 0; }" >> $source_file
-
-#     echo -n "Testing Header File $header_name ... "
-
-#     # Use make -s for silent operation
-#     if make -s -C $LIBMESH_DIR contrib/bin/$app_name 2>> $errlog; then
-# 	echo -e $gotocolumn $white"["$green"   OK   "$white"]";
-#     else
-# 	echo -e $gotocolumn $white"["$red" FAILED "$white"]";
-#         returnval=1
-#     fi
-    
-#     echo -e -n $colorreset;    
-#     rm -f $source_file $app_file
-# done
-
-# exit $returnval
