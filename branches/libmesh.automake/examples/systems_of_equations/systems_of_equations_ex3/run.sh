56116ad50978e8510c758e95ab13288c782b455c
Paul Bauman <pbauman@ices.utexas.edu>
2012-03-01 03:54:52 +0000

    [automake-branch]: systems_of_equations examples
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5231 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ branches/libmesh.automake/examples/systems_of_equations/systems_of_equations_ex3/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#set -x
+
+source $LIBMESH_DIR/examples/run_common.sh
+
+example_name=systems_of_equations_ex3
+
+message_running "$example_name" 
+
+run_example "$example_name" 
+
+message_done_running "$example_name"
