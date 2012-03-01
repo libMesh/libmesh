adea9c3f59868c6f53ca28f5d61346480523c668
Paul Bauman <pbauman@ices.utexas.edu>
2012-03-01 04:13:39 +0000

    [automake-branch]: Added transient examples.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5232 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ branches/libmesh.automake/examples/transient/transient_ex2/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#set -x
+
+source $LIBMESH_DIR/examples/run_common.sh
+
+example_name=transient_ex2
+example_dir=examples/transient/$example_name
+
+ln -sf $LIBMESH_DIR/$example_dir/pipe-mesh.unv .
+
+message_running "$example_name" 
+
+options="pipe-mesh.unv"
+run_example "$example_name" "$options"
+
+message_done_running "$example_name"
