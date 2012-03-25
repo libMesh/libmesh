0f904726e5f63fb0bc2dbaa4c8e03ac5e472aa68
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-03-25 22:38:11 +0000

    Let clang ignore warnings on 3rd party files

--- libmesh/include/utils/restore_warnings.h
+++ libmesh/include/utils/restore_warnings.h
@@ -20,6 +20,11 @@
 #include "libmesh_config.h"
 
 #ifndef NDEBUG
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif 
+
 #if defined(__GNUC__) && !defined(__INTEL_COMPILER) && !defined(__clang__)
 #if __GNUC__ > 3 && __GNUC_MINOR__ > 1
 // TODO: use the gcc 4.6 push/pop when available
