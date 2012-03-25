16b656bbe19ba3de3fb09b70e39c1cd558a2f7bf
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2012-03-25 18:45:47 +0000

    not sure why run_example is having a problem here -- workaround
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5403 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- branches/libmesh.automake/examples/adaptivity/adaptivity_ex5/run.sh
+++ branches/libmesh.automake/examples/adaptivity/adaptivity_ex5/run.sh
@@ -16,28 +16,34 @@ n_refinements=5
 # Specify how often to write output files
 output_freq=10
 
-options=" \[-read_solution\] -n_timesteps $n_timesteps -n_refinements $n_refinements -init_timestep \[0\|$n_timesteps\]"
-message_running "$example_name" "$options"
+# options=" \[-read_solution\] -n_timesteps $n_timesteps -n_refinements $n_refinements -init_timestep \[0\|$n_timesteps\]"
+# message_running "$example_name" "$options"
 
-options="-n_timesteps $n_timesteps -n_refinements $n_refinements \
-    -output_freq $output_freq -init_timestep 0"
-run_example "$example_name" "$options"
+# options="-n_timesteps $n_timesteps -n_refinements $n_refinements \
+#     -output_freq $output_freq -init_timestep 0"
+# run_example "$example_name" "$options"
 
-echo " "
-echo "***** Finished first" $n_timesteps "steps, now read in" \
-    "saved solution and continue *****"
-echo " "
+# echo " "
+# echo "***** Finished first" $n_timesteps "steps, now read in" \
+#     "saved solution and continue *****"
+# echo " "
 
-options="-read_solution -n_timesteps $n_timesteps \
-    -output_freq $output_freq -init_timestep $n_timesteps"
-run_example "$example_name" "$options"
+# options="-read_solution -n_timesteps $n_timesteps \
+#     -output_freq $output_freq -init_timestep $n_timesteps"
+# run_example "$example_name" "$options"
 
-echo " "
+# echo " "
 
 options="-n_timesteps $n_timesteps -n_refinements $n_refinements \
     -output_freq $output_freq -init_timestep 0 \
     -exact_solution '10*exp(-(pow(x-0.8*t-0.2,2) + pow(y-0.8*t-0.2,2))/(0.01*(4*t+1)))/(4*t+1)'"
-run_example "$example_name" "$options"
+
+#run_example "$example_name" "$options"
+
+$LIBMESH_RUN $example_name -n_timesteps $n_timesteps -n_refinements $n_refinements \
+                   -output_freq $output_freq -init_timestep 0 \
+                   -exact_solution '10*exp(-(pow(x-0.8*t-0.2,4) + pow(y-0.8*t-0.2,2))/(0.01*(4*t+1)))/(4*t+1)' \
+                   $LIBMESH_OPTIONS || exit 1
 
 echo " "
 echo "***** Finished first" $n_timesteps "steps, now read in" \
