fcfbdd25f584dcf7f95e571d499c062934a9f6ff
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-03-25 02:15:41 +0000

    Don't rely on implicitly casting false to NULL

--- libmesh/include/base/getpot.h
+++ libmesh/include/base/getpot.h
@@ -1498,7 +1498,7 @@ GetPot::_match_starting_string(const char* StartString)
 	{ cursor = c; search_failed_f = false; return &(argv[c].c_str()[N]); }
     }
 
-    if( ! search_loop_f ) return false;
+    if( ! search_loop_f ) return NULL;
 
     // (*) second loop from 0 to old cursor position
     for(c = 1; c < OldCursor; c++) {
