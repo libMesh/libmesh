68a34a7b808ea752234d460aed142fdd1943b8ec
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-03-27 23:03:08 +0000

    FunctionBase::clone ought to be const

--- libmesh/include/numerics/function_base.h
+++ libmesh/include/numerics/function_base.h
@@ -88,7 +88,7 @@ public:
    * ``deep'' as necessary to allow independent destruction and
    * simultaneous evaluations of the copies in different threads.
    */
-  virtual AutoPtr<FunctionBase<Output> > clone () = 0;
+  virtual AutoPtr<FunctionBase<Output> > clone () const = 0;
 
 
   // ------------------------------------------------------
