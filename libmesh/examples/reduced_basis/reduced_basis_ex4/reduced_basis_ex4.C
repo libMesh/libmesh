d98fd8926f3dbbbad7e115e1be6c454059c23c08
David Knezevic <knezed01@users.sourceforge.net>
2012-04-07 19:37:55 +0000

    Fixed up comments in RB examples for the website

--- libmesh/examples/reduced_basis/reduced_basis_ex4/reduced_basis_ex4.C
+++ libmesh/examples/reduced_basis/reduced_basis_ex4/reduced_basis_ex4.C
@@ -1,21 +1,20 @@
-// rbOOmit: An implementation of the Certified Reduced Basis method.
-// Copyright (C) 2009, 2010 David J. Knezevic
-//
-//     This file is part of rbOOmit.
+/* rbOOmit: An implementation of the Certified Reduced Basis method. */
+/* Copyright (C) 2009, 2010 David J. Knezevic */
+/*     This file is part of rbOOmit. */
 
-// rbOOmit is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
+/* rbOOmit is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
   
-// rbOOmit is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+/* rbOOmit is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
   
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 
 // <h1>Reduced Basis Example 4 - Empirical Interpolation Method</h1>
 
