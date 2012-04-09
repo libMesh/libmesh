36e489c1d54322332773d13a779a5f298eef1f1b
John W. Peterson <jwpeterson@users.sourceforge.net>
2012-04-09 22:40:04 +0000

    Adding example from Dmitry Karpeev which makes use of the new PetscDMNonlinearSolver.

---
+++ libmesh/examples/miscellaneous/miscellaneous_ex7/biharmonic.h
@@ -0,0 +1,134 @@
+#ifndef __biharmonic_h__
+#define __biharmonic_h__
+
+#include "equation_systems.h"
+#include "mesh.h"
+#include "exodusII_io.h"
+
+// libmesh_error() and libmesh_assert() macros with a message
+#define ERROR(message)                                                                                         \
+  do {                                                                                                         \
+    libMesh::err << "Error: " << message << "\n";                                                              \
+    libmesh_error();							                                       \
+  } while(0)
+
+#define ASSERT(asserted, message) \
+  do {                    \
+    if(!(asserted)) {     \
+      libMesh::err << "Assertion '" #asserted "' violated: " #message; \
+      libmesh_error();    \
+    }                     \
+  } while(0)
+
+
+/**
+ * The Biharmonic class encapsulates most of the data structures
+ * necessary to calculate the biharmonic residual and Jacobian,
+ * auxiliary quantities, to take a timestep, and to output the state --
+ * biharmonic solution and vectors of auxiliary quantities.
+ *
+ * The main reason for this design is to have a data structure that
+ * has all of the necessary data in one place, where all of the
+ * calculation subroutines can access these data. Currently these data
+ * are split up among several interdependent objects with no clear
+ * hierarchy between them: mesh, equation system, equation system
+ * bundle, residual/Jacobian calculator.
+ *
+ * Since no object contains all others and the data are distributed
+ * among many objects, the natural control and data flow resides outside
+ * of these objects and is typically implemented in main().  We,
+ * however, would like to split the calculation into natural chunks --
+ * subroutines -- while retaining these subroutines access to the common
+ * necessary data -- biharmonic parameters, mesh and time interval
+ * sizes, etc. Thus, class Biharmonic.  Finally, making Biharmonic
+ * inherit from EquationSystems makes it possible to include it in the
+ * most common callbacks that do not pass back a user context, but only
+ * an EquationSystems object.
+ */
+class Biharmonic : public EquationSystems
+{
+public:
+  // Initial state enumeration
+  enum InitialStateEnum {STRIP = 0,
+			 ROD   = 1,
+			 BALL  = 2};
+
+  // Free energy enumeration
+  enum FreeEnergyEnum {DOUBLE_WELL         = 1,
+		       DOUBLE_OBSTACLE     = 2,
+		       LOG_DOUBLE_WELL     = 3,
+		       LOG_DOUBLE_OBSTACLE = 4};
+
+  /**
+   * Static creation/destruction routines.  FIXME - this looks like
+   * object-oriented C, can we get rid of it?
+   */
+  static void Create(Biharmonic** b);
+  static void Destroy(Biharmonic** b);
+
+
+  /**
+   * Constructor retrieves command-line options, setting  defaults, if necessary.
+   * It then builds the mesh using these options, then the equations systems around it,
+   * and, finally, sets up the output.
+   * We recommend that this be used through the factory Create function, which allocates
+   * the mesh. In that case don't forget to call Destroy at the end, to free the mesh up.
+   */
+  Biharmonic(Mesh* m);
+
+
+  /**
+   * Destructor
+   */
+  ~Biharmonic()
+  {
+    // delete _meshRefinement;
+  };
+
+
+  // Misc. getters
+  bool verbose()         { return _verbose; }
+  Real dt0()             { return _dt0; }
+  Real dt()              { return _dt; }
+
+
+  // Public interface functions
+  void viewParameters();
+  void init();
+  void step(const Real& dt = -1.0);
+  void output(int timestep, const Real& t, Real& o_t, bool force = false);
+  void run();
+
+private:
+  unsigned int  _dim, _N;
+  Real _kappa, _theta, _theta_c;
+  Real _tol;
+  bool _growth, _degenerate, _cahn_hillard, _vi, _netforce;
+  FreeEnergyEnum  _energy;
+  int _log_truncation;
+  bool _verbose;
+  InitialStateEnum  _initialState;
+  Point _initialCenter;
+  Real _initialWidth;
+  Real _dt0, _dt, _t0, _T, _t1;
+  Real _cnWeight;
+  //
+  std::string  _ofile_base, _ofile;
+  ExodusII_IO* _exio;
+  Real    _o_dt;
+  int     _o_count;
+  //
+  friend class JR;
+  class JR;       // forward
+  Mesh*                                   _mesh;
+  MeshRefinement*                         _meshRefinement;
+  JR*                                     _jr;
+};
+
+
+
+
+
+
+
+#endif // __biharmonic_h__
