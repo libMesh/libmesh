6822e9d667d1057521d2fde3aefe4b48c7bc5a1e
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2012-04-19 18:07:36 +0000

    cleanup
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5564 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ branches/libmesh.automake/contrib/bin/buildall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./bootstrap
+
+top_dir=`pwd`
+install_dir=/tmp/foo
+
+rm -rf $install_dir
+
+for METHOD in dbg devel opt ; do
+
+    cd $top_dir
+    rm -rf $METHOD && mkdir $METHOD && cd $METHOD
+
+    echo " "
+    echo " Configuring via ../configure --prefix=$install_dir $@"
+    echo " "
+
+    ../configure --prefix=$install_dir $@
+    make -j 24 install
+done
+    
