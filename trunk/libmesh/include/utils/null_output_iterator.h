27f9f360769eae51de60f7f2bcf5592feb9c93a2
Roy Stogner <roystgnr@ices.utexas.edu>
2012-07-03 23:05:12 +0000

    Some output iterator options to make Parallel:: packed_range calls useful.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5801 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/include/utils/null_output_iterator.h
@@ -0,0 +1,57 @@
+
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2012 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef __null_output_iterator_h__
+#define __null_output_iterator_h__
+
+// Local includes
+
+// C++ includes
+#include <iterator>
+
+namespace libMesh
+{
+
+// A do-nothing class for templated methods that expect output
+// iterator arguments.
+template <typename T>
+struct null_output_iterator
+  : std::iterator<std::output_iterator_tag, T>
+{
+  template <typename T>
+  void operator=(const T&) {}
+
+  null_output_iterator& operator++() { 
+    return *this; 
+  }
+
+  null_output_iterator operator++(int) { 
+    return null_output_iterator(*this); 
+  }
+
+  // We don't return a reference-to-T here because we don't want to
+  // construct one or have any of its methods called.
+  null_output_iterator& operator*() { return *this; }
+};
+
+} // namespace libMesh
+
+
+#endif // __null_output_iterator_h__
