a86d7d1b8090c6da1c2ebfac44e3b605336ae101
David Knezevic <knezed01@users.sourceforge.net>
2012-07-09 15:01:36 +0000

    rbOOmit change: Modified how we store temporal discretization parameters in TransientRBEvaluation and TransientRBConstruction. Now inherit from RBTemporalDiscretization, rather than store an object.

---
+++ libmesh/src/reduced_basis/rb_temporal_discretization.C
@@ -0,0 +1,103 @@
+// rbOOmit: An implementation of the Certified Reduced Basis method.
+// Copyright (C) 2009, 2010 David J. Knezevic
+
+// This file is part of rbOOmit.
+
+// rbOOmit is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// rbOOmit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+// rbOOmit includes
+#include "rb_temporal_discretization.h"
+
+// libMesh includes
+#include "getpot.h"
+
+namespace libMesh
+{
+
+RBTemporalDiscretization::RBTemporalDiscretization()
+: _delta_t(0.),
+  _euler_theta(0.),
+  _current_time_step(0),
+  _n_time_steps(0)
+{}
+
+Real RBTemporalDiscretization::get_delta_t() const
+{
+  return _delta_t;
+}
+
+void RBTemporalDiscretization::set_delta_t(const Real delta_t_in)
+{
+  _delta_t = delta_t_in;
+}
+
+Real RBTemporalDiscretization::get_euler_theta() const
+{
+  return _euler_theta;
+}
+
+void RBTemporalDiscretization::set_euler_theta(const Real euler_theta_in)
+{
+  libmesh_assert((0. <= euler_theta_in ) && (euler_theta_in <= 1.));
+  _euler_theta = euler_theta_in;
+}
+
+unsigned int RBTemporalDiscretization::get_time_step() const
+{
+  return _current_time_step;
+}
+
+void RBTemporalDiscretization::set_time_step(const unsigned int k)
+{
+  libmesh_assert(k <= get_n_time_steps());
+  this->_current_time_step = k;
+}
+
+unsigned int RBTemporalDiscretization::get_n_time_steps() const
+{
+  return _n_time_steps;
+}
+
+void RBTemporalDiscretization::set_n_time_steps(const unsigned int K)
+{
+  _n_time_steps = K;
+}
+
+void RBTemporalDiscretization::process_temporal_parameters_file (const std::string& parameters_filename)
+{
+  // Read in data from parameters_filename
+  GetPot infile(parameters_filename);
+
+  // Read in parameters related to temporal discretization
+  unsigned int n_time_steps_in = infile("n_time_steps", get_n_time_steps());
+  const Real delta_t_in        = infile("delta_t",      get_delta_t());
+  const Real euler_theta_in    = infile("euler_theta",  get_euler_theta());
+
+  // and set the relevant member variables
+  set_n_time_steps(n_time_steps_in);
+  set_delta_t(delta_t_in);
+  set_euler_theta(euler_theta_in);
+  set_time_step(0);
+}
+
+void RBTemporalDiscretization::pull_temporal_discretization_data(RBTemporalDiscretization& other)
+{
+  this->set_delta_t( other.get_delta_t() );
+  this->set_euler_theta( other.get_euler_theta() );
+  this->set_n_time_steps( other.get_n_time_steps() );
+  this->set_time_step( other.get_time_step() );
+}
+
+}
