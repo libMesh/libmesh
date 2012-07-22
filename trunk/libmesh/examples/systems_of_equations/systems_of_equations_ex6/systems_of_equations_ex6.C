8767f901c414b749f1536d215d340b92a6baba9f
David Knezevic <dknezevic@seas.harvard.edu>
2012-07-22 16:46:27 +0000

    Added systems_of_equations_ex6, a 3D cantilever. Note the default iterative solvers in PETSc seem to struggle a bit for this problem.
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5871 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

---
+++ trunk/libmesh/examples/systems_of_equations/systems_of_equations_ex6/systems_of_equations_ex6.C
@@ -0,0 +1,436 @@
+/* The Next Great Finite Element Library. */
+/* Copyright (C) 2003  Benjamin S. Kirk */
+
+/* This library is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
+
+/* This library is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
+
+ // <h1> Systems Example 6 - 3D Linear Elastic Cantilever </h1>
+ //      By David Knezevic
+ //
+ // This is a 3D version of systems_of_equations_ex4
+
+
+// C++ include files that we need
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+
+// libMesh includes
+#include "libmesh.h"
+#include "mesh.h"
+#include "mesh_generation.h"
+#include "exodusII_io.h"
+#include "gnuplot_io.h"
+#include "linear_implicit_system.h"
+#include "equation_systems.h"
+#include "fe.h"
+#include "quadrature_gauss.h"
+#include "dof_map.h"
+#include "sparse_matrix.h"
+#include "numeric_vector.h"
+#include "dense_matrix.h"
+#include "dense_submatrix.h"
+#include "dense_vector.h"
+#include "dense_subvector.h"
+#include "perf_log.h"
+#include "elem.h"
+#include "boundary_info.h"
+#include "zero_function.h"
+#include "dirichlet_boundaries.h"
+#include "string_to_enum.h"
+#include "getpot.h"
+
+#define x_scaling 1.3
+#define x_load 0.0
+#define y_load 0.0
+#define z_load -1.0
+
+// boundary IDs
+#define BOUNDARY_ID_MIN_Z 0
+#define BOUNDARY_ID_MIN_Y 1
+#define BOUNDARY_ID_MAX_X 2
+#define BOUNDARY_ID_MAX_Y 3
+#define BOUNDARY_ID_MIN_X 4
+#define BOUNDARY_ID_MAX_Z 5
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+// Matrix and right-hand side assembly
+void assemble_elasticity(EquationSystems& es,
+                         const std::string& system_name);
+
+// The Kronecker delta function, used in eval_elasticity_tensor
+Real kronecker_delta(unsigned int i,
+                     unsigned int j);
+
+// Define the elasticity tensor, which is a fourth-order tensor
+// i.e. it has four indices i,j,k,l
+Real eval_elasticity_tensor(unsigned int i,
+                            unsigned int j,
+                            unsigned int k,
+                            unsigned int l);
+
+// Begin the main program.
+int main (int argc, char** argv)
+{
+  // Initialize libMesh and any dependent libaries
+  LibMeshInit init (argc, argv);
+
+  // Initialize the cantilever mesh
+  const unsigned int dim = 3;
+
+  // Make sure libMesh was compiled for 3D
+  libmesh_example_assert(dim == LIBMESH_DIM, "3D support");
+
+  Mesh mesh(dim);
+  MeshTools::Generation::build_cube (mesh,
+                                     40,
+                                     8,
+                                     4,
+                                     0., 1.*x_scaling,
+                                     0., 0.3,
+                                     0., 0.1,
+                                     HEX8);
+
+  
+  // Print information about the mesh to the screen.
+  mesh.print_info();
+  
+  
+  // Create an equation systems object.
+  EquationSystems equation_systems (mesh);
+  
+  // Declare the system and its variables.
+  // Create a system named "Elasticity"
+  LinearImplicitSystem& system =
+    equation_systems.add_system<LinearImplicitSystem> ("Elasticity");
+  
+  // Add three displacement variables, u and v, to the system
+  unsigned int u_var = system.add_variable("u", FIRST, LAGRANGE);
+  unsigned int v_var = system.add_variable("v", FIRST, LAGRANGE);
+  unsigned int w_var = system.add_variable("w", FIRST, LAGRANGE);
+
+  system.attach_assemble_function (assemble_elasticity);
+
+
+  std::set<boundary_id_type> boundary_ids;
+  boundary_ids.insert(BOUNDARY_ID_MIN_X);
+
+  // Create a vector storing the variable numbers which the BC applies to
+  std::vector<unsigned int> variables;
+  variables.push_back(u_var);
+  variables.push_back(v_var);
+  variables.push_back(w_var);
+  
+  // Create a ZeroFunction to initialize dirichlet_bc
+  ZeroFunction<> zf;
+  
+  DirichletBoundary dirichlet_bc(boundary_ids,
+                                 variables,
+                                 &zf);
+
+  // We must add the Dirichlet boundary condition _before_ 
+  // we call equation_systems.init()
+  system.get_dof_map().add_dirichlet_boundary(dirichlet_bc);
+  
+  // Initialize the data structures for the equation system.
+  equation_systems.init();
+
+  // Print information about the system to the screen.
+  equation_systems.print_info();
+
+  // Solve the system
+  system.solve();
+
+  // Plot the solution
+#ifdef LIBMESH_HAVE_EXODUS_API
+  ExodusII_IO (mesh).write_equation_systems("displacement.e",equation_systems);
+#endif // #ifdef LIBMESH_HAVE_EXODUS_API
+
+  // All done.  
+  return 0;
+}
+
+
+void assemble_elasticity(EquationSystems& es,
+                         const std::string& system_name)
+{
+  libmesh_assert (system_name == "Elasticity");
+  
+  const MeshBase& mesh = es.get_mesh();
+
+  const unsigned int dim = mesh.mesh_dimension();
+
+  LinearImplicitSystem& system = es.get_system<LinearImplicitSystem>("Elasticity");
+
+  const unsigned int n_components = 3;
+  const unsigned int u_var = system.variable_number ("u");
+  const unsigned int v_var = system.variable_number ("v");
+  const unsigned int w_var = system.variable_number ("w");
+
+  const DofMap& dof_map = system.get_dof_map();
+  FEType fe_type = dof_map.variable_type(u_var);
+  AutoPtr<FEBase> fe (FEBase::build(dim, fe_type));
+  QGauss qrule (dim, fe_type.default_quadrature_order());
+  fe->attach_quadrature_rule (&qrule);
+
+  AutoPtr<FEBase> fe_face (FEBase::build(dim, fe_type));
+  QGauss qface(dim-1, fe_type.default_quadrature_order());
+  fe_face->attach_quadrature_rule (&qface);
+
+  const std::vector<Real>& JxW = fe->get_JxW();
+  const std::vector<std::vector<RealGradient> >& dphi = fe->get_dphi();
+
+  DenseMatrix<Number> Ke;
+  DenseVector<Number> Fe;
+
+  DenseSubMatrix<Number>
+    Kuu(Ke), Kuv(Ke), Kuw(Ke),
+    Kvu(Ke), Kvv(Ke), Kvw(Ke),
+    Kwu(Ke), Kwv(Ke), Kww(Ke);
+
+  DenseSubVector<Number>
+    Fu(Fe),
+    Fv(Fe),
+    Fw(Fe);
+
+  std::vector<unsigned int> dof_indices;
+  std::vector<unsigned int> dof_indices_u;
+  std::vector<unsigned int> dof_indices_v;
+  std::vector<unsigned int> dof_indices_w;
+
+  MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+  const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
+
+  for ( ; el != end_el; ++el)
+    {
+      const Elem* elem = *el;
+
+      dof_map.dof_indices (elem, dof_indices);
+      dof_map.dof_indices (elem, dof_indices_u, u_var);
+      dof_map.dof_indices (elem, dof_indices_v, v_var);
+      dof_map.dof_indices (elem, dof_indices_w, w_var);
+
+      const unsigned int n_dofs   = dof_indices.size();
+      const unsigned int n_u_dofs = dof_indices_u.size(); 
+      const unsigned int n_v_dofs = dof_indices_v.size();
+      const unsigned int n_w_dofs = dof_indices_w.size();
+
+      fe->reinit (elem);
+
+      Ke.resize (n_dofs, n_dofs);
+      Fe.resize (n_dofs);
+
+      Kuu.reposition (u_var*n_u_dofs, u_var*n_u_dofs, n_u_dofs, n_u_dofs);
+      Kuv.reposition (u_var*n_u_dofs, v_var*n_u_dofs, n_u_dofs, n_v_dofs);
+      Kuw.reposition (u_var*n_u_dofs, w_var*n_u_dofs, n_u_dofs, n_w_dofs);
+      
+      Kvu.reposition (v_var*n_u_dofs, u_var*n_u_dofs, n_v_dofs, n_u_dofs);
+      Kvv.reposition (v_var*n_u_dofs, v_var*n_u_dofs, n_v_dofs, n_v_dofs);
+      Kvw.reposition (v_var*n_u_dofs, w_var*n_u_dofs, n_v_dofs, n_w_dofs);
+
+      Kwu.reposition (w_var*n_u_dofs, u_var*n_u_dofs, n_w_dofs, n_u_dofs);
+      Kwv.reposition (w_var*n_u_dofs, v_var*n_u_dofs, n_w_dofs, n_v_dofs);
+      Kww.reposition (w_var*n_u_dofs, w_var*n_u_dofs, n_w_dofs, n_w_dofs);
+      
+      Fu.reposition (u_var*n_u_dofs, n_u_dofs);
+      Fv.reposition (v_var*n_u_dofs, n_v_dofs);
+      Fw.reposition (w_var*n_u_dofs, n_w_dofs);
+
+      for (unsigned int qp=0; qp<qrule.n_points(); qp++)
+      {
+          for (unsigned int i=0; i<n_u_dofs; i++)
+            for (unsigned int j=0; j<n_u_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i=0, C_k=0;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kuu(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+
+          for (unsigned int i=0; i<n_u_dofs; i++)
+            for (unsigned int j=0; j<n_v_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i=0, C_k=1;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kuv(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+
+          for (unsigned int i=0; i<n_u_dofs; i++)
+            for (unsigned int j=0; j<n_w_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i=0, C_k=2;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kuw(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+
+          for (unsigned int i=0; i<n_v_dofs; i++)
+            for (unsigned int j=0; j<n_u_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 1, C_k = 0;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kvu(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+
+          for (unsigned int i=0; i<n_v_dofs; i++)
+            for (unsigned int j=0; j<n_v_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 1, C_k = 1;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kvv(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+            
+          for (unsigned int i=0; i<n_v_dofs; i++)
+            for (unsigned int j=0; j<n_w_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 1, C_k = 2;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kvw(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+            
+          for (unsigned int i=0; i<n_w_dofs; i++)
+            for (unsigned int j=0; j<n_u_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 2, C_k = 0;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kwu(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+            
+          for (unsigned int i=0; i<n_w_dofs; i++)
+            for (unsigned int j=0; j<n_v_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 2, C_k = 1;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kwv(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+            
+          for (unsigned int i=0; i<n_w_dofs; i++)
+            for (unsigned int j=0; j<n_w_dofs; j++)
+            {
+              // Tensor indices
+              unsigned int C_i = 2, C_k = 2;
+
+              for(unsigned int C_j=0; C_j<n_components; C_j++)
+                for(unsigned int C_l=0; C_l<n_components; C_l++)
+                {
+                  Kww(i,j) += JxW[qp]*(eval_elasticity_tensor(C_i,C_j,C_k,C_l) * dphi[i][qp](C_j)*dphi[j][qp](C_l));
+                }
+            }
+      }
+
+      {
+        for (unsigned int side=0; side<elem->n_sides(); side++)
+          if (elem->neighbor(side) == NULL)
+            {
+              boundary_id_type bc_id = mesh.boundary_info->boundary_id (elem,side);
+              if (bc_id==BoundaryInfo::invalid_id)
+                  libmesh_error();
+
+              const std::vector<std::vector<Real> >&  phi_face = fe_face->get_phi();
+              const std::vector<Real>& JxW_face = fe_face->get_JxW();
+
+              fe_face->reinit(elem, side);
+
+              for (unsigned int qp=0; qp<qface.n_points(); qp++)
+              {
+                if( bc_id == BOUNDARY_ID_MAX_X ) // Apply a traction
+                {
+                  for (unsigned int i=0; i<n_v_dofs; i++)
+                  {
+                    Fu(i) += JxW_face[qp] * x_load * phi_face[i][qp];
+                  }
+                  for (unsigned int i=0; i<n_v_dofs; i++)
+                  {
+                    Fv(i) += JxW_face[qp] * y_load * phi_face[i][qp];
+                  }
+                  for (unsigned int i=0; i<n_v_dofs; i++)
+                  {
+                    Fw(i) += JxW_face[qp] * z_load * phi_face[i][qp];
+                  }
+                }
+              }
+            }
+      } 
+
+      dof_map.constrain_element_matrix_and_vector (Ke, Fe, dof_indices);
+      
+      system.matrix->add_matrix (Ke, dof_indices);
+      system.rhs->add_vector    (Fe, dof_indices);
+    }
+}
+
+Real kronecker_delta(unsigned int i,
+                     unsigned int j)
+{
+  return i == j ? 1. : 0.;
+}
+
+Real eval_elasticity_tensor(unsigned int i,
+                            unsigned int j,
+                            unsigned int k,
+                            unsigned int l)
+{
+  // Define the Poisson ratio and Young's modulus
+  const Real nu = 0.3;
+  const Real E  = 1.;
+
+  // Define the Lame constants (lambda_1 and lambda_2) based on nu and E
+  const Real lambda_1 = E * nu / ( (1. + nu) * (1. - 2.*nu) );
+  const Real lambda_2 = 0.5 * E / (1. + nu);
+
+  return lambda_1 * kronecker_delta(i,j) * kronecker_delta(k,l)
+       + lambda_2 * (kronecker_delta(i,k) * kronecker_delta(j,l) + kronecker_delta(i,l) * kronecker_delta(j,k));
+}
