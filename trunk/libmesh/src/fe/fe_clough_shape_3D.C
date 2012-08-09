e9231c308496533950efe5d95ef92cc3829d7512
Roy Stogner <roystgnr@ices.utexas.edu>
2012-08-09 19:02:21 +0000

    We don't need more than stubs for the unimplemented 3D Clough-Tocher case, and the cut-n-pasted code was triggering clang warnings
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@5931 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/src/fe/fe_clough_shape_3D.C
+++ trunk/libmesh/src/fe/fe_clough_shape_3D.C
@@ -16,7 +16,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-// C++ inlcludes
+// C++ includes
 
 // Local includes
 #include "fe.h"
@@ -24,138 +24,6 @@
 
 
 // FIXME: 3D C1 finite elements are still a work in progress
-
-
-// Anonymous namespace for persistant variables.
-// This allows us to cache the global-to-local mapping transformation
-// This should also screw up multithreading royally
-namespace
-{
-  using namespace libMesh;
-
-  static unsigned int old_elem_id = libMesh::invalid_uint;
-  // Coefficient naming: d(1)d(2n) is the coefficient of the
-  // global shape function corresponding to value 1 in terms of the
-  // local shape function corresponding to normal derivative 2
-
-Real clough_raw_shape_second_deriv(const unsigned int basis_num,
-                                   const unsigned int deriv_type,
-                                   const Point& p);
-Real clough_raw_shape_deriv(const unsigned int basis_num,
-                            const unsigned int deriv_type,
-                            const Point& p);
-Real clough_raw_shape(const unsigned int basis_num,
-                      const Point& p);
-
-
-// Compute the static coefficients for an element
-void clough_compute_coefs(const Elem* elem)
-{
-  // Using static globals for old_elem_id, etc. will fail
-  // horribly with more than one thread.
-  libmesh_assert(libMesh::n_threads() == 1);
-
-  // Coefficients are cached from old elements
-  if (elem->id() == old_elem_id)
-    return;
-
-  old_elem_id = elem->id();
-
-#if 0
-  const Order mapping_order        (elem->default_order());
-  const ElemType mapping_elem_type (elem->type());
-  const int n_mapping_shape_functions =
-    FE<3,LAGRANGE>::n_shape_functions(mapping_elem_type,
-				      mapping_order);
-
-  // Degrees of freedom are at vertices and edge midpoints
-  std::vector<Point> dofpt;
-#endif
-
-}
-
-
-unsigned char subtriangle_lookup(const Point&)
-{
-  return 0;
-}
-
-  // Return shape function second derivatives on the unit right
-  // triangle
-Real clough_raw_shape_second_deriv(const unsigned int basis_num,
-                            const unsigned int deriv_type,
-                            const Point& p)
-{
-  Real xi = p(0), eta = p(1), zeta = p(2);
-
-  switch (deriv_type)
-  {
-
-  // second derivative in xi-xi direction
-  case 0:
-  switch (basis_num)
-    {
-      case 0:
-        switch (subtriangle_lookup(p))
-          {
-            case 0:
-              break;
-          }
-    }
-  }
-
-  libmesh_error();
-  return xi + eta + zeta;
-}
-
-
-
-Real clough_raw_shape_deriv(const unsigned int basis_num,
-                            const unsigned int deriv_type,
-                            const Point& p)
-{
-  Real xi = p(0), eta = p(1), zeta = p(2);
-
-  switch (deriv_type)
-  {
-  case 0:
-  switch (basis_num)
-    {
-      case 0:
-        switch (subtriangle_lookup(p))
-          {
-            case 0:
-              break;
-          }
-    }
-  }
-
-  libmesh_error();
-  return xi + eta + zeta;
-}
-
-Real clough_raw_shape(const unsigned int basis_num,
-                      const Point& p)
-{
-  Real xi = p(0), eta = p(1), zeta = p(2);
-
-  switch (basis_num)
-    {
-      case 0:
-        switch (subtriangle_lookup(p))
-          {
-            case 0:
-              break;
-          }
-    }
-
-  libmesh_error();
-  return xi + eta + zeta;
-}
-
-
-} // end anonymous namespace
-
 
 namespace libMesh
 {
@@ -179,40 +47,13 @@ Real FE<3,CLOUGH>::shape(const ElemType,
 
 template <>
 Real FE<3,CLOUGH>::shape(const Elem* elem,
-			     const Order order,
+			     const Order,
 			     const unsigned int,
 			     const Point&)
 {
   libmesh_assert (elem != NULL);
 
-  libMesh::err << "3D Clough elements not yet implemented."
-	        << std::endl;
-
-  libmesh_error();
-
-  clough_compute_coefs(elem);
-
-  const ElemType type = elem->type();
-
-  switch (order+elem->p_level())
-    {
-      // 3rd-order Clough-Tocher element
-    case THIRD:
-      {
-	switch (type)
-	  {
-	  default:
-            libMesh::err << "ERROR: Unsupported element type!" << std::endl;
-	    libmesh_error();
-	  }
-      }
-      // by default throw an error
-    default:
-      libMesh::err << "ERROR: Unsupported polynomial order!" << std::endl;
-      libmesh_error();
-    }
-
-  libmesh_error();
+  libmesh_not_implemented();
   return 0.;
 }
 
@@ -237,41 +78,13 @@ Real FE<3,CLOUGH>::shape_deriv(const ElemType,
 
 template <>
 Real FE<3,CLOUGH>::shape_deriv(const Elem* elem,
-				   const Order order,
+				   const Order,
 				   const unsigned int,
 				   const unsigned int,
 				   const Point&)
 {
   libmesh_assert (elem != NULL);
-
-  libMesh::err << "3D Clough elements not yet implemented."
-	        << std::endl;
-
-  libmesh_error();
-
-  clough_compute_coefs(elem);
-
-  const ElemType type = elem->type();
-
-  switch (order+elem->p_level())
-    {
-      // 3rd-order Clough-Tocher element
-    case THIRD:
-      {
-	switch (type)
-	  {
-	  default:
-            libMesh::err << "ERROR: Unsupported element type!" << std::endl;
-	    libmesh_error();
-	  }
-      }
-      // by default throw an error
-    default:
-      libMesh::err << "ERROR: Unsupported polynomial order!" << std::endl;
-      libmesh_error();
-    }
-
-  libmesh_error();
+  libmesh_not_implemented();
   return 0.;
 }
 
@@ -279,41 +92,13 @@ Real FE<3,CLOUGH>::shape_deriv(const Elem* elem,
 
 template <>
 Real FE<3,CLOUGH>::shape_second_deriv(const Elem* elem,
-                                      const Order order,
+                                      const Order,
                                       const unsigned int,
                                       const unsigned int,
                                       const Point&)
 {
   libmesh_assert (elem != NULL);
-
-  libMesh::err << "3D Clough elements not yet implemented."
-	    << std::endl;
-
-  libmesh_error();
-
-  clough_compute_coefs(elem);
-
-  const ElemType type = elem->type();
-
-  switch (order+elem->p_level())
-    {
-      // 3rd-order Clough-Tocher element
-    case THIRD:
-      {
-	switch (type)
-	  {
-	  default:
-            libMesh::err << "ERROR: Unsupported element type!" << std::endl;
-	    libmesh_error();
-	  }
-      }
-      // by default throw an error
-    default:
-      libMesh::err << "ERROR: Unsupported polynomial order!" << std::endl;
-      libmesh_error();
-    }
-
-  libmesh_error();
+  libmesh_not_implemented();
   return 0.;
 }
 
