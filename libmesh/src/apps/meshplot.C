836ddf377a30dd2cdcd28d2f433dae41af5fd150
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-10-02 23:11:56 +0000

    This could probably be merged with meshtool, but I don't have the time to dust the cobwebs off meshtool.C right now

---
+++ libmesh/src/apps/meshplot.C
@@ -0,0 +1,65 @@
+// Open the mesh and solution file named in command line arguments,
+// and plot them out to the given filename.
+
+#include "libmesh.h"
+
+#include "mesh.h"
+#include "equation_systems.h"
+
+#include "exodusII_io.h"
+#include "gmsh_io.h"
+#include "gmv_io.h"
+#include "gnuplot_io.h"
+#include "medit_io.h"
+#include "nemesis_io.h"
+#include "tecplot_io.h"
+#include "vtk_io.h"
+
+int main(int argc, char** argv)
+{
+  LibMeshInit init(argc, argv);
+
+  Mesh mesh;
+  EquationSystems es(mesh);
+
+  std::cout << "Usage: " << argv[0]
+            << " inputmesh inputsolution outputplot" << std::endl;
+
+  START_LOG("mesh.read()", "main");
+  mesh.read(argv[1]);
+  std::cout << "Loaded mesh " << argv[1] << std::endl;
+  STOP_LOG("mesh.read()", "main");
+
+  START_LOG("es.read()", "main");
+  std::string solnname = argv[2];
+
+  es.read(argv[2],
+          EquationSystems::READ_HEADER |
+          EquationSystems::READ_DATA |
+          EquationSystems::READ_ADDITIONAL_DATA);
+  std::cout << "Loaded solution " << argv[2] << std::endl;
+  STOP_LOG("es.read()", "main");
+
+  START_LOG("write_equation_systems()", "main");
+  std::string outputname(argv[3]);
+
+  if (outputname.find(".gnuplot") != std::string::npos)
+    GnuPlotIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".gmv") != std::string::npos)
+    GMVIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".mesh") != std::string::npos)
+    MEDITIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".msh") != std::string::npos)
+    GmshIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".plt") != std::string::npos)
+    TecplotIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".vtu") != std::string::npos)
+    VTKIO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".e") != std::string::npos)
+    ExodusII_IO(mesh).write_equation_systems (outputname, es);
+  else if (outputname.find(".n") != std::string::npos)
+    Nemesis_IO(mesh).write_equation_systems (outputname, es);
+    
+  STOP_LOG("write_equation_systems()", "main");
+  std::cout << "Wrote output " << argv[3] << std::endl;
+}
