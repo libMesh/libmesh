f96e2ce5f6b06c0b954e75871c92e5af17aeb57f
John W. Peterson <jwpeterson@users.sourceforge.net>
2012-10-04 15:11:45 +0000

    More documentation fixes while we're at it.

--- libmesh/include/numerics/petsc_matrix.h
+++ libmesh/include/numerics/petsc_matrix.h
@@ -251,13 +251,9 @@ public:
   /**
    * Return the value of the entry
    * \p (i,j).  This may be an
-   * expensive operation and you
-   * should always take care where
-   * to call this function.  In
-   * order to avoid abuse, this
-   * function throws an exception
-   * if the required element does
-   * not exist in the matrix.
+   * expensive operation, and you
+   * should always be careful where
+   * you call this function.
    */
   T operator () (const unsigned int i,
 		 const unsigned int j) const;
