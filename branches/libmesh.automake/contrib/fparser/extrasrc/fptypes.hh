3ff8c91a0f8020bcbaa78c8fb6dcabb374df90a9
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2012-10-08 21:36:48 +0000

    merges from trunk
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@6170 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- branches/libmesh.automake/contrib/fparser/extrasrc/fptypes.hh
+++ branches/libmesh.automake/contrib/fparser/extrasrc/fptypes.hh
@@ -67,6 +67,7 @@ namespace FUNCTIONPARSERTYPES
         cLog2by, /* log2by(x,y) = log2(x) * y */
         cNop,    /* Used by fpoptimizer internally; should not occur in bytecode */
 #endif
+
         cSinCos,   /* sin(x) followed by cos(x) (two values are pushed to stack) */
         cSinhCosh, /* hyperbolic equivalent of sincos */
         cAbsAnd,    /* As cAnd,       but assume both operands are absolute values */
