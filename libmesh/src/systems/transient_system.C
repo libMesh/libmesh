e01741054bd05c0390fa76d76147ecdfa3cceb61
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-10-09 04:03:17 +0000

    Fix TransientSystem vectors to be consistent in basic_system_only case

--- libmesh/src/systems/transient_system.C
+++ libmesh/src/systems/transient_system.C
@@ -41,12 +41,21 @@ TransientSystem<Base>::TransientSystem (EquationSystems& es,
 
   Base                 (es, name, number)
 {
+#ifdef LIBMESH_ENABLE_GHOSTED
   old_local_solution =
     AutoPtr<NumericVector<Number> >
-      (&(this->add_vector("_transient_old_local_solution")));
+      (&(this->add_vector("_transient_old_local_solution", true, GHOSTED)));
   older_local_solution =
     AutoPtr<NumericVector<Number> >
-      (&(this->add_vector("_transient_older_local_solution")));
+      (&(this->add_vector("_transient_older_local_solution", true, GHOSTED)));
+#else
+  old_local_solution =
+    AutoPtr<NumericVector<Number> >
+      (&(this->add_vector("_transient_old_local_solution", true, SERIAL)));
+  older_local_solution =
+    AutoPtr<NumericVector<Number> >
+      (&(this->add_vector("_transient_older_local_solution", true, SERIAL)));
+#endif
 }
 
 
