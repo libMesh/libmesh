edf8b8874eaf59bd45b67e491bd9caab94df4e22
Roy H. Stogner <roystgnr@users.sourceforge.net>
2012-10-10 22:23:58 +0000

    Adding another adjoints example from Vikram, this one testing out the AdjointRefinementErrorEstimator
    There's still a bug in the example code that's hurting adaptive
    performance; he's working on it.

---
+++ libmesh/examples/adjoints/adjoints_ex4/element_postprocess.C
@@ -0,0 +1,62 @@
+// General libMesh includes
+#include "libmesh/libmesh_common.h"
+#include "libmesh/elem.h"
+#include "libmesh/fe_base.h"
+#include "libmesh/fem_context.h"
+#include "libmesh/point.h"
+#include "libmesh/quadrature.h"
+
+// Local includes
+#include "L-shaped.h"
+
+// Bring in everything from the libMesh namespace
+using namespace libMesh;
+
+// Last modified: Feb 13, 2009
+
+// Define the postprocess function to compute QoI 0, the integral of the the solution 
+// over a subdomain
+
+void LaplaceSystem::element_postprocess (DiffContext &context)
+
+{  
+  FEMContext &c = libmesh_cast_ref<FEMContext&>(context);
+            
+  // Element Jacobian * quadrature weights for interior integration
+  const std::vector<Real> &JxW = c.element_fe_var[0]->get_JxW();
+
+  const std::vector<Point> &xyz = c.element_fe_var[0]->get_xyz();
+
+  // The number of local degrees of freedom in each variable
+
+  unsigned int n_qpoints = (c.get_element_qrule())->n_points();
+
+  // The function R = int_{omega} T dR 
+  // omega is a subset of Omega (the whole domain), omega = [0.75, 1.0] x [0.0, 0.25]
+
+  Number dQoI_0 = 0.;
+  
+  // Loop over quadrature points  
+  
+  for (unsigned int qp = 0; qp != n_qpoints; qp++)    
+    {
+      // Get co-ordinate locations of the current quadrature point
+      const Real x = xyz[qp](0);
+      const Real y = xyz[qp](1);
+
+      // If in the sub-domain omega, add the contribution to the integral R
+      if(fabs(x - 0.875) <= 0.125 && fabs(y - 0.125) <= 0.125)
+  	{      
+  	  // Get the solution value at the quadrature point
+  	  Number T = c.interior_value(0, qp);
+
+  	  // Update the elemental increment dR for each qp
+  	  dQoI_0 += JxW[qp] * T;
+  	}
+    }
+
+  // Update the computed value of the global functional R, by adding the contribution from this element
+
+  computed_QoI[0] = computed_QoI[0] + dQoI_0;    
+  
+}
