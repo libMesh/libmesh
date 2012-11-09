64ec7b2d261fb18a389b0e0c9cb942450bd0cf69
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2012-11-09 14:32:20 +0000

    The reduced basis IO needs to pass the version to read

--- libmesh/src/reduced_basis/rb_evaluation.C
+++ libmesh/src/reduced_basis/rb_evaluation.C
@@ -21,6 +21,7 @@
 #include "libmesh/rb_evaluation.h"
 
 // libMesh includes
+#include "libmesh/libmesh_version.h"
 #include "libmesh/system.h"
 #include "libmesh/numeric_vector.h"
 #include "libmesh/parallel.h"
@@ -882,6 +883,11 @@ void RBEvaluation::write_out_basis_functions(System& sys,
     Xdr bf_data(file_name.str(),
                 write_binary_basis_functions ? ENCODE : WRITE);
 
+    // set the current version
+    bf_data.set_version(LIBMESH_VERSION(LIBMESH_MAJOR_VERSION,
+					LIBMESH_MINOR_VERSION,
+					LIBMESH_MICRO_VERSION));
+    
     sys.write_serialized_data(bf_data, false);
 
     // Synchronize before moving on
@@ -957,6 +963,9 @@ void RBEvaluation::read_in_basis_functions(System& sys,
     Xdr bf_data(file_name.str(),
                 read_binary_basis_functions ? DECODE : READ);
 
+    // The bf_data needs to know which version to read.
+    bf_data.set_version(LIBMESH_VERSION(ver_major, ver_minor, ver_patch));
+    
     sys.read_serialized_data(bf_data, false);
 
     basis_functions[i] = NumericVector<Number>::build().release();
@@ -978,9 +987,9 @@ void RBEvaluation::read_in_basis_functions(System& sys,
 std::string RBEvaluation::get_io_version_string()
 {
 #ifdef LIBMESH_ENABLE_INFINITE_ELEMENTS
-  return "libMesh-0.7.2 with infinite elements";
+  return "libMesh-0.7.4 with infinite elements";
 #else
-  return "libMesh-0.7.2";
+  return "libMesh-0.7.4";
 #endif
 }
 
