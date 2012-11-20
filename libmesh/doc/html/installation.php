804098e18ebfbf369517c803a123d9a80bb81a1b
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2012-11-20 16:26:37 +0000

    updating for new build system

--- libmesh/doc/html/installation.php
+++ libmesh/doc/html/installation.php
@@ -54,46 +54,43 @@ if you want to build some of the contributed packages that add functionality
 to the library.
 
 <p>
-The library is currently tested with the GNU GCC 4.x and 3.x
-compilers, as well as Intel ICC 11.x, 10.x and 9.x.
+The library is continuously tested with the following compilers:
 
-<p>
-Previous library versions were also tested with:
 <br>
 <ul>
   <li>GNU GCC</li>
     <ul>
-      <li><code>gcc</code> 2.95.3</li>
-      <li><code>gcc</code> 2.96 (RedHat's compiler in the 7.x series)</li>
+      <li><code>gcc</code> 4.2</li>
+      <li><code>gcc</code> 4.4</li>
+      <li><code>gcc</code> 4.5</li>
+      <li><code>gcc</code> 4.6</li>
+      <li><code>gcc</code> 4.7</li>
     </ul>
-  <li>Intel ICC/ECC</li>
+  <li>Intel ICC</li>
     <ul>
-      <li><code>icc/ifort</code> 8.x</li>
-      <li><code>icc/ifc</code> 7.x</li>
-      <li><code>icc/ifc</code> 6.0</li>
-      <li>Earlier versions (&lt;= 5.0) not supported</li>
+      <li><code>icc</code> 11.1</li>
+      <li><code>icc</code> 12.0</li>
+      <li><code>icc</code> 12.1</li>
+      <li><code>icc</code> 13.0</li>
     </ul>
-  <li>SGI Altix</li>
+  <li>Clang</li>
     <ul>
-      <li><code>icc/ifort</code> 9.x, 8.x</li>
-      <li><code>ecc/efc</code> 7.1</li>
-      <li><code>MPT MPI</code> 1.9.1</li>
-    </ul>
-  <li>SGI IRIX 6.5 with MIPSPro Compilers</li>
+      <li><code>clang++</code> 2.9</li>
+      <li><code>clang++</code> 3.0</li>
+      <li><code>clang++</code> 3.1</li>
+      <li><code>clang++</code> 3.2</li>
+    </ul>     
+  <li>Sun Studio/Oracle</li>
     <ul>
-      <li><code>CC/cc</code> version 7.4.2</li>
+      <li><code>CC</code> 12.3</li>
+      <tt>  $ ./configure --disable-fparser CXXFLAGS=-library=stlport4 --disable-unordered-containers</tt>
     </ul>
-  <li>Sun ONE Studio Compilers</li>
+  <li> Portland Group</li>
     <ul>
-      <li>Version 5.5 tested <code>(CXXFLAGS=-library=stlport4 CPPFLAGS=-library=stlport4 CXX=CC CC=cc ./configure [...])</code></li>
-      <li>Must use the STLPort C++ standard library provided with the compiler</li>
+      <li><code>pgi</code> 11.7</li>
+      <li><code>pgi</code> 12.9</li>
+      <tt>  $ ./configure --disable-unordered-containers --disable-fparser --enable-static --disable-shared --disable-tecplot --enable-tecio</tt>
     </ul>
-  <li>HP, use <code>CXX="aCC -AA" CC="cc -Aa"</code> to get full std support</li>
-    <ul>
-      <li><code>aCC</code> A.03.37</li>
-    </ul>
-  <li>IBM <code>xlC</code> version 5.0, 6.0</li>
-  <li>HP/Compaq/DEC <code>cxx</code> 6.3.9.6</li>
 </ul>
 
 
@@ -118,19 +115,22 @@ in the top-level project directory. To see all the configuration options type
 </div>
 
 <br>
-The configure script will find your compilers and create the <code>Make.common</code>
-file with the configuration for your site. If you want to use different compilers than
-those found by configure you can specify them in environment variables. For example,
-the following will build with the <code>MIPS</code> compilers on an SGI:
+The configure script will find your compilers and
+create <code>Makefile</code>s with the configuration specific for your
+site. If you want to use different compilers than those found by
+configure you can specify them in environment variables. For example,
+the following will build with the macports <code>Clang</code>
+compilers, and also specifies nonstandard search paths for a number of
+optional libraries:
 
 <br>
 <div class="fragment">
-<pre>CXX=CC CC=cc F77=f77 CXXFLAGS=-LANG:std ./configure</pre>
+<pre>./configure --prefix=/tmp/foo --with-glpk-include=/opt/local/include --with-glpk-lib=/opt/local/lib --with-vtk-include=/opt/local/include/vtk-5.10 --with-vtk-lib=/opt/local/lib/vtk-5.10 --with-eigen-include=/opt/local/include/eigen3 --with-cxx=clang++-mp-3.2 --with-cc=clang-mp-3.2 --disable-fortran
 </div>
 
 <br>
-Note that the FORTRAN compiler is not actually used to compile any part of the library,
-but <code>configure</code> uses it to find out how to link FORTRAN libraries with C++ code.
+Note that the Fortran compiler is not actually used to compile any part of the library,
+but <code>configure</code> uses it to find out how to link Fortran libraries with C++ code, and it is possible to compile <code>libMesh</code> without a Fortran compiler.
 
 
 <a name="build"></a><h2>Building the Library</h2>
@@ -159,15 +159,20 @@ Once the library is configured you can build it simply by typing
 directory. From the top-level directory you can build and run the example programs 
 by typing
 <div class="fragment">
-<pre>make run_examples</pre>
+<pre>make check</pre>
 </div>
 
 <br>
-Note that the example programs all create output in the <code>GMV</code> format,
-since you can <a href="http://laws.lanl.gov/XCM/gmv/GMVHome.html">download GMV</a>
-for free from Los Alamos National Lab. It is a simple matter to change the source
-in the example to write a different format, just replace the <code>write_gmv</code>
-function call with whatever you like.
+<!-- Note that the example programs all create output in the <code>GMV</code> format, -->
+<!-- since you can <a href="http://laws.lanl.gov/XCM/gmv/GMVHome.html">download GMV</a> -->
+<!-- for free from Los Alamos National Lab. It is a simple matter to change the source -->
+<!-- in the example to write a different format, just replace the <code>write_gmv</code> -->
+<!-- function call with whatever you like. -->
+
+Note that many of the the example programs create output in the <code>ExodusII</code> format,
+since you can <a href="http://www.paraview.org">download Paraview</a>
+for free, and it is a highly capable postprocessing tool. It is a simple matter to change the source
+in the example to write a different formats, however.
 
 
 <h3>Unit Tests</h3>
@@ -194,7 +199,7 @@ This script may be used to determine the relevant compilation and linking flags
 used by <code>libMesh</code>. For example, you could build the application <code>foo</code> from
 <code>foo.C</code> like this:
 <div class="fragment">
-<pre>c++ -o foo foo.C `libmesh-config --cxxflags --include --ldflags`</pre>
+<pre> `libmesh-config --cxx` -o foo foo.C `libmesh-config --cxxflags --include --ldflags`</pre>
 </div>
 
 
