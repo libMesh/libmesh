46457cd2d355f156425c376d5aa3d0f004876f96
Benjamin Kirk <benjamin.kirk-1@nasa.gov>
2012-11-21 23:37:23 +0000

    Install buildable examples by copying
    contrib/utils/Makefile & run.sh
    
    into each example directory.  Need to refactor
    the Makefile not to include Make.common, unless we want
    to continue to encourage that approach.
    
    
    
    git-svn-id: file:///Users/petejw/Documents/libmesh_svn_bak@6433 434f946d-2f3d-0410-ba4c-cb9f52fb0dbf

--- trunk/libmesh/examples/run_common.sh
+++ trunk/libmesh/examples/run_common.sh
@@ -2,36 +2,38 @@
 
 # Write pretty status message
 message_running() {
+
+    executable=$1
+    if (test ! -x $executable -a -x example-$METHOD); then
+	executable=example-$METHOD
+    fi
+
     echo "***************************************************************"
-    echo "* Running Example " $LIBMESH_RUN $1 $2 $LIBMESH_OPTIONS
+    echo "* Running Example " $LIBMESH_RUN $executable $2 $LIBMESH_OPTIONS
     echo "***************************************************************"
     echo " "
 }
 
 # Write pretty status message
 message_done_running() {
+
+    executable=$1
+    if (test ! -x $executable -a -x example-$METHOD); then
+	executable=example-$METHOD
+    fi
+
     echo " "
     echo "***************************************************************"
-    echo "* Done Running Example " $LIBMESH_RUN $1 $2 $LIBMESH_OPTIONS
+    echo "* Done Running Example " $LIBMESH_RUN $executable $2 $LIBMESH_OPTIONS
     echo "***************************************************************"
 }
 
 run_example() {
-    $LIBMESH_RUN ./$1 $2 $LIBMESH_OPTIONS || exit 1
+
+    executable=$1
+    if (test ! -x $executable -a -x example-$METHOD); then
+	executable=example-$METHOD
+    fi
+
+    $LIBMESH_RUN ./$executable $2 $LIBMESH_OPTIONS || exit 1
 }
-
-# link_if_needed() {
-#     if (test -f `basename $1`); then
-# 	exit 0
-#     fi
-#     echo "--> linking $1 for setup"
-#     ln -s $1 .
-# }
-
-# discard_link() {
-#     file=`basename $1`
-#     if (test -L $file); then
-# 	echo "--> unlinking $file for cleanup"
-# 	rm $file
-#     fi
-# }
