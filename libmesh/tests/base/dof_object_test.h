9535e6732e4b8beefd96dbeb240b0f5d4f14deae
Benjamin S. Kirk <benkirk@users.sourceforge.net>
2012-12-10 01:03:16 +0000

    VariableGroup unit test

--- libmesh/tests/base/dof_object_test.h
+++ libmesh/tests/base/dof_object_test.h
@@ -10,7 +10,9 @@
   CPPUNIT_TEST( testInvalidateId ); \
   CPPUNIT_TEST( testSetProcId ); \
   CPPUNIT_TEST( testValidProcId ); \
-  CPPUNIT_TEST( testInvalidateProcId );
+  CPPUNIT_TEST( testInvalidateProcId ); \
+  CPPUNIT_TEST( testSetNSystems ); \
+  CPPUNIT_TEST( testSetNVariableGroups );
 
 using namespace libMesh;
 
@@ -82,6 +84,40 @@ public:
     aobject.invalidate_processor_id();
 
     CPPUNIT_ASSERT( !aobject.valid_processor_id() );
+  }
+
+  void testSetNSystems()
+  {
+    DofObject aobject(*instance);    
+    
+    aobject.set_n_systems (10);
+    
+    CPPUNIT_ASSERT_EQUAL( (unsigned int) 10, aobject.n_systems() );
+  }
+
+  void testSetNVariableGroups()
+  {
+    DofObject aobject(*instance);    
+    
+    aobject.set_n_systems (2);
+    
+    std::vector<unsigned int> nvpg;
+    
+    nvpg.push_back(10);
+    nvpg.push_back(20);
+    nvpg.push_back(30);
+
+    aobject.set_n_vars_per_group (0, nvpg);
+    aobject.set_n_vars_per_group (1, nvpg);
+    
+    for (unsigned int s=0; s<2; s++)
+      {
+	CPPUNIT_ASSERT_EQUAL( (unsigned int) 60, aobject.n_vars(s) );
+	CPPUNIT_ASSERT_EQUAL( (unsigned int) 3,  aobject.n_var_groups(s) );
+
+	for (unsigned int vg=0; vg<3; vg++)
+	  CPPUNIT_ASSERT_EQUAL( nvpg[vg], aobject.n_vars(s,vg) );
+      }
   }
 };
 
