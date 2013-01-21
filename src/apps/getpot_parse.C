9da7de409e3fcca466bea8d9ff6ccd040b4231c5
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-01-20 18:14:28 -0600

    Add getpot_parse utility for debugging purposes

---
+++ src/apps/getpot_parse.C
@@ -0,0 +1,36 @@
+// Open the getpot input file given by the input file name; write out
+// all GetPot object data to the output file name
+
+#include "libmesh/libmesh_config.h"
+#include "libmesh/getpot.h"
+
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+  using namespace libMesh;
+
+  if (argc < 2)
+    {
+      std::cerr << "Usage: " << argv[0]
+                << " inputconfigfile [outputconfigfile]" << std::endl;
+
+      libmesh_error();
+    }
+
+  GetPot gp(argv[1]);
+
+  std::ostream *my_out;
+  std::ofstream fout;
+  fout.exceptions ( std::ofstream::failbit | std::ofstream::badbit );
+  
+  if (argc < 3)
+    my_out = &std::cout;
+  else
+    {
+      fout.open(argv[2]);
+      my_out = &fout;
+    }
+
+  gp.print("", *my_out, 1);
+}
