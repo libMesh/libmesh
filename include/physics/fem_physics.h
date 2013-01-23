0a988cc16e0565d137c41b98b3ec818994e277c8
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-01-23 16:38:22 -0600

    Moving mesh stuff moved to DiffPhysics
    We still need FEMPhysics for the eulerian_residual default
    implementation, but there's nothing about moving meshes in general
    that's FEM specific, so let's move the moving mesh variables from
    FEMPhysics down to DiffPhysics.
    
    We should do the same with FEMContext->DiffContext, for the same
    aesthetic reasons, but I'm doing this move first since it also rids us
    of an otherwise unavoidable dynamic_cast and thus an RTTI dependency.

--- include/physics/fem_physics.h
+++ include/physics/fem_physics.h
@@ -54,100 +54,12 @@ public:
    */
   FEMPhysics () :
     DifferentiablePhysics(),
-    _mesh_sys                         (NULL),
-    _mesh_x_var                       (libMesh::invalid_uint),
-    _mesh_y_var                       (libMesh::invalid_uint),
-    _mesh_z_var                       (libMesh::invalid_uint)
     {}
 
   /**
    * Destructor.
    */
   virtual ~FEMPhysics () {}
-
-  /**
-   * Tells the FEMPhysics that system \p sys contains the
-   * isoparametric Lagrangian variables which correspond to the
-   * coordinates of mesh nodes, in problems where the mesh itself is
-   * expected to move in time.
-   *
-   * The system with mesh coordinate data (which may be \p this system
-   * itself, for fully coupled moving mesh problems) is currently
-   * assumed to have new (end of time step) mesh coordinates stored in
-   * solution, old (beginning of time step) mesh coordinates stored in
-   * _old_nonlinear_solution, and constant velocity motion during each
-   * time step.
-   *
-   * Activating this function ensures that local (but not neighbor!) element
-   * geometry is correctly repositioned when evaluating element residuals.
-   *
-   * Currently \p sys must be \p *this for a tightly coupled moving
-   * mesh problem or NULL to stop mesh movement; loosely coupled
-   * moving mesh problems are not implemented.
-   *
-   * This code is experimental.  "Trust but verify, and not in that
-   * order"
-   */
-  virtual void set_mesh_system(System* sys);
-
-  /**
-   * Returns a const reference to the system with variables corresponding to
-   * mesh nodal coordinates, or NULL if the mesh is fixed.
-   * Useful for ALE calculations.
-   */
-  const System* get_mesh_system() const;
-
-  /**
-   * Returns a reference to the system with variables corresponding to
-   * mesh nodal coordinates, or NULL if the mesh is fixed.
-   */
-  System* get_mesh_system();
-
-  /**
-   * Tells the FEMPhysics that variable \p var from the mesh system
-   * should be used to update the x coordinate of mesh nodes, in problems where
-   * the mesh itself is expected to move in time.
-   *
-   * The system with mesh coordinate data (which may be this system itself, for
-   * fully coupled moving mesh problems) is currently assumed to have new (end
-   * of time step) mesh coordinates stored in solution, old (beginning of time
-   * step) mesh coordinates stored in _old_nonlinear_solution, and constant
-   * velocity motion during each time step.
-   *
-   * Activating this function ensures that local (but not neighbor!) element
-   * geometry is correctly repositioned when evaluating element residuals.
-   */
-  virtual void set_mesh_x_var(unsigned int var);
-
-  /**
-   * Returns the variable number corresponding to the
-   * mesh x coordinate. Useful for ALE calculations.
-   */
-  unsigned int get_mesh_x_var() const;
-
-  /**
-   * Tells the FEMPhysics that variable \p var from the mesh system
-   * should be used to update the y coordinate of mesh nodes.
-   */
-  virtual void set_mesh_y_var(unsigned int var);
-
-  /**
-   * Returns the variable number corresponding to the
-   * mesh y coordinate. Useful for ALE calculations.
-   */
-  unsigned int get_mesh_y_var() const;
-
-  /**
-   * Tells the FEMPhysics that variable \p var from the mesh system
-   * should be used to update the z coordinate of mesh nodes.
-   */
-  virtual void set_mesh_z_var(unsigned int var);
-
-  /**
-   * Returns the variable number corresponding to the
-   * mesh z coordinate. Useful for ALE calculations.
-   */
-  unsigned int get_mesh_z_var() const;
 
   /**
    * Adds a pseudo-convection contribution on \p elem to
@@ -180,99 +92,7 @@ public:
    */
   virtual bool mass_residual (bool request_jacobian,
                               DiffContext &);
-
-protected:
-
-  /**
-   * System from which to acquire moving mesh information
-   */
-  System *_mesh_sys;
-
-  /**
-   * Variables from which to acquire moving mesh information
-   */
-  unsigned int _mesh_x_var, _mesh_y_var, _mesh_z_var;
 };
-
-
-
-// ------------------------------------------------------------
-// FEMPhysics inline methods
-
-
-inline
-void FEMPhysics::set_mesh_system(System* sys)
-{
-  // For now we assume that we're doing fully coupled mesh motion
-//  if (sys && sys != this)
-//    libmesh_not_implemented();
-
-  // For the foreseeable future we'll assume that we keep these
-  // Systems in the same EquationSystems
-  // libmesh_assert_equal_to (&this->get_equation_systems(),
-  //                          &sys->get_equation_systems());
-
-  // And for the immediate future this code may not even work
-  libmesh_experimental();
-
-  _mesh_sys = sys;
-}
-
-
-
-inline
-void FEMPhysics::set_mesh_x_var (unsigned int var)
-{
-  _mesh_x_var = var;
-}
-
-
-
-inline
-void FEMPhysics::set_mesh_y_var (unsigned int var)
-{
-  _mesh_y_var = var;
-}
-
-
-
-inline
-void FEMPhysics::set_mesh_z_var (unsigned int var)
-{
-  _mesh_z_var = var;
-}
-
-
-
-inline
-const System* FEMPhysics::get_mesh_system() const
-{
-  return _mesh_sys;
-}
-
-inline
-System* FEMPhysics::get_mesh_system()
-{
-  return _mesh_sys;
-}
-
-inline
-unsigned int FEMPhysics::get_mesh_x_var() const
-{
-  return _mesh_x_var;
-}
-
-inline
-unsigned int FEMPhysics::get_mesh_y_var() const
-{
-  return _mesh_y_var;
-}
-
-inline
-unsigned int FEMPhysics::get_mesh_z_var() const
-{
-  return _mesh_z_var;
-}
 
 
 
