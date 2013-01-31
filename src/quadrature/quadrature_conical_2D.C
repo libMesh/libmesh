157bee7b83f280dbbf810774465b54ae40a562f2
John Peterson <jwpeterson@gmail.com>
2013-01-31 15:47:44 -0700

    Changes in quadrature_conical_2D.C for -Wshadow.
    Issue #24.

--- src/quadrature/quadrature_conical_2D.C
+++ src/quadrature/quadrature_conical_2D.C
@@ -24,10 +24,10 @@ namespace libMesh
 
 
 
-void QConical::init_2D(const ElemType _type,
+void QConical::init_2D(const ElemType type_in,
 		       unsigned int p)
 {
-  switch (_type)
+  switch (type_in)
     {
     case TRI3:
     case TRI6:
@@ -43,10 +43,10 @@ void QConical::init_2D(const ElemType _type,
       // Unsupported element type
     default:
       {
-	libMesh::err << "ERROR: Unsupported element type: " << _type << std::endl;
+	libMesh::err << "ERROR: Unsupported element type: " << type_in << std::endl;
 	libmesh_error();
       }
-    } // end switch (_type)
+    } // end switch (type_in)
 
   // We must have returned or errored-out by this point.  If not,
   // throw an error now.
