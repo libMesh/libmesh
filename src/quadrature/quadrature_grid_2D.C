d16d7463e6101cd61b341800e8f005ad73d06cf4
John Peterson <jwpeterson@gmail.com>
2013-01-31 16:13:10 -0700

    Changes in quadrature_grid_2D.C for -Wshadow.
    Issue #24.

--- src/quadrature/quadrature_grid_2D.C
+++ src/quadrature/quadrature_grid_2D.C
@@ -24,7 +24,7 @@ namespace libMesh
 {
 
 
-void QGrid::init_2D(const ElemType _type,
+void QGrid::init_2D(const ElemType type_in,
                     unsigned int)
 {
 #if LIBMESH_DIM > 1
@@ -34,7 +34,7 @@ void QGrid::init_2D(const ElemType _type,
 
   // We ignore p - the grid rule is just for experimentation
 
-  switch (_type)
+  switch (type_in)
     {
 
 
@@ -80,7 +80,7 @@ void QGrid::init_2D(const ElemType _type,
       // Unsupported type
     default:
       {
-	libMesh::err << "Element type not supported!:" << _type << std::endl;
+	libMesh::err << "Element type not supported!:" << type_in << std::endl;
 	libmesh_error();
       }
     }
