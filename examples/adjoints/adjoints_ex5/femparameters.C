c6e16ca4c4d5f281a0c40830dc92755ca6ec2616
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2013-02-03 08:54:14 -0600

    copyright info, fix for barebones build

--- examples/adjoints/adjoints_ex5/femparameters.C
+++ examples/adjoints/adjoints_ex5/femparameters.C
@@ -1,3 +1,21 @@
+/* The libMesh Finite Element Library. */
+/* Copyright (C) 2002-2013 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner */
+
+/* This library is free software; you can redistribute it and/or */
+/* modify it under the terms of the GNU Lesser General Public */
+/* License as published by the Free Software Foundation; either */
+/* version 2.1 of the License, or (at your option) any later version. */
+
+/* This library is distributed in the hope that it will be useful, */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU */
+/* Lesser General Public License for more details. */
+
+/* You should have received a copy of the GNU Lesser General Public */
+/* License along with this library; if not, write to the Free Software */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
 
 #include "femparameters.h"
 
@@ -5,6 +23,7 @@
 #include "libmesh/parsed_function.h"
 #include "libmesh/zero_function.h"
 #include "libmesh/periodic_boundaries.h"
+#include "libmesh/vector_value.h" // RealVectorValue
 
 #define GETPOT_INPUT(A) { A = input(#A, A);\
   const std::string stringval = input(#A, std::string());\
