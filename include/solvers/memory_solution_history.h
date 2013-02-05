6e1c876c06d9da6974ab2f9c3e74c9beaa2c3117
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-02-05 11:27:32 -0600

    Mark MemorySolutionHistory as still experimental

--- include/solvers/memory_solution_history.h
+++ include/solvers/memory_solution_history.h
@@ -37,7 +37,8 @@ namespace libMesh
     
     // Constructor, reference to system to be passed by user, set the  
     // stored_sols iterator to some initial value
-  MemorySolutionHistory(System & system_) : stored_sols(stored_solutions.end()), _system(system_) {} ;
+  MemorySolutionHistory(System & system_) : stored_sols(stored_solutions.end()), _system(system_)
+  { libmesh_experimental(); } ;
 
     // Destructor
     ~MemorySolutionHistory();
