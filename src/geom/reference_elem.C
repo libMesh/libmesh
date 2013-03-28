03f1f4c6ad9e3fc5b39e2db36b04e7a0224fafc9
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2013-03-28 14:13:20 -0500

    Singletons for each reference element type.
    This patch implements singleton objects for each reference element type.  At presenent they are read from our "reference_elements/" files, but that does not scale.  My next attempt will be to embed those definitions automatically through static stringstream objects.
    
    This is an incrimental feature to support the following usage:
    
    Elem *phys_elem;
    
    Elem const * ref_elem = phys_elem->reference_elem();
    
    where phys_elem is a typical element and ref_elem is its reference element in parametric coordinates.

---
+++ src/geom/reference_elem.C
@@ -0,0 +1,349 @@
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2013 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+// Local includes
+#include "libmesh/node.h"
+#include "libmesh/elem.h"
+#include "libmesh/reference_elem.h"
+#include "libmesh/threads.h"
+
+// C++ includes
+#include <map>
+
+
+
+// anonymous namespace for implelemntation details
+namespace
+{
+  using namespace libMesh;
+  
+  typedef Threads::spin_mutex InitMutex;
+  
+  // Mutex for thread safety.
+  InitMutex init_mtx;
+
+  // simple flag denoting if we are intitialized
+  bool initialized = false;
+
+  // map from ElemType to reference element file system object name
+  typedef std::map<ElemType, std::string> FileMapType;
+  FileMapType ref_elem_file;
+  Elem* ref_elem_map[INVALID_ELEM];
+
+
+  
+  class SingletonCache
+  {
+  public:
+    void clear()
+    {
+      for (unsigned int e=0; e<elem_list.size(); e++)
+	if (elem_list[e])
+	  {
+	    delete elem_list[e];
+	    elem_list[e] = NULL;
+	  }
+      
+      for (unsigned int n=0; n<node_list.size(); n++)
+	if (node_list[n])
+	  {
+	    delete node_list[n];
+	    node_list[n] = NULL;
+	  }
+    }
+
+    ~SingletonCache()
+    { this->clear(); }
+      
+    std::vector<Node*> node_list;
+    std::vector<Elem*> elem_list;
+  } singleton_cache;
+
+
+
+  
+  void find_input_file (const std::string &file_name_in,
+			std::ifstream &ref_elem_stream)
+  {
+    std::cout << "--> Reading reference element from \""
+	      << file_name_in << "\"\n";
+    
+    if (ref_elem_stream.is_open())
+      ref_elem_stream.close();
+
+    std::string file_name (file_name_in);
+
+    // Try the naked filename
+    ref_elem_stream.open(file_name.c_str()); if (ref_elem_stream.good()) return;
+    
+
+    // LIBMESH_ROOT?
+    const char *LIBMESH_ROOT = std::getenv("LIBMESH_ROOT");
+    
+    if (LIBMESH_ROOT)
+      {
+	// $LIBMESH_ROOT/$filename
+	ref_elem_stream.close();
+
+	file_name  = LIBMESH_ROOT;
+	file_name += "/";
+	file_name += file_name_in;
+
+	ref_elem_stream.open(file_name.c_str()); if (ref_elem_stream.good()) return;
+
+	// $LIBMESH_ROOT/share/$filename
+	ref_elem_stream.close();
+
+	file_name  = LIBMESH_ROOT;
+	file_name += "/share/";
+	file_name += file_name_in;
+
+	ref_elem_stream.open(file_name.c_str()); if (ref_elem_stream.good()) return;	
+      }
+
+    
+    // LIBMESH_DATA?
+    const char *LIBMESH_DATA = std::getenv("LIBMESH_DATA");
+    
+    if (LIBMESH_DATA)
+      {
+	// $LIBMESH_DATA/$filename
+	ref_elem_stream.close();
+
+	file_name  = LIBMESH_DATA;
+	file_name += "/";
+	file_name += file_name_in;
+
+	ref_elem_stream.open(file_name.c_str()); if (ref_elem_stream.good()) return;
+
+	// $LIBMESH_DATA/share/$filename
+	ref_elem_stream.close();
+
+	file_name  = LIBMESH_DATA;
+	file_name += "/share/";
+	file_name += file_name_in;
+
+	ref_elem_stream.open(file_name.c_str()); if (ref_elem_stream.good()) return;	
+      }
+
+
+    libMesh::err << "ERROR reading file name: "
+		 << file_name_in
+		 << std::endl;
+    
+    libmesh_file_error(file_name_in);
+  }
+
+
+
+  Elem* read_ref_elem (std::istream &in)
+  {
+    static const unsigned int comm_len = 1024;
+    char comm[comm_len];
+    
+    // For reference:
+    // reference_elements/3D/one_hex.xda:
+    
+    //     libMesh-0.7.0+	 
+    // 1	 # number of elements
+    // 8	 # number of nodes
+    // .	 # boundary condition specification file
+    // n/a	 # subdomain id specification file
+    // n/a	 # processor id specification file
+    // n/a	 # p-level specification file
+    // 1	 # n_elem at level 0, [ type (n0 ... nN-1) ]
+    // 10 0 1 2 3 4 5 6 7 
+    // -1.00000000000000000e+00 -1.00000000000000000e+00 -1.00000000000000000e+00 
+    // 1.00000000000000000e+00 -1.00000000000000000e+00 -1.00000000000000000e+00 
+    // 1.00000000000000000e+00 1.00000000000000000e+00 -1.00000000000000000e+00 
+    // -1.00000000000000000e+00 1.00000000000000000e+00 -1.00000000000000000e+00 
+    // -1.00000000000000000e+00 -1.00000000000000000e+00 1.00000000000000000e+00 
+    // 1.00000000000000000e+00 -1.00000000000000000e+00 1.00000000000000000e+00 
+    // 1.00000000000000000e+00 1.00000000000000000e+00 1.00000000000000000e+00 
+    // -1.00000000000000000e+00 1.00000000000000000e+00 1.00000000000000000e+00 
+    // 6	 # number of boundary conditions
+    // 0 0 1 
+    // 0 1 2 
+    // 0 2 3 
+    // 0 3 4 
+    // 0 4 5 
+    // 0 5 6
+
+    std::string foo;
+    unsigned int n_elem, n_nodes, elem_type, nn;
+    double x, y, z;
+    
+    in >> foo;
+    in >> n_elem;  /**/ in.getline (comm, comm_len); libmesh_assert_equal_to (n_elem, 1);
+    in >> n_nodes; /**/ in.getline (comm, comm_len);
+    in >> foo;     /**/ in.getline (comm, comm_len);
+    in >> foo;     /**/ in.getline (comm, comm_len);
+    in >> foo;     /**/ in.getline (comm, comm_len);
+    in >> foo;     /**/ in.getline (comm, comm_len);
+    in >> n_elem;  /**/ in.getline (comm, comm_len); libmesh_assert_equal_to (n_elem, 1);
+
+    in >> elem_type;
+
+    libmesh_assert_less (elem_type, INVALID_ELEM);
+    libmesh_assert_equal_to (n_nodes, Elem::type_to_n_nodes_map[elem_type]);
+    
+    // Construct the elem
+    Elem *elem = Elem::build(static_cast<ElemType>(elem_type)).release();
+    
+    // We are expecing an identity map, so assert it!
+    for (unsigned int n=0; n<n_nodes; n++)
+      {
+	in >> nn;
+	libmesh_assert_equal_to (n,nn);
+      }
+    
+    for (unsigned int n=0; n<n_nodes; n++)
+      {
+	in >> x >> y >> z;
+
+	Node *node = new Node(x,y,z,n);
+	singleton_cache.node_list.push_back(node);
+
+	elem->set_node(n) = node;
+      }
+
+
+    // it is entirely possible we ran out of file or encountered
+    // another error.  If so, cleanly abort.
+    if (!in)
+      {
+	delete elem;
+	elem = NULL;
+	libMesh::err << "ERROR while creating element singleton!\n";
+	libmesh_error();
+      }
+
+    else
+      singleton_cache.elem_list.push_back (elem);
+
+    return elem;
+  }
+
+  
+  
+  void read_ref_elem (const ElemType Type,
+		      const std::string &fname)
+  {
+    std::ifstream in;
+
+    find_input_file(fname, in); 
+				      
+    if (!in.good())
+      libmesh_file_error("error reading reference element file!");
+
+    libmesh_assert_less (Type, INVALID_ELEM);
+
+    ref_elem_map[Type] = read_ref_elem(in);
+  }
+
+
+  
+  void init_ref_elem_table()
+  {
+    // ouside mutex - if this flag is set, we can
+    // trust it.
+    if (initialized) return;
+    
+    // playing with fire here - lock before touching shared
+    // data structures
+    InitMutex::scoped_lock lock(init_mtx);
+
+    // inside mutex - flag may have changed while waiting
+    // for the lock to acquire, check it again.
+    if (initialized) return;
+
+    // OK, if we get here we have the lock and we are not
+    // initialized.  populate singletons.
+    singleton_cache.clear();
+    
+    // initialize the reference file table
+    {
+      ref_elem_file.clear();
+      
+      // // 1D elements
+      // ref_elem_file[EDGE2]   = "not_implemented";
+      // ref_elem_file[EDGE3]   = "not_implemented";
+      // ref_elem_file[EDGE4]   = "not_implemented";
+      
+      // 2D elements
+      ref_elem_file[TRI3]    = "reference_elements/2D/one_tri.xda";
+      ref_elem_file[TRI6]    = "reference_elements/2D/one_tri6.xda";
+      
+      ref_elem_file[QUAD4]   = "reference_elements/2D/one_quad.xda";
+      ref_elem_file[QUAD8]   = "reference_elements/2D/one_quad8.xda";
+      ref_elem_file[QUAD9]   = "reference_elements/2D/one_quad9.xda";
+      
+      // 3D elements
+      ref_elem_file[HEX8]    = "reference_elements/3D/one_hex.xda";
+      ref_elem_file[HEX20]   = "reference_elements/3D/one_hex20.xda";
+      ref_elem_file[HEX27]   = "reference_elements/3D/one_hex27.xda";
+      
+      ref_elem_file[TET4]    = "reference_elements/3D/one_tet.xda";
+      ref_elem_file[TET10]   = "reference_elements/3D/one_tet10.xda";
+      
+      ref_elem_file[PRISM6]  = "reference_elements/3D/one_prism.xda";
+      ref_elem_file[PRISM15] = "reference_elements/3D/one_prism15.xda";
+      ref_elem_file[PRISM18] = "reference_elements/3D/one_prism18.xda";
+    }
+
+    // Read'em
+    for (FileMapType::const_iterator it=ref_elem_file.begin();
+	 it != ref_elem_file.end(); ++it)
+      read_ref_elem(it->first,
+		    it->second);
+
+    // set the initialized flag.
+    initialized = true;
+  }
+}
+
+
+
+//----------------------------------------------------------------------------
+// external API Implementation 
+namespace libMesh
+{
+  namespace ReferenceElem
+  {
+
+
+    Elem const* get (const ElemType Type)
+    {
+      libmesh_assert_less (Type, INVALID_ELEM);
+      
+      init_ref_elem_table();
+      
+      libmesh_assert (ref_elem_map[Type] != NULL);
+
+      return ref_elem_map[Type];
+    }
+
+
+
+    void clear ()
+    {
+      singleton_cache.clear();
+    }
+
+    
+  } // namespace ReferenceElem
+} // namespace libMesh
