038ade527e591e657f74a4c6ffe51071414785e9
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2013-04-01 07:46:52 -0500

    no trailing whitespace

--- include/geom/elem_cutter.h
+++ include/geom/elem_cutter.h
@@ -38,7 +38,7 @@ class Elem;
 class SerialMesh;
 class TriangleInterface;
 class TetGenMeshInterface;
-  
+
 /**
  * This class implements cutting a single element into a collection
  * of subelements.
@@ -81,7 +81,7 @@ public:
 
   /**
    * @returns \p true if the element is cut by the interface defined
-   * implicitly by the vertex values of the signed 
+   * implicitly by the vertex values of the signed
    * \p vertex_distance_func.
    */
   bool is_cut (const Elem &elem,
@@ -102,19 +102,19 @@ public:
   /**
    * Returns a list of in general element pieces considered inside the
    * cutting surface.  These are subelements whose geometric union
-   * defines the spatial domain of the inside portion of the cut element.   
+   * defines the spatial domain of the inside portion of the cut element.
    */
   const std::vector<Elem const*> & inside_elements () const
   { return _inside_elem; }
-  
+
   /**
    * Returns a list of in general element pieces considered outside the
    * cutting surface.  These are subelements whose geometric union
-   * defines the spatial domain of the outside portion of the cut element.   
+   * defines the spatial domain of the outside portion of the cut element.
    */
   const std::vector<Elem const*> & outside_elements() const
   { return _outside_elem; }
-  
+
 protected:
 
   /**
@@ -123,23 +123,23 @@ protected:
    */
   void find_intersection_points(const Elem &elem,
 				const std::vector<Real> &vertex_distance_func);
-  
+
   /**
    * cutting algoritm in 1D.
    */
   void cut_1D();
-  
+
   /**
    * cutting algoritm in 2D.
    */
   void cut_2D(const Elem &elem,
 	      const std::vector<Real> &vertex_distance_func);
-  
+
   /**
    * cutting algoritm in 3D.
    */
   void cut_3D();
-  
+
   std::vector<Elem const*> _inside_elem;
   std::vector<Elem const*> _outside_elem;
 
@@ -161,4 +161,3 @@ protected:
 
 
 #endif // LIBMESH_ELEM_CUTTER_H
-
