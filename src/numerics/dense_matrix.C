fe0a40a7e2240979497acdc2e150fd7807a2bd20
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-07-23 17:02:44 -0500

    Add a bunch of instantiations
    This is why I'm starting to like header-only library design...

--- src/numerics/dense_matrix.C
+++ src/numerics/dense_matrix.C
@@ -1052,16 +1052,61 @@ void DenseMatrix<T>::_cholesky_back_substitute (const DenseVector<T2>& b,
 
 //--------------------------------------------------------------
 // Explicit instantiations
+#define LIBMESH_VMA_INSTANTIATE(T1,T2,T3) \
+template void DenseMatrix<T1>::vector_mult_add \
+  (DenseVector< \
+     typename CompareTypes<T2, \
+       typename CompareTypes<T2,T3>::supertype>::supertype>& dest, \
+   const T2 factor, \
+   const DenseVector<T3>& arg) const \
+
 template class DenseMatrix<Real>;
 template void DenseMatrix<Real>::cholesky_solve(const DenseVector<Real>&, DenseVector<Real>&);
 template void DenseMatrix<Real>::_cholesky_back_substitute(const DenseVector<Real>&, DenseVector<Real>&) const;
 template void DenseMatrix<Real>::cholesky_solve(const DenseVector<Complex>&, DenseVector<Complex>&);
 template void DenseMatrix<Real>::_cholesky_back_substitute(const DenseVector<Complex>&, DenseVector<Complex>&) const;
+LIBMESH_VMA_INSTANTIATE(Real,int,Real);
+#ifndef LIBMESH_DEFAULT_SINGLE_PRECISION
+LIBMESH_VMA_INSTANTIATE(Real,float,Real);
+#endif
+#ifndef LIBMESH_DEFAULT_DOUBLE_PRECISION
+LIBMESH_VMA_INSTANTIATE(Real,double,Real);
+#endif
 
 #ifdef LIBMESH_USE_COMPLEX_NUMBERS
 template class DenseMatrix<Complex>;
 template void DenseMatrix<Complex>::cholesky_solve(const DenseVector<Complex>&,DenseVector<Complex>&);
 template void DenseMatrix<Complex>::_cholesky_back_substitute(const DenseVector<Complex>&, DenseVector<Complex>&) const;
+template void DenseMatrix<Real>::vector_mult
+  (DenseVector<typename CompareTypes<Real,Complex>::supertype>& dest,
+   const DenseVector<Complex>& arg) const;
+template void DenseMatrix<Real>::vector_mult_transpose
+  (DenseVector<typename CompareTypes<Real,Complex>::supertype>& dest,
+   const DenseVector<Complex>& arg) const;
+LIBMESH_VMA_INSTANTIATE(Real,int,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,int,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,int,Real);
+LIBMESH_VMA_INSTANTIATE(Real,std::complex<int>,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<int>,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<int>,Real);
+
+LIBMESH_VMA_INSTANTIATE(Real,float,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,float,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,float,Real);
+LIBMESH_VMA_INSTANTIATE(Real,std::complex<float>,Complex);
+#ifndef LIBMESH_DEFAULT_SINGLE_PRECISION
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<float>,Complex);
+#endif
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<float>,Real);
+
+LIBMESH_VMA_INSTANTIATE(Real,double,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,double,Complex);
+LIBMESH_VMA_INSTANTIATE(Complex,double,Real);
+LIBMESH_VMA_INSTANTIATE(Real,std::complex<double>,Complex);
+#ifndef LIBMESH_DEFAULT_DOUBLE_PRECISION
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<double>,Complex);
+#endif
+LIBMESH_VMA_INSTANTIATE(Complex,std::complex<double>,Real);
 #endif
 
 } // namespace libMesh
