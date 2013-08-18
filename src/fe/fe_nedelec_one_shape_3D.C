140f39a5eb346916dfd14bf4a5996d2a01a52570
Paul T. Bauman <ptbauman@gmail.com>
2013-08-18 14:45:21 -0500

    NEDLEC_ONE linear hex second derivatives
    This function is actually implemented now.

--- src/fe/fe_nedelec_one_shape_3D.C
+++ src/fe/fe_nedelec_one_shape_3D.C
@@ -552,7 +552,6 @@ RealGradient FE<3,NEDELEC_ONE>::shape_second_deriv(const Elem* elem,
   // j = 4 ==> d^2 phi / deta dzeta
   // j = 5 ==> d^2 phi / dzeta^2
   libmesh_assert_less (j, 6);
-  libmesh_not_implemented();
 
   const Order totalorder = static_cast<Order>(order + elem->p_level());
 
@@ -578,143 +577,54 @@ RealGradient FE<3,NEDELEC_ONE>::shape_second_deriv(const Elem* elem,
 
 	      switch (j)
 		{
-		  // d()/dxi
+		  // d^2()/dxi^2
 		case 0:
 		  {
-		    switch(i)
-		      {
-		      case 0:
-		      case 2:
-                      case 8:
-                      case 10:
-			return RealGradient();
-		      case 1:
-			{
-			  if( elem->point(1) > elem->point(2) )
-			    return RealGradient( 0.0, -0.125*(1.0-zeta) );
-			  else
-			    return RealGradient( 0.0, 0.125*(1.0-zeta) );
-			}
-		      case 3:
-			{
-			  if( elem->point(3) > elem->point(0) )
-			    return RealGradient( 0.0, -0.125*(-1.0+zeta) );
-			  else
-			    return RealGradient( 0.0, 0.125*(-1.0+zeta) );
-			}
-                      case 4:
-                        {
-                          if( elem->point(0) > elem->point(4) )
-                            return RealGradient( 0.0, 0.0, -0.125*(-1.0+eta) );
-                          else
-                            return RealGradient( 0.0, 0.0,  0.125*(-1.0+eta) );
-                        }
-                      case 5:
-                        {
-                          if( elem->point(1) > elem->point(5) )
-                            return RealGradient( 0.0, 0.0, -0.125*(1.0-eta) );
-                          else
-                            return RealGradient( 0.0, 0.0,  0.125*(1.0-eta) );
-                        }
-                      case 6:
-                        {
-                          if( elem->point(2) > elem->point(6) )
-                            return RealGradient( 0.0, 0.0, -0.125*(1.0+eta) );
-                          else
-                            return RealGradient( 0.0, 0.0,  0.125*(1.0+eta) );
-                        }
-                      case 7:
-                        {
-                          if( elem->point(3) > elem->point(7) )
-                            return RealGradient( 0.0, 0.0, -0.125*(-1.0-eta) );
-                          else
-                            return RealGradient( 0.0, 0.0,  0.125*(-1.0-eta) );
-                        }
-                      case 9:
-                        {
-                          if( elem->point(5) > elem->point(6) )
-                            return RealGradient( 0.0, -0.125*(1.0+zeta), 0.0 );
-                          else
-                            return RealGradient( 0.0,  0.125*(1.0+zeta), 0.0 );
-                        }
-                      case 11:
-                        {
-                          if( elem->point(4) > elem->point(7) )
-                            return RealGradient( 0.0, -0.125*(-1.0-zeta), 0.0 );
-                          else
-                            return RealGradient( 0.0,  0.125*(-1.0-zeta), 0.0 );
-                        }
-		      default:
-			libmesh_error();
-		      } // switch(i)
+                    // All d^2()/dxi^2 derivatives for linear hexes are zero.
+                    return RealGradient();
+                  } // j=0
 
-		  } // j=0
-
-		  // d()/deta
+		  // d^2()/dxideta
 		case 1:
 		  {
 		    switch(i)
 		      {
+                      case 0:
 		      case 1:
+                      case 2:
 		      case 3:
+                      case 8:
                       case 9:
+                      case 10:
                       case 11:
 			return RealGradient();
-		      case 0:
-                        {
-                          if( elem->point(0) > elem->point(1) )
-                            return RealGradient( -0.125*(-1.0+zeta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(-1.0+zeta), 0.0, 0.0 );
-                        }
-		      case 2:
-                        {
-                          if( elem->point(2) > elem->point(3) )
-                            return RealGradient( -0.125*(1.0-zeta), 0.0, 0.0 );
-                          else
-                            return RealGradient( 0.125*(1.0-zeta), 0.0, 0.0 );
-                        }
                       case 4:
                         {
                           if( elem->point(0) > elem->point(4) )
-                            return RealGradient( 0.0, 0.0, -0.125*(-1.0+xi) );
+                            return RealGradient( 0.0, 0.0, -0.125 );
                           else
-                            return RealGradient( 0.0, 0.0,  0.125*(-1.0+xi) );
+                            return RealGradient( 0.0, 0.0,  0.125 );
                         }
                       case 5:
                         {
                           if( elem->point(1) > elem->point(5) )
-                            return RealGradient( 0.0, 0.0, -0.125*(-1.0-xi) );
+                            return RealGradient( 0.0, 0.0,  0.125 );
                           else
-                            return RealGradient( 0.0, 0.0,  0.125*(-1.0-xi) );
+                            return RealGradient( 0.0, 0.0, -0.125 );
                         }
                       case 6:
                         {
                           if( elem->point(2) > elem->point(6) )
-                            return RealGradient( 0.0, 0.0, -0.125*(1.0+xi) );
+                            return RealGradient( 0.0, 0.0, -0.125 );
                           else
-                            return RealGradient( 0.0, 0.0,  0.125*(1.0+xi) );
+                            return RealGradient( 0.0, 0.0,  0.125 );
                         }
                       case 7:
                         {
                           if( elem->point(3) > elem->point(7) )
-                            return RealGradient( 0.0, 0.0, -0.125*(1.0-xi) );
+                            return RealGradient( 0.0, 0.0,  0.125 );
                           else
-                            return RealGradient( 0.0, 0.0,  0.125*(1.0-xi) );
-                        }
-                      case 8:
-                        {
-                          if( elem->point(4) > elem->point(5) )
-                            return RealGradient( -0.125*(-1.0-zeta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(-1.0-zeta), 0.0, 0.0 );
-                        }
-                      case 10:
-                        {
-                          if( elem->point(7) > elem->point(6) )
-                            return RealGradient( -0.125*(1.0+zeta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(1.0+zeta), 0.0, 0.0 );
+                            return RealGradient( 0.0, 0.0, -0.125 );
                         }
 		      default:
 			libmesh_error();
@@ -722,78 +632,117 @@ RealGradient FE<3,NEDELEC_ONE>::shape_second_deriv(const Elem* elem,
 
 		  } // j=1
 
-                  // d()/dzeta
+                  // d^2()/deta^2
 		case 2:
 		  {
-		    switch(i)
+		    // All d^2()/deta^2 derivatives for linear hexes are zero.
+                    return RealGradient();
+                  } // j = 2
+
+                  // d^2()/dxidzeta
+		case 3:
+                  {
+                    switch(i)
 		      {
+                      case 0:
+		      case 2:
                       case 4:
                       case 5:
                       case 6:
                       case 7:
+                      case 8:
+                      case 10:
 			return RealGradient();
 
-                      case 0:
-                        {
-                          if( elem->point(0) > elem->point(1) )
-                            return RealGradient( -0.125*(-1.0+eta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(-1.0+eta), 0.0, 0.0 );
-                        }
                       case 1:
-                        {
-                          if( elem->point(1) > elem->point(2) )
-                            return RealGradient( 0.0, -0.125*(-1.0-xi), 0.0 );
-                          else
-                            return RealGradient( 0.0,  0.125*(-1.0-xi), 0.0 );
-                        }
-                      case 2:
-                        {
-                          if( elem->point(2) > elem->point(3) )
-                            return RealGradient( -0.125*(-1.0-eta), 0.0, 0.0 );
-                          else
-                            return RealGradient( 0.125*(-1.0-eta), 0.0, 0.0 );
-                        }
-                      case 3:
-                        {
-                          if( elem->point(3) > elem->point(0) )
-                            return RealGradient( 0.0, -0.125*(-1.0+xi), 0.0 );
-                          else
-                            return RealGradient( 0.0,  0.125*(-1.0+xi), 0.0 );
-                        }
-                      case 8:
-                        {
-                          if( elem->point(4) > elem->point(5) )
-                            return RealGradient( -0.125*(1.0-eta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(1.0-eta), 0.0, 0.0 );
-                        }
+			{
+			  if( elem->point(1) > elem->point(2) )
+			    return RealGradient( 0.0,  0.125 );
+			  else
+			    return RealGradient( 0.0, -0.125 );
+			}
+		      case 3:
+			{
+			  if( elem->point(3) > elem->point(0) )
+			    return RealGradient( 0.0, -0.125 );
+			  else
+			    return RealGradient( 0.0,  0.125 );
+			}
                       case 9:
                         {
                           if( elem->point(5) > elem->point(6) )
-                            return RealGradient( 0.0, -0.125*(1.0+xi), 0.0 );
+                            return RealGradient( 0.0, -0.125, 0.0 );
                           else
-                            return RealGradient( 0.0,  0.125*(1.0+xi), 0.0 );
-                        }
-                      case 10:
-                        {
-                          if( elem->point(7) > elem->point(6) )
-                            return RealGradient( -0.125*(1.0+eta), 0.0, 0.0 );
-                          else
-                            return RealGradient(  0.125*(1.0+eta), 0.0, 0.0 );
+                            return RealGradient( 0.0,  0.125, 0.0 );
                         }
                       case 11:
                         {
                           if( elem->point(4) > elem->point(7) )
-                            return RealGradient( 0.0, -0.125*(1.0-xi), 0.0 );
+                            return RealGradient( 0.0,  0.125, 0.0 );
                           else
-                            return RealGradient( 0.0,  0.125*(1.0-xi), 0.0 );
+                            return RealGradient( 0.0, -0.125, 0.0 );
                         }
                       default:
 			libmesh_error();
 		      } // switch(i)
 
-                  } // j = 2
+                  } // j = 3
+
+                  // d^2()/detadzeta
+		case 4:
+                  {
+                    switch(i)
+		      {
+		      case 1:
+		      case 3:
+                      case 4:
+                      case 5:
+                      case 6:
+                      case 7:
+                      case 9:
+                      case 11:
+			return RealGradient();
+
+                      case 0:
+                        {
+                          if( elem->point(0) > elem->point(1) )
+                            return RealGradient( -0.125, 0.0, 0.0 );
+                          else
+                            return RealGradient(  0.125, 0.0, 0.0 );
+                        }
+		      case 2:
+                        {
+                          if( elem->point(2) > elem->point(3) )
+                            return RealGradient(  0.125, 0.0, 0.0 );
+                          else
+                            return RealGradient( -0.125, 0.0, 0.0 );
+                        }
+                      case 8:
+                        {
+                          if( elem->point(4) > elem->point(5) )
+                            return RealGradient(  0.125, 0.0, 0.0 );
+                          else
+                            return RealGradient( -0.125, 0.0, 0.0 );
+                        }
+                      case 10:
+                        {
+                          if( elem->point(7) > elem->point(6) )
+                            return RealGradient( -0.125, 0.0, 0.0 );
+                          else
+                            return RealGradient(  0.125, 0.0, 0.0 );
+                        }
+                      default:
+			libmesh_error();
+		      } // switch(i)
+
+                  } // j = 4
+
+                  // d^2()/dzeta^2
+		case 5:
+                  {
+                    // All d^2()/dzeta^2 derivatives for linear hexes are zero.
+                    return RealGradient();
+                  } // j = 5
 
 		default:
 		  libmesh_error();
