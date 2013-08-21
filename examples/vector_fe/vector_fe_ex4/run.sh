772f4926e229cf513db49d2d5cbe90a547ae56ca
John Peterson <jwpeterson@gmail.com>
2013-08-21 16:16:02 -0600

    Using direct solver for the vector FE examples.

--- examples/vector_fe/vector_fe_ex4/run.sh
+++ examples/vector_fe/vector_fe_ex4/run.sh
@@ -6,8 +6,12 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=vector_fe_ex4
 
-options="element_type=HEX20 -pc_type jacobi"
+# Note: these problems are particularly ill-conditioned, and standard
+# preconditioners like jacobi and ILU don't seem to work very well.
+# We've therefore instead used LU in these examples.
+
+options="element_type=HEX20 -pc_type lu"
 run_example "$example_name" "$options"
 
-options="element_type=HEX27 -pc_type jacobi"
+options="element_type=HEX27 -pc_type lu"
 run_example "$example_name" "$options"
