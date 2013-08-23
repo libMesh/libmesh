82165a45742487ac4e88dbc995bbe21794b77e9f
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-08-22 19:32:40 -0500

    PETSc LU doesn't work in parallel
    But these examples are fine with -pc_type jacobi as long as we disable
    any competing options

--- examples/vector_fe/vector_fe_ex3/run.sh
+++ examples/vector_fe/vector_fe_ex3/run.sh
@@ -6,15 +6,15 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=vector_fe_ex3
 
-# Note: these problems are particularly ill-conditioned, and standard
-# preconditioners like jacobi and ILU don't seem to work very well.
-# We've therefore instead used LU in these examples.
+# Note: these problems are particularly ill-conditioned, and common
+# preconditioners like ILU don't all work very well.  We currently
+# force Jacobi in these examples.
 
-options="element_type=TRI6 -pc_type lu"
-run_example "$example_name" "$options"
+options="element_type=TRI6 -pc_type jacobi"
+run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=QUAD8 -pc_type lu"
-run_example "$example_name" "$options"
+options="element_type=QUAD8 -pc_type jacobi"
+run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=QUAD9 -pc_type lu"
-run_example "$example_name" "$options"
+options="element_type=QUAD9 -pc_type jacobi"
+run_example_no_extra_options "$example_name" "$options"
--- examples/vector_fe/vector_fe_ex4/run.sh
+++ examples/vector_fe/vector_fe_ex4/run.sh
@@ -6,12 +6,12 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=vector_fe_ex4
 
-# Note: these problems are particularly ill-conditioned, and standard
-# preconditioners like jacobi and ILU don't seem to work very well.
-# We've therefore instead used LU in these examples.
+# Note: these problems are particularly ill-conditioned, and common
+# preconditioners like ILU don't all work very well.  We currently
+# force Jacobi in these examples.
 
-options="element_type=HEX20 -pc_type lu"
-run_example "$example_name" "$options"
+options="element_type=HEX20 -pc_type jacobi"
+run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=HEX27 -pc_type lu"
-run_example "$example_name" "$options"
+options="element_type=HEX27 -pc_type jacobi"
+run_example_no_extra_options "$example_name" "$options"
