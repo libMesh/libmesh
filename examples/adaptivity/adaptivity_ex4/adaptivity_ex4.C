5161ea3c192675f2f62397abddfb184aea0700c3
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-12-19 16:43:11 -0600

    First swath of fixes for cppcheck warnings

--- examples/adaptivity/adaptivity_ex4/adaptivity_ex4.C
+++ examples/adaptivity/adaptivity_ex4/adaptivity_ex4.C
@@ -181,8 +181,6 @@ int main(int argc, char** argv)
   else
     output_file += "uniform";
 
-  std::string exd_file = output_file;
-  exd_file += ".e";
   output_file += ".m";
 
   std::ofstream out (output_file.c_str());
@@ -358,6 +356,9 @@ int main(int argc, char** argv)
 
 #ifdef LIBMESH_HAVE_EXODUS_API
   // Write out the solution
+  std::string exd_file = output_file;
+  exd_file += ".e";
+
   // After solving the system write the solution
   // to a ExodusII-formatted plot file.
   ExodusII_IO (mesh).write_equation_systems (exd_file,
