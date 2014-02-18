c25f93716cf393ade6186f41a7fe5875a08f0a9d
Manav Bhatia <bhatiamanav@gmail.com>
2014-02-18 10:11:34 -0500

    Changing Grid Quadrature class so that the quadrature points do not include the domain boundaries, but the center of each subdomain.

--- src/quadrature/quadrature_grid_1D.C
+++ src/quadrature/quadrature_grid_1D.C
@@ -37,10 +37,11 @@ void QGrid::init_1D(const ElemType,
 
   _points.resize(_order + 1);
   _weights.resize(_order + 1);
+  Real dx = 2.0/(double)(_order+1);
   for (int i = 0; i != _order + 1; ++i)
     {
-      _points[i](0) = 2.0 * (double)i / (double)_order - 1.0;
-      _weights[i] = 2.0 / (double)(_order + 1);
+      _points[i](0) = ((i+0.5)*dx-1.0);
+      _weights[i] = dx;
     }
   return;
 }
