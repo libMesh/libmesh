95c0333beab9ebe06a723d7ed70b24d068bfb9a2
John Peterson <jwpeterson@gmail.com>
2014-02-20 09:53:26 -0700

    Adding script which can be used to remove tabs from source code, and reindent the offending lines using Emacs.
    May eventually be used as part of 'make beautify'.

---
+++ contrib/bin/detab.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script detabs (and reindents) a file using emacs-style indentation.
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 file.C"
+    exit 1
+fi
+
+# Use perl to get a list of line numbers that have <TAB> characters
+tabbed_lines=(`perl -ne "print \"$. \" if /\t/" $1`)
+
+# Now actually use perl to remove those tab characters
+perl -pli -e 's/\t//g' $1
+
+# Print status message
+echo "Reindenting ${#tabbed_lines[@]} lines in $1."
+
+# Initialize the list of eval commands with the command which causes
+# Emacs to use spaces for tabs.
+eval_commands="--eval=\"(setq-default indent-tabs-mode nil)\""
+
+# Make a long string of --eval commands
+for i in "${tabbed_lines[@]}"
+do
+    eval_commands+=" --eval \"(goto-line $i)\" --eval \"(indent-for-tab-command)\""
+done
+
+# Construct the command you want to run as a string, and then use
+# 'eval' to run it.  Note that this is *not* the same thing as
+# running, say, `$cmd`.
+cmd="/usr/bin/emacs -batch $1 $eval_commands -f save-buffer &> /dev/null"
+eval $cmd
