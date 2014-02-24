b6841cfa29894977fb5e2c67fe884b23294894e4
John Peterson <jwpeterson@gmail.com>
2014-02-24 10:27:03 -0700

    If there are no lines to indent, don't run emacs.

--- contrib/bin/detab.sh
+++ contrib/bin/detab.sh
@@ -12,21 +12,28 @@ tabbed_lines=(`perl -ne "print \"$. \" if /\t/" $1`)
 # Now actually use perl to remove those tab characters
 perl -pli -e 's/\t//g' $1
 
-# Print status message
-echo "Reindenting ${#tabbed_lines[@]} lines in $1."
+# How much work is there to be done?
+n=${#tabbed_lines[@]}
 
-# Initialize the list of eval commands with the command which causes
-# Emacs to use spaces for tabs.
-eval_commands="--eval=\"(setq-default indent-tabs-mode nil)\" --eval=\"(c++-mode)\""
+if [ $n -gt 0 ]; then
+  # Print status message
+  echo "Reindenting $n lines in $1."
 
-# Make a long string of --eval commands
-for i in "${tabbed_lines[@]}"
-do
-    eval_commands+=" --eval \"(goto-line $i)\" --eval \"(indent-for-tab-command)\""
-done
+  # Initialize the list of eval commands with the command which causes
+  # Emacs to use spaces for tabs.
+  eval_commands="--eval=\"(setq-default indent-tabs-mode nil)\" --eval=\"(c++-mode)\""
 
-# Construct the command you want to run as a string, and then use
-# 'eval' to run it.  Note that this is *not* the same thing as
-# running, say, `$cmd`.
-cmd="/usr/bin/emacs -batch $1 $eval_commands -f save-buffer &> /dev/null"
-eval $cmd
+  # Make a long string of --eval commands
+  for i in "${tabbed_lines[@]}"
+  do
+      eval_commands+=" --eval \"(goto-line $i)\" --eval \"(indent-for-tab-command)\""
+  done
+
+  # Construct the command you want to run as a string, and then use
+  # 'eval' to run it.  Note that this is *not* the same thing as
+  # running, say, `$cmd`.
+  cmd="/usr/bin/emacs -batch $1 $eval_commands -f save-buffer &> /dev/null"
+  eval $cmd
+else
+  echo "Nothing to be done in $1."
+fi
