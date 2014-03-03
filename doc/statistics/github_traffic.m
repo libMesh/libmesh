beeda2ff3daa8656b774fce74a0fc7bd78757f4b
John Peterson <jwpeterson@gmail.com>
2014-03-02 20:27:50 -0700

    Recording github site traffic.  They don't make it easy...

---
+++ doc/statistics/github_traffic.m
@@ -0,0 +1,25 @@
+% Github has a "traffic" page now, but it doesn't seem like you can
+% put in an arbitrary date range?  When I looked at it, it showed the
+% numbers of unique visitors and views for the last two weeks only...
+% So as long as I check back at least once every two weeks I can keep
+% a record of it?  Great...
+
+% https://github.com/libMesh/libmesh/graphs/traffic
+
+% Date, Views, Unique Visitors
+data = {
+    {'2014-Feb-17', 274, 25}
+    {'2014-Feb-18', 145, 30}
+    {'2014-Feb-19', 129, 27}
+    {'2014-Feb-20', 202, 24}
+    {'2014-Feb-21', 240, 22}
+    {'2014-Feb-22', 62,  17}
+    {'2014-Feb-23', 28,  12}
+    {'2014-Feb-24', 217, 19}
+    {'2014-Feb-25', 90,  25}
+    {'2014-Feb-26', 189, 36}
+    {'2014-Feb-27', 112, 26}
+    {'2014-Feb-28', 81,  20}
+    {'2014-Mar-01', 113, 17}
+    {'2014-Mar-02', 41,  14}
+    };
