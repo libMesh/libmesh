81000aa953caf217ce7ed00d9a8606ae966796a5
Daniel Schwen <daniel.schwen@inl.gov>
2014-04-28 13:41:33 -0600

    Add fparser auto-differentiation module

---
+++ contrib/fparser/examples/autodiff.cc
@@ -0,0 +1,59 @@
+// Simple example file for teh auto-differentiation module
+// (c) 2014 by Daniel Schwen
+// =======================================================
+
+#include "../fparser_ad.hh"
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string function;
+  FunctionParserAD fparser;
+
+  //std::string func = "x^2+3.345*x+4*a+3";
+  //std::string func = "(x^2+3.345*x)*(4*a+3)";
+  //std::string func = "3*x^2+4*x+5";
+  //std::string func = "4*log(4*x^2+6)-7*x";
+
+  // works:
+  //std::string func = "log(2+8*x^2)";
+  //std::string func = "(4*x+8*x^2)+(3*x+4*x^2+7)";
+  std::string func = "x*log(x)+(1-x)*log(1-x)";
+
+  //std::string func = "3*a^2 + 7*x^2";
+  //std::string func = "3*x + 4*x^2";
+
+  //std::string func = "sin(x^2)";
+  //std::string func = "sqrt(3*x)";
+
+  std::cout << "Input Expression:\n" << func << std::endl;
+
+  // Parse the input expression into bytecode
+  fparser.Parse(func, "x,a");
+  fparser.PrintByteCode(std::cout);
+  std::cout << std::endl;
+
+  // Get a copy of the original function
+  FunctionParserAD fparser2(fparser);
+
+  // Generate derivative with respect to x
+  fparser.AutoDiff("x");
+  std::cout << "Unsimplified derivative:\n";
+  fparser.PrintByteCode(std::cout);
+  std::cout << std::endl;
+
+  // Run the bytecode optimizer on the derivative
+  fparser.Optimize();
+  std::cout << "Simplified derivative:\n";
+  fparser.PrintByteCode(std::cout);
+  std::cout << std::endl;
+
+  // Check if the copied instance is still the original function
+  std::cout << "Copy of the original:\n";
+  fparser2.PrintByteCode(std::cout);
+  std::cout << std::endl;
+
+  return 0;
+}
---
+++ contrib/fparser/fparser_ad.cc
@@ -0,0 +1,388 @@
+#include "fparser_ad.hh"
+#include "extrasrc/fptypes.hh"
+#include <stdlib.h>
+
+using namespace FUNCTIONPARSERTYPES;
+
+#include <iostream>
+
+template<typename Value_t>
+FunctionParserADBase<Value_t>::FunctionParserADBase() :
+    FunctionParserBase<Value_t>(),
+    mData(this->getParserData())
+{
+  std::cout << "CTOR     mData->mReferenceCounter = " << mData->mReferenceCounter << std::endl;
+}
+
+template<typename Value_t>
+FunctionParserADBase<Value_t>::FunctionParserADBase(const FunctionParserADBase& cpy) :
+    FunctionParserBase<Value_t>(cpy),
+    mData(this->getParserData())
+{
+  std::cout << "CopyCTOR mData->mReferenceCounter = " << mData->mReferenceCounter << std::endl;
+}
+
+template<typename Value_t>
+int FunctionParserADBase<Value_t>::OpcodeSize(unsigned op)
+{
+  if (int(op) >= VarBegin || op == cImmed) {
+    return -1;
+  }
+  else {
+    switch(op)
+    {
+      // these opcode takes one argument off the stack
+      case cInv: case cNeg:
+      case cNot: case cAbsNot:
+      case cNotNot: case cAbsNotNot:
+      case cSqr:
+      case cSqrt: case cRSqrt:
+      case cDeg: case cRad:
+      case cExp: case cExp2:
+      case cLog:
+      case cSin: case cCos:
+        return 1;
+
+      // these opcode takes two arguments off the stack
+      case cAdd: case cSub: case cRSub:
+      case cMul: case cDiv: case cRDiv:
+      case cPow: case cMod:
+      case cEqual: case cNEqual: case cLess:
+      case cLessOrEq: case cGreater: case cGreaterOrEq:
+      case cAnd: case cAbsAnd:
+      case cOr: case cAbsOr:
+        return 2;
+
+      // these opcodes effectively take nothing off the stack (but put one value on there)
+      //case cDup:
+      //  return 0;
+
+      default:
+        std::cerr << cDup<< " Unhandled opcode " << op << std::endl;;
+        exit(1);
+    }
+  }
+}
+
+template<typename Value_t>
+typename FunctionParserADBase<Value_t>::Interval
+FunctionParserADBase<Value_t>::GetArgument(const DiffProgramFragment & orig)
+{
+  // count the number of elements on the stack (needs to reach 1)
+  int stack_size = 0;
+
+  // Extract the opcode sequence(s) that is responsible for the
+  // top stack entry
+  typename DiffProgramFragment::const_iterator ip = orig.end();
+  do {
+    // take one step back
+    if (ip == orig.begin())
+    {
+      std::cerr << "Not enough arguments on the stack!\n";
+      exit(1);
+    }
+    ip--;
+
+    // a size two opcode needs two elements from teh stack, but also puts one back on!
+    int this_size = OpcodeSize((*ip).first);
+    stack_size -= this_size > 0 ? this_size-1 : this_size;
+  } while (stack_size < 1);
+
+  return Interval(ip, orig.end());
+}
+
+template<typename Value_t>
+typename FunctionParserADBase<Value_t>::DiffProgramFragment
+FunctionParserADBase<Value_t>::DiffFunction(const DiffProgramFragment & orig)
+{
+  // check for empty DiffProgramFragments
+  if (orig.empty()) {
+    std::cout << "Empty programm passed in!\n";
+    exit(1);
+  }
+
+  // this stores the opcode of the differentiated function
+  DiffProgramFragment outer, prog_a, prog_b, prog_da, prog_db;
+
+  // current opcode
+  unsigned op = orig.back().first;
+
+  // size of the current end opcode
+  int op_size = OpcodeSize(op);
+
+  // variable or immediate value
+  if (op_size == -1)
+  {
+    if (op == cImmed)
+      outer.push_back(OpcodeDataPair(cImmed, 0.0));
+    else
+    {
+      if (op == mVarOp)
+        outer.push_back(OpcodeDataPair(cImmed, 1.0));
+      else
+        outer.push_back(OpcodeDataPair(cImmed, 0.0));
+    }
+
+    return outer;
+  }
+
+  // Extract the opcode sequence(s) that generates
+  // the argument(s) for the current opcode
+
+  // get the opcode sequence preceeding the current opcode
+  DiffProgramFragment head, head2;
+  std::copy(orig.begin(), orig.end()-1, std::back_inserter(head));
+
+  // get the opcode interval of the argument immediately preceeding the current opcode
+  Interval arg = GetArgument(head);
+
+  if (op_size == 1)
+    std::copy(arg.first, arg.second, std::back_inserter(prog_a));
+  else if (op_size == 2)
+  {
+    std::copy(arg.first, arg.second, std::back_inserter(prog_b));
+    // get opcode squence before that argument interval
+    std::copy<typename DiffProgramFragment::const_iterator>(head.begin(), arg.first, std::back_inserter(head2));
+    arg = GetArgument(head2);
+    std::copy(arg.first, arg.second, std::back_inserter(prog_a));
+  }
+  else
+  {
+    std::cerr << "Unhandled opcode argument count.\n";
+    exit(1);
+  }
+
+  // create derivatives
+  switch (op)
+  {
+    case cRSub:
+      // db - da
+    case cAdd:
+      // da + db
+    case cSub:
+      // da - db
+      prog_da = DiffFunction(prog_a);
+      prog_db = DiffFunction(prog_b);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_db.begin(), prog_db.end());
+      outer.push_back(OpcodeDataPair(op, 0));
+      return outer;
+
+    case cMul:
+      // a*db + da*b
+      prog_da = DiffFunction(prog_a);
+      prog_db = DiffFunction(prog_b);
+      outer = prog_a;
+      outer.insert(outer.end(), prog_db.begin(), prog_db.end());
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.insert(outer.end(), prog_da.begin(), prog_da.end());
+      outer.insert(outer.end(), prog_b.begin(), prog_b.end());
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.push_back(OpcodeDataPair(cAdd, 0));
+      return outer;
+
+    case cSqr:
+      // a*da*2
+      prog_da = DiffFunction(prog_a);
+      outer = prog_a;
+      outer.insert(outer.end(), prog_da.begin(), prog_da.end());
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.push_back(OpcodeDataPair(cImmed, 2.0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      return outer;
+
+    case cLog:
+      // da/a
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      return outer;
+
+    case cExp:
+    case cExp2:
+      // da/a
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(op, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      if (op == cExp2) {
+        outer.push_back(OpcodeDataPair(cImmed, std::log(2.0)));
+        outer.push_back(OpcodeDataPair(cMul, 0));
+      }
+      return outer;
+
+    case cNeg:
+      // -da
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.push_back(OpcodeDataPair(cNeg, 0));
+      return outer;
+
+    // also capture cRdiv here but switch a and b first!
+    case cRDiv:
+      std::swap(prog_a, prog_b);
+    case cDiv:
+      // db/a - a*db/b^2
+      prog_da = DiffFunction(prog_a);
+      prog_db = DiffFunction(prog_b);
+      outer = prog_db;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.insert(outer.end(), prog_db.begin(), prog_db.end());
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.insert(outer.end(), prog_b.begin(), prog_b.end());
+      outer.push_back(OpcodeDataPair(cSqr, 0));
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      outer.push_back(OpcodeDataPair(cSub, 0));
+      return outer;
+
+    case cInv:
+      // -da/a^2
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cSqr, 0));
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      outer.push_back(OpcodeDataPair(cNeg, 0));
+      return outer;
+
+    case cPow:
+      // a**b * ( db*log(a) + b*da/a)
+      prog_da = DiffFunction(prog_a);
+      prog_db = DiffFunction(prog_b);
+      outer = prog_a;
+      outer.insert(outer.end(), prog_b.begin(), prog_b.end());
+      outer.push_back(OpcodeDataPair(cPow, 0));
+      outer.insert(outer.end(), prog_db.begin(), prog_db.end());
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cLog, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.insert(outer.end(), prog_b.begin(), prog_b.end());
+      outer.insert(outer.end(), prog_da.begin(), prog_da.end());
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      outer.push_back(OpcodeDataPair(cAdd, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      return outer;
+
+    case cSin :
+      // da*cos(a)
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cCos, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      return outer;
+
+    case cCos :
+      // -da*sin(a)
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cSin, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.push_back(OpcodeDataPair(cNeg, 0));
+      return outer;
+
+    case cSqrt :
+      // da/(2*sqrt(a))
+      prog_da = DiffFunction(prog_a);
+      outer = prog_da;
+      outer.push_back(OpcodeDataPair(cImmed, 2.0));
+      outer.insert(outer.end(), prog_a.begin(), prog_a.end());
+      outer.push_back(OpcodeDataPair(cSqrt, 0));
+      outer.push_back(OpcodeDataPair(cMul, 0));
+      outer.push_back(OpcodeDataPair(cDiv, 0));
+      return outer;
+  }
+
+  //outer.insert( v1.end(), v2.begin(), v2.end() );
+  std::cout << "Unknown opcode!\n";
+  exit(1);
+  //return outer;
+}
+
+template<typename Value_t>
+void FunctionParserADBase<Value_t>::Commit(const DiffProgramFragment & diff)
+{
+  // loop over diff and fill in mByteCode and mImmed
+  mData->mByteCode.clear();
+  mData->mImmed.clear();
+
+  // compressed immediate data representation
+  for (unsigned int i = 0; i < diff.size(); ++i)
+  {
+    mData->mByteCode.push_back(diff[i].first);
+    if (diff[i].first == cImmed)
+      mData->mImmed.push_back(diff[i].second);
+  }
+}
+
+template<typename Value_t>
+int FunctionParserADBase<Value_t>::AutoDiff(const std::string& var)
+{
+  this->ForceDeepCopy();
+  mData = this->getParserData();
+
+  // get c string and length of var argument
+  const unsigned len = unsigned(var.size());
+  const char* name = var.c_str();
+
+  // reset opcode of the variable we diff for
+  mVarOp = 0;
+
+  // figure out the opcode number that corresponds to 'var', the variable we diff for
+  typename FUNCTIONPARSERTYPES::NamePtrsMap<Value_t> & NamePtrs = mData->mNamePtrs;
+  typename FUNCTIONPARSERTYPES::NamePtrsMap<Value_t>::iterator vi;
+  for (vi = NamePtrs.begin(); vi != NamePtrs.end(); ++vi)
+  {
+    if (len == vi->first.nameLength &&
+        std::memcmp(name, vi->first.name, len) == 0) {
+      mVarOp = vi->second.index;
+      break;
+    }
+  }
+
+  // invalid var argument, variable not found
+  if (mVarOp == 0) return 1;
+
+  // get a reference to the stored bytecode
+  const std::vector<unsigned>& ByteCode = mData->mByteCode;
+
+  // get a reference to the immediate values
+  const std::vector<Value_t>& Immed = mData->mImmed;
+
+  // uncompressed immediate data representation
+  DiffProgramFragment orig;
+  unsigned op;
+  unsigned int nImmed = 0;
+  for (unsigned int i = 0; i < ByteCode.size(); ++i)
+  {
+    op = ByteCode[i];
+    if (op == cImmed)
+      orig.push_back(OpcodeDataPair(op, Immed[nImmed++]));
+    else if (op == cDup)
+    {
+      // substitute full code for cDup opcodes
+      Interval arg = GetArgument(orig);
+      orig.insert(orig.end(), arg.first, arg.second);
+    }
+    else
+      orig.push_back(OpcodeDataPair(op, 0.0));
+  }
+
+  DiffProgramFragment diff  = DiffFunction(orig);
+
+  // create compressed program representation
+  Commit(diff);
+
+  return 0;
+}
+
+
+// Instantiate class
+template class FunctionParserADBase<double>;
