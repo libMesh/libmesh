571b76130fe80af0352737f16cbb290ba0b355b1
John Peterson <jwpeterson@gmail.com>
2014-11-03 11:29:39 -0700

    Converting the mailing list plots from GNU/Octave to Matplotlib.

---
+++ doc/statistics/libmesh_mailinglists.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+import numpy as np
+from operator import add
+
+# Number of messages to libmesh-devel and libmesh-users over the life
+# of the project.  I cut and pasted these from the sf website: they
+# are in basically the same format as those pages, which is to say,
+# not particularly useful for plotting.
+
+# Approximate subscriber counts:
+# https://lists.sourceforge.net/lists/admindb/libmesh-users
+# https://lists.sourceforge.net/lists/admindb/libmesh-devel
+
+# Month, year, libmesh-devel subscriber count, libmesh-users subscriber count
+membership_data = [
+    'Jan 2010', 75, 143,
+    'Jul 2011', 92, 185,
+    'Aug 2011', 91, 184,
+    'Sep 2011', 90, 184,
+    'Nov 2011', 92, 184,
+    'Dec 2011', 93, 185,
+    'Jan 2012', 96, 190,
+    'Feb 2012', 98, 196,
+    'Mar 2012', 99, 199,
+    'Apr 2012', 103, 204,
+    'May 2012', 105, 210,
+    'Jun 2012', 105, 209,
+    'Jul 2012', 107, 210,
+    'Aug 2012', 109, 213,
+    'Sep 2012', 111, 220,
+    'Oct 2012', 111, 222,
+    'Nov 2012', 112, 225,
+    'Dec 2012', 111, 225,
+    'Jan 2013', 111, 225,
+    'Feb 2013', 112, 228,
+    'Mar 2013', 112, 231,
+    'Apr 2013', 112, 228,
+    'May 2013', 113, 233,
+    'Jun 2013', 113, 237,
+    'Jul 2013', 114, 240,
+    'Aug 2013', 114, 242,
+    'Sep 2013', 113, 241,
+    'Oct 2013', 112, 241,
+    'Nov 2013', 112, 241,
+    'Dec 2013', 111, 240,
+    'Jan 2014', 112, 244,
+    'Feb 2014', 113, 244,
+    'Mar 2014', 113, 247,
+    'Apr 2014', 113, 248,
+    'May 2014', 113, 249,
+    'Jun 2014', 113, 247,
+    'Jul 2014', 115, 249,
+    'Aug 2014', 115, 251,
+    'Sep 2014', 117, 254,
+    'Oct 2014', 118, 257
+] # remember to update the indices below!
+
+# The early membership data is spotty, so set indices which are meaningful
+# based on the chronology.  Arbitrarily start with index=1.  Note that +=
+# concatenates lists in Python.
+membership_indices = [1]                                         # 2010
+membership_indices += [i+12 for i in [7, 8, 9, 11, 12]]          # 2011
+membership_indices += [int(i)+24 for i in np.linspace(1,24,24)]  # 2012, 2013
+membership_indices += [int(i)+48 for i in np.linspace(1,10,10)]; # 2014 <-- Update me
+
+# print membership_indices
+
+# Strip out the dates from membership_data
+dates = membership_data[0::3]
+
+# Strip out the number of libmesh-devel subscribers from membership_data
+devel_count = membership_data[1::3]
+
+# Strip out the number of libmesh-users subscribers from membership_data
+users_count = membership_data[2::3]
+
+# print devel_count
+# print users_count
+
+# Get a reference to the figure
+fig = plt.figure()
+
+# 111 is equivalent to Matlab's subplot(1,1,1) command
+ax = fig.add_subplot(111)
+
+# Plot libmesh-devel mailing list membership over time
+devel_line, = ax.plot(membership_indices, devel_count, 'bo-')
+
+# Plot libmesh-users mailing list membership over time
+users_line, = ax.plot(membership_indices, users_count, 'gs--')
+
+# Add a legend to the plot. loc=2 corresponds to "upper left" of the plot.
+plt.legend([devel_line, users_line], ['libmesh-devel', 'libmesh-users'], loc=2)
+
+# Create title
+fig.suptitle('LibMesh Mailing List Membership Size')
+
+# Set up xticks and xticklabels
+N = len(devel_count)
+xtick_indexes = [0, 1, N-1]
+
+xticks = []
+xticklabels = []
+for index in xtick_indexes:
+    xticks.append(membership_indices[index])
+    xticklabels.append(dates[index])
+
+ax.set_xticks(xticks)
+ax.set_xticklabels(xticklabels)
+
+# Save as PDF
+plt.savefig('libmesh_mailinglists_membership.pdf')
+
+
+
+
+# libmesh-devel
+# https://sourceforge.net/mailarchive/forum.php?forum_name=libmesh-devel
+#            jan  feb  mar  apr  may  jun  jul  aug  sep  oct  nov  dec
+devel_data = [
+    '2003',    4,   1,   9,   2,   7,   1,   1,   4,  12,   8,   3,   4,
+    '2004',    1,  21,  31,  10,  12,  15,   4,   6,   5,  11,  43,  13,
+    '2005',   25,  12,  49,  19, 104,  60,  10,  42,  15,  12,   6,   4,
+    '2006',    1,   6,  31,  17,   5,  95,  38,  44,   6,   8,  21,   0,
+    '2007',    5,  46,   9,  23,  17,  51,  41,   4,  28,  71, 193,  20,
+    '2008',   46,  46,  18,  38,  14, 107,  50, 115,  84,  96, 105,  34,
+    '2009',   89,  93, 119,  73,  39,  51,  27,   8,  91,  90,  77,  67,
+    '2010',   24,  36,  98,  45,  25,  60,  17,  36,  48,  45,  65,  39,
+    '2011',   26,  48, 151, 108,  61, 108,  27,  50,  43,  43,  27,  37,
+    '2012',   56, 120,  72,  57,  82,  66,  51,  75, 166, 232, 284, 105, # Dec 10, 2012 libmesh moved to github
+    '2013',  168, 151,  30, 145,  26,  53,  76,  33,  23,  72, 125,  38,
+    '2014',   47,  62,  27,   8,  12,   2,  22,  22,   0,  17
+]
+
+# libmesh-users starts in Sept 2003!
+# https://sourceforge.net/mailarchive/forum.php?forum_name=libmesh-users
+#            jan  feb  mar  apr  may  jun  jul  aug  sep  oct  nov     dec
+users_data = [
+    '2003',    0,   0,   0,   0,   0,   0,   0,   0,   2,   2,  27,  31,
+    '2004',    6,  15,  33,  10,  46,  11,  21,  15,  13,  23,   1,   8,
+    '2005',   27,  57,  86,  23,  37,  34,  24,  17,  50,  24,  10,  60,
+    '2006',   47,  46, 127,  19,  26,  62,  47,  51,  61,  42,  50,  33,
+    '2007',   60,  55,  77, 102,  82, 102, 169, 117,  80,  37,  51,  43,
+    '2008',   71,  94,  98, 125,  54, 119,  60, 111, 118, 125, 119,  94,
+    '2009',  109,  38,  93,  88,  29,  57,  53,  48,  68, 151,  23,  35,
+    '2010',   84,  60, 184, 112,  60,  90,  23,  70, 119,  27,  47,  54,
+    '2011',   22,  19,  92,  93,  35,  91,  32,  61,   7,  69,  81,  23,
+    '2012',   64,  95,  35,  36,  63,  98,  70, 171, 149,  64,  67, 126, # Dec 10, 2012 libmesh moved to github
+    '2013',  108, 104, 171, 133, 108, 100,  93, 126,  74,  59, 145,  93,
+    '2014',   38,  45,  26,  41, 125,  70,  61,  66,  60, 110
+]
+
+# Make plot of monthly data
+fig.clf()
+
+month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+# Strip out the year strings
+year_strings = devel_data[0::13]
+
+for i in range(0, 12):
+    # Strip out the devel_data for the current month.  Note that the
+    # stride is 13 since the year string also appears in each row.
+    devel_data_current_month = devel_data[i+1::13]
+
+    # Strip out the users_data for the current month
+    users_data_current_month = users_data[i+1::13]
+
+    # Use the map function to add these lists element-by-element
+    combined_data_current_month = map(add, devel_data_current_month, users_data_current_month)
+
+    # Get reference to the ith axes on a 3x4 grid.  Note that the plot
+    # numbering starts at 1, simiarly to Matlab.
+    ax = fig.add_subplot(3, 4, i+1)
+
+    # The width of the bars to use in the bar chart
+    width=.8
+
+    # Make an x-axis to plot against
+    N = len(devel_data_current_month)
+    x = np.linspace(1, N, N)
+
+    # Plot the summed data
+    ax.bar(x, combined_data_current_month, width, color='c')
+
+    # Plot only the libmesh-devel data
+    ax.bar(x, devel_data_current_month, width, color='b')
+
+    # Set up the xticks and labels
+    xticks = [0, N-1]
+    xticks = [x + 1.5*width for x in xticks]
+    xticklabels = [year_strings[0], year_strings[N-1]]
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xticklabels, fontsize=10)
+
+    # Set month name as subplot title
+    ax.set_title(month_names[i] + ' (max ' + str(max(combined_data_current_month)) + ')', fontsize=10)
+
+    # Set an empty set of ticks for the y-axis to turn it off.  This
+    # is necessary to declutter the figure.
+    ax.get_yaxis().set_ticks([])
+
+# We need to leave a bit more room between the subplots to balance the
+# font size we can use with the amount of space available on the
+# figure.
+# wspace = the amount of width reserved for blank space between subplots
+# hspace = the amount of height reserved for white space between subplots
+plt.subplots_adjust(wspace=.2, hspace=.4)
+
+# Save as PDF
+plt.savefig('libmesh_mailinglists_by_month.pdf')
+
+
+
+# Make plot of data from all time
+fig.clf()
+
+# Strip out all the list entries which are numbers.  Not sure if this
+# is the best or most Python way to do this, but it works...
+devel_numbers = [x for x in devel_data if (isinstance(x, str)==False)]
+users_numbers = [x for x in users_data if (isinstance(x, str)==False)]
+
+# Get the combined data using map
+combined_devel_users_number = map(add, devel_numbers, users_numbers)
+
+# 111 is equivalent to Matlab's subplot(1,1,1) command
+ax = fig.add_subplot(111)
+
+# Make an x-axis to plot against
+N = len(combined_devel_users_number)
+x = np.linspace(1, N, N)
+
+# Plot the combined data
+combined_bars = ax.bar(x, combined_devel_users_number, width, color='c')
+
+# Plot the libmesh-devel data alone
+devel_bars = ax.bar(x, devel_numbers, width, color='b')
+
+# Set the xticks and xticklabels
+xticks = [1, 25, 49, 73, 97, 121]
+xticks = [x+width/2. for x in xticks]
+ax.set_xticks(xticks)
+ax.set_xticklabels(['2003', '2005', '2007', '2009', '2011', '2013'])
+
+# Add a legend to the plot. loc=2 corresponds to "upper left" of the plot.
+plt.legend([combined_bars[0], devel_bars[0]], ['libmesh-users', 'libmesh-devel'], loc=2)
+
+# Set the xlimits
+plt.xlim(0, N+2);
+
+# Save as PDF
+plt.savefig('libmesh_mailinglists.pdf')
