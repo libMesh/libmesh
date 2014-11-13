d3fba007415099755cba533cee9643241a0ef107
Daniel Schwen <daniel.schwen@inl.gov>
2014-11-13 10:47:37 -0700

    rebuild fpoptimizer.cc

--- contrib/fparser/fpoptimizer.cc
+++ contrib/fparser/fpoptimizer.cc
@@ -13256,43 +13256,7 @@ FUNCTIONPARSER_INSTANTIATE_OPTIMIZE(long)
 
 #endif //FP_DUMMY_OPTIMIZER
 
-#line 1 "fpoptimizer/instantiate_for_ad.cc"
-// the space is needed to preserve the include in the postprocessor
-# include "instantiate.hh"
-
-namespace FPoptimizer_CodeTree
-{
-
-#define FP_INSTANTIATE(type) \
-    template void CodeTree<type>::SynthesizeByteCode( \
-        std::vector<unsigned>& ByteCode, \
-        std::vector<type>&   Immed, \
-        size_t& stacktop_max);
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-
-#define FP_INSTANTIATE(type) \
-    template \
-    void CodeTree<type>::GenerateFrom( \
-        const FunctionParserBase<type>::Data& fpdata, \
-        bool keep_powi);
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-
-#define FP_INSTANTIATE(type) \
-    template class CodeTree<type>; \
-    template struct CodeTreeData<type>;
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-
-#ifdef FUNCTIONPARSER_SUPPORT_DEBUGGING
-#define FP_INSTANTIATE(type) \
-    template void DumpTreeWithIndent<type>(const CodeTree<type>&, std::ostream&, const std::string&);
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-#endif
-
-}
-
 
 #endif
+
+#include "instantiate_for_ad.hh"
