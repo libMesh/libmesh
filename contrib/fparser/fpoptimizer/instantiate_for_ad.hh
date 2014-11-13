55ce1bce00d34fe817f4c457b953c357ae98ac67
Daniel Schwen <daniel.schwen@inl.gov>
2014-11-13 14:28:09 -0700

    fixup explicit instantiation again

--- contrib/fparser/fpoptimizer/instantiate_for_ad.hh
+++ contrib/fparser/fpoptimizer/instantiate_for_ad.hh
@@ -4,36 +4,9 @@
 namespace FPoptimizer_CodeTree
 {
 
-#warning "---- Explicitly instantiating CodeTree ----"
 #define FP_INSTANTIATE(type) \
     template class CodeTree<type>; \
     template struct CodeTreeData<type>;
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-
-}
-
-#ifdef FP_DUMMY_OPTIMIZER
-
-#warning "---- Using the DUMMY Optimizer ----"
-
-/*
-namespace FPoptimizer_CodeTree
-{
-
-#define FP_INSTANTIATE(type) \
-    template void CodeTree<type>::SynthesizeByteCode( \
-        std::vector<unsigned>& ByteCode, \
-        std::vector<type>&   Immed, \
-        size_t& stacktop_max);
-    FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
-#undef FP_INSTANTIATE
-
-#define FP_INSTANTIATE(type) \
-    template \
-    void CodeTree<type>::GenerateFrom( \
-        const FunctionParserBase<type>::Data& fpdata, \
-        bool keep_powi);
     FPOPTIMIZER_EXPLICITLY_INSTANTIATE(FP_INSTANTIATE)
 #undef FP_INSTANTIATE
 
@@ -45,8 +18,4 @@ namespace FPoptimizer_CodeTree
 #endif
 
 }
-*/
 
-#else
-#warning "---- Using the REAL Optimizer, with fpoptimizer.cc ----"
-#endif
