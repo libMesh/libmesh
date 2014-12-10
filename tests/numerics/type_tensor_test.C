0e337b0175df68ebb5f738884361c778450cf982
John Peterson <jwpeterson@gmail.com>
2014-12-10 16:11:47 -0700

    Use 'double' to avoid issues with 'Real' and finite precision literals.

--- tests/numerics/type_tensor_test.C
+++ tests/numerics/type_tensor_test.C
@@ -33,19 +33,19 @@ private:
 
     // The base class, TypeTensor, has a protected constructor.  We
     // are using the derived class, TensorValue, for our tests...
-    TensorValue<Real> tensor(9.08973348886179e-01, 3.36455579239923e-01, 5.16389236893863e-01,
-                             9.44156071777472e-01, 1.35610910092516e-01, 1.49881119060538e-02,
-                             1.15988384086146e-01, 6.79845197685518e-03, 3.77028969454745e-01);
+    TensorValue<double> tensor(9.08973348886179e-01, 3.36455579239923e-01, 5.16389236893863e-01,
+                               9.44156071777472e-01, 1.35610910092516e-01, 1.49881119060538e-02,
+                               1.15988384086146e-01, 6.79845197685518e-03, 3.77028969454745e-01);
 
-    TensorValue<Real> inverse = tensor.inverse();
+    TensorValue<double> inverse = tensor.inverse();
 
-    TensorValue<Real> true_inverse(-6.57484735104482e-01, 1.58926633961497e+00,  8.37330721137561e-01,
-                                   4.56430940967411e+00, -3.64404559823061e+00, -6.10654107858520e+00,
-                                   1.19965194510943e-01, -4.23210359257434e-01,  2.50483242797707e+00);
+    TensorValue<double> true_inverse(-6.57484735104482e-01, 1.58926633961497e+00,  8.37330721137561e-01,
+                                     4.56430940967411e+00, -3.64404559823061e+00, -6.10654107858520e+00,
+                                     1.19965194510943e-01, -4.23210359257434e-01,  2.50483242797707e+00);
 
     for (unsigned i=0; i<3; ++i)
       for (unsigned j=0; j<3; ++j)
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(inverse(i,j), true_inverse(i,j), TOLERANCE*TOLERANCE);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(inverse(i,j), true_inverse(i,j), 1.e-12);
   }
 
 };
