e98b0baf5025a4027a52c2fe1d782845b883e47d
Paul T. Bauman <ptbauman@gmail.com>
2015-01-15 13:03:41 -0500

    recalculate_n_partitions() should use local iterators
    and be called on every processor

--- src/mesh/mesh_base.C
+++ src/mesh/mesh_base.C
@@ -363,8 +363,11 @@ void MeshBase::partition (const unsigned int n_parts)
 
 unsigned int MeshBase::recalculate_n_partitions()
 {
-  const_element_iterator       el  = this->active_elements_begin();
-  const const_element_iterator end = this->active_elements_end();
+  // This requires an inspection on every processor
+  parallel_object_only();
+
+  const_element_iterator el  = this->active_local_elements_begin();
+  const_element_iterator end = this->active_local_elements_end();
 
   unsigned int max_proc_id=0;
 
