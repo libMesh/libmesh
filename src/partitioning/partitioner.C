1ceb97733793f9dc055a280f0e97b453826e5f3b
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-02-18 10:34:26 -0600

    Comment out overzealous assertions
    When we hit set_node_processor_ids() in a skip_partitioning()
    codepath after doing enough coarsening, we may legitimately be in a
    state where orphaned nodes still exist but don't still have valid
    processor ids.
    
    The best thing to do in the long term may be to reorder the deletion
    of orphan nodes so as to avoid this state, but that's tricky to do,
    so for now we'll just remove the assertions that are giving us
    false-positives.

--- src/partitioning/partitioner.C
+++ src/partitioning/partitioner.C
@@ -597,8 +597,13 @@ void Partitioner::set_node_processor_ids(MeshBase& mesh)
           Node *node = mesh.node_ptr(request_to_fill[i]);
           libmesh_assert(node);
           const processor_id_type new_pid = node->processor_id();
-          libmesh_assert_not_equal_to (new_pid, DofObject::invalid_processor_id);
-          libmesh_assert_less (new_pid, mesh.n_partitions()); // this is the correct test --
+
+// We may have an invalid processor_id() on nodes that have been
+// "detatched" from coarsened-away elements but that have not yet
+// themselves been removed.
+//          libmesh_assert_not_equal_to (new_pid, DofObject::invalid_processor_id);
+//          libmesh_assert_less (new_pid, mesh.n_partitions()); // this is the correct test --
+
           request_to_fill[i] = new_pid;           //  the number of partitions may
         }                                         //  not equal the number of processors
 
@@ -616,7 +621,11 @@ void Partitioner::set_node_processor_ids(MeshBase& mesh)
 
           // this is the correct test -- the number of partitions may
           // not equal the number of processors
-          libmesh_assert_less (filled_request[i], mesh.n_partitions());
+
+          // But: we may have an invalid processor_id() on nodes that
+          // have been "detatched" from coarsened-away elements but
+          // that have not yet themselves been removed.
+          // libmesh_assert_less (filled_request[i], mesh.n_partitions());
 
           node->processor_id(cast_int<processor_id_type>(filled_request[i]));
         }
