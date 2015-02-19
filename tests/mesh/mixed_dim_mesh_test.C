dc637d118b59ec55e459de6cf5d247c1cbc1b6c5
Paul T. Bauman <ptbauman@gmail.com>
2015-02-19 13:21:13 -0500

    Add PointLocator testing to MixedDimensionMeshTest
    We'll do more with this later, but for now, setting this up to make
    sure we don't muck up the behavior of PointLocator as the API gets
    adjusted to support mixed dimension meshes.

--- tests/mesh/mixed_dim_mesh_test.C
+++ tests/mesh/mixed_dim_mesh_test.C
@@ -29,6 +29,7 @@ public:
   CPPUNIT_TEST( testMesh );
   CPPUNIT_TEST( testDofOrdering );
   CPPUNIT_TEST( testPointLocatorList );
+  CPPUNIT_TEST( testPointLocatorTree );
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -86,7 +87,7 @@ protected:
     }
 
     // libMesh will renumber, but we numbered according to its scheme
-    // anyway. We do this because when we call uniformly_refine subsequenly,
+    // anyway. We do this because when we call uniformly_refine subsequently,
     // it's going use skip_renumber=false.
     _mesh->prepare_for_use(false /*skip_renumber*/);
   }
@@ -176,6 +177,23 @@ public:
     CPPUNIT_ASSERT_EQUAL( (dof_id_type)1, bottom_elem->id() );
   }
 
+  void testPointLocatorTree()
+  {
+    AutoPtr<PointLocatorBase> locator = _mesh->sub_point_locator();
+
+    Point top_point(0.5, 0.5);
+    const Elem* top_elem = (*locator)(top_point);
+
+    // We should have gotten back the top quad
+    CPPUNIT_ASSERT_EQUAL( (dof_id_type)0, top_elem->id() );
+
+    Point bottom_point(0.5, -0.5);
+    const Elem* bottom_elem = (*locator)(bottom_point);
+
+    // We should have gotten back the bottom quad
+    CPPUNIT_ASSERT_EQUAL( (dof_id_type)1, bottom_elem->id() );
+  }
+
 };
 
 class MixedDimensionRefinedMeshTest : public MixedDimensionMeshTest {
@@ -193,7 +211,7 @@ public:
 
   CPPUNIT_TEST_SUITE_END();
 
-  // Yes, this is necesarry. Somewhere in those macros is a protected/private
+  // Yes, this is necessary. Somewhere in those macros is a protected/private
 public:
 
   void setUp()
