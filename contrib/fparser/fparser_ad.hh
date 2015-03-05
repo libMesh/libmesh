e8b5968bcd23e3ac4cf1497684112ed1f5ac74e0
Daniel Schwen <daniel.schwen@inl.gov>
2015-03-05 14:41:38 -0700

    Re-run JITCompile after Optimize (#486)
    Conflicts:
    	contrib/fparser/fparser_ad.cc

--- contrib/fparser/fparser_ad.hh
+++ contrib/fparser/fparser_ad.hh
@@ -51,6 +51,12 @@ public:
    */
   bool JITCompile(bool cacheFunction = true);
 
+  /**
+   * wrap Optimize of the parent class to check for a JIT compiled version and redo
+   * the compilation after Optimization
+   */
+  void Optimize();
+
 #if LIBMESH_HAVE_FPARSER_JIT
   /**
    * Overwrite the Exec function with one that tests for a JIT compiled version
@@ -76,6 +82,9 @@ private:
 
   /// JIT function pointer
   Value_t (*compiledFunction)(const Value_t *, const Value_t *, const Value_t);
+
+  /// pointer to the mImmed values (or NULL if the mImmed vector is empty)
+  Value_t * pImmed;
 
   /**
    * In certain applications derivatives are built proactively and may never be used.
