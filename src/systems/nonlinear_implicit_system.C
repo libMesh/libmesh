6f883b5a7ad65a50a94b2c0ad29a569ab1dd20af
John Peterson <jwpeterson@gmail.com>
2015-03-13 08:25:50 -0600

    Fix unused variable warning.

--- src/systems/nonlinear_implicit_system.C
+++ src/systems/nonlinear_implicit_system.C
@@ -210,7 +210,7 @@ std::pair<unsigned int, Real> NonlinearImplicitSystem::get_linear_solve_paramete
 
 void NonlinearImplicitSystem::assembly(bool get_residual,
                                        bool get_jacobian,
-                                       bool apply_heterogeneous_constraints)
+                                       bool /*apply_heterogeneous_constraints*/)
 {
   // Get current_local_solution in sync
   this->update();
