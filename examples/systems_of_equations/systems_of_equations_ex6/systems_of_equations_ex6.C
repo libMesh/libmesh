55e2b789e5de7d9d724250786539316eeab7286e
John Peterson <jwpeterson@gmail.com>
2015-03-24 11:00:28 -0600

    Fixing unused variable warnings in systems_of_equations_ex6.

--- examples/systems_of_equations/systems_of_equations_ex6/systems_of_equations_ex6.C
+++ examples/systems_of_equations/systems_of_equations_ex6/systems_of_equations_ex6.C
@@ -130,10 +130,7 @@ public:
 
     LinearImplicitSystem& system = es.get_system<LinearImplicitSystem>("Elasticity");
 
-    const unsigned int n_components = 3;
     const unsigned int u_var = system.variable_number ("u");
-    const unsigned int v_var = system.variable_number ("v");
-    const unsigned int w_var = system.variable_number ("w");
 
     const DofMap& dof_map = system.get_dof_map();
     FEType fe_type = dof_map.variable_type(u_var);
