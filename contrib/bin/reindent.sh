58ad13ea274cef37d003e032e80151222e7c7a6e
John Peterson <jwpeterson@gmail.com>
2015-05-27 22:27:28 -0600

    Allow the user to specify what emacs to use when running reindent.sh

--- contrib/bin/reindent.sh
+++ contrib/bin/reindent.sh
@@ -7,6 +7,18 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
+# You can specify what Emacs to use whe you run this script with e.g.
+#
+# EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs reindent.sh foo.C
+#
+# otherwise /usr/bin/emacs will be used.  The /usr/bin/emacs on OSX is
+# very old (circa 2007 22.1.1 on Yosemite in 2015) and there are some
+# bugs in the way that it indents C++ code, so we definitely recommend
+# using something newer if possible.
+if [ -z $EMACS ]; then
+  EMACS=/usr/bin/emacs
+fi
+
 # Print file we are working on
 echo "Indenting $1"
 
@@ -16,7 +28,7 @@ echo "Indenting $1"
 # .) Sets the indentation level within namespaces to 0
 # .) Runs indent-region on the entire file
 # .) Saves the buffer
-/usr/bin/emacs -batch $1  \
+$EMACS -batch $1  \
   --eval="(setq-default indent-tabs-mode nil)" \
   --eval="(c++-mode)" \
   --eval="(c-set-offset 'innamespace 0)" \
