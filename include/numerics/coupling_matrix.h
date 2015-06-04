5f771ed6c5e91fcc258b645d29fa162981078e63
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-06-04 14:34:29 -0500

    Make CouplingMatrix use sparse storage
    So far this is a decrease in memory use at the cost of an increase in
    CPU time; but now we'll be able to add new sparsity-aware APIs to
    enable a big decrease in CPU time.

--- include/numerics/coupling_matrix.h
+++ include/numerics/coupling_matrix.h
@@ -24,13 +24,17 @@
 #include "libmesh/libmesh_common.h"
 
 // C++ includes
+#include <algorithm>
+#include <limits>
+#include <utility> // std::pair
 #include <vector>
 
 namespace libMesh
 {
 
-
-
+// Forward declarations
+class ConstCouplingAccessor;
+class CouplingAccessor;
 
 /**
  * This class defines a coupling matrix.  A coupling
@@ -52,15 +56,15 @@ public:
   /**
    * @returns the (i,j) entry of the matrix.
    */
-  unsigned char operator() (const unsigned int i,
-                            const unsigned int j) const;
+  bool operator() (const unsigned int i,
+                   const unsigned int j) const;
 
   /**
    * @returns the (i,j) entry of the matrix as
-   * a writeable reference.
+   * a smart-reference.
    */
-  unsigned char & operator() (const unsigned int i,
-                              const unsigned int j);
+  CouplingAccessor operator() (const unsigned int i,
+                               const unsigned int j);
 
   /**
    * @returns the size of the matrix, i.e. N for an
@@ -86,13 +90,23 @@ public:
 
 private:
 
+  friend class ConstCouplingAccessor;
+  friend class CouplingAccessor;
+
   /**
-   * The actual matrix values.  These
-   * are stored as unsigned chars because
-   * a vector of bools is not what you
-   * think.
+   * Coupling matrices are typically either full or very sparse, and
+   * all values are only zero or one.
+   *
+   * We store non-zeros as ranges: the first entry of each range pair
+   * is the location of the first non-zero, and the second is the
+   * location of the last subsequent non-zero (*not* the next
+   * subsequent zero; we drop empty ranges).
+   * 
+   * We store locations (i,j) as long integers i*_size+j
    */
-  std::vector<unsigned char> _values;
+  typedef std::pair<std::size_t, std::size_t> range_type;
+  typedef std::vector<range_type> rc_type;
+  rc_type _ranges;
 
   /**
    * The size of the matrix.
@@ -102,14 +116,208 @@ private:
 
 
 
+/**
+ * This accessor class allows simple access to CouplingMatrix values.
+ */
+class ConstCouplingAccessor
+{
+public:
+  ConstCouplingAccessor(std::size_t loc_in,
+                        const CouplingMatrix& mat_in) :
+    _location(loc_in), _mat(mat_in)
+  {
+    libmesh_assert_less(_location, _mat.size() * _mat.size());
+  }
+
+  operator bool() const {
+    const std::size_t max_size = std::numeric_limits<std::size_t>::max();
+
+    // Find the range that might contain i,j
+    // lower_bound isn't *quite* what we want
+    CouplingMatrix::rc_type::const_iterator lb = std::upper_bound
+      (_mat._ranges.begin(), _mat._ranges.end(),
+       std::make_pair(_location, max_size));
+    if (lb!=_mat._ranges.begin())
+      --lb;
+    else
+      lb=_mat._ranges.end();
+
+    // If no range might contain i,j then it's 0
+    if (lb == _mat._ranges.end())
+      return false;
+
+    const std::size_t firstloc = lb->first;
+    const std::size_t lastloc  = lb->second;
+    libmesh_assert_less_equal(firstloc, lastloc);
+    libmesh_assert_less_equal(firstloc, _location);
+
+#ifdef DEBUG
+    CouplingMatrix::rc_type::const_iterator next = lb;
+    next++;
+    if (next != _mat._ranges.end())
+      {
+        // Ranges should be sorted and should not touch
+        libmesh_assert_greater(next->first, lastloc+1);
+      }
+#endif
+
+    return (lastloc >= _location);
+  }
+
+protected:
+
+  std::size_t _location;
+  const CouplingMatrix& _mat;
+};
 
 
+/**
+ * This accessor class allows simple setting of CouplingMatrix values.
+ */
+class CouplingAccessor : public ConstCouplingAccessor
+{
+public:
+  CouplingAccessor(std::size_t loc_in,
+                   CouplingMatrix& mat_in) :
+    ConstCouplingAccessor(loc_in, mat_in), _my_mat(mat_in) {}
+
+  template <typename T>
+  CouplingAccessor& operator = (T new_value)
+  {
+    // For backwards compatibility we take integer arguments,
+    // but coupling matrix entries are really all zero or one.
+    const bool as_bool = new_value;
+    libmesh_assert_equal_to(new_value, as_bool);
+
+    *this = as_bool;
+    return *this;
+  }
+
+  CouplingAccessor& operator = (bool new_value)
+  {
+    const std::size_t max_size = std::numeric_limits<std::size_t>::max();
+
+    // Find the range that might contain i,j
+    // lower_bound isn't *quite* what we want
+    CouplingMatrix::rc_type::iterator lb =
+      std::upper_bound (_my_mat._ranges.begin(), _my_mat._ranges.end(),
+                        std::make_pair(_location, max_size));
+    if (lb!=_my_mat._ranges.begin())
+      --lb;
+    else
+      lb=_my_mat._ranges.end();
+
+    // If no range might contain i,j then we might need to make a new
+    // one.
+    if (lb == _my_mat._ranges.end())
+      {
+        if (new_value == true)
+          _my_mat._ranges.insert(_my_mat._ranges.begin(),
+                                 std::make_pair(_location, _location));
+      }
+    else
+      {
+        const std::size_t firstloc = lb->first;
+        const std::size_t lastloc  = lb->second;
+        libmesh_assert_less_equal(firstloc, lastloc);
+        libmesh_assert_less_equal(firstloc, _location);
+
+#ifdef DEBUG
+        CouplingMatrix::rc_type::const_iterator next = lb;
+        next++;
+        if (next != _my_mat._ranges.end())
+          {
+            // Ranges should be sorted and should not touch
+            libmesh_assert_greater(next->first, lastloc+1);
+          }
+#endif
+
+        // If we're in this range, we might need to shorten or remove
+        // or split it
+        if (new_value == false)
+          {
+            if (_location == firstloc)
+              {
+                if (_location == lastloc)
+                  {
+                    _my_mat._ranges.erase(lb);
+                  }
+                else
+                  {
+                    libmesh_assert_less (lb->first, lastloc);
+                    lb->first++;
+                  }
+              }
+            else if (_location == lastloc)
+              {
+                libmesh_assert_less (firstloc, lb->second);
+
+                lb->second--;
+              }
+            else if (_location < lastloc)
+              {
+                libmesh_assert_less_equal(_location+1, lastloc);
+
+                lb->first = _location+1;
+
+                libmesh_assert_less_equal(firstloc, _location-1);
+
+                _my_mat._ranges.insert
+                  (lb, std::make_pair(firstloc, _location-1));
+              }
+          }
+
+        // If we're not in this range, we might need to extend it or
+        // join it with its neighbor or add a new one.
+        else // new_value == true
+          {
+            CouplingMatrix::rc_type::iterator next = lb;
+            next++;
+            const std::size_t nextloc =
+              (next == _my_mat._ranges.end()) ?
+              std::numeric_limits<std::size_t>::max() :
+              next->first;
+
+            // Ranges should be sorted and should not touch
+            libmesh_assert_greater(nextloc, lastloc+1);
+
+            if (_location > lastloc)
+              {
+                if (_location == lastloc + 1)
+                  {
+                    if (_location == nextloc - 1)
+                      {
+                        next->first = firstloc;
+                        _my_mat._ranges.erase(lb);
+                      }
+                    else
+                      lb->second++;
+                  }
+                else
+                  {
+                    if (_location == nextloc - 1)
+                      next->first--;
+                    else
+                      _my_mat._ranges.insert
+                        (next, std::make_pair(_location, _location));
+                  }
+              }
+          }
+      }
+
+    return *this;
+  }
+
+private:
+
+  CouplingMatrix& _my_mat;
+};
 
 //--------------------------------------------------
 // CouplingMatrix inline methods
 inline
 CouplingMatrix::CouplingMatrix (const unsigned int n) :
-  _values(), _size(n)
+  _ranges(), _size(n)
 {
   this->resize(n);
 }
@@ -117,25 +325,27 @@ CouplingMatrix::CouplingMatrix (const unsigned int n) :
 
 
 inline
-unsigned char CouplingMatrix::operator() (const unsigned int i,
-                                          const unsigned int j) const
+bool CouplingMatrix::operator() (const unsigned int i,
+                                 const unsigned int j) const
 {
   libmesh_assert_less (i, _size);
   libmesh_assert_less (j, _size);
 
-  return _values[i*_size + j];
+  const std::size_t location = std::size_t(i)*_size + j;
+
+  return bool(ConstCouplingAccessor(location, *this));
 }
 
 
 
-inline
-unsigned char & CouplingMatrix::operator() (const unsigned int i,
-                                            const unsigned int j)
-{
-  libmesh_assert_less (i, _size);
-  libmesh_assert_less (j, _size);
 
-  return _values[i*_size + j];
+inline
+CouplingAccessor CouplingMatrix::operator() (const unsigned int i,
+                                             const unsigned int j)
+{
+  const std::size_t location = std::size_t(i)*_size + j;
+
+  return CouplingAccessor(location, *this);
 }
 
 
@@ -153,10 +363,7 @@ void CouplingMatrix::resize(const unsigned int n)
 {
   _size = n;
 
-  _values.resize(_size*_size);
-
-  for (unsigned int i=0; i<_values.size(); i++)
-    _values[i] = 0;
+  _ranges.clear();
 }
 
 
@@ -164,9 +371,7 @@ void CouplingMatrix::resize(const unsigned int n)
 inline
 void CouplingMatrix::clear()
 {
-  _size = 0;
-
-  _values.clear();
+  this->resize(0);
 }
 
 
