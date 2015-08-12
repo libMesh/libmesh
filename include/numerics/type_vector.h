libMesh/libmesh

commit 25f36346950e30c372be16ce49ade052638e3212

Allow vector construction with mixed scalars
This fixes a number of bugs when Real>double

include/numerics/type_vector.h
@@ -79,12 +79,16 @@ protected:
    * Constructor-from-scalars.  By default sets higher dimensional
    * entries to 0.
    */
-  template <typename Scalar>
-  TypeVector (const Scalar x,
-              const Scalar y=0,
+  template <typename Scalar1, typename Scalar2, typename Scalar3>
+  TypeVector (typename
+              boostcopy::enable_if_c<ScalarTraits<Scalar1>::value,
+              const Scalar1>::type x,
               typename
-              boostcopy::enable_if_c<ScalarTraits<Scalar>::value,
-              const Scalar>::type z=0);
+              boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
+              const Scalar2>::type y,
+              typename
+              boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
+              const Scalar3>::type z=0);
 
 public:
 
@@ -394,13 +398,17 @@ TypeVector<T>::TypeVector (const T x,
 
 
 template <typename T>
-template <typename Scalar>
+template <typename Scalar1, typename Scalar2, typename Scalar3>
 inline
-TypeVector<T>::TypeVector (const Scalar x,
-                           const Scalar y,
+TypeVector<T>::TypeVector (typename
+                           boostcopy::enable_if_c<ScalarTraits<Scalar1>::value,
+                           const Scalar1>::type x,
                            typename
-                           boostcopy::enable_if_c<ScalarTraits<Scalar>::value,
-                           const Scalar>::type z)
+                           boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
+                           const Scalar2>::type y,
+                           typename
+                           boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
+                           const Scalar3>::type z)
 {
   _coords[0] = x;
 

include/numerics/vector_value.h
@@ -59,12 +59,16 @@ public:
    * Constructor-from-scalars.  By default sets higher dimensional
    * entries to 0.
    */
-  template <typename Scalar>
-  VectorValue (const Scalar x,
-               const Scalar y=0,
+  template <typename Scalar1, typename Scalar2, typename Scalar3>
+  VectorValue (typename
+               boostcopy::enable_if_c<ScalarTraits<Scalar1>::value,
+               const Scalar1>::type x,
                typename
-               boostcopy::enable_if_c<ScalarTraits<Scalar>::value,
-               const Scalar>::type z=0);
+               boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
+               const Scalar2>::type y,
+               typename
+               boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
+               const Scalar3>::type z = 0);
 
   /**
    * Copy-constructor.
@@ -140,13 +144,17 @@ VectorValue<T>::VectorValue (const T x,
 
 
 template <typename T>
-template <typename Scalar>
+template <typename Scalar1, typename Scalar2, typename Scalar3>
 inline
-VectorValue<T>::VectorValue (const Scalar x,
-                             const Scalar y,
+VectorValue<T>::VectorValue (typename
+                             boostcopy::enable_if_c<ScalarTraits<Scalar1>::value,
+                             const Scalar1>::type x,
                              typename
-                             boostcopy::enable_if_c<ScalarTraits<Scalar>::value,
-                             const Scalar>::type z) :
+                             boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
+                             const Scalar2>::type y,
+                             typename
+                             boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
+                             const Scalar3>::type z) :
   TypeVector<T> (x,y,z)
 {
 }

commit 6bef5e97ef0ef594e155139758be70cee692bbc0

Restore default y=0 in vector constructor
This bug was unintentionally introduced by 25f3634 and broke
--enable-complex builds.  Thanks to @dknez for catching it.

include/numerics/type_vector.h
@@ -85,7 +85,7 @@ protected:
               const Scalar1>::type x,
               typename
               boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
-              const Scalar2>::type y,
+              const Scalar2>::type y=0,
               typename
               boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
               const Scalar3>::type z=0);

include/numerics/vector_value.h
@@ -65,7 +65,7 @@ public:
                const Scalar1>::type x,
                typename
                boostcopy::enable_if_c<ScalarTraits<Scalar2>::value,
-               const Scalar2>::type y,
+               const Scalar2>::type y = 0,
                typename
                boostcopy::enable_if_c<ScalarTraits<Scalar3>::value,
                const Scalar3>::type z = 0);