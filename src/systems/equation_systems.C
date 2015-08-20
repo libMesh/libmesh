libMesh/libmesh

commit 103227d63838ce55d24af8bd05a6735188e5dedc

Updates to work with PETSc 3.6.0 when debugging is enabled.
There were several places in the library where we were closing PETSc
Vecs unnecessarily or at the wrong time, which produces the following
error when debugging is enabled:

[0]PETSC ERROR: Object is in wrong state
[0]PETSC ERROR:  Vec is locked read only, argument # 1

Other specific situations are as follows:

* We can assume that the Vecs we are passed in the residual and
  jacobian callback functions are closed, and therefore if we don't call
  VecSetValues on them, we shouldn't attempt to close them ourselves.

* Remove the close() call from the PetscVector constructor that takes
  a Vec.  We can't assume the incoming Vec is not closed, and with PETSc
  debugging enabled, it's an error to close it in that case...

* Avoid redundant solution->close() in EquationSystems.  We had one in
  both build_solution_vector() and get_solution().  This issue arose
  with a debug-enabled PETSc in a MOOSE test where we write output
  from the PETSc SNESMonitor callback function.  The solution vector
  is locked read-only in this context, so it is either an error (or
  simply redundant) to close it here.

Refs #604.
Refs idaholab/moose#5235.

src/systems/equation_systems.C
@@ -790,7 +790,13 @@ void EquationSystems::build_solution_vector (std::vector<Number>& soln,
       // Update the current_local_solution
       {
         System & non_const_sys = const_cast<System &>(system);
-        non_const_sys.solution->close();
+        // We used to simply call non_const_sys.solution->close()
+        // here, but that is not allowed when the solution vector is
+        // locked read-only, for example when printing the solution
+        // during during the middle of a solve...  So try to be a bit
+        // more careful about calling close() unnecessarily.
+        if (!non_const_sys.solution->closed())
+          non_const_sys.solution->close();
         non_const_sys.update();
       }
 
@@ -952,7 +958,13 @@ void EquationSystems::get_solution (std::vector<Number>& soln,
       // Update the current_local_solution
       {
         System & non_const_sys = const_cast<System &>(system);
-        non_const_sys.solution->close();
+        // We used to simply call non_const_sys.solution->close()
+        // here, but that is not allowed when the solution vector is
+        // locked read-only, for example when printing the solution
+        // during during the middle of a solve...  So try to be a bit
+        // more careful about calling close() unnecessarily.
+        if (!non_const_sys.solution->closed())
+          non_const_sys.solution->close();
         non_const_sys.update();
       }
 

include/numerics/petsc_vector.h
@@ -746,8 +746,6 @@ PetscVector<T>::PetscVector (Vec v,
     }
   else
     this->_type = SERIAL;
-
-  this->close();
 }
 
 

src/solvers/petsc_nonlinear_solver.C
@@ -148,7 +148,6 @@ extern "C"
       libmesh_error_msg("Error! Unable to compute residual and/or Jacobian!");
 
     R.close();
-    X_global.close();
 
     STOP_LOG("residual()", "PetscNonlinearSolver");
 
@@ -243,7 +242,6 @@ extern "C"
 
     PC.close();
     Jac.close();
-    X_global.close();
 #if PETSC_RELEASE_LESS_THAN(3,5,0)
     *msflag = SAME_NONZERO_PATTERN;
 #endif

commit 8f04070837da034e0a1574ce3417fd4b0e571bc6

Verify that processors all agree on closed()
This catches the bug introduced in 103227d6, and is easier to debug
than the loss of sync which otherwise ensues.

src/systems/equation_systems.C
@@ -795,6 +795,7 @@ void EquationSystems::build_solution_vector (std::vector<Number>& soln,
         // locked read-only, for example when printing the solution
         // during during the middle of a solve...  So try to be a bit
         // more careful about calling close() unnecessarily.
+        libmesh_assert(this->comm().verify(non_const_sys.solution->closed()));
         if (!non_const_sys.solution->closed())
           non_const_sys.solution->close();
         non_const_sys.update();
@@ -963,6 +964,7 @@ void EquationSystems::get_solution (std::vector<Number>& soln,
         // locked read-only, for example when printing the solution
         // during during the middle of a solve...  So try to be a bit
         // more careful about calling close() unnecessarily.
+        libmesh_assert(this->comm().verify(non_const_sys.solution->closed()));
         if (!non_const_sys.solution->closed())
           non_const_sys.solution->close();
         non_const_sys.update();