72fbeda6d66e762d163b3614673ddd17903fca30
Benjamin S. Kirk <benjamin.kirk@nasa.gov>
2015-09-03 14:38:59 -0500

    fix for --enable-complex

--- examples/adjoints/adjoints_ex5/initial.C
+++ examples/adjoints/adjoints_ex5/initial.C
@@ -51,6 +51,6 @@ Gradient initial_grad(const Point& p,
 {
   Real x = p(0), y = p(1);
 
-  return Gradient(M_PI*cos(M_PI * x) * sin(M_PI * y),
-                  M_PI*sin(M_PI * x) * cos(M_PI * y));
+  return Gradient(Number(M_PI*cos(M_PI * x) * sin(M_PI * y)),
+                  Number(M_PI*sin(M_PI * x) * cos(M_PI * y)));
 }
