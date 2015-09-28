0c8cbf9f75ef24703a54967a15e55c91ad7563f9
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-09-28 12:03:41 -0500

    Test Parsed*Function::set_inline_value()
    Only a couple basic tests, but they caught multiple bugs at first.

--- tests/numerics/parsed_function_test.C
+++ tests/numerics/parsed_function_test.C
@@ -30,6 +30,7 @@ public:
 
   CPPUNIT_TEST(testValues);
   CPPUNIT_TEST(testInlineGetter);
+  CPPUNIT_TEST(testInlineSetter);
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -70,6 +71,31 @@ private:
 
     CPPUNIT_ASSERT_DOUBLES_EQUAL
       (libmesh_real(cxy8.get_inline_value("c")), 2.5, TOLERANCE*TOLERANCE);
+  }
+
+  void testInlineSetter()
+  {
+    ParsedFunction<Number> ax2("a:=4.5;a*x*2");
+    ax2.set_inline_value("a", 2.5);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL
+      (libmesh_real(ax2(Point(0.25,0.25,0.25))), 1.25, TOLERANCE*TOLERANCE);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL
+      (libmesh_real(ax2.get_inline_value("a")), 2.5, TOLERANCE*TOLERANCE);
+
+    ParsedFunction<Number> cxy8
+      ("a := 4 ; b := a/2+1; c:=b-a+3.5; c*x*2*y*4");
+    cxy8.set_inline_value("a", 2);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL
+      (libmesh_real(cxy8(Point(0.5,0.5,0.5))), 7.0, TOLERANCE*TOLERANCE);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL
+      (libmesh_real(cxy8.get_inline_value("b")), 2.0, TOLERANCE*TOLERANCE);
+
+    CPPUNIT_ASSERT_DOUBLES_EQUAL
+      (libmesh_real(cxy8.get_inline_value("c")), 3.5, TOLERANCE*TOLERANCE);
   }
 
 };
