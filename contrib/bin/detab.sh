9d141692174559eb6998d5c6613ab58a3d045060
John Peterson <jwpeterson@gmail.com>
2015-10-12 13:16:51 -0600

    Allow user to specify emacs to use for detabbing.

--- contrib/bin/detab.sh
+++ contrib/bin/detab.sh
@@ -6,6 +6,18 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
+# You can specify what Emacs to use whe you run this script with e.g.
+#
+# EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs detab.sh foo.C
+#
+# otherwise /usr/bin/emacs will be used.  The /usr/bin/emacs on OSX is
+# very old (circa 2007 22.1.1 on Yosemite in 2015) and there are some
+# bugs in the way that it indents C++ code, so we definitely recommend
+# using something newer if possible.
+if [ -z $EMACS ]; then
+  EMACS=/usr/bin/emacs
+fi
+
 # Use perl to get a list of line numbers that have <TAB> characters
 tabbed_lines=(`perl -ne "print \"$. \" if /\t/" $1`)
 
@@ -32,7 +44,7 @@ if [ $n -gt 0 ]; then
   # Construct the command you want to run as a string, and then use
   # 'eval' to run it.  Note that this is *not* the same thing as
   # running, say, `$cmd`.
-  cmd="/usr/bin/emacs -batch $1 $eval_commands -f save-buffer &> /dev/null"
+  cmd="$EMACS -batch $1 $eval_commands -f save-buffer &> /dev/null"
   eval $cmd
 else
   echo "Nothing to be done in $1."
