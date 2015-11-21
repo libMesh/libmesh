a062e046d7ad06756b320d73da5bcd8bc7a4c294
John Peterson <jwpeterson@gmail.com>
2015-11-21 12:10:16 -0700

    Refactor InfHex18::build_side()

--- src/geom/cell_inf_hex18.C
+++ src/geom/cell_inf_hex18.C
@@ -190,78 +190,20 @@ UniquePtr<Elem> InfHex18::build_side (const unsigned int i,
       // Think of a unit cube: (-1,1) x (-1,1) x (1,1)
       switch (i)
         {
-        case 0: // the base face
+          // the base face
+        case 0:
           {
             face = new Quad9;
-
-            // This is the exception: all other face elements' normals
-            // point outwards; but the base element's normal points inward
-            face->set_node(0) = this->get_node(0);
-            face->set_node(1) = this->get_node(1);
-            face->set_node(2) = this->get_node(2);
-            face->set_node(3) = this->get_node(3);
-            face->set_node(4) = this->get_node(8);
-            face->set_node(5) = this->get_node(9);
-            face->set_node(6) = this->get_node(10);
-            face->set_node(7) = this->get_node(11);
-            face->set_node(8) = this->get_node(16);
-
             break;
           }
 
-        case 1:  // connecting to another infinite element
+          // connecting to another infinite element
+        case 1:
+        case 2:
+        case 3:
+        case 4:
           {
             face = new InfQuad6;
-
-            face->set_node(0) = this->get_node(0);
-            face->set_node(1) = this->get_node(1);
-            face->set_node(2) = this->get_node(4);
-            face->set_node(3) = this->get_node(5);
-            face->set_node(4) = this->get_node(8);
-            face->set_node(5) = this->get_node(12);
-
-            break;
-          }
-
-        case 2:  // connecting to another infinite element
-          {
-            face = new InfQuad6;
-
-            face->set_node(0) = this->get_node(1);
-            face->set_node(1) = this->get_node(2);
-            face->set_node(2) = this->get_node(5);
-            face->set_node(3) = this->get_node(6);
-            face->set_node(4) = this->get_node(9);
-            face->set_node(5) = this->get_node(13);
-
-            break;
-          }
-
-        case 3:  // connecting to another infinite element
-          {
-            face = new InfQuad6;
-
-            face->set_node(0) = this->get_node(2);
-            face->set_node(1) = this->get_node(3);
-            face->set_node(2) = this->get_node(6);
-            face->set_node(3) = this->get_node(7);
-            face->set_node(4) = this->get_node(10);
-            face->set_node(5) = this->get_node(14);
-
-            break;
-          }
-
-        case 4:  // connecting to another infinite element
-          {
-            face = new InfQuad6;
-
-            face->set_node(0) = this->get_node(3);
-            face->set_node(1) = this->get_node(0);
-            face->set_node(2) = this->get_node(7);
-            face->set_node(3) = this->get_node(4);
-            face->set_node(4) = this->get_node(11);
-            face->set_node(5) = this->get_node(15);
-
             break;
           }
 
@@ -270,6 +212,11 @@ UniquePtr<Elem> InfHex18::build_side (const unsigned int i,
         }
 
       face->subdomain_id() = this->subdomain_id();
+
+      // Set the nodes
+      for (unsigned n=0; n<face->n_nodes(); ++n)
+        face->set_node(n) = this->get_node(InfHex18::side_nodes_map[i][n]);
+
       return UniquePtr<Elem>(face);
     }
 
