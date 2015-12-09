f4ccba9af9131f3adaef3392fd86d67403d27814
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-12-09 15:08:00 -0600

    More packed range unit tests

--- tests/parallel/packed_range_test.C
+++ tests/parallel/packed_range_test.C
@@ -92,7 +92,8 @@ public:
   CPPUNIT_TEST_SUITE( PackedRangeTest );
 
   CPPUNIT_TEST( testNullAllGather );
-//  CPPUNIT_TEST( testNullSendReceive );
+  CPPUNIT_TEST( testNullSendReceive );
+  CPPUNIT_TEST( testContainerSendReceive );
 //  CPPUNIT_TEST( testAdapterSendReceive );
 //  CPPUNIT_TEST( testPointerAdapterSendReceive );
 
@@ -122,6 +123,68 @@ public:
     TestCommWorld->allgather_packed_range
       ((void *)(NULL), send.begin(), send.end(),
        libMesh::null_output_iterator<std::string>());
+  }
+
+
+  void testNullSendReceive()
+  {
+    std::vector<processor_id_type> vals;
+
+    std::vector<std::string> send(1);
+    const unsigned int my_rank = TestCommWorld->rank();
+    const unsigned int dest_rank =
+      (my_rank + 1) % TestCommWorld->size();
+    const unsigned int source_rank =
+      (my_rank + TestCommWorld->size() - 1) % TestCommWorld->size();
+
+    {
+      std::ostringstream os;
+      os << my_rank;
+      send[0] = os.str();
+    }
+
+    TestCommWorld->send_receive_packed_range
+      (dest_rank, (void *)(NULL), send.begin(), send.end(),
+       source_rank, (void *)(NULL),
+       libMesh::null_output_iterator<std::string>(),
+       (std::string*)NULL);
+  }
+
+
+  void testContainerSendReceive()
+  {
+    std::vector<processor_id_type> vals;
+
+    std::vector<std::string> send(1), recv;
+
+    const unsigned int my_rank = TestCommWorld->rank();
+    const unsigned int dest_rank =
+      (my_rank + 1) % TestCommWorld->size();
+    const unsigned int source_rank =
+      (my_rank + TestCommWorld->size() - 1) % TestCommWorld->size();
+
+    {
+      std::ostringstream os;
+      os << my_rank;
+      send[0] = os.str();
+    }
+
+    TestCommWorld->send_receive_packed_range
+      (dest_rank, (void *)(NULL), send.begin(), send.end(),
+       source_rank, (void *)(NULL),
+       std::back_inserter(recv),
+       (std::string*)NULL);
+
+    CPPUNIT_ASSERT_EQUAL(recv.size(), std::size_t(1));
+
+    std::string check;
+    {
+      std::ostringstream os;
+      os << source_rank;
+      check = os.str();
+    }
+
+    CPPUNIT_ASSERT_EQUAL(recv[0], check);
   }
 
 };
