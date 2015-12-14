7650ed9c627a1d485801a16a2321c3487f0a71e9
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-12-14 10:53:07 -0600

    Widen NewtonSolver immediate-convergence test
    Not only should we converge on our initial iterate if it has a zero
    residual, we should converge if it has a residual under our
    absolute_residual_tolerance.
    
    This should fix the cases where we try to re-solve an oversolved
    solution and we tell PETSc to use a linear tolerance above 1.0

--- src/solvers/newton_solver.C
+++ src/solvers/newton_solver.C
@@ -324,24 +324,29 @@ unsigned int NewtonSolver::solve()
           continue;
         }
 
-      if (current_residual == 0)
+      if (current_residual <= absolute_residual_tolerance)
         {
           if (verbose)
-            libMesh::out << "Linear solve unnecessary; residual = 0"
+            libMesh::out << "Linear solve unnecessary; residual "
+                         << current_residual
+                         << " meets absolute tolerance "
+                         << absolute_residual_tolerance
                          << std::endl;
 
           // We're not doing a solve, but other code may reuse this
           // matrix.
           matrix.close();
 
-          if (absolute_residual_tolerance > 0)
-            _solve_result |= CONVERGED_ABSOLUTE_RESIDUAL;
-          if (relative_residual_tolerance > 0)
-            _solve_result |= CONVERGED_RELATIVE_RESIDUAL;
-          if (absolute_step_tolerance > 0)
-            _solve_result |= CONVERGED_ABSOLUTE_STEP;
-          if (relative_step_tolerance > 0)
-            _solve_result |= CONVERGED_RELATIVE_STEP;
+          _solve_result |= CONVERGED_ABSOLUTE_RESIDUAL;
+          if (current_residual == 0)
+            {
+              if (relative_residual_tolerance > 0)
+                _solve_result |= CONVERGED_RELATIVE_RESIDUAL;
+              if (absolute_step_tolerance > 0)
+                _solve_result |= CONVERGED_ABSOLUTE_STEP;
+              if (relative_step_tolerance > 0)
+                _solve_result |= CONVERGED_RELATIVE_STEP;
+            }
 
           break;
         }
