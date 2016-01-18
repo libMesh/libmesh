a2db298adc355333fdd121cd6c911aa77ba7163a
John Peterson <jwpeterson@gmail.com>
2016-01-18 11:00:03 -0700

    Update copyright notice for 2016.
    find {examples,src,include} -name "*.[Ch]" | xargs perl -pli -e 's/2002-2015/2002-2016/'

--- src/fe/fe_type.C
+++ src/fe/fe_type.C
@@ -1,6 +1,6 @@
 
 // The libMesh Finite Element Library.
-// Copyright (C) 2002-2015 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+// Copyright (C) 2002-2016 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
 
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
