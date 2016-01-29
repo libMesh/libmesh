102c779793ae723b658a7e83fac508d75b22fc59
John Peterson <jwpeterson@gmail.com>
2016-01-29 10:47:21 -0700

    Optimize Hex8::volume() slightly.
    If my calculations are correct, the "geometric" formula I used
    previously had about 12 dot products and 12 cross products, while the
    current one only has 4 of each.  This formula is derived by writing out the
    standard volume formula and dropping terms which are zero due to the
    symmetry of the integrand and/or triple products containing two copies
    of the same vector.
    
    While the original geometric formula was already pretty fast, this one
    is about 1.7x faster (about 0.1697s vs 0.2866s to call volume() on
    3.375M Hex8 elements).

--- src/geom/cell_hex8.C
+++ src/geom/cell_hex8.C
@@ -320,51 +320,35 @@ const float Hex8::_embedding_matrix[8][8][8] =
 
 Real Hex8::volume () const
 {
-  // Compute the volume of the tri-linear hex by splitting it
-  // into 6 sub-pyramids and applying the formula in:
-  // "Calculation of the Volume of a General Hexahedron
-  // for Flow Predictions", AIAA Journal v.23, no.6, 1984, p.954-
+  // Make copies of our points.  It makes the subsequent calculations a bit
+  // shorter and avoids dereferencing the same pointer multiple times.
+  Point
+    x0 = point(0), x1 = point(1), x2 = point(2), x3 = point(3),
+    x4 = point(4), x5 = point(5), x6 = point(6), x7 = point(7);
 
-  static const unsigned char sub_pyr[6][4] =
-    {
-      {0, 3, 2, 1},
-      {6, 7, 4, 5},
-      {0, 1, 5, 4},
-      {3, 7, 6, 2},
-      {0, 4, 7, 3},
-      {1, 2, 6, 5}
-    };
+  // Construct constant data vectors.  The notation is:
+  // \vec{x}_{\xi}   = \vec{a1}*eta*zeta + \vec{b1}*eta + \vec{c1}*zeta + \vec{d1}
+  // \vec{x}_{\eta}  = \vec{a2}*xi*zeta  + \vec{b2}*xi  + \vec{c2}*zeta + \vec{d2}
+  // \vec{x}_{\zeta} = \vec{a3}*xi*eta   + \vec{b3}*xi  + \vec{c3}*eta  + \vec{d3}
+  // but it turns out that a1, a2, and a3 are not needed for the volume calculation.
+  // This is copy-pasted directly from the output of a Python script, other
+  // than division by 8, which is done at the end when the volume is returned.
+  Point
+    b1 = x0 - x1 + x2 - x3 + x4 - x5 + x6 - x7,
+    c1 = x0 - x1 - x2 + x3 - x4 + x5 + x6 - x7,
+    d1 = -x0 + x1 + x2 - x3 - x4 + x5 + x6 - x7,
 
-  // The centroid is a convenient point to use
-  // for the apex of all the pyramids.
-  const Point R = this->centroid();
-  Node * pyr_base[4];
+    b2 = b1,
+    c2 = x0 + x1 - x2 - x3 - x4 - x5 + x6 + x7,
+    d2 = -x0 - x1 + x2 + x3 - x4 - x5 + x6 + x7,
 
-  Real vol=0.;
+    b3 = c1,
+    c3 = c2,
+    d3 = -x0 - x1 - x2 - x3 + x4 + x5 + x6 + x7;
 
-  // Compute the volume using 6 sub-pyramids
-  for (unsigned int n=0; n<6; ++n)
-    {
-      // Set the nodes of the pyramid base
-      for (unsigned int i=0; i<4; ++i)
-        pyr_base[i] = this->_nodes[sub_pyr[n][i]];
-
-      // Compute diff vectors
-      Point a ( *pyr_base[0] - R );
-      Point b ( *pyr_base[1] - *pyr_base[3] );
-      Point c ( *pyr_base[2] - *pyr_base[0] );
-      Point d ( *pyr_base[3] - *pyr_base[0] );
-      Point e ( *pyr_base[1] - *pyr_base[0] );
-
-      // Compute pyramid volume
-      Real sub_vol = (1./6.)*(a*(b.cross(c))) + (1./12.)*(c*(d.cross(e)));
-
-      libmesh_assert (sub_vol>0.);
-
-      vol += sub_vol;
-    }
-
-  return vol;
+  // We could check for a quick return, but it's almost faster to just
+  // compute the result...
+  return (b1 * d2.cross(c3) + d1 * b2.cross(b3) + c1 * c2.cross(d3)) / 192. + (d1 * d2.cross(d3)) / 64.;
 }
 
 } // namespace libMesh
