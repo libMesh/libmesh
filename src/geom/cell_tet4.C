30ef9790f13aeec31e5ae4134c05ec6076ca376d
John Peterson <jwpeterson@gmail.com>
2016-02-06 16:47:32 -0700

    Use triple_product in Tet4::volume().
    Also use point() instead of dereferencing the result of get_node().

--- src/geom/cell_tet4.C
+++ src/geom/cell_tet4.C
@@ -287,15 +287,15 @@ Real Tet4::volume () const
 {
   // The volume of a tetrahedron is 1/6 the box product formed
   // by its base and apex vectors
-  Point a ( *this->get_node(3) - *this->get_node(0) );
+  Point a = point(3) - point(0);
 
   // b is the vector pointing from 0 to 1
-  Point b ( *this->get_node(1) - *this->get_node(0) );
+  Point b = point(1) - point(0);
 
   // c is the vector pointing from 0 to 2
-  Point c ( *this->get_node(2) - *this->get_node(0) );
+  Point c = point(2) - point(0);
 
-  return (1.0 / 6.0) * (a * (b.cross(c)));
+  return triple_product(a, b, c) / 6.;
 }
 
 
