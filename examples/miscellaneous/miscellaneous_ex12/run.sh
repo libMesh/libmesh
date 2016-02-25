ef1ad13fb2d237ef333aaf4fda64936baf7871d8
John Peterson <jwpeterson@gmail.com>
2016-02-25 10:19:15 -0700

    Actually this example works just fine with Jacobi+CG.
    On the other hand, it doesn't work well at all with ILU+GMRES, taking
    over 5000 iterations and getting the wrong answer:
    
    4999 KSP Residual norm 2.017539331897e-08
    5000 KSP Residual norm 2.017539331897e-08
    z-displacement of the point C: 77.8326
    Analytic solution: 164.24
    y-displacement of the point D: -0.0530033
    Analytic solution: 4.114

--- examples/miscellaneous/miscellaneous_ex12/run.sh
+++ examples/miscellaneous/miscellaneous_ex12/run.sh
@@ -6,6 +6,6 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=miscellaneous_ex12
 
-options="-pc_type lu -pc_factor_mat_solver_package mumps"
+options="-pc_type jacobi -ksp_type cg"
 
 run_example "$example_name" "$options"
