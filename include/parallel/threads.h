libMesh/libmesh

commit bca60c6aa668b84b47003106f31823375a3206b2

Removing pthread_unique_id() from Threads
This method is buggy and unused by libMesh
it should simply be removed.

closes #801

include/parallel/threads.h
@@ -433,15 +433,6 @@ private:
   pthread_mutexattr_t attr;
 };
 
-extern std::map<pthread_t, unsigned int> _pthread_unique_ids;
-extern spin_mutex _pthread_unique_id_mutex;
-
-/**
- * When called by a thread this will return a unique number from 0 to num_pthreads-1
- * Very useful for creating long-lived thread local storage
- */
-unsigned int pthread_unique_id();
-
 template <typename Range>
 unsigned int num_pthreads(Range & range)
 {
@@ -548,14 +539,12 @@ void parallel_for (const Range & range, const Body & body)
     {
 #if LIBMESH_HAVE_OPENMP
       run_body<Range, Body>((void *)&range_bodies[i]);
-#else // Just use Pthreads
-      spin_mutex::scoped_lock lock(_pthread_unique_id_mutex);
+#elif LIBMESH_HAVE_PTHREAD
       pthread_create(&threads[i], NULL, &run_body<Range, Body>, (void *)&range_bodies[i]);
-      _pthread_unique_ids[threads[i]] = i;
 #endif
     }
 
-#if !LIBMESH_HAVE_OPENMP
+#if LIBMESH_HAVE_PTHREAD
   // Wait for them to finish
 
   // The use of 'int' instead of unsigned for the iteration variable
@@ -565,11 +554,7 @@ void parallel_for (const Range & range, const Body & body)
   // behavior and optimization.
   // http://blog.llvm.org/2011/05/what-every-c-programmer-should-know.html
   for (int i=0; i<static_cast<int>(n_threads); i++)
-    {
       pthread_join(threads[i], NULL);
-      spin_mutex::scoped_lock lock(_pthread_unique_id_mutex);
-      _pthread_unique_ids.erase(threads[i]);
-    }
 #endif
 
   // Clean up
@@ -661,21 +646,15 @@ void parallel_reduce (const Range & range, Body & body)
     {
 #if LIBMESH_HAVE_OPENMP
       run_body<Range, Body>((void *)&range_bodies[i]);
-#else // Just use Pthreads
-      spin_mutex::scoped_lock lock(_pthread_unique_id_mutex);
+#elif LIBMESH_HAVE_PTHREAD
       pthread_create(&threads[i], NULL, &run_body<Range, Body>, (void *)&range_bodies[i]);
-      _pthread_unique_ids[threads[i]] = i;
 #endif
     }
 
-#if !LIBMESH_HAVE_OPENMP
+#if LIBMESH_HAVE_PTHREAD
   // Wait for them to finish
   for(unsigned int i=0; i<n_threads; i++)
-    {
       pthread_join(threads[i], NULL);
-      spin_mutex::scoped_lock lock(_pthread_unique_id_mutex);
-      _pthread_unique_ids.erase(threads[i]);
-    }
 #endif
 
   // Join them all down to the original Body

src/parallel/threads.C
@@ -28,26 +28,10 @@
 namespace libMesh
 {
 
-#if !defined(LIBMESH_HAVE_TBB_API) && defined(LIBMESH_HAVE_PTHREAD)
-std::map<pthread_t, unsigned int> Threads::_pthread_unique_ids;
-Threads::spin_mutex Threads::_pthread_unique_id_mutex;
-
-unsigned int Threads::pthread_unique_id()
-{
-#if LIBMESH_HAVE_OPENMP
-  return omp_get_thread_num();
-#else
-  spin_mutex::scoped_lock lock(_pthread_unique_id_mutex);
-  return _pthread_unique_ids[pthread_self()];
-#endif
-}
-#endif
-
 //-------------------------------------------------------------------------
 // Threads:: object instantiation
 Threads::spin_mutex Threads::spin_mtx;
 Threads::recursive_mutex Threads::recursive_mtx;
 bool Threads::in_threads = false;
-
 
 } // namespace libMesh

commit 5f783cbcb79e6ddc8870b068608aacee88889fe8

Fix in Threads::parallel_for().
This reverts a regression introduced by bca60c6aa, but is not the
complete overhaul of threads.h that is discussed in #838. It fixes
a definite issue with parallel_for() when both OpenMP and pthreads are
active at the same time.

include/parallel/threads.h
@@ -549,7 +549,7 @@ void parallel_for (const Range & range, const Body & body)
 #endif
     }
 
-#if LIBMESH_HAVE_PTHREAD
+#if !LIBMESH_HAVE_OPENMP
   // Wait for them to finish
 
   // The use of 'int' instead of unsigned for the iteration variable