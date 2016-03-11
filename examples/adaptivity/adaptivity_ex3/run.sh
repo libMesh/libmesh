e39f6f89fa73d210d923a4b19bf3108b44a4d175
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-03-11 13:19:27 -0600

    Only run matchedhp to what default solvers handle

--- examples/adaptivity/adaptivity_ex3/run.sh
+++ examples/adaptivity/adaptivity_ex3/run.sh
@@ -10,4 +10,6 @@ example_dir=examples/adaptivity/$example_name
 run_example "$example_name" refinement_type=h
 run_example "$example_name" refinement_type=p
 run_example "$example_name" refinement_type=hp
-run_example "$example_name" refinement_type=matchedhp
+
+# Solvers still give us trouble with too much matchedhp?
+run_example "$example_name" refinement_type=matchedhp max_r_steps=5
