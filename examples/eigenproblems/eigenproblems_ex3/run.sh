b36010a4ce1f934ed0d9e416c76316242a998990
David <david.knezevic@akselos.com>
2016-03-18 18:11:33 -0400

    Updated eigenproblem_ex3 based on update to CondensedEigenSystem
    We now condense out all constrained dofs, so we can now impose boundary conditions
    via a DirichletBoundary object. Also, updated run.sh to use -st_type sinvert, since
    that converges much more efficiently.

--- examples/eigenproblems/eigenproblems_ex3/run.sh
+++ examples/eigenproblems/eigenproblems_ex3/run.sh
@@ -8,7 +8,7 @@ example_name=eigenproblems_ex3
 
 example_dir=examples/eigenproblems/$example_name
 
-slepc_options="-st_ksp_type gmres -st_pc_type bjacobi -st_sub_pc_type ilu"
+slepc_options="-st_ksp_type gmres -st_pc_type bjacobi -st_sub_pc_type ilu -st_type sinvert"
 options="-n_evals 5 -mesh_name drum1 -plotting_index 2 $slepc_options"
 run_example_no_extra_options "$example_name" "$options"
 
