0ef76b2f24b3189d835b67d92d6604f6598171fb
Derek Gaston <friedmud@gmail.com>
2016-08-04 12:24:40 -0600

    Add get_array() capability to PetscVector closes #1050

--- tests/numerics/petsc_vector_test.C
+++ tests/numerics/petsc_vector_test.C
@@ -27,7 +27,52 @@ public:
 
   NUMERICVECTORTEST
 
+  CPPUNIT_TEST( testGetArray );
+
   CPPUNIT_TEST_SUITE_END();
+
+  void testGetArray()
+  {
+    unsigned int block_size  = 2;
+
+    // a different size on each processor.
+    unsigned int local_size  = block_size;
+    unsigned int global_size = 0;
+
+    for (libMesh::processor_id_type p=0; p<my_comm->size(); p++)
+      global_size += (block_size + static_cast<unsigned int>(p));
+
+    PetscVector<Number> v(*my_comm, global_size, local_size);
+
+    PetscScalar * values = v.get_array();
+
+    for (unsigned int i=0; i<local_size; i++)
+      values[i] = i;
+
+    v.restore_array();
+
+    v.close();
+
+    // Check the values through the interface
+    for (unsigned int i=0; i<local_size; i++)
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(v(my_comm->rank()*2 + i), i, TOLERANCE*TOLERANCE);
+
+    // Check that we can see the same thing with get_array_read
+    const PetscScalar * read_only_values = v.get_array_read();
+
+    for (unsigned int i=0; i<local_size; i++)
+      CPPUNIT_ASSERT_DOUBLES_EQUAL(read_only_values[i], i, TOLERANCE*TOLERANCE);
+
+    v.restore_array();
+
+    // Test getting a read only array after getting a writeable array
+    values = v.get_array();
+    read_only_values = v.get_array_read();
+    CPPUNIT_ASSERT_EQUAL((intptr_t)read_only_values, (intptr_t)values);
+
+    v.restore_array();
+  }
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION( PetscVectorTest );
