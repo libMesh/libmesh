6900d1c43db620f8f609eac09d090bbd6c239b1a
John Peterson <jwpeterson@gmail.com>
2016-08-04 08:38:11 -0600

    Make reindent.sh work on multiple files.
    Also make an alternate fix for headers with the 'final' keyword as
    this still seems to break header file indentation in Emacs.

--- contrib/bin/reindent.sh
+++ contrib/bin/reindent.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# This script reindents a file using emacs-style indentation,
-# with no indentation in namespaces.
+# This script reindents files according to the libmesh style, which is
+# the standard emacs style with no indentation in namespaces.
 if [[ $# -lt 1 ]]; then
-    echo "Usage: $0 file.C"
+    echo "Usage: $0 file1.C file2.C ..."
     exit 1
 fi
 
@@ -19,32 +19,35 @@ if [ -z $EMACS ]; then
   EMACS=/usr/bin/emacs
 fi
 
-# Even the most recent version of Emacs (25.1.50.1 at the time of this
-# writing) fails to properly indent classes which have been marked
-# "libmesh_final", presumably because this is our own macro which is
-# not valid C++.  It does, however, properly indent classes marked as
-# "final".  So, a possible workaround is to text-replace
-# "libmesh_final" with "final" before doing the indentation, and then
-# swap it back again afterward.
-perl -pli -e 's/ libmesh_final : / final : /g' $1
+for i in $*; do
+  # Even the most recent version of Emacs (25.1.50.1 at the time of this
+  # writing) fails to properly indent classes which have been marked
+  # "libmesh_final", presumably because this is our own macro which is
+  # not valid C++.  It does, however, properly indent classes marked as
+  # "final".  So, a possible workaround is to text-replace
+  # "libmesh_final" with "final" before doing the indentation, and then
+  # swap it back again afterward.
+  perl -pli -e 's/ libmesh_final : / \/\*\*\/ : /g' $i
 
-# Print name of file we are working on
-echo "Indenting $1"
+  # Print name of file we are working on
+  echo "Indenting $i"
 
-# The following command:
-# .) Uses only spaces for tabs
-# .) Forces emacs to use C++ mode
-# .) Sets the indentation level within namespaces to 0
-# .) Runs indent-region on the entire file
-# .) Saves the buffer
-$EMACS -batch $1  \
-  --eval="(setq-default indent-tabs-mode nil)" \
-  --eval="(c++-mode)" \
-  --eval="(c-set-offset 'innamespace 0)" \
-  --eval="(indent-region (point-min) (point-max) nil)" \
-  -f save-buffer &> /dev/null
+  # The following command:
+  # .) Uses only spaces for tabs
+  # .) Forces emacs to use C++ mode
+  # .) Sets the indentation level within namespaces to 0
+  # .) Runs indent-region on the entire file
+  # .) Saves the buffer
+  $EMACS -batch $i  \
+    --eval="(setq-default indent-tabs-mode nil)" \
+    --eval="(c++-mode)" \
+    --eval="(c-set-offset 'innamespace 0)" \
+    --eval="(indent-region (point-min) (point-max) nil)" \
+    -f save-buffer &> /dev/null
 
-perl -pli -e 's/ final : / libmesh_final : /g' $1
+  perl -pli -e 's/ \/\*\*\/ : / libmesh_final : /g' $i
+done
+
 
 # Local Variables:
 # sh-basic-offset: 2
