7cf702debaaee26ea4a1b110eb960a4433b622c1
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-10-27 16:55:32 -0500

    Fix scripting atavism
    This wasn't breaking the runs (since later options override earlier
    ones) but it was making the information printed to screen nonsensical.

--- examples/adaptivity/adaptivity_ex4/run.sh
+++ examples/adaptivity/adaptivity_ex4/run.sh
@@ -6,6 +6,6 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=adaptivity_ex4
 
-run_example "$example_name" "$options" approx_type=HERMITE
-run_example "$example_name" "$options" approx_type=CLOUGH
-#run_example "$example_name" "$options" approx_type=SECOND  # Broken?
+run_example "$example_name" approx_type=HERMITE
+run_example "$example_name" approx_type=CLOUGH
+#run_example "$example_name" approx_type=SECOND  # Broken?
