1a8f0344e80e6f8da9432ea9df0e36c9a1f61483
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-11-14 16:21:46 -0600

    1D hp tests in examples
    This should also cover the p refinement of 1D HERMITE elements

--- examples/adaptivity/adaptivity_ex3/run.sh
+++ examples/adaptivity/adaptivity_ex3/run.sh
@@ -13,3 +13,15 @@ run_example "$example_name" refinement_type=p
 # Some solvers still give us trouble with too much hp
 run_example "$example_name" refinement_type=hp max_r_steps=8
 run_example "$example_name" refinement_type=matchedhp max_r_steps=4
+
+# Let's get some 1D coverage too; that's cheap.
+run_example "$example_name" dimension=1 refinement_type=h
+run_example "$example_name" dimension=1 refinement_type=p
+run_example "$example_name" dimension=1 refinement_type=hp max_r_steps=8
+run_example "$example_name" dimension=1 refinement_type=matchedhp max_r_steps=4
+
+# And try out another element type
+run_example "$example_name" dimension=1 approx_type=HERMITE approx_order=3 refinement_type=h
+run_example "$example_name" dimension=1 approx_type=HERMITE approx_order=3 refinement_type=p
+run_example "$example_name" dimension=1 approx_type=HERMITE approx_order=3 refinement_type=hp max_r_steps=8
+run_example "$example_name" dimension=1 approx_type=HERMITE approx_order=3 refinement_type=matchedhp max_r_steps=4
