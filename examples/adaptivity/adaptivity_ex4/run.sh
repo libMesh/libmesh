1201497af3188b0dbaf40a66fa8ca21ce0bf4590
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-11-21 15:14:29 -0600

    Comment on why we turn off Clough-Tocher threads

--- examples/adaptivity/adaptivity_ex4/run.sh
+++ examples/adaptivity/adaptivity_ex4/run.sh
@@ -7,5 +7,7 @@ source $LIBMESH_DIR/examples/run_common.sh
 example_name=adaptivity_ex4
 
 run_example "$example_name" approx_type=HERMITE
+
+# CLOUGH elements don't currently support threads
 run_example_no_extra_options "$example_name" approx_type=CLOUGH $LIBMESH_OPTIONS --n_threads=1
 #run_example "$example_name" approx_type=SECOND  # Broken?
