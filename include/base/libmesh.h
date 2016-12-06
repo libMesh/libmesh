4c8b88ae62da287e77dd3ea410f3bda2bcf1bff9
John Peterson <jwpeterson@gmail.com>
2016-12-06 13:28:10 -0700

    Add code for initializing/finalizing vtkMPIController.
    Refs #1179.

--- include/base/libmesh.h
+++ include/base/libmesh.h
@@ -31,6 +31,12 @@
 #include <string>
 #include <vector>
 
+// For dealing with MPI stuff in VTK.
+#if defined(LIBMESH_HAVE_MPI) && defined(LIBMESH_HAVE_VTK)
+# include "vtkSmartPointer.h"
+class vtkMPIController;
+#endif
+
 /**
  * The \p libMesh namespace provides an interface to certain functionality
  * in the library.  It provides a uniform \p init() method that
@@ -79,6 +85,11 @@ public:
 
 private:
   Parallel::Communicator _comm;
+
+#if defined(LIBMESH_HAVE_MPI) && defined(LIBMESH_HAVE_VTK)
+  // VTK object for dealing with MPI stuff in VTK.
+  vtkSmartPointer<vtkMPIController> _vtk_mpi_controller;
+#endif
 };
 
 /**
