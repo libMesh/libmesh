libMesh/libmesh

commit 07639a3887a27272031a727271275f2328f69a4e

Fix unused parameter warnings found with new flags.

src/base/dof_map_constraints.C
@@ -2333,10 +2333,12 @@ DofMap::max_constraint_error (const System & system,
               global_dof >= vec.first_local_index() &&
               global_dof < vec.last_local_index())
             {
+#ifdef DEBUG
               DofConstraints::const_iterator
                 pos = _dof_constraints.find(global_dof);
 
               libmesh_assert (pos != _dof_constraints.end());
+#endif
 
               Number exact_value = 0;
               DofConstraintValueMap::const_iterator rhsit =

src/mesh/distributed_mesh.C
@@ -878,9 +878,11 @@ void DistributedMesh::libmesh_assert_valid_parallel_object_ids(const mapvector<T
       libmesh_assert(!obj || obj->id() == i);
 
       // All processors with an object should agree on id
+#ifdef DEBUG
       const dof_id_type dofid = obj && obj->valid_id() ?
         obj->id() : DofObject::invalid_id;
       libmesh_assert(this->comm().semiverify(obj ? &dofid : libmesh_nullptr));
+#endif
 
       // All processors with an object should agree on processor id
       const dof_id_type procid = obj && obj->valid_processor_id() ?
@@ -905,7 +907,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_object_ids(const mapvector<T
                       (min_procid != this->processor_id())
                       );
 
-#ifdef LIBMESH_ENABLE_UNIQUE_ID
+#if defined(LIBMESH_ENABLE_UNIQUE_ID) && defined(DEBUG)
       // All processors with an object should agree on unique id
       const unique_id_type uniqueid = obj ? obj->unique_id() : 0;
       libmesh_assert(this->comm().semiverify(obj ? &uniqueid : libmesh_nullptr));
@@ -925,6 +927,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_ids () const
 
 void DistributedMesh::libmesh_assert_valid_parallel_p_levels () const
 {
+#ifdef DEBUG
   // This function must be run on all processors at once
   parallel_object_only();
 
@@ -939,6 +942,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_p_levels () const
       // All processors with an active element should agree on p level
       libmesh_assert(this->comm().semiverify((el && el->active()) ? &p_level : libmesh_nullptr));
     }
+#endif
 }
 
 
@@ -946,7 +950,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_p_levels () const
 
 void DistributedMesh::libmesh_assert_valid_parallel_flags () const
 {
-#ifdef LIBMESH_ENABLE_AMR
+#if defined(LIBMESH_ENABLE_AMR) && defined(DEBUG)
   // This function must be run on all processors at once
   parallel_object_only();
 

src/mesh/nemesis_io.C
@@ -68,7 +68,7 @@ inline unsigned int to_uint ( const T & t )
 
 // test equality for a.first -> a.second mapping.  since we can only map to one
 // value only test the first entry
-#if defined(LIBMESH_HAVE_EXODUS_API) && defined(LIBMESH_HAVE_NEMESIS_API)
+#if defined(LIBMESH_HAVE_EXODUS_API) && defined(LIBMESH_HAVE_NEMESIS_API) && defined(DEBUG)
 inline bool global_idx_mapping_equality (const std::pair<unsigned int, unsigned int> & a,
                                          const std::pair<unsigned int, unsigned int> & b)
 {

src/parallel/parallel_elem.C
@@ -40,7 +40,10 @@ static const unsigned int header_size = 12;
 static const unsigned int header_size = 11;
 #endif
 
+#ifdef DEBUG
+// Currently this constant is only used for debugging.
 static const largest_id_type elem_magic_header = 987654321;
+#endif
 }
 
 

src/parallel/parallel_node.C
@@ -43,7 +43,10 @@ static const unsigned int header_size = 2;
 static const unsigned int idtypes_per_Real =
   (sizeof(Real) + sizeof(largest_id_type) - 1) / sizeof(largest_id_type);
 
+#ifdef DEBUG
+// Currently this constant is only used for debugging.
 static const largest_id_type node_magic_header = 1234567890;
+#endif
 }
 
 

src/systems/fem_context.C
@@ -1432,6 +1432,7 @@ void FEMContext::elem_position_get()
   //    {
   unsigned int n_nodes = this->get_elem().n_nodes();
 
+#ifdef DEBUG
   const unsigned char dim = this->get_elem_dim();
 
   // For simplicity we demand that mesh coordinates be stored
@@ -1451,6 +1452,7 @@ void FEMContext::elem_position_get()
                   == LAGRANGE &&
                   this->get_element_fe(this->get_mesh_z_var(), dim)->get_fe_type().order.get_order()
                   == this->get_elem().default_order()));
+#endif
 
   // Get degree of freedom coefficients from point coordinates
   if (this->get_mesh_x_var() != libMesh::invalid_uint)
@@ -1489,13 +1491,15 @@ void FEMContext::_do_elem_position_set(Real)
   // operating on elements which share a node
   libmesh_assert_equal_to (libMesh::n_threads(), 1);
 
-  const unsigned char dim = this->get_elem_dim();
-
   // If the coordinate data is in our own system, it's already
   // been set up for us, and we can ignore our input parameter theta
   //  if (_mesh_sys == this->number())
   //    {
   unsigned int n_nodes = this->get_elem().n_nodes();
+
+#ifdef DEBUG
+  const unsigned char dim = this->get_elem_dim();
+
   // For simplicity we demand that mesh coordinates be stored
   // in a format that allows a direct copy
   libmesh_assert(this->get_mesh_x_var() == libMesh::invalid_uint ||
@@ -1510,6 +1514,7 @@ void FEMContext::_do_elem_position_set(Real)
                  (this->get_element_fe(this->get_mesh_z_var(), dim)->get_fe_type().family
                   == LAGRANGE &&
                   this->get_elem_solution(this->get_mesh_z_var()).size() == n_nodes));
+#endif
 
   // Set the new point coordinates
   if (this->get_mesh_x_var() != libMesh::invalid_uint)

include/numerics/type_tensor.h
@@ -140,7 +140,7 @@ public:
   typename boostcopy::enable_if_c<
     ScalarTraits<Scalar>::value,
     TypeTensor &>::type
-  operator = (const Scalar & p)
+  operator = (const Scalar & libmesh_dbg_var(p))
   { libmesh_assert_equal_to (p, Scalar(0)); this->zero(); return *this; }
 
   /**

include/numerics/type_vector.h
@@ -134,7 +134,7 @@ public:
   typename boostcopy::enable_if_c<
     ScalarTraits<Scalar>::value,
     TypeVector &>::type
-  operator = (const Scalar & p)
+  operator = (const Scalar & libmesh_dbg_var(p))
   { libmesh_assert_equal_to (p, Scalar(0)); this->zero(); return *this; }
 
   /**

commit 8352e5b61aa29d6e013ed3b17497e790893c17ef

DEBUG is not defined in devel mode.
But asserts *are* active in devel mode.  Fixing the issues introduced
by 07639a3.

src/base/dof_map_constraints.C
@@ -2333,7 +2333,7 @@ DofMap::max_constraint_error (const System & system,
               global_dof >= vec.first_local_index() &&
               global_dof < vec.last_local_index())
             {
-#ifdef DEBUG
+#ifndef NDEBUG
               DofConstraints::const_iterator
                 pos = _dof_constraints.find(global_dof);
 

src/mesh/distributed_mesh.C
@@ -878,7 +878,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_object_ids(const mapvector<T
       libmesh_assert(!obj || obj->id() == i);
 
       // All processors with an object should agree on id
-#ifdef DEBUG
+#ifndef NDEBUG
       const dof_id_type dofid = obj && obj->valid_id() ?
         obj->id() : DofObject::invalid_id;
       libmesh_assert(this->comm().semiverify(obj ? &dofid : libmesh_nullptr));
@@ -907,7 +907,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_object_ids(const mapvector<T
                       (min_procid != this->processor_id())
                       );
 
-#if defined(LIBMESH_ENABLE_UNIQUE_ID) && defined(DEBUG)
+#if defined(LIBMESH_ENABLE_UNIQUE_ID) && !defined(NDEBUG)
       // All processors with an object should agree on unique id
       const unique_id_type uniqueid = obj ? obj->unique_id() : 0;
       libmesh_assert(this->comm().semiverify(obj ? &uniqueid : libmesh_nullptr));
@@ -927,7 +927,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_ids () const
 
 void DistributedMesh::libmesh_assert_valid_parallel_p_levels () const
 {
-#ifdef DEBUG
+#ifndef NDEBUG
   // This function must be run on all processors at once
   parallel_object_only();
 
@@ -950,7 +950,7 @@ void DistributedMesh::libmesh_assert_valid_parallel_p_levels () const
 
 void DistributedMesh::libmesh_assert_valid_parallel_flags () const
 {
-#if defined(LIBMESH_ENABLE_AMR) && defined(DEBUG)
+#if defined(LIBMESH_ENABLE_AMR) && !defined(NDEBUG)
   // This function must be run on all processors at once
   parallel_object_only();
 

src/mesh/nemesis_io.C
@@ -68,7 +68,7 @@ inline unsigned int to_uint ( const T & t )
 
 // test equality for a.first -> a.second mapping.  since we can only map to one
 // value only test the first entry
-#if defined(LIBMESH_HAVE_EXODUS_API) && defined(LIBMESH_HAVE_NEMESIS_API) && defined(DEBUG)
+#if defined(LIBMESH_HAVE_EXODUS_API) && defined(LIBMESH_HAVE_NEMESIS_API) && !defined(NDEBUG)
 inline bool global_idx_mapping_equality (const std::pair<unsigned int, unsigned int> & a,
                                          const std::pair<unsigned int, unsigned int> & b)
 {

src/parallel/parallel_elem.C
@@ -40,7 +40,7 @@ static const unsigned int header_size = 12;
 static const unsigned int header_size = 11;
 #endif
 
-#ifdef DEBUG
+#ifndef NDEBUG
 // Currently this constant is only used for debugging.
 static const largest_id_type elem_magic_header = 987654321;
 #endif

src/parallel/parallel_node.C
@@ -43,7 +43,7 @@ static const unsigned int header_size = 2;
 static const unsigned int idtypes_per_Real =
   (sizeof(Real) + sizeof(largest_id_type) - 1) / sizeof(largest_id_type);
 
-#ifdef DEBUG
+#ifndef NDEBUG
 // Currently this constant is only used for debugging.
 static const largest_id_type node_magic_header = 1234567890;
 #endif

src/systems/fem_context.C
@@ -1432,7 +1432,7 @@ void FEMContext::elem_position_get()
   //    {
   unsigned int n_nodes = this->get_elem().n_nodes();
 
-#ifdef DEBUG
+#ifndef NDEBUG
   const unsigned char dim = this->get_elem_dim();
 
   // For simplicity we demand that mesh coordinates be stored
@@ -1497,7 +1497,7 @@ void FEMContext::_do_elem_position_set(Real)
   //    {
   unsigned int n_nodes = this->get_elem().n_nodes();
 
-#ifdef DEBUG
+#ifndef NDEBUG
   const unsigned char dim = this->get_elem_dim();
 
   // For simplicity we demand that mesh coordinates be stored