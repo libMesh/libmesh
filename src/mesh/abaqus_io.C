84de531a51ef19556b75d565363fbcda3c6dfd07
John Peterson <jwpeterson@gmail.com>
2017-02-15 12:16:13 -0700

    AbaqusIO: Allow the shorter string 'S3' instead of 'S3R'.

--- src/mesh/abaqus_io.C
+++ src/mesh/abaqus_io.C
@@ -513,7 +513,7 @@ void AbaqusIO::read_elements(std::string upper, std::string elset_name)
       elems_of_dimension[2] = true;
     }
   else if (upper.find("CPS3") != std::string::npos ||
-           upper.find("S3R") != std::string::npos)
+           upper.find("S3") != std::string::npos)
     {
       elem_type = TRI3;
       n_nodes_per_elem = 3;
