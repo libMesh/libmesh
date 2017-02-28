466185772165fd382169015791b49dc2f8fde3a2
Paul T. Bauman <ptbauman@gmail.com>
2017-02-28 15:19:53 -0500

    More documentation on new FirstOrderUnsteadySolver capability
    In particular, for dealing with second order systems as first order
    systems.

--- include/solvers/first_order_unsteady_solver.h
+++ include/solvers/first_order_unsteady_solver.h
@@ -28,6 +28,40 @@ namespace libMesh
  * Subclasses of this class are meant to solve problems of the form
  * \f[ M(u)\dot{u} = F(u)\f]
  *
+ * There is also infrastructure for subclasses to support solving second
+ * order-in-time systems (or both first order and second order).
+ * In particular, consider the second system
+ * \f[ M(u)\ddot{u} + C(u)\dot{u} + F(u) = 0 \f]
+ * If we introduce the equation \f$\dot{u} = v\f$, then, we have the
+ * following first order system:
+ * \f{eqnarray}{ \dot{u} &=& v \\ M(u)\dot{v} + C(u)v + F(u) &=& 0\f}
+ * Such systems are supported by the user specifying that the time order
+ * of the variable \f$u\f$ is 2 when adding the variable using
+ * FEMSystem::time_evolving. This class will then add the \f$v\f$ variables
+ * to the FEMSystem (named "dot_u" if the second order variable name is "u").
+ * Sublasses will then need to make sure to use the function
+ * FirstOrderUnsteadySolver::prepare_accel() to prepare data structures for the
+ * users to use from FEMContext. Furthermore, subclasses will need to appropriately
+ * call damping_residual functions. Finally, subclasses will call
+ * FirstOrderUnsteadySolver::compute_second_order_eqns() to actually assemble
+ * residual and Jacobians for the \f$ \dot{u} = v\f$ equations. These aspects
+ * should be invisible ot users during their element assembly.
+ *
+ * Unfortunately, complete usage of the feature of treating second order
+ * equations as a system of first order equations is not completely invisible
+ * to the user. The user must assemble their residual in the correct equation.
+ * In particular, the residual must go in the \f$v\f$ residual equation, i.e.
+ * \f$ M(u)\dot{v} + C(u)v + F(u) = 0 \f$, and,
+ * subsequently, they must also be careful to populate to the correct Jacobian
+ * blocks. The function to help facilitate this is get_second_order_dot_var.
+ * If you have a second order variable, you pass that variable index and
+ * get_second_order_dot_var will return the index of the corresponding velocity
+ * variable, \f$v\f$ in the notation above. Then, the user knows what blocks to
+ * populate. Note that the API is designed so that if the user codes to this
+ * paradigm, the TimeSolver will be interchangeable for those element kernels.
+ * That is, they'll be able to use either a FirstOrderUnsteadySolver or a
+ * SecondOrderUnsteadySolver.
+ *
  * This class is part of the new DifferentiableSystem framework,
  * which is still experimental.  Users of this framework should
  * beware of bugs and future API changes.
