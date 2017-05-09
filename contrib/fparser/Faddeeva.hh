06760dbd4aa9bb4605e7df6a2915bf8d9375bcd4
John Peterson <jwpeterson@gmail.com>
2017-05-09 08:47:49 -0600

    Remove unused parts from Faddeeva.hh/cc
    Drops the Dawson, erfi, erfc implementations and test code. We can
    always add these back later if necessary. In the meantime, it's not
    necessary to compile extra stuff.
    
    Refs #1333.

--- contrib/fparser/Faddeeva.hh
+++ contrib/fparser/Faddeeva.hh
@@ -45,18 +45,6 @@ extern double erfcx(double x); // special case for real x
 extern std::complex<double> erf(std::complex<double> z, double relerr=0);
 extern double erf(double x); // special case for real x
 
-// compute erfi(z) = -i erf(iz), the imaginary error function
-extern std::complex<double> erfi(std::complex<double> z, double relerr=0);
-extern double erfi(double x); // special case for real x
-
-// compute erfc(z) = 1 - erf(z), the complementary error function
-extern std::complex<double> erfc(std::complex<double> z, double relerr=0);
-extern double erfc(double x); // special case for real x
-
-// compute Dawson(z) = sqrt(pi)/2  *  exp(-z^2) * erfi(z)
-extern std::complex<double> Dawson(std::complex<double> z, double relerr=0);
-extern double Dawson(double x); // special case for real x
-
 } // namespace Faddeeva
 
 #endif // FADDEEVA_HH
