libMesh/libmesh

commit 353e0fc80eaec0de77a60389c0374eca5e22f37c

Added a no constraints option to FEMSystem assembly.
Fixed all function signatures. Also, you need a jacobian if you have constraints. Compiles and gives correct results.

Removed whitespace, removed extraneous if(_no_constraints) statements.

Removed no_constraints option from assemble_unconstrained_element_system.

Fixed indenting using detab.sh

More whitespace fixes

src/systems/fem_system.C
@@ -74,7 +74,7 @@ void assemble_unconstrained_element_system(const FEMSystem & _sys,
 
   // We need jacobians to do heterogeneous residual constraints
   const bool need_jacobian =
-    _get_jacobian || _constrain_heterogeneously;
+    (_get_jacobian || _constrain_heterogeneously) ;
 
   bool jacobian_computed =
     _sys.time_solver->element_residual(need_jacobian, _femcontext);
@@ -251,6 +251,7 @@ void add_element_system(const FEMSystem & _sys,
                         const bool _get_residual,
                         const bool _get_jacobian,
                         const bool _constrain_heterogeneously,
+                        const bool _no_constraints,
                         FEMContext & _femcontext)
 {
 #ifdef LIBMESH_ENABLE_CONSTRAINTS
@@ -280,6 +281,7 @@ void add_element_system(const FEMSystem & _sys,
           (_femcontext.get_elem_jacobian(),
            _femcontext.get_elem_residual(),
            _femcontext.get_dof_indices(), false);
+      // Do nothing if(_no_constraints)
     }
   else if (_get_residual)
     {
@@ -291,13 +293,16 @@ void add_element_system(const FEMSystem & _sys,
       else
         _sys.get_dof_map().constrain_element_vector
           (_femcontext.get_elem_residual(), _femcontext.get_dof_indices(), false);
+      // Do nothing if(_no_constraints)
     }
   else if (_get_jacobian)
     {
       // Heterogeneous and homogeneous constraints are the same on the
       // matrix
-      _sys.get_dof_map().constrain_element_matrix
-        (_femcontext.get_elem_jacobian(), _femcontext.get_dof_indices(), false);
+      // Only get these contribs if we are applying some constraints
+      if(!_no_constraints)
+        _sys.get_dof_map().constrain_element_matrix
+          (_femcontext.get_elem_jacobian(), _femcontext.get_dof_indices(), false);
     }
 #endif // #ifdef LIBMESH_ENABLE_CONSTRAINTS
 
@@ -348,11 +353,13 @@ public:
   AssemblyContributions(FEMSystem & sys,
                         bool get_residual,
                         bool get_jacobian,
-                        bool constrain_heterogeneously) :
+                        bool constrain_heterogeneously,
+bool no_constraints) :
     _sys(sys),
     _get_residual(get_residual),
     _get_jacobian(get_jacobian),
-    _constrain_heterogeneously(constrain_heterogeneously) {}
+    _constrain_heterogeneously(constrain_heterogeneously),
+    _no_constraints(no_constraints){}
 
   /**
    * operator() for use with Threads::parallel_for().
@@ -377,7 +384,7 @@ public:
 
         add_element_system
           (_sys, _get_residual, _get_jacobian,
-           _constrain_heterogeneously, _femcontext);
+           _constrain_heterogeneously, _no_constraints, _femcontext);
       }
   }
 
@@ -385,7 +392,7 @@ private:
 
   FEMSystem & _sys;
 
-  const bool _get_residual, _get_jacobian, _constrain_heterogeneously;
+  const bool _get_residual, _get_jacobian, _constrain_heterogeneously, _no_constraints;
 };
 
 class PostprocessContributions
@@ -843,7 +850,8 @@ void FEMSystem::init_data ()
 
 
 void FEMSystem::assembly (bool get_residual, bool get_jacobian,
-                          bool apply_heterogeneous_constraints)
+                          bool apply_heterogeneous_constraints,
+                          bool apply_no_constraints)
 {
   libmesh_assert(get_residual || get_jacobian);
   std::string log_name;
@@ -914,7 +922,8 @@ void FEMSystem::assembly (bool get_residual, bool get_jacobian,
     (elem_range.reset(mesh.active_local_elements_begin(),
                       mesh.active_local_elements_end()),
      AssemblyContributions(*this, get_residual, get_jacobian,
-                           apply_heterogeneous_constraints));
+                           apply_heterogeneous_constraints,
+                           apply_no_constraints));
 
   // Check and see if we have SCALAR variables
   bool have_scalar = false;
@@ -999,7 +1008,7 @@ void FEMSystem::assembly (bool get_residual, bool get_jacobian,
 
           add_element_system
             (*this, get_residual, get_jacobian,
-             apply_heterogeneous_constraints, _femcontext);
+             apply_heterogeneous_constraints, apply_no_constraints, _femcontext);
         }
     }
 

src/systems/linear_implicit_system.C
@@ -365,7 +365,8 @@ void LinearImplicitSystem::release_linear_solver(LinearSolver<Number> *) const
 
 void LinearImplicitSystem::assembly(bool,
                                     bool,
-                                    bool)
+                                    bool,
+    bool)
 {
   // Residual R(u(p),p) := A(p)*u(p) - b(p)
   // partial R / partial u = A

include/systems/diff_system.h
@@ -123,7 +123,8 @@ public:
    */
   virtual void assembly (bool get_residual,
                          bool get_jacobian,
-                         bool apply_heterogeneous_constraints = false) libmesh_override = 0;
+                         bool apply_heterogeneous_constraints = false,
+                         bool apply_no_constraints = false) libmesh_override = 0;
 
   /**
    * Invokes the solver associated with the system.  For steady state

include/systems/fem_system.h
@@ -84,7 +84,8 @@ public:
    */
   virtual void assembly (bool get_residual,
                          bool get_jacobian,
-                         bool apply_heterogeneous_constraints = false) libmesh_override;
+                         bool apply_heterogeneous_constraints = false,
+                         bool apply_no_constraints = false) libmesh_override;
 
   /**
    * Invokes the solver associated with the system.  For steady state

include/systems/implicit_system.h
@@ -150,7 +150,8 @@ public:
    */
   virtual void assembly (bool /* get_residual */,
                          bool /* get_jacobian */,
-                         bool /* apply_heterogeneous_constraints */ = false)
+                         bool /* apply_heterogeneous_constraints */ = false,
+                         bool /* apply_no_constraints */ = false)
   { libmesh_not_implemented(); }
 
   /**

include/systems/linear_implicit_system.h
@@ -133,7 +133,8 @@ public:
    */
   virtual void assembly(bool get_residual,
                         bool get_jacobian,
-                        bool apply_heterogeneous_constraints = false) libmesh_override;
+                        bool apply_heterogeneous_constraints = false,
+                        bool apply_no_constraints = false) libmesh_override;
 
   /**
    * @returns \p "LinearImplicit".  Helps in identifying

include/systems/nonlinear_implicit_system.h
@@ -237,7 +237,8 @@ public:
    */
   virtual void assembly(bool get_residual,
                         bool get_jacobian,
-                        bool apply_heterogeneous_constraints = false) libmesh_override;
+                        bool apply_heterogeneous_constraints = false,
+                        bool apply_no_constraints = false) libmesh_override;
 
   /**
    * @returns \p "NonlinearImplicit".  Helps in identifying

src/systems/nonlinear_implicit_system.C
@@ -210,7 +210,8 @@ std::pair<unsigned int, Real> NonlinearImplicitSystem::get_linear_solve_paramete
 
 void NonlinearImplicitSystem::assembly(bool get_residual,
                                        bool get_jacobian,
-                                       bool /*apply_heterogeneous_constraints*/)
+                                       bool /*apply_heterogeneous_constraints*/,
+                                       bool /*apply_no_constraints*/)
 {
   // Get current_local_solution in sync
   this->update();

commit a177c9f8d531ba8cf9c610ecdf6c239b0f6869ed

Fix formatting errors introduced by 353e0fc8.

src/systems/fem_system.C
@@ -74,7 +74,7 @@ void assemble_unconstrained_element_system(const FEMSystem & _sys,
 
   // We need jacobians to do heterogeneous residual constraints
   const bool need_jacobian =
-    (_get_jacobian || _constrain_heterogeneously) ;
+    (_get_jacobian || _constrain_heterogeneously);
 
   bool jacobian_computed =
     _sys.time_solver->element_residual(need_jacobian, _femcontext);
@@ -281,7 +281,7 @@ void add_element_system(const FEMSystem & _sys,
           (_femcontext.get_elem_jacobian(),
            _femcontext.get_elem_residual(),
            _femcontext.get_dof_indices(), false);
-      // Do nothing if(_no_constraints)
+      // Do nothing if (_no_constraints)
     }
   else if (_get_residual)
     {
@@ -293,16 +293,17 @@ void add_element_system(const FEMSystem & _sys,
       else
         _sys.get_dof_map().constrain_element_vector
           (_femcontext.get_elem_residual(), _femcontext.get_dof_indices(), false);
-      // Do nothing if(_no_constraints)
+      // Do nothing if (_no_constraints)
     }
   else if (_get_jacobian)
     {
       // Heterogeneous and homogeneous constraints are the same on the
       // matrix
       // Only get these contribs if we are applying some constraints
-      if(!_no_constraints)
-        _sys.get_dof_map().constrain_element_matrix
-          (_femcontext.get_elem_jacobian(), _femcontext.get_dof_indices(), false);
+      if (!_no_constraints)
+        _sys.get_dof_map().constrain_element_matrix (_femcontext.get_elem_jacobian(),
+                                                     _femcontext.get_dof_indices(),
+                                                     false);
     }
 #endif // #ifdef LIBMESH_ENABLE_CONSTRAINTS
 
@@ -354,12 +355,12 @@ public:
                         bool get_residual,
                         bool get_jacobian,
                         bool constrain_heterogeneously,
-bool no_constraints) :
+                        bool no_constraints) :
     _sys(sys),
     _get_residual(get_residual),
     _get_jacobian(get_jacobian),
     _constrain_heterogeneously(constrain_heterogeneously),
-    _no_constraints(no_constraints){}
+    _no_constraints(no_constraints) {}
 
   /**
    * operator() for use with Threads::parallel_for().

src/systems/linear_implicit_system.C
@@ -366,7 +366,7 @@ void LinearImplicitSystem::release_linear_solver(LinearSolver<Number> *) const
 void LinearImplicitSystem::assembly(bool,
                                     bool,
                                     bool,
-    bool)
+                                    bool)
 {
   // Residual R(u(p),p) := A(p)*u(p) - b(p)
   // partial R / partial u = A