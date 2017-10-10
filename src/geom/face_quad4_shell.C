103329f137ba586ed22e6907b2d59e175d910fce
Sylvain Vallaghe <sylvain.vallaghe@akselos.com>
2017-10-10 17:55:27 +0200

    fix typos

--- src/geom/face_quad4_shell.C
+++ src/geom/face_quad4_shell.C
@@ -15,4 +15,4 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-// The QuadShell4 functions are all declined inline.
+// The QuadShell4 functions are all declared inline.
