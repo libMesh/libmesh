83d89aa1f481f37b27df1f76ed59a8ee07586f5b
Alex Lindsay <alexlindsay239@gmail.com>
2017-10-20 10:13:57 -0600

    Use dummy vectors and increase tol. Also include correct headers.

--- tests/mesh/write_vec_and_scalar.C
+++ tests/mesh/write_vec_and_scalar.C
@@ -12,10 +12,8 @@
 #include "libmesh/mesh_generation.h"
 #include "libmesh/string_to_enum.h"
 #include "libmesh/exodusII_io.h"
-
-// The systems and solvers we may use
-#include "libmesh/diff_solver.h"
-#include "libmesh/steady_solver.h"
+#include "libmesh/explicit_system.h"
+#include "libmesh/numeric_vector.h"
 
 #include "test_comm.h"
 
@@ -49,7 +47,7 @@ public:
     Mesh mesh(*TestCommWorld);
 
     MeshTools::Generation::build_square(
-        mesh, 2, 2, -1., 1., -1., 1., Utility::string_to_enum<ElemType>("TRI6"));
+        mesh, 1, 1, -1., 1., -1., 1., Utility::string_to_enum<ElemType>("TRI6"));
 
     // Create an equation systems object.
     EquationSystems equation_systems(mesh);
@@ -61,35 +59,9 @@ public:
     // Initialize the system
     equation_systems.init();
 
-    std::vector<Real> gold_solution({0.000000000010418385555305976,
-                                     0.1743561867338195,
-                                     -0.041899936369794721,
-                                     -0.19643153680419781,
-                                     -0.0000000000067936995445650305,
-                                     -0.000000000010194851982520449,
-                                     -0.000000000010194851982520419,
-                                     0.20284410873063882,
-                                     0.19643153680181882,
-                                     -0.17435618673478825,
-                                     -0.13942148221042558,
-                                     0.0000000000087210975169893594,
-                                     0.0000000000065418642410839133,
-                                     0.000000000010446687286090026,
-                                     -0.021522690153508699,
-                                     -0.0000000000089446310895455743,
-                                     0,
-                                     0,
-                                     0.25000000000000006,
-                                     0,
-                                     0,
-                                     0,
-                                     0,
-                                     0,
-                                     0});
-
     NumericVector<Number> & sys_solution = *(system.solution);
-    for (unsigned i = 0; i < gold_solution.size(); i++)
-      sys_solution.set(i, gold_solution[i]);
+    for (unsigned i = 0; i < 9; i++)
+      sys_solution.set(i, Real(2 * i));
 
 #ifdef LIBMESH_HAVE_EXODUS_API
 
@@ -114,18 +86,10 @@ public:
     for (const auto & var_name : nodal_vars)
       exio.copy_nodal_solution(sys2, var_name, var_name, 1);
 
-    std::vector<Real> gold_vector(
-        {-0.020949, -0.020949, 0., 0.009495,  0.183852,  0.,       -0.010570, -0.010570, 0.250000,
-         0.000000,  0.066228,  0., 0.002165,  0.174356,  0.125000, -0.113646, 0.071746,  0.125000,
-         -0.177428, 0.019003,  0., -0.196431, 0.009296,  0.125000, -0.119165, -0.000000, 0.,
-         -0.000000, -0.000000, 0., 0.198569,  0.002137,  0.,       -0.000000, 0.101422,  0.,
-         0.101422,  -0.000000, 0., 0.106940,  0.095903,  0.,       0.196431,  -0.009296, 0.125000,
-         0.011644,  -0.162711, 0., -0.002165, -0.174356, 0.125000, -0.075229, -0.064191, 0.,
-         0.000000,  0.000000,  0., 0.000000,  -0.069710, 0.,       -0.069710, 0.000000,  0.,
-         -0.010761, -0.010761, 0., 0.087454,  0.000000,  0.,       0.081935,  -0.103458, 0.125000,
-         -0.000000, -0.097939, 0});
+    std::vector<Real> gold_vector({-1, 3,   10,  0,  1, 12, 2,  0, 14,  0,  1.5, 11, 0.5, 1,
+                                   13, 0.5, 1.5, 12, 3, 4,  16, 3, 1.5, 15, 0.5, 4,  13});
 
-    Real tol = 1e-5;
+    Real tol = 1e-12;
     NumericVector<Number> & sys2_soln(*sys2.solution);
     for (unsigned i = 0; i < gold_vector.size(); i++)
     {
