11ef70a0dd355392093c95ff6fa3002a27bbeb80
John Peterson <jwpeterson@gmail.com>
2017-10-26 16:07:57 -0600

    Use rangefor in EquationSystems::reinit().

--- src/systems/equation_systems.C
+++ src/systems/equation_systems.C
@@ -144,11 +144,8 @@ void EquationSystems::reinit ()
       for ( ; node_it != node_end; ++node_it)
         (*node_it)->set_n_systems(n_sys);
 
-      MeshBase::element_iterator       elem_it  = _mesh.elements_begin();
-      const MeshBase::element_iterator elem_end = _mesh.elements_end();
-
-      for ( ; elem_it != elem_end; ++elem_it)
-        (*elem_it)->set_n_systems(n_sys);
+      for (auto & elem : _mesh.elements_range())
+        elem->set_n_systems(n_sys);
 
       // And any new systems will need initialization
       for (unsigned int i=0; i != n_sys; ++i)
