703dcf4fd1cb6d7cb4a4b090ce5ec74bd60fbdba
John Peterson <jwpeterson@gmail.com>
2017-10-26 15:55:02 -0600

    Use rangefor in MeshTools::libmesh_assert_valid_refinement_tree().

--- src/mesh/mesh_tools.C
+++ src/mesh/mesh_tools.C
@@ -1721,12 +1721,8 @@ void MeshTools::libmesh_assert_valid_refinement_tree(const MeshBase & mesh)
 {
   LOG_SCOPE("libmesh_assert_valid_refinement_tree()", "MeshTools");
 
-  const MeshBase::const_element_iterator el_end =
-    mesh.elements_end();
-  for (MeshBase::const_element_iterator el =
-         mesh.elements_begin(); el != el_end; ++el)
+  for (const auto & elem : mesh.elements_range())
     {
-      const Elem * elem = *el;
       libmesh_assert(elem);
       if (elem->has_children())
         for (auto & child : elem->child_ref_range())
