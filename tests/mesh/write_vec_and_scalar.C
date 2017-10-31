6e2fb943bf0010d6dbd2323990575124b530afc4
Roy H. Stogner <roystgnr@ices.utexas.edu>
2017-10-31 16:15:32 -0500

    Fix for --enable-complex
    I probably should be testing this manually rather than abusing
    CI...

--- tests/mesh/write_vec_and_scalar.C
+++ tests/mesh/write_vec_and_scalar.C
@@ -150,24 +150,27 @@ public:
         const dof_id_type gold_i_uy = gold_i_ux + 1;
         const dof_id_type gold_i_v  = gold_i_uy + 1;
 
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,0,0)),
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,0,0))),
                                      gold_vector[gold_i_ux], tol);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,v2,0)),
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,v2,0))),
                                      gold_vector[gold_i_uy], tol);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,2*v2,0)),
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,2*v2,0))),
                                      gold_vector[gold_i_v], tol);
 
         // Let's check imaginary parts and magnitude for good measure
 #ifdef LIBMESH_USE_COMPLEX_NUMBERS
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,1,0)), 0.0);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,2,0)),
-                                     gold_vector[dof_id_ux], tol);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,4,0)), 0.0);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,5,0)),
-                                     gold_vector[dof_id_uy], tol);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,7,0)), 0.0);
-        CPPUNIT_ASSERT_DOUBLES_EQUAL(sys2_soln(node->dof_number(0,8,0)),
-                                     gold_vector[dof_id_v], tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,1,0))),
+                                     0.0, tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,2,0))),
+                                     gold_vector[gold_i_ux], tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,4,0))),
+                                     0.0, tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,5,0))),
+                                     gold_vector[gold_i_uy], tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,7,0))),
+                                     0.0, tol);
+        CPPUNIT_ASSERT_DOUBLES_EQUAL(libmesh_real(sys2_soln(node->dof_number(0,8,0))),
+                                     gold_vector[gold_i_v], tol);
 #endif
       }
 #endif // #ifdef LIBMESH_HAVE_EXODUS_API
