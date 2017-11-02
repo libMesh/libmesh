23acd94646bf978d88f8d3aa424af06724267257
Roy H. Stogner <roystgnr@ices.utexas.edu>
2017-11-02 09:28:48 -0500

    Fix PerfLog::get_perf_data()
    We can no longer directly look up strings in the log.  We'll have to
    search for the corresponding character pointers first.

--- src/utils/perf_log.C
+++ src/utils/perf_log.C
@@ -18,6 +18,7 @@
 #include "libmesh/perf_log.h"
 
 // C++ includes
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
 #include <cstring>
@@ -694,7 +695,23 @@ void PerfLog::print_log() const
 
 PerfData PerfLog::get_perf_data(const std::string & label, const std::string & header)
 {
-  return log[std::make_pair(header.c_str(), label.c_str())];
+  if (non_temporary_strings.count(label) &&
+      non_temporary_strings.count(header))
+    {
+      const char * label_c_str = non_temporary_strings[label];
+      const char * header_c_str = non_temporary_strings[header];
+      return log[std::make_pair(header_c_str, label_c_str)];
+    }
+
+  auto iter = std::find_if
+    (log.begin(), log.end(),
+     [&label, &header] (const auto & a)
+     { return !std::strcmp(header.c_str(), a.first.first) &&
+              !std::strcmp(label.c_str(), a.first.second); });
+
+  libmesh_assert(iter != log.end());
+
+  return iter->second;
 }
 
 void PerfLog::start_event(const std::string & label,
