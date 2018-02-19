9643b758485df8fd2c163194af64b67d19ced1df
BalticPinguin <hu.bert@xemail.me>
2018-02-19 22:37:54 +0100

    minor corrections for real-valued numbers

--- examples/eigenproblems/eigenproblems_ex3/eigenproblems_ex3.C
+++ examples/eigenproblems/eigenproblems_ex3/eigenproblems_ex3.C
@@ -110,10 +110,10 @@ int main (int argc, char ** argv)
   libmesh_example_requires(false, "--disable-singleprecision");
 #endif
 
-#ifdef LIBMESH_USE_COMPLEX_NUMBERS
-  // SLEPc currently gives us an "inner product not well defined" with
-  // Number==complex
-  libmesh_example_requires(false, "--disable-complex");
+#ifdef LIBMESH_USE_COMPLEX_NUMBERS && SLEPC_VERSION_LESS_THAN(3,6,2)
+  // SLEPc may give us an "inner product not well defined" with
+  // Number==complex; but it seems to be solved by the time.
+  libmesh_example_requires(false, "--disable-complex or use SLEPc>=3.6.2");
 #endif
 
   // Tell the user what we are doing.
