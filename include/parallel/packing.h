96e5fad6f4600e90a9bee16d2dc36b2ca4f2a58d
Roy H. Stogner <roystgnr@ices.utexas.edu>
2018-04-26 15:07:36 -0500

    Parallel:: packing implementations go in packing.h

--- include/parallel/packing.h
+++ include/parallel/packing.h
@@ -24,6 +24,7 @@
 
 // C++ includes
 #include <cstddef>
+#include <iterator>
 #include <vector>
 
 namespace libMesh
@@ -106,6 +107,111 @@ template <typename Context, typename Iter>
 inline std::size_t packed_range_size (const Context * context,
                                       Iter range_begin,
                                       const Iter range_end);
+
+// ------------------------------------------------------------
+// Packing member functions, global functions
+
+/**
+ * Helper function for range packing
+ */
+template <typename Context, typename Iter>
+inline std::size_t packed_range_size (const Context * context,
+                                      Iter range_begin,
+                                      const Iter range_end)
+{
+  typedef typename std::iterator_traits<Iter>::value_type T;
+
+  std::size_t buffer_size = 0;
+  for (Iter range_count = range_begin;
+       range_count != range_end;
+       ++range_count)
+    {
+      buffer_size += Parallel::Packing<T>::packable_size(*range_count, context);
+    }
+  return buffer_size;
+}
+
+
+/**
+ * Helper function for range packing
+ */
+template <typename Context, typename buffertype, typename Iter>
+inline Iter pack_range (const Context * context,
+                        Iter range_begin,
+                        const Iter range_end,
+                        std::vector<buffertype> & buffer,
+                        // When we serialize into buffers, we need to use large buffers to optimize MPI
+                        // bandwidth, but not so large as to risk allocation failures.  max_buffer_size
+                        // is measured in number of buffer type entries; number of bytes may be 4 or 8
+                        // times larger depending on configuration.
+                        std::size_t approx_buffer_size)
+{
+  typedef typename std::iterator_traits<Iter>::value_type T;
+
+  // Count the total size of and preallocate buffer for efficiency.
+  // Prepare to stop early if the buffer would be too large.
+  std::size_t buffer_size = 0;
+  Iter range_stop = range_begin;
+  for (; range_stop != range_end && buffer_size < approx_buffer_size;
+       ++range_stop)
+    {
+      std::size_t next_buffer_size =
+        Parallel::Packing<T>::packable_size(*range_stop, context);
+      buffer_size += next_buffer_size;
+    }
+  buffer.reserve(buffer.size() + buffer_size);
+
+  // Pack the objects into the buffer
+  for (; range_begin != range_stop; ++range_begin)
+    {
+#ifndef NDEBUG
+      std::size_t old_size = buffer.size();
+#endif
+
+      Parallel::Packing<T>::pack
+        (*range_begin, back_inserter(buffer), context);
+
+#ifndef NDEBUG
+      unsigned int my_packable_size =
+        Parallel::Packing<T>::packable_size(*range_begin, context);
+      unsigned int my_packed_size =
+        Parallel::Packing<T>::packed_size (buffer.begin() + old_size);
+      libmesh_assert_equal_to (my_packable_size, my_packed_size);
+      libmesh_assert_equal_to (buffer.size(), old_size + my_packable_size);
+#endif
+    }
+
+  return range_stop;
+}
+
+
+
+/**
+ * Helper function for range unpacking
+ */
+template <typename Context, typename buffertype,
+          typename OutputIter, typename T>
+inline void unpack_range (const std::vector<buffertype> & buffer,
+                          Context * context,
+                          OutputIter out_iter,
+                          const T * /* output_type */)
+{
+  // Loop through the buffer and unpack each object, returning the
+  // object pointer via the output iterator
+  typename std::vector<buffertype>::const_iterator
+    next_object_start = buffer.begin();
+
+  while (next_object_start < buffer.end())
+    {
+      *out_iter++ = Parallel::Packing<T>::unpack(next_object_start, context);
+      next_object_start +=
+        Parallel::Packing<T>::packed_size(next_object_start);
+    }
+
+  // We should have used up the exact amount of data in the buffer
+  libmesh_assert (next_object_start == buffer.end());
+}
+
 
 } // namespace Parallel
 
