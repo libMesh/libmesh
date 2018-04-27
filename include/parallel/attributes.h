e089b86cad1b9ba6273fc2da27edf4012d2b98a4
Roy H. Stogner <roystgnr@ices.utexas.edu>
2018-04-27 10:53:08 -0500

    Attributes specializations -> attributes.h

--- include/parallel/attributes.h
+++ include/parallel/attributes.h
@@ -19,6 +19,12 @@
 #ifndef LIBMESH_ATTRIBUTES_H
 #define LIBMESH_ATTRIBUTES_H
 
+// C++ includes
+#include <limits>
+#include <set>
+#include <vector>
+
+
 namespace libMesh
 {
 
@@ -39,6 +45,59 @@ struct Attributes
   static void set_lowest(T &) {}
   static void set_highest(T &) {}
 };
+
+// ------------------------------------------------------------
+// Declare Attributes specializations for C++ built-in types
+
+#define LIBMESH_INT_TYPE(cxxtype)                                       \
+  template<>                                                            \
+  struct Attributes<cxxtype>                                            \
+  {                                                                     \
+    static const bool has_min_max = true;                               \
+    static void set_lowest(cxxtype & x) { x = std::numeric_limits<cxxtype>::min(); } \
+    static void set_highest(cxxtype & x) { x = std::numeric_limits<cxxtype>::max(); } \
+  }
+
+#define LIBMESH_FLOAT_TYPE(cxxtype)                                     \
+  template<>                                                            \
+  struct Attributes<cxxtype>                                            \
+  {                                                                     \
+    static const bool has_min_max = true;                               \
+    static void set_lowest(cxxtype & x) { x = -std::numeric_limits<cxxtype>::infinity(); } \
+    static void set_highest(cxxtype & x) { x = std::numeric_limits<cxxtype>::infinity(); } \
+  }
+
+#define LIBMESH_CONTAINER_TYPE(cxxtype)                                 \
+  template<typename T>                                                  \
+  struct Attributes<cxxtype<T>>                                         \
+  {                                                                     \
+    static const bool has_min_max = Attributes<T>::has_min_max;         \
+    static void set_lowest(cxxtype<T> & x) {                            \
+      for (auto & val : x)                                              \
+        Attributes<T>::set_lowest(val); }                               \
+    static void set_highest(cxxtype<T> & x) {                           \
+      for (auto & val : x)                                              \
+        Attributes<T>::set_highest(val); }                              \
+  }
+
+
+LIBMESH_INT_TYPE(char);
+LIBMESH_INT_TYPE(signed char);
+LIBMESH_INT_TYPE(unsigned char);
+LIBMESH_INT_TYPE(short int);
+LIBMESH_INT_TYPE(unsigned short int);
+LIBMESH_INT_TYPE(int);
+LIBMESH_INT_TYPE(unsigned int);
+LIBMESH_INT_TYPE(long);
+LIBMESH_INT_TYPE(long long);
+LIBMESH_INT_TYPE(unsigned long);
+LIBMESH_INT_TYPE(unsigned long long);
+
+LIBMESH_FLOAT_TYPE(float);
+LIBMESH_FLOAT_TYPE(double);
+LIBMESH_FLOAT_TYPE(long double);
+LIBMESH_CONTAINER_TYPE(std::set);
+LIBMESH_CONTAINER_TYPE(std::vector);
 
 } // namespace Parallel
 
