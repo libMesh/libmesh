6fea5939a1c8c30d570d00a664bbbe9ed5198bac
Roy H. Stogner <roystgnr@ices.utexas.edu>
2018-06-13 16:23:06 -0500

    Require PETSc for projection matrix construction
    None of our other numerics shims support rectangular matrices!?

--- tests/systems/systems_test.C
+++ tests/systems/systems_test.C
@@ -56,11 +56,13 @@ public:
 
 #ifdef LIBMESH_ENABLE_AMR
 #ifdef LIBMESH_HAVE_METAPHYSICL
+#ifdef LIBMESH_HAVE_PETSC
   CPPUNIT_TEST( testProjectMatrixEdge2 );
   CPPUNIT_TEST( testProjectMatrixQuad4 );
   CPPUNIT_TEST( testProjectMatrixTri3 );
   CPPUNIT_TEST( testProjectMatrixHex8 );
   CPPUNIT_TEST( testProjectMatrixTet4 );
+#endif // LIBMESH_HAVE_PETSC
 #endif // LIBMESH_HAVE_METAPHYSICL
 #endif // LIBMESH_ENABLE_AMR
 
@@ -214,6 +216,7 @@ public:
 
 #ifdef LIBMESH_ENABLE_AMR
 #ifdef LIBMESH_HAVE_METAPHYSICL
+#ifdef LIBMESH_HAVE_PETSC
   void testProjectMatrix1D(const ElemType elem_type)
   {
     // Use ReplicatedMesh to get consistent child element node
@@ -669,6 +672,7 @@ public:
     Real diff_norm = proj_mat.linfty_norm();
     CPPUNIT_ASSERT(diff_norm/gold_norm < TOLERANCE*TOLERANCE);
   }
+#endif // LIBMESH_HAVE_PETSC
 #endif // LIBMESH_HAVE_METAPHYSICL
 #endif // LIBMESH_ENABLE_AMR
 
@@ -681,12 +685,14 @@ public:
 
 #ifdef LIBMESH_ENABLE_AMR
 #ifdef LIBMESH_HAVE_METAPHYSICL
+#ifdef LIBMESH_HAVE_PETSC
   // projection matrix tests
   void testProjectMatrixEdge2() { testProjectMatrix1D(EDGE2); }
   void testProjectMatrixQuad4() { testProjectMatrix2D(QUAD4); }
   void testProjectMatrixTri3() { testProjectMatrix2D(TRI3); }
   void testProjectMatrixHex8() { testProjectMatrix3D(HEX8); }
   void testProjectMatrixTet4() { testProjectMatrix3D(TET4); }
+#endif // LIBMESH_HAVE_PETSC
 #endif // LIBMESH_HAVE_METAPHYSICL
 #endif // LIBMESH_ENABLE_AMR
 
