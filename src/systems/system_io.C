b0c2967f348b2ae7a1a30d0d39296acea9e6174f
Roy H. Stogner <roystgnr@ices.utexas.edu>
2018-06-15 17:14:14 -0500

    Keep more MessageTag objects through both uses

--- src/systems/system_io.C
+++ src/systems/system_io.C
@@ -856,6 +856,8 @@ std::size_t System::read_serialized_blocked_dof_objects (const dof_id_type n_obj
   std::vector<std::vector<Number>>      recv_vals(num_blks); // The raw values for the local objects in all blocks
   std::vector<Parallel::Request>
     id_requests(num_blks), val_requests(num_blks);
+  std::vector<Parallel::MessageTag>
+    id_tags(num_blks), val_tags(num_blks);
 
   // ------------------------------------------------------
   // First pass - count the number of objects in each block
@@ -926,14 +928,14 @@ std::size_t System::read_serialized_blocked_dof_objects (const dof_id_type n_obj
             }
 
 #ifdef LIBMESH_HAVE_MPI
-      Parallel::MessageTag id_tag  = this->comm().get_unique_tag(100*num_blks + blk);
-      Parallel::MessageTag val_tag = this->comm().get_unique_tag(200*num_blks + blk);
+      id_tags[blk]  = this->comm().get_unique_tag(100*num_blks + blk);
+      val_tags[blk] = this->comm().get_unique_tag(200*num_blks + blk);
 
       // nonblocking send the data for this block
-      this->comm().send (0, ids,  id_requests[blk],  id_tag);
+      this->comm().send (0, ids,  id_requests[blk],  id_tags[blk]);
 
       // Go ahead and post the receive too
-      this->comm().receive (0, vals, val_requests[blk], val_tag);
+      this->comm().receive (0, vals, val_requests[blk], val_tags[blk]);
 #endif
     }
 
@@ -972,9 +974,6 @@ std::size_t System::read_serialized_blocked_dof_objects (const dof_id_type n_obj
           ThreadedIO<InValType> threaded_io(io, input_vals_tmp);
           Threads::Thread async_io(threaded_io);
 
-          Parallel::MessageTag id_tag  = this->comm().get_unique_tag(100*num_blks + blk);
-          Parallel::MessageTag val_tag = this->comm().get_unique_tag(200*num_blks + blk);
-
           // offset array. this will define where each object's values
           // map into the actual input_vals buffer.  this must get
           // 0-initialized because 0-component objects are not actually sent
@@ -990,10 +989,10 @@ std::size_t System::read_serialized_blocked_dof_objects (const dof_id_type n_obj
             {
 #ifdef LIBMESH_HAVE_MPI
               // blocking receive indices for this block, imposing no particular order on processor
-              Parallel::Status id_status (this->comm().probe (Parallel::any_source, id_tag));
+              Parallel::Status id_status (this->comm().probe (Parallel::any_source, id_tags[blk]));
               std::vector<dof_id_type> & ids (recv_ids[id_status.source()]);
               std::size_t & n_vals_proc (recv_vals_size[id_status.source()]);
-              this->comm().receive (id_status.source(), ids, id_tag);
+              this->comm().receive (id_status.source(), ids, id_tags[blk]);
 #else
               // straight copy without MPI
               std::vector<dof_id_type> & ids (recv_ids[0]);
@@ -1068,7 +1067,7 @@ std::size_t System::read_serialized_blocked_dof_objects (const dof_id_type n_obj
 
 #ifdef LIBMESH_HAVE_MPI
               // send the relevant values to this processor
-              this->comm().send (proc, vals, reply_requests[proc], val_tag);
+              this->comm().send (proc, vals, reply_requests[proc], val_tags[blk]);
 #else
               recv_vals[blk] = vals;
 #endif
