20e6dc58295487a51f9f783148bf74c8a20801d7
John Peterson <jwpeterson@gmail.com>
2018-06-20 11:28:31 -0600

    Drop workaround libmesh_nulltpr_t class.

--- include/utils/libmesh_nullptr.h
+++ include/utils/libmesh_nullptr.h
@@ -18,61 +18,8 @@
 #ifndef LIBMESH_NULLPTR_H
 #define LIBMESH_NULLPTR_H
 
-#include "libmesh/libmesh_config.h"
-
-#if defined(LIBMESH_HAVE_CXX11_NULLPTR)
-
-// Use the built-in nullptr keyword
+// The built-in nullptr keyword is now required, but we still provide
+// libmesh_nullptr for app codes that were using it.
 #define libmesh_nullptr nullptr
-
-#elif defined(LIBMESH_HAVE_CXX11_NULLPTR_WORKAROUND)
-
-/**
- * A C++03-compatible nullptr type implemented as a library solution
- * rather than a language keyword.  Originally due to Scott Meyers,
- * Effective C++, 2nd Edition, Item 25.  The main benefits of using
- * this type over NULL are
- * 1.) Expressiveness:
- * 2.) Disambiguating overloads, e.g.
- *     void f(int);
- *     void f(double *);
- *     f(NULL); // ambiguous
- *     f(nullptr); // unambiguous - calls the version of f taking a pointer
- *
- * We are specifically *not* attempting to redefine the 'nullptr'
- * keyword itself -- that can lead to issues when C++11 is disabled,
- * yet the compiler still has 'nullptr' defined.  Therefore, client
- * code that wishes to be more expressive/correct should use
- * 'libmesh_nullptr' for maximum backwards compatibility.
- *
- * \note The leading const applies to the following nullptr instance.
- *
- * Code originally coped from:
- * https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/nullptr#Solution_and_Sample_Code
- */
-const class libmesh_nullptr_t
-{
-public:
-  // convertible to any type of null non-member pointer...
-  template<class T>
-  inline operator T * () const { return 0; }
-
-  // or any type of null member pointer...
-  template<class C, class T>
-  inline operator T C::*() const { return 0; }
-
-private:
-  // Can't take address of nullptr
-  void operator & () const;
-} libmesh_nullptr = {};
-
-#else
-
-// No nullptr and workaround doesn't compile.  This should not be
-// common, but in this case, We have to fall back on the C++03
-// definition of NULL.
-#define libmesh_nullptr NULL
-
-#endif // LIBMESH_HAVE_CXX11_NULLPTR
 
 #endif // LIBMESH_NULLPTR_H
