3f4b3b0768131c0a68d3135290eed1035d80e720
Derek Gaston <derek.gaston@inl.gov>
2018-07-27 13:34:28 -0600

    Skip partitioning in copy_nodes_and_elems if we should

--- src/mesh/mesh_base.C
+++ src/mesh/mesh_base.C
@@ -238,7 +238,8 @@ void MeshBase::prepare_for_use (const bool skip_renumber_nodes_and_elements, con
     }
 
   // Partition the mesh.
-  this->partition();
+  if (!skip_partitioning())
+    this->partition();
 
   // If we're using DistributedMesh, we'll probably want it
   // parallelized.
