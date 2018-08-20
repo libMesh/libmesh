a7348945053355038fad3f1b5fb9bad9e317ed64
Alex Lindsay <alexlindsay239@gmail.com>
2018-08-20 17:29:18 -0600

    Remove metaphysicl/0.2.0

--- contrib/metaphysicl/0.2.0/test/shadow_vector_navier_unit.C
+++
@@ -1,7 +0,0 @@
-#include "metaphysicl_config.h"
-
-#define USE_SHADOW
-
-#include "testing.h"
-
-#include "navier_unit.h"
--- contrib/metaphysicl/0.2.0/test/sparse_struct_pde_unit.C
+++
@@ -1,8 +0,0 @@
-#include "metaphysicl_config.h"
-
-#define USE_SPARSE
-#define USE_STRUCT
-
-#include "testing.h"
-
-#include "pde_unit.h"
