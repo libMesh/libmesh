b6a6d0e57952f00e99b1b8aac84c19aa5c07e111
Roy Stogner <roy@stogners.org>
2018-11-15 15:11:14 -0600

    Use new build_side_ptr in Cell::loose_bounding_box
    This should shave off a few nanoseconds and give us a bit more test
    coverage.

--- src/geom/cell.C
+++ src/geom/cell.C
@@ -29,10 +29,14 @@ BoundingBox Cell::loose_bounding_box () const
   // through 4-D space, so the full bounding box is just the merger of
   // the sides' bounding boxes.
 
-  BoundingBox bbox = this->build_side_ptr(0)->loose_bounding_box();
+  std::unique_ptr<const Elem> side_ptr { this->build_side_ptr(0, false) };
+  BoundingBox bbox = side_ptr->loose_bounding_box();
   unsigned int my_n_sides = this->n_sides();
   for (unsigned s=1; s < my_n_sides; ++s)
-    bbox.union_with(this->build_side_ptr(s)->loose_bounding_box());
+    {
+      this->build_side_ptr(side_ptr, s);
+      bbox.union_with(side_ptr->loose_bounding_box());
+    }
 
   return bbox;
 }
