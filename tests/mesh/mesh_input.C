70c1f090bdef543ebe36de5bbd2bf5eba27bddc9
John Peterson <jwpeterson@gmail.com>
2019-01-28 17:56:39 -0600

    Add unit test.
    New test is currently disabled for complex variables, but it should
    not be too hard to add support for that by looking at how it's done
    for write_element_data().

--- tests/mesh/mesh_input.C
+++ tests/mesh/mesh_input.C
@@ -10,6 +10,7 @@
 #include <libmesh/mesh_generation.h>
 #include <libmesh/replicated_mesh.h>
 #include <libmesh/exodusII_io.h>
+#include <libmesh/dof_map.h>
 
 #include "test_comm.h"
 
@@ -45,6 +46,10 @@ public:
 #if LIBMESH_DIM > 1
 #ifdef LIBMESH_HAVE_EXODUS_API
   CPPUNIT_TEST( testExodusCopyElementSolution );
+#ifndef LIBMESH_USE_COMPLEX_NUMBERS
+  // Eventually this will support complex numbers.
+  CPPUNIT_TEST( testExodusWriteElementDataFromDiscontinuousNodalData );
+#endif
 #endif
 
   CPPUNIT_TEST( testMeshMoveConstructor );
@@ -125,7 +130,92 @@ public:
           }
     }
   }
-#endif
+
+
+#ifndef LIBMESH_USE_COMPLEX_NUMBERS
+  void testExodusWriteElementDataFromDiscontinuousNodalData()
+  {
+    // first scope: write file
+    {
+      Mesh mesh(*TestCommWorld);
+
+      EquationSystems es(mesh);
+      System & sys = es.add_system<System> ("SimpleSystem");
+      sys.add_variable("u", FIRST, L2_LAGRANGE);
+
+      MeshTools::Generation::build_cube
+        (mesh, 2, 2, 2, 0., 1., 0., 1., 0., 1., HEX8);
+
+      es.init();
+
+      // Set solution u^e_i = i, for the ith vertex of a given element e.
+      const DofMap & dof_map = sys.get_dof_map();
+      std::vector<dof_id_type> dof_indices;
+      for (const auto & elem : mesh.element_ptr_range())
+        {
+          dof_map.dof_indices(elem, dof_indices, /*var_id=*/0);
+          for (unsigned int i=0; i<dof_indices.size(); ++i)
+            sys.solution->set(dof_indices[i], i);
+        }
+      sys.solution->close();
+
+      // Now write to file.
+      ExodusII_IO exii(mesh);
+
+      // Don't try to write element data as averaged nodal data.
+      std::set<std::string> sys_list;
+      exii.write_equation_systems("elemental_from_nodal.e", es, &sys_list);
+
+      // Write one elemental data field per vertex value.
+      sys_list = {"SimpleSystem"};
+
+      exii.write_element_data_from_discontinuous_nodal_data
+        (es, &sys_list, /*var_suffix=*/"_elem_corner_");
+    } // end first scope
+
+    // second scope: read values back in, verify they are correct.
+    {
+      std::vector<std::string> file_var_names =
+        {"u_elem_corner_0",
+         "u_elem_corner_1",
+         "u_elem_corner_2",
+         "u_elem_corner_3"};
+      std::vector<Real> expected_values = {0., 1., 2., 3.};
+
+      // copy_elemental_solution currently requires ReplicatedMesh
+      ReplicatedMesh mesh(*TestCommWorld);
+
+      EquationSystems es(mesh);
+      System & sys = es.add_system<System> ("SimpleSystem");
+      for (auto i : index_range(file_var_names))
+        sys.add_variable(file_var_names[i], CONSTANT, MONOMIAL);
+
+      ExodusII_IO exii(mesh);
+
+      if (mesh.processor_id() == 0)
+        exii.read("elemental_from_nodal.e");
+      MeshCommunication().broadcast(mesh);
+      mesh.prepare_for_use();
+
+      es.init();
+
+      for (auto i : index_range(file_var_names))
+        exii.copy_elemental_solution
+          (sys, sys.variable_name(i), file_var_names[i]);
+
+      // Check that the values we read back in are as expected.
+      for (const auto & elem : mesh.active_element_ptr_range())
+        for (auto i : index_range(file_var_names))
+          {
+            Real read_val = sys.point_value(i, elem->centroid());
+            CPPUNIT_ASSERT_DOUBLES_EQUAL
+              (read_val, expected_values[i], TOLERANCE*TOLERANCE);
+          }
+    } // end second scope
+  } // end testExodusWriteElementDataFromDiscontinuousNodalData
+
+#endif // !LIBMESH_USE_COMPLEX_NUMBERS
+#endif // LIBMESH_HAVE_EXODUS_API
 
   void testMeshMoveConstructor ()
   {
