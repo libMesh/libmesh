cab5d4c6abb960f055a706bc8d624c97b8b008d8
Roy Stogner <roy@stogners.org>
2019-05-01 15:58:34 -0500

    Restrict adaptivity ex3 max_r_steps
    Now that we're asserting error indicator sanity, we need to be sure
    that our linear solvers really converge even when no special
    LIBMESH_OPTIONS are used.

--- examples/adaptivity/adaptivity_ex3/run.sh
+++ examples/adaptivity/adaptivity_ex3/run.sh
@@ -8,16 +8,15 @@ example_name=adaptivity_ex3
 example_dir=examples/adaptivity/$example_name
 
 run_example "$example_name" refinement_type=h
-run_example "$example_name" refinement_type=p
-
-# Some solvers still give us trouble with too much hp
-run_example "$example_name" refinement_type=hp max_r_steps=8
+# Some solvers still give us trouble with too much p
+run_example "$example_name" refinement_type=p max_r_steps=4
+run_example "$example_name" refinement_type=hp max_r_steps=6
 run_example "$example_name" refinement_type=matchedhp max_r_steps=4
 
 # Let's get some 1D coverage too; that's cheap.
 run_example "$example_name" dimension=1 refinement_type=h
 run_example "$example_name" dimension=1 refinement_type=p
-run_example "$example_name" dimension=1 refinement_type=hp max_r_steps=8
+run_example "$example_name" dimension=1 refinement_type=hp max_r_steps=6
 run_example "$example_name" dimension=1 refinement_type=matchedhp max_r_steps=4
 
 # And try out another element type
