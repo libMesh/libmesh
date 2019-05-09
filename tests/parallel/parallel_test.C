b194bfef0aa6c7c9cc96f14904ca30b0bb5c1b5e
Roy Stogner <roy@stogners.org>
2019-05-09 15:19:12 -0500

    Assert sane split communicator sizes
    Hopefully this is enough to make sure we don't screw up our shims to
    the MPI split functions, and then we can trust that the MPI
    implementation itself is fine.

--- tests/parallel/parallel_test.C
+++ tests/parallel/parallel_test.C
@@ -621,6 +621,10 @@ public:
     unsigned int rank = TestCommWorld->rank();
     unsigned int color = rank % 2;
     TestCommWorld->split(color, rank, subcomm);
+
+    CPPUNIT_ASSERT(subcomm.size() >= 1);
+    CPPUNIT_ASSERT(subcomm.size() >= TestCommWorld->size() / 2);
+    CPPUNIT_ASSERT(subcomm.size() <= TestCommWorld->size() / 2 + 1);
   }
 
 
@@ -634,6 +638,9 @@ public:
     type = MPI_COMM_TYPE_SHARED;
 #endif
     TestCommWorld->split_by_type(type, rank, i, subcomm);
+
+    CPPUNIT_ASSERT(subcomm.size() >= 1);
+    CPPUNIT_ASSERT(subcomm.size() <= TestCommWorld->size());
   }
 };
 
