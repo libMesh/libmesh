469f31ebd24f271eb24fbd2e6f529622e9c4fff3
Roy Stogner <roy@stogners.org>
2019-09-05 15:30:11 -0500

    Add assert to QuadratureComposite
    I don't understand this code well enough to be confident in upgrading it
    to non-Lagrange mappings.

--- src/quadrature/quadrature_composite.C
+++ src/quadrature/quadrature_composite.C
@@ -71,6 +71,10 @@ void QComposite<QSubCell>::init (const Elem & elem,
   libmesh_assert_equal_to (vertex_distance_func.size(), elem.n_vertices());
   libmesh_assert_equal_to (_dim, elem.dim());
 
+  // We already initialized a Lagrange map; we're not supporting
+  // others yet
+  libmesh_assert_equal_to (elem.mapping_type(), Elem::LAGRANGE_MAP);
+
   // if we are not cut, revert to simple base class init() method.
   if (!_elem_cutter.is_cut (elem, vertex_distance_func))
     {
