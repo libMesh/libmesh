efcfdc247f41d0d88ce3f4886008e7a0f84b833c
Alex Lindsay <alexlindsay239@gmail.com>
2019-12-10 16:56:56 -0800

    Add all second order distributed test

---
+++ tests/mesh/all_second_order.C
@@ -0,0 +1,37 @@
+#include <libmesh/libmesh.h>
+#include <libmesh/distributed_mesh.h>
+#include <libmesh/mesh_generation.h>
+
+#include "test_comm.h"
+#include "libmesh_cppunit.h"
+
+using namespace libMesh;
+
+class AllSecondOrderTest : public CppUnit::TestCase
+{
+public:
+  CPPUNIT_TEST_SUITE( AllSecondOrderTest );
+
+  CPPUNIT_TEST( allSecondOrder );
+
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp() {}
+
+  void tearDown() {}
+
+  void allSecondOrder()
+  {
+    DistributedMesh mesh(*TestCommWorld, /*dim=*/2);
+
+    mesh.allow_remote_element_removal(false);
+
+    MeshTools::Generation::build_square(mesh, 2, 2);
+
+    mesh.all_second_order();
+  }
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION( AllSecondOrderTest );
