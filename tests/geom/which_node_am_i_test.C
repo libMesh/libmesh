3f59e1a877d9dd699cb7ee7007ba742937a7520b
John Peterson <jwpeterson@gmail.com>
2020-02-09 18:05:06 -0600

    Add unit tests for Elem::local_edge_node()
    Refs #2297

--- tests/geom/which_node_am_i_test.C
+++ tests/geom/which_node_am_i_test.C
@@ -24,9 +24,15 @@ public:
 
   void testPyramids()
   {
-    // The last node on the right side (1) should be node 4 (apex node).
     const Elem & pyr5 = ReferenceElem::get(PYRAMID5);
+    // The last node on the right side (1) should be node 4 (apex node).
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4), pyr5.local_side_node(/*side=*/1, /*node=*/2));
+    // (Edge 0, Node 0) -> Local node 0
+    CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(0), pyr5.local_edge_node(/*edge=*/0, /*node=*/0));
+    // Second node of edges 4-7 is the apex node (4).
+    for (unsigned int edge=4; edge<8; ++edge)
+      CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4),
+                           pyr5.local_edge_node(edge, /*node=*/1));
 
     // Test the libmesh_asserts when they are enabled and exceptions
     // are available. If exceptions aren't available, libmesh_assert
@@ -53,13 +59,21 @@ public:
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(99), n);
 #endif
 
-    // The last node on the right side (1) should be node 10.
     const Elem & pyr13 = ReferenceElem::get(PYRAMID13);
+    // The last node on the right side (1) should be node 10.
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(10), pyr13.local_side_node(/*side=*/1, /*node=*/5));
+    // Second node of edges 4-7 is the apex node (4).
+    for (unsigned int edge=4; edge<8; ++edge)
+      CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4),
+                           pyr13.local_edge_node(edge, /*node=*/1));
 
-    // The central node of the base should be node 13
     const Elem & pyr14 = ReferenceElem::get(PYRAMID14);
+    // The central node of the base should be node 13
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(13), pyr14.local_side_node(/*side=*/4, /*node=*/8));
+    // Second node of edges 4-7 is the apex node (4).
+    for (unsigned int edge=4; edge<8; ++edge)
+      CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4),
+                           pyr14.local_edge_node(edge, /*node=*/1));
   }
 
 
@@ -69,6 +83,14 @@ public:
     // A PRISM6 has four nodes on some sides and three nodes on others
     const Elem & prism6 = ReferenceElem::get(PRISM6);
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4), prism6.local_side_node(/*side=*/4, /*node=*/1));
+    // Edges 3, 4, 5 are the "vertical" Prism edges.
+    for (unsigned int edge=3; edge<6; ++edge)
+      {
+        CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(edge-3),
+                             prism6.local_edge_node(/*edge=*/edge, /*node=*/0));
+        CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(edge),
+                             prism6.local_edge_node(/*edge=*/edge, /*node=*/1));
+      }
 
     // Test the libmesh_asserts when they are enabled and exceptions
     // are available. If exceptions aren't available, libmesh_assert
@@ -101,6 +123,14 @@ public:
     // Test the Prism15.
     const Elem & prism15 = ReferenceElem::get(PRISM15);
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(3), prism15.local_side_node(/*side=*/1, /*node=*/3));
+    // Edges 3, 4, 5 are the "vertical" Prism edges.
+    for (unsigned int edge=3; edge<6; ++edge)
+      {
+        CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(edge-3),
+                             prism15.local_edge_node(/*edge=*/edge, /*node=*/0));
+        CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(edge),
+                             prism15.local_edge_node(/*edge=*/edge, /*node=*/1));
+      }
   }
 
 
@@ -110,9 +140,18 @@ public:
     const Elem & tet4 = ReferenceElem::get(TET4);
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(0), tet4.local_side_node(/*side=*/0, /*node=*/0));
 
-    // Node 4 is a mid-edge node on side 1.
+    // Edges 3, 4, 5 all connect to the "apex" node 3
+    for (unsigned int edge=3; edge<6; ++edge)
+      CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(3),
+                           tet4.local_edge_node(edge, /*node=*/1));
+
     const Elem & tet10 = ReferenceElem::get(TET10);
+    // Node 4 is a mid-edge node on side 1.
     CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(4), tet10.local_side_node(/*side=*/1, /*node=*/3));
+    // Nodes 4, 5, 6 are mid-edge nodes of the first three edges
+    for (unsigned int edge=0; edge<3; ++edge)
+      CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(edge + 4),
+                           tet10.local_edge_node(edge, /*node=*/2));
   }
 
 
@@ -137,6 +176,18 @@ public:
           // Make sure the Hex20 and Hex27 implementations agree.
           CPPUNIT_ASSERT_EQUAL(hex20.local_side_node(side, node),
                                hex27.local_side_node(side, node));
+        }
+
+    for (unsigned int edge=0; edge<hex8.n_edges(); ++edge)
+      for (unsigned int node=0; node<2; ++node)
+        {
+          // Make sure the Hex8 and Hex20 implementations agree.
+          CPPUNIT_ASSERT_EQUAL(hex8.local_edge_node(edge, node),
+                               hex20.local_edge_node(edge, node));
+
+          // Make sure the Hex20 and Hex27 implementations agree.
+          CPPUNIT_ASSERT_EQUAL(hex20.local_edge_node(edge, node),
+                               hex27.local_edge_node(edge, node));
         }
   }
 };
