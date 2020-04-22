453339e776dc431126ce26763facc6bb55d42258
Fande Kong <fdkong.jd@gmail.com>
2020-04-21 18:02:08 -0600

    PETSc-3.13 does if Vec is consistent with DM
    So Vec and DM need to know each other

--- src/solvers/petscdmlibmeshimpl.C
+++ src/solvers/petscdmlibmeshimpl.C
@@ -786,6 +786,7 @@ static PetscErrorCode DMCreateGlobalVector_libMesh(DM dm, Vec *x)
     ierr = VecDuplicate(v,x); CHKERRQ(ierr);
   }
   ierr = PetscObjectCompose((PetscObject)*x,"DM",(PetscObject)dm); CHKERRQ(ierr);
+  ierr = VecSetDM(*x, dm);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
