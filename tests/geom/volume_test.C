bab42e8fb72642289236fc2f6f18c6784cb50e66
John Peterson <jwpeterson@gmail.com>
2020-04-23 15:02:34 -0500

    Add tests of curved element volume.

--- tests/geom/volume_test.C
+++ tests/geom/volume_test.C
@@ -38,18 +38,33 @@ public:
     // Check unperturbed, straight edge case
     LIBMESH_ASSERT_FP_EQUAL(1.0, edge3->volume(), TOLERANCE*TOLERANCE);
 
-    // Check middle node perturbed in +x direction case. This should
-    // not change the volume because it's still a straight line
-    // element.
-    Real & middle_node_x = edge3->node_ref(2)(0);
-    middle_node_x += 1.e-3;
-    LIBMESH_ASSERT_FP_EQUAL(1.0, edge3->volume(), TOLERANCE*TOLERANCE);
+    // Get references to the individual Edge3 nodes
+    auto & middle_node = edge3->node_ref(2);
+    auto & right_node = edge3->node_ref(1);
 
     // Check middle node perturbed in +x direction case. This should
     // not change the volume because it's still a straight line
     // element.
-    middle_node_x -= 2.e-3;
+    middle_node = Point(0.5 + 1.e-3, 0., 0.);
     LIBMESH_ASSERT_FP_EQUAL(1.0, edge3->volume(), TOLERANCE*TOLERANCE);
+
+    // Check middle node perturbed in -x direction case. This should
+    // not change the volume because it's still a straight line
+    // element.
+    middle_node = Point(0.5 - 1.e-3, 0., 0.);
+    LIBMESH_ASSERT_FP_EQUAL(1.0, edge3->volume(), TOLERANCE*TOLERANCE);
+
+    // Check volume of actual curved element against pre-computed value.
+    middle_node = Point(0.5, 0.25, 0.);
+    right_node = Point(1., 1., 0.);
+    LIBMESH_ASSERT_FP_EQUAL(1.4789428575446, edge3->volume(), TOLERANCE);
+
+    // Compare with volume computed by base class Elem::volume() call
+    // which uses quadrature.  We don't expect this to have full
+    // floating point accuracy.
+    middle_node = Point(0.5, 0.1, 0.);
+    right_node = Point(1., 0., 0.);
+    LIBMESH_ASSERT_FP_EQUAL(edge3->Elem::volume(), edge3->volume(), std::sqrt(TOLERANCE));
   }
 };
 
