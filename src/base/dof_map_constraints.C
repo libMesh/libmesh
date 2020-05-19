0e37f074b1fa62809798feca9cf78ce34ef3b678
Roy Stogner <roy@stogners.org>
2020-05-19 14:14:39 -0500

    Early break from loops
    Those bools aren't going to get any *more* true

--- src/base/dof_map_constraints.C
+++ src/base/dof_map_constraints.C
@@ -451,6 +451,7 @@ private:
                   is_boundary_node[n] = true;
                   is_boundary_nodeset[n] = true;
                   has_dirichlet_constraint = true;
+                  break;
                 }
           }
 
@@ -469,6 +470,8 @@ private:
                   for (unsigned int n = 0; n != n_nodes; ++n)
                     if (elem->is_node_on_edge(n,e))
                       is_boundary_node[n] = true;
+
+                  break;
                 }
           }
 
@@ -483,6 +486,7 @@ private:
                 {
                   is_boundary_shellface[shellface] = true;
                   has_dirichlet_constraint = true;
+                  break;
                 }
           }
 
