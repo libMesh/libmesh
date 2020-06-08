da1b150487312aa7649488010354c6a4a8fb228d
John Peterson <jwpeterson@gmail.com>
2020-06-08 13:28:31 -0500

    Call new version of FEInterface::shape() in fe_szabab.C

--- src/fe/fe_szabab.C
+++ src/fe/fe_szabab.C
@@ -38,8 +38,7 @@ namespace {
 void szabab_nodal_soln(const Elem * elem,
                        const Order order,
                        const std::vector<Number> & elem_soln,
-                       std::vector<Number> &       nodal_soln,
-                       unsigned Dim)
+                       std::vector<Number> & nodal_soln)
 {
   const unsigned int n_nodes = elem->n_nodes();
 
@@ -51,7 +50,6 @@ void szabab_nodal_soln(const Elem * elem,
 
   // FEType object to be passed to various FEInterface functions below.
   FEType fe_type(order, SZABAB);
-  FEType p_refined_fe_type(totalorder, SZABAB);
 
   switch (totalorder)
     {
@@ -79,7 +77,6 @@ void szabab_nodal_soln(const Elem * elem,
     case SIXTH:
     case SEVENTH:
       {
-
         const unsigned int n_sf =
           FEInterface::n_shape_functions(fe_type, elem);
 
@@ -97,7 +94,7 @@ void szabab_nodal_soln(const Elem * elem,
             // u_i = Sum (alpha_i phi_i)
             for (unsigned int i=0; i<n_sf; i++)
               nodal_soln[n] += elem_soln[i] *
-                FEInterface::shape(Dim, p_refined_fe_type, elem, i, refspace_nodes[n]);
+                FEInterface::shape(fe_type, elem, i, refspace_nodes[n]);
           }
 
         return;
@@ -1226,28 +1223,28 @@ void FE<0,SZABAB>::nodal_soln(const Elem * elem,
                               const Order order,
                               const std::vector<Number> & elem_soln,
                               std::vector<Number> & nodal_soln)
-{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/0); }
+{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<1,SZABAB>::nodal_soln(const Elem * elem,
                               const Order order,
                               const std::vector<Number> & elem_soln,
                               std::vector<Number> & nodal_soln)
-{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/1); }
+{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<2,SZABAB>::nodal_soln(const Elem * elem,
                               const Order order,
                               const std::vector<Number> & elem_soln,
                               std::vector<Number> & nodal_soln)
-{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/2); }
+{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<3,SZABAB>::nodal_soln(const Elem * elem,
                               const Order order,
                               const std::vector<Number> & elem_soln,
                               std::vector<Number> & nodal_soln)
-{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/3); }
+{ szabab_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 
 // Full specialization of n_dofs() function for every dimension
