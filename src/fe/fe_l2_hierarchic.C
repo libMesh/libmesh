f94c8ec72d75824c293a26683a52ed7b06f50fa7
John Peterson <jwpeterson@gmail.com>
2020-06-08 13:49:01 -0500

    Call new version of FEInterface::shape() in fe_l2_hierarchic.C

--- src/fe/fe_l2_hierarchic.C
+++ src/fe/fe_l2_hierarchic.C
@@ -35,8 +35,7 @@ namespace {
 void l2_hierarchic_nodal_soln(const Elem * elem,
                               const Order order,
                               const std::vector<Number> & elem_soln,
-                              std::vector<Number> &       nodal_soln,
-                              unsigned Dim)
+                              std::vector<Number> & nodal_soln)
 {
   const unsigned int n_nodes = elem->n_nodes();
 
@@ -48,7 +47,6 @@ void l2_hierarchic_nodal_soln(const Elem * elem,
 
   // FEType object to be passed to various FEInterface functions below.
   FEType fe_type(order, L2_HIERARCHIC);
-  FEType p_refined_fe_type(totalorder, L2_HIERARCHIC);
 
   switch (totalorder)
     {
@@ -87,7 +85,7 @@ void l2_hierarchic_nodal_soln(const Elem * elem,
             // u_i = Sum (alpha_i phi_i)
             for (unsigned int i=0; i<n_sf; i++)
               nodal_soln[n] += elem_soln[i] *
-                FEInterface::shape(Dim, p_refined_fe_type, elem, i, refspace_nodes[n]);
+                FEInterface::shape(fe_type, elem, i, refspace_nodes[n]);
           }
 
         return;
@@ -145,28 +143,28 @@ void FE<0,L2_HIERARCHIC>::nodal_soln(const Elem * elem,
                                      const Order order,
                                      const std::vector<Number> & elem_soln,
                                      std::vector<Number> & nodal_soln)
-{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/0); }
+{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<1,L2_HIERARCHIC>::nodal_soln(const Elem * elem,
                                      const Order order,
                                      const std::vector<Number> & elem_soln,
                                      std::vector<Number> & nodal_soln)
-{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/1); }
+{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<2,L2_HIERARCHIC>::nodal_soln(const Elem * elem,
                                      const Order order,
                                      const std::vector<Number> & elem_soln,
                                      std::vector<Number> & nodal_soln)
-{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/2); }
+{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 template <>
 void FE<3,L2_HIERARCHIC>::nodal_soln(const Elem * elem,
                                      const Order order,
                                      const std::vector<Number> & elem_soln,
                                      std::vector<Number> & nodal_soln)
-{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln, /*Dim=*/3); }
+{ l2_hierarchic_nodal_soln(elem, order, elem_soln, nodal_soln); }
 
 // Full specialization of n_dofs() function for every dimension
 template <> unsigned int FE<0,L2_HIERARCHIC>::n_dofs(const ElemType t, const Order o) { return l2_hierarchic_n_dofs(t, o); }
