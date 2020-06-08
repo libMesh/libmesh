a30b11f716641a0776c5387bfefd428e688e588e
John Peterson <jwpeterson@gmail.com>
2020-06-08 11:04:05 -0500

    Use new FEInterface::n_shape_functions() in fe_monomial.C

--- src/fe/fe_monomial.C
+++ src/fe/fe_monomial.C
@@ -64,11 +64,11 @@ void monomial_nodal_soln(const Elem * elem,
     default:
       {
         // FEType object to be passed to various FEInterface functions below.
-        FEType fe_type(totalorder, MONOMIAL);
+        FEType fe_type(order, MONOMIAL);
+        FEType p_refined_fe_type(totalorder, MONOMIAL);
 
         const unsigned int n_sf =
-          // FE<Dim,T>::n_shape_functions(elem_type, totalorder);
-          FEInterface::n_shape_functions(Dim, fe_type, elem_type);
+          FEInterface::n_shape_functions(fe_type, elem);
 
         std::vector<Point> refspace_nodes;
         FEBase::get_refspace_nodes(elem_type,refspace_nodes);
@@ -84,8 +84,7 @@ void monomial_nodal_soln(const Elem * elem,
             // u_i = Sum (alpha_i phi_i)
             for (unsigned int i=0; i<n_sf; i++)
               nodal_soln[n] += elem_soln[i] *
-                // FE<Dim,T>::shape(elem, order, i, mapped_point);
-                FEInterface::shape(Dim, fe_type, elem, i, refspace_nodes[n]);
+                FEInterface::shape(Dim, p_refined_fe_type, elem, i, refspace_nodes[n]);
           }
 
         return;
