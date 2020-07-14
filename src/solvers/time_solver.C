9af147911e8ef0befcf1e5aa162be435a64d322f
Roy Stogner <roystgnr@ices.utexas.edu>
2020-07-14 10:35:34 -0500

    Move header inclusion
    This is libMesh style, and should make it a little easier to catch any
    mistakes with forward declarations in that header.

--- src/solvers/time_solver.C
+++ src/solvers/time_solver.C
@@ -15,10 +15,11 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+#include "libmesh/time_solver.h"
+
 #include "libmesh/diff_solver.h"
 #include "libmesh/diff_system.h"
 #include "libmesh/linear_solver.h"
-#include "libmesh/time_solver.h"
 #include "libmesh/no_solution_history.h"
 #include "libmesh/auto_ptr.h" // libmesh_make_unique
 
