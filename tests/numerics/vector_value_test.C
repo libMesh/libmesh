52e5e61a51ad1d617769335c3509939a8d4a4d57
Alex Lindsay <alexlindsay239@gmail.com>
2020-10-28 13:31:42 -0700

    Fix CompareTypes typos in type_vector
    Also add a test this time
    
    Refs #2711

--- tests/numerics/vector_value_test.C
+++ tests/numerics/vector_value_test.C
@@ -2,6 +2,8 @@
 
 #include "type_vector_test.h"
 
+#include <type_traits>
+
 
 using namespace libMesh;
 
@@ -36,6 +38,75 @@ public:
   CPPUNIT_TEST_SUITE_END();
 };
 
+class VectorCompareTypesTest : public CppUnit::TestCase {
+public:
+  CPPUNIT_TEST_SUITE( VectorCompareTypesTest );
+
+  CPPUNIT_TEST( testCompareTypes );
+
+  CPPUNIT_TEST_SUITE_END();
+
+private:
+  template <typename T, typename T2>
+  typename CompareTypes<T, T2>::supertype
+  average(const T & val1, const T2 & val2)
+    {
+      return (val1 + val2) / 2;
+    }
+
+public:
+  void
+  testCompareTypes()
+    {
+      VectorValue<float> fvec;
+      VectorValue<double> dvec;
+
+      auto ftype = fvec * 1;
+      auto dtype = dvec * 1;
+
+      {
+        bool assertion = std::is_same<decltype(ftype), TypeVector<float>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        bool assertion = std::is_same<decltype(dtype), TypeVector<double>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(ftype, ftype);
+        bool assertion = std::is_same<decltype(temp), TypeVector<float>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(ftype, dtype);
+        bool assertion = std::is_same<decltype(temp), TypeVector<double>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(fvec, fvec);
+        bool assertion = std::is_same<decltype(temp), VectorValue<float>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(fvec, dvec);
+        bool assertion = std::is_same<decltype(temp), VectorValue<double>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(fvec, dtype);
+        bool assertion = std::is_same<decltype(temp), VectorValue<double>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+      {
+        auto temp = average(ftype, dvec);
+        bool assertion = std::is_same<decltype(temp), VectorValue<double>>::value;
+        CPPUNIT_ASSERT(assertion);
+      }
+    }
+};
+
+
 CPPUNIT_TEST_SUITE_REGISTRATION( RealVectorValueTest );
 CPPUNIT_TEST_SUITE_REGISTRATION( NumberVectorValueTest );
 CPPUNIT_TEST_SUITE_REGISTRATION( ComplexVectorValueTest );
+CPPUNIT_TEST_SUITE_REGISTRATION( VectorCompareTypesTest );
