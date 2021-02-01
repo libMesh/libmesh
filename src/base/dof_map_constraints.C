071e2a4119ef01f65240a5d82c1501d215e2dce5
John Peterson <jwpeterson@gmail.com>
2021-02-01 07:19:28 -0600

    Undo header file doc change from 59397ffe

--- src/base/dof_map_constraints.C
+++ src/base/dof_map_constraints.C
@@ -15,8 +15,10 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-// libMesh includes
+// Local includes
 #include "libmesh/dof_map.h"
+
+// libMesh includes
 #include "libmesh/boundary_info.h" // needed for dirichlet constraints
 #include "libmesh/dense_matrix.h"
 #include "libmesh/dense_vector.h"
