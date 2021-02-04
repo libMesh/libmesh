4cbdf6045a94874e4b210f8070f9e37ab8f7ddda
Alex Lindsay <alexlindsay239@gmail.com>
2021-02-04 11:13:38 -0800

    Add unit test

--- tests/mesh/mesh_stitch.C
+++ tests/mesh/mesh_stitch.C
@@ -2,10 +2,13 @@
 #include <libmesh/mesh_generation.h>
 #include <libmesh/node.h>
 #include <libmesh/replicated_mesh.h>
+#include <libmesh/mesh_modification.h>
+#include <libmesh/boundary_info.h>
 
 #include "test_comm.h"
 #include "libmesh_cppunit.h"
 
+#include <algorithm>
 
 using namespace libMesh;
 
@@ -16,6 +19,7 @@ public:
 
 #if LIBMESH_DIM > 2
   CPPUNIT_TEST( testMeshStitch );
+  CPPUNIT_TEST( testBoundaryInfo );
 #endif // LIBMESH_DIM > 2
 
   CPPUNIT_TEST_SUITE_END();
@@ -28,6 +32,77 @@ public:
 
   void tearDown()
   {}
+
+  void renameAndShift(ReplicatedMesh & mesh,
+                      const boundary_id_type boundary_id_offset,
+                      const std::string & boundary_name_prefix)
+  {
+    BoundaryInfo & boundary_info = mesh.get_boundary_info();
+    const auto & mesh_boundary_ids = boundary_info.get_boundary_ids();
+    for (auto rit = mesh_boundary_ids.rbegin(); rit != mesh_boundary_ids.rend(); ++rit)
+    {
+      boundary_info.sideset_name(*rit + boundary_id_offset) =
+          boundary_name_prefix + boundary_info.sideset_name(*rit);
+      boundary_info.nodeset_name(*rit + boundary_id_offset) =
+          boundary_name_prefix + boundary_info.nodeset_name(*rit);
+
+      MeshTools::Modification::change_boundary_id(mesh, *rit, *rit + boundary_id_offset);
+    }
+  }
+
+  void testBoundaryInfo()
+  {
+    ReplicatedMesh mesh0(*TestCommWorld), mesh1(*TestCommWorld);
+
+    int ps = 2;
+    MeshTools::Generation::build_cube(mesh0, ps, ps, ps, -1, 0, 0, 1, 0, 1, HEX8);
+    MeshTools::Generation::build_cube(mesh1, ps, ps, ps, 0, 1, 0, 1, 0, 1, HEX8);
+
+    // rename and shift boundaries
+    renameAndShift(mesh0, 0, "zero_");
+    renameAndShift(mesh1, 6, "one_");
+
+    mesh0.stitch_meshes(mesh1, 2, 10, TOLERANCE, true, true, false, false);
+
+    CPPUNIT_ASSERT_EQUAL(mesh0.n_elem(), dof_id_type(16));
+    CPPUNIT_ASSERT_EQUAL(mesh0.n_nodes(), dof_id_type(45));
+
+    const BoundaryInfo & bi = mesh0.get_boundary_info();
+    const auto & sbi = bi.get_side_boundary_ids();
+    typename std::decay<decltype(sbi.size())>::type expected_size = 10;
+    CPPUNIT_ASSERT_EQUAL(expected_size, sbi.size());
+
+    const auto & nbi = bi.get_node_boundary_ids();
+    CPPUNIT_ASSERT_EQUAL(expected_size, nbi.size());
+
+    const auto & ss_id_to_name = bi.get_sideset_name_map();
+    std::set<std::string> ss_names;
+    std::for_each(ss_id_to_name.begin(),
+                  ss_id_to_name.end(),
+                  [&ss_names, this](const std::pair<boundary_id_type, std::string> & map_pr) {
+                    ss_names.insert(map_pr.second);
+                  });
+    std::set<std::string> expected_names = {{"zero_left",
+                                             "zero_top",
+                                             "zero_front",
+                                             "zero_back",
+                                             "zero_bottom",
+                                             "one_right",
+                                             "one_top",
+                                             "one_front",
+                                             "one_back",
+                                             "one_bottom"}};
+    CPPUNIT_ASSERT(ss_names == expected_names);
+
+    const auto & ns_id_to_name = bi.get_nodeset_name_map();
+    std::set<std::string> ns_names;
+    std::for_each(ns_id_to_name.begin(),
+                  ns_id_to_name.end(),
+                  [&ns_names, this](const std::pair<boundary_id_type, std::string> & map_pr) {
+                    ns_names.insert(map_pr.second);
+                  });
+    CPPUNIT_ASSERT(ns_names == expected_names);
+  }
 
   void testMeshStitch ()
   {
