21ca74d02e6d177dd4f3f8c009d78039bfc093e9
Roy Stogner <roy@stogners.org>
2021-02-09 10:19:01 -0600

    Re-reserve non-zero pattern after Eigen::setZero
    `Eigen::SparseMatrix::setZero` clears the entire non-zero pattern, so we have to
    re-reserve after zeroing in order to not have very expensive recalls to
    `Eigen::SparseMatrix::reserveInnerVectors`

--- src/numerics/eigen_sparse_matrix.C
+++ src/numerics/eigen_sparse_matrix.C
@@ -179,7 +179,15 @@ void EigenSparseMatrix<T>::clear ()
 template <typename T>
 void EigenSparseMatrix<T>::zero ()
 {
+  // This doesn't just zero, it clears the entire non-zero structure!
   _mat.setZero();
+
+  // Re-reserve our non-zero structure
+  if (this->_dof_map)
+    {
+      const std::vector<numeric_index_type> & n_nz = this->_dof_map->get_n_nz();
+      _mat.reserve(n_nz);
+    }
 }
 
 
