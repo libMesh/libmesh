374ad6b5a966c78b9e418f5221dcde7b89d6e2b7
BalticPinguin <hu.bert@xemail.me>
2021-04-08 16:05:19 +0200

    added new example: miscellaneous 15

---
+++ examples/miscellaneous/miscellaneous_ex15/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#set -x
+
+source $LIBMESH_DIR/examples/run_common.sh
+
+example_name=miscellaneous_ex15
+
+example_dir=examples/miscellaneous/miscellaneous_ex15
+
+run_example "$example_name"
+
