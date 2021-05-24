7bfe2e6e28412a4d13ce810ecb730ce64c68ed29
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-24 16:11:39 -0500

    Add benchmark parameters to adaptivity ex2 and ex4

--- examples/adaptivity/adaptivity_ex2/run.sh
+++ examples/adaptivity/adaptivity_ex2/run.sh
@@ -18,15 +18,23 @@ output_freq=10
 
 options=" \[-read_solution\] -n_timesteps $n_timesteps -n_refinements $n_refinements -init_timestep \[0\|$n_timesteps\]"
 
+# Run from scratch with either example parameters or benchmark parameters
+
 options="-n_timesteps $n_timesteps -n_refinements $n_refinements -output_freq $output_freq -init_timestep 0"
 run_example "$example_name" "$options"
+
+benchmark_options="-n_timesteps 25 -n_refinements 7 -max_h_level 7 -output_freq 10 -init_timestep 0"
+benchmark_example 1 "$example_name" "$benchmark_options"
 
 echo " "
 echo "***** Finished first" $n_timesteps "steps, now read in" \
     "saved solution and continue *****"
 echo " "
 
+# Then run from the restart with either example parameters or benchmark parameters
+
 options="-read_solution -n_timesteps $n_timesteps -output_freq $output_freq -init_timestep $n_timesteps"
 run_example "$example_name" "$options"
 
-options=" \[-read_solution\] -n_timesteps $n_timesteps -n_refinements $n_refinements -init_timestep \[0\|$n_timesteps\]"
+benchmark_options="-read_solution -n_timesteps 25 -max_h_level 7 -output_freq 10 -init_timestep 25"
+benchmark_example 1 "$example_name" "$benchmark_options"
