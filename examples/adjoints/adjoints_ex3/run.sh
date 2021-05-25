4fe4760d426e0ce330800c8de852e831c5463cdf
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-25 11:23:49 -0500

    Benchmark parameters for adjoints examples

--- examples/adjoints/adjoints_ex3/run.sh
+++ examples/adjoints/adjoints_ex3/run.sh
@@ -11,3 +11,6 @@ example_dir=examples/adjoints/$example_name
 options="-pc_type jacobi"
 
 run_example "$example_name" $options
+
+# Benchmark parameters
+benchmark_example 1 "$example_name" max_adaptivesteps=18
