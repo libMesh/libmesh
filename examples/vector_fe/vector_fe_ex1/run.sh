be946bb97670b3967e364874adb7f4f2abfe11c2
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-26 15:51:32 -0500

    vector_fe benchmark, comments

--- examples/vector_fe/vector_fe_ex1/run.sh
+++ examples/vector_fe/vector_fe_ex1/run.sh
@@ -7,3 +7,6 @@ source $LIBMESH_DIR/examples/run_common.sh
 example_name=vector_fe_ex1
 
 run_example "$example_name" 
+
+# No benchmark here - this spends 95+% of time in the linear solve
+# benchmark_example 1 "$example_name" "-nx 200 -ny 200"
