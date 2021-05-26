cce3ec95620f4b2e2eca098cb8580ff76860e2fb
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-26 14:50:54 -0500

    Transient benchmark, comment

--- examples/transient/transient_ex2/run.sh
+++ examples/transient/transient_ex2/run.sh
@@ -9,3 +9,5 @@ example_dir=examples/transient/$example_name
 
 options="pipe-mesh.unv"
 run_example "$example_name" "$options"
+
+# No benchmark here; "result_node = 274" makes mesh modification iffy
