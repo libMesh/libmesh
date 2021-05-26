6b43ebd5c08b167615e14744b05b50561f021442
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-26 11:47:31 -0500

    subdomains benchmarks, comments

--- examples/subdomains/subdomains_ex1/run.sh
+++ examples/subdomains/subdomains_ex1/run.sh
@@ -14,3 +14,7 @@ run_example "$example_name" "$options"
 
 options="-d 3 -n 15"
 run_example "$example_name" "$options"
+
+benchmark_example 1 "$example_name" "-d 1 -n 1000000"
+benchmark_example 1 "$example_name" "-d 2 -n 1000"
+benchmark_example 1 "$example_name" "-d 3 -n 120"
--- examples/subdomains/subdomains_ex3/run.sh
+++ examples/subdomains/subdomains_ex3/run.sh
@@ -11,3 +11,8 @@ run_example "$example_name" "$options"
 
 options="-d 3"
 run_example "$example_name" "$options"
+
+# No benchmarks here - if I bump n_refinements above 4 I get "must
+# have 3 input vertices" errors
+# benchmark_example 1 "$example_name" "-d 2 -n_refinements 6"
+# benchmark_example 1 "$example_name" "-d 3 -n_refinements 6"
