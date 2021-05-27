ef80e537cfabad3b5bf8b65b4202a86a36701c0a
Roy Stogner <Roy.Stogner@inl.gov>
2021-05-27 10:23:13 -0500

    Disable systems_of_equations ex2 benchmark
    After tweaking the solver settings to avoid excessive reassembly, we
    finish in half the time but with much less of that time outside the
    solver.

--- examples/systems_of_equations/systems_of_equations_ex2/run.sh
+++ examples/systems_of_equations/systems_of_equations_ex2/run.sh
@@ -8,5 +8,6 @@ example_name=systems_of_equations_ex2
 
 run_example "$example_name" 
 
-# This is borderline, already ~85% in the linear solve.
-benchmark_example 1 "$example_name" "-n_elem 40"
+# When we allow more complete linear solves, we finish faster ... but
+# we also end up with 93% of our time in the linear solver.
+#benchmark_example 1 "$example_name" "-n_elem 40 -max_iter 2500"
