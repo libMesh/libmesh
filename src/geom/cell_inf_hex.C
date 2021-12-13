61105f3540777a5b09d7b45336f472e8430cedca
Logan Harbour <loganhharbour@gmail.com>
2021-12-13 11:50:35 -0700

    Fix edge -> side mapping for InfHex and consolidate edge_sides_map

--- src/geom/cell_inf_hex.C
+++ src/geom/cell_inf_hex.C
@@ -66,7 +66,17 @@ const Real InfHex::_master_points[18][3] =
     {0, 0, 1}
   };
 
-
+const unsigned int InfHex::edge_sides_map[8][2] =
+  {
+    {0, 1}, // Edge 0
+    {0, 2}, // Edge 1
+    {0, 3}, // Edge 2
+    {0, 4}, // Edge 3
+    {1, 4}, // Edge 4
+    {1, 2}, // Edge 5
+    {2, 3}, // Edge 6
+    {3, 4}  // Edge 7
+  };
 
 
 
@@ -222,15 +232,16 @@ bool InfHex::is_edge_on_side (const unsigned int e,
   libmesh_assert_less (e, this->n_edges());
   libmesh_assert_less (s, this->n_sides());
 
-  return (InfHex8::edge_sides_map[e][0] == s ||
-          InfHex8::edge_sides_map[e][1] == s);
+  return (edge_sides_map[e][0] == s || edge_sides_map[e][1] == s);
 }
 
 
 
 std::vector<unsigned int> InfHex::sides_on_edge(const unsigned int e) const
 {
-  return {InfHex8::edge_sides_map[e][0], InfHex8::edge_sides_map[e][1]};
+  libmesh_assert_less(e, this->n_edges());
+
+  return {edge_sides_map[e][0], edge_sides_map[e][1]};
 }
 
 
