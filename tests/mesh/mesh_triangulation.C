d5b5f1e0f878de028725b5b71e362364bf7eaa1c
Roy Stogner <Roy.Stogner@inl.gov>
2022-02-09 17:59:29 -0600

    Unit test for Poly2Tri refinement

--- tests/mesh/mesh_triangulation.C
+++ tests/mesh/mesh_triangulation.C
@@ -29,6 +29,7 @@ public:
   CPPUNIT_TEST( testPoly2Tri );
   CPPUNIT_TEST( testPoly2TriHoles );
   CPPUNIT_TEST( testPoly2TriSegments );
+  CPPUNIT_TEST( testPoly2TriRefined );
 #endif
 
 #ifdef LIBMESH_HAVE_TRIANGLE
@@ -303,6 +304,41 @@ public:
 #endif
   }
 
+
+  void testPoly2TriRefined()
+  {
+    Mesh mesh(*TestCommWorld);
+    mesh.add_point(Point(0,0));
+    mesh.add_point(Point(1,0));
+    mesh.add_point(Point(1,2));
+    mesh.add_point(Point(0,1));
+
+    Poly2TriTriangulator triangulator(mesh);
+
+    // Use the point order to define the boundary, because our
+    // Poly2Tri implementation doesn't do convex hulls yet, even when
+    // that would give the same answer.
+    triangulator.triangulation_type() = TriangulatorInterface::PSLG;
+
+    // Try to insert points!
+    triangulator.desired_area() = 0.1;
+    triangulator.minimum_angle() = 0;
+    triangulator.smooth_after_generating() = false;
+
+    triangulator.triangulate();
+
+    CPPUNIT_ASSERT(mesh.n_elem() > 2);
+
+    Real area = 0;
+    for (const auto & elem : mesh.element_ptr_range())
+      {
+        CPPUNIT_ASSERT_EQUAL(elem->type(), TRI3);
+
+        area += elem->volume();
+      }
+
+    LIBMESH_ASSERT_FP_EQUAL(area, 1.5, TOLERANCE*TOLERANCE);
+  }
 
 };
 
