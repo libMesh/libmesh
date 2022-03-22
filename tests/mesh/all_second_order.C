fec09faf9562f34d8275d0010bf34ede03af5b8f
John Peterson <jwpeterson@gmail.com>
2022-03-22 11:01:29 -0500

    Add unit test for MeshBase::all_second_order_range()

--- tests/mesh/all_second_order.C
+++ tests/mesh/all_second_order.C
@@ -1,7 +1,11 @@
+// libMesh includes
 #include <libmesh/libmesh.h>
 #include <libmesh/distributed_mesh.h>
+#include <libmesh/replicated_mesh.h>
 #include <libmesh/mesh_generation.h>
+#include <libmesh/edge_edge2.h>
 
+// cppunit includes
 #include "test_comm.h"
 #include "libmesh_cppunit.h"
 
@@ -13,6 +17,7 @@ public:
   LIBMESH_CPPUNIT_TEST_SUITE( AllSecondOrderTest );
 
   CPPUNIT_TEST( allSecondOrder );
+  CPPUNIT_TEST( allSecondOrderRange );
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -32,6 +37,34 @@ public:
     MeshTools::Generation::build_square(mesh, 2, 2);
 
     mesh.all_second_order();
+  }
+
+  void allSecondOrderRange()
+  {
+    ReplicatedMesh mesh(*TestCommWorld);
+
+    // Construct a single-element Quad4 mesh
+    MeshTools::Generation::build_square(mesh, /*nx=*/1, /*ny=*/1);
+
+    // Add extra nodes above the four original nodes
+    Point z_trans(0,0,1);
+    for (dof_id_type n=0; n<4; ++n)
+      mesh.add_point(mesh.point(n) + z_trans, /*id=*/4+n, /*proc_id=*/0);
+
+    // Construct Edge2 elements attached to Quad4 at individual points
+    // and in subdomain 1.
+    for (dof_id_type n=0; n<4; ++n)
+      {
+        Elem * elem = mesh.add_elem(new Edge2);
+        elem->set_node(0) = mesh.node_ptr(n);
+        elem->set_node(1) = mesh.node_ptr(n+4);
+        elem->subdomain_id() = 1;
+      }
+
+    // Convert only Edge2 elements (all elements in subdomain 1) to
+    // SECOND-order.
+    mesh.all_second_order_range(mesh.active_subdomain_elements_ptr_range(1),
+                                /*full_ordered=*/true);
   }
 };
 
