166e1d792bcbe366997894bd1c9f6dd9d99ddd69
John Peterson <jwpeterson@gmail.com>
2022-04-08 13:35:07 -0500

    Add CPPUNIT assertions to unit test
    * Drop debugging print statement in unit test
    * Turn off verbose mode in test

--- tests/mesh/write_elemset_data.C
+++ tests/mesh/write_elemset_data.C
@@ -88,14 +88,14 @@ public:
     mesh.add_elemset_code(3, {1,2});
 
     // Debugging: print valid elemset_codes values
-    for (const auto & elem : mesh.element_ptr_range())
-      {
-        dof_id_type elemset_code =
-          elem->get_extra_integer(elemset_index);
-
-        if (elemset_code != DofObject::invalid_id)
-          libMesh::out << "Elem " << elem->id() << ", elemset_code = " << elemset_code << std::endl;
-      }
+    // for (const auto & elem : mesh.element_ptr_range())
+    //   {
+    //     dof_id_type elemset_code =
+    //       elem->get_extra_integer(elemset_index);
+    //
+    //     if (elemset_code != DofObject::invalid_id)
+    //       libMesh::out << "Elem " << elem->id() << ", elemset_code = " << elemset_code << std::endl;
+    //   }
 
     // Write the file in the ExodusII format, including the element set information.
     // Note: elemsets should eventually be written during ExodusII_IO::write(), this
@@ -106,105 +106,50 @@ public:
       writer.write_elemsets();
     }
 
-   // Make sure that the writing is done before the reading starts.
-   TestCommWorld->barrier();
+    // Make sure that the writing is done before the reading starts.
+    TestCommWorld->barrier();
 
-   // Now read it back in
-   Mesh read_mesh(*TestCommWorld);
-   IOClass reader(read_mesh);
-   reader.verbose(true); // additional messages while debugging
-   reader.read(filename);
+    // Now read it back in
+    Mesh read_mesh(*TestCommWorld);
 
-//    if (write_vars)
-//      {
-//        std::vector<std::string> read_in_var_names;
-//        std::vector<std::set<boundary_id_type>> read_in_side_ids;
-//        std::vector<std::map<BoundaryInfo::BCTuple, Real>> read_in_bc_vals;
-//        reader.read_sideset_data
-//          (/*timestep=*/1, read_in_var_names, read_in_side_ids, read_in_bc_vals);
-//
-//        // Assert that we got back out what we put in.
-//        CPPUNIT_ASSERT(read_in_var_names == var_names);
-//        CPPUNIT_ASSERT(read_in_side_ids == side_ids);
-//        CPPUNIT_ASSERT(read_in_bc_vals == bc_vals);
-//      } // if (write_vars)
-//
-//    // Also check that the flat indices match those in the file
-//    std::map<BoundaryInfo::BCTuple, unsigned int> bc_array_indices;
-//    reader.get_sideset_data_indices(bc_array_indices);
-//
-//    // Debugging
-//    // for (const auto & pr : bc_array_indices)
-//    //   {
-//    //     const auto & t = pr.first;
-//    //     const auto & elem_id = std::get<0>(t);
-//    //     const auto & side_id = std::get<1>(t);
-//    //     const auto & boundary_id = std::get<2>(t);
-//    //     libMesh::out << "(elem, side, boundary_id) = "
-//    //                  << "(" << elem_id << ", " << side_id << ", " << boundary_id << ")"
-//    //                  << std::endl;
-//    //   }
-//
-//    // For this test case, the sideset arrays are ordered as follows:
-//    // elem_ss1 = 1, 2, 3, 4, 5 ;
-//    // side_ss1 = 1, 1, 1, 1, 1 ;
-//    // elem_ss2 = 5, 10, 15, 20, 25 ;
-//    // side_ss2 = 2, 2, 2, 2, 2 ;
-//    // elem_ss3 = 21, 22, 23, 24, 25 ;
-//    // side_ss3 = 3, 3, 3, 3, 3 ;
-//    // elem_ss4 = 1, 6, 11, 16, 21 ;
-//    // side_ss4 = 4, 4, 4, 4, 4 ;
-//
-//    // Check that the 0th side of the 0th element on boundary 0
-//    // corresponds to array index 0.
-//    // CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(0),
-//    //                      libmesh_map_find(bc_array_indices,
-//    //                                       std::make_tuple(/*elem id*/static_cast<dof_id_type>(0),
-//    //                                                       /*side id*/static_cast<unsigned short int>(0),
-//    //                                                       /*boundary id*/static_cast<boundary_id_type>(0))));
-//
-//    // CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(0),
-//    //                      libmesh_map_find(bc_array_indices, std::make_tuple(0,0,0)));
-//    //
-//    // CPPUNIT_ASSERT_EQUAL(static_cast<unsigned int>(1),
-//    //                      libmesh_map_find(bc_array_indices, std::make_tuple(1,0,0)));
-//
-//    // Check the first five elements, which all have side 0 on
-//    // boundary 0, and which happen to be in order in the BC array.
-//    for (unsigned int i=0; i<5; ++i)
-//      CPPUNIT_ASSERT_EQUAL
-//        (static_cast<unsigned int>(i),
-//         libmesh_map_find(bc_array_indices,
-//                          std::make_tuple(/*elem_id=*/cast_int<dof_id_type>(i),
-//                                          /*side_id=*/0,
-//                                          /*b_id=*/0)));
-//
-//    // Check side 1 of every fifth element starting with element 4, they are all in sideset 1.
-//    for (unsigned int i=0; i<5; ++i)
-//      CPPUNIT_ASSERT_EQUAL
-//        (static_cast<unsigned int>(i),
-//         libmesh_map_find(bc_array_indices,
-//                          std::make_tuple(/*elem_id=*/cast_int<dof_id_type>(5*i + 4),
-//                                          /*side_id*/1,
-//                                          /*b_id=*/1)));
-//
-//    // Check side 2 of the 5 consecutive elements starting with Elem 20. They are all in sideset 2.
-//    for (unsigned int i=0; i<5; ++i)
-//      CPPUNIT_ASSERT_EQUAL
-//        (static_cast<unsigned int>(i),
-//         libmesh_map_find(bc_array_indices,
-//                          std::make_tuple(/*elem_id=*/cast_int<dof_id_type>(20+i),
-//                                          /*side_id*/2,
-//                                          /*b_id=*/2)));
-//
-//    // Check side 3 of every fifth element, they are all in sideset 3
-//    for (unsigned int i=0; i<5; ++i)
-//      CPPUNIT_ASSERT_EQUAL
-//        (static_cast<unsigned int>(i),
-//         libmesh_map_find(bc_array_indices,
-//                          std::make_tuple(/*elem_id=*/cast_int<dof_id_type>(5*i),
-//                                          /*side_id*/3,
-//                                          /*b_id=*/3)));
+    // Do not allow renumbering on this mesh either.
+    read_mesh.allow_renumbering(false);
+
+    IOClass reader(read_mesh);
+    // reader.verbose(true); // additional messages while debugging
+    reader.read(filename);
+
+    // Check that the elements in read_mesh are in the correct elemsets.
+    // The elemset_codes will not in general match because they are
+    // created by a generic algorithm in the Exodus reader while above
+    // they were hard-coded.
+
+    // Make sure that the mesh actually has an extra_integer for "elemset_code"
+    CPPUNIT_ASSERT(read_mesh.has_elem_integer("elemset_code"));
+
+    // Make sure the extra integer is in the same index as before
+    CPPUNIT_ASSERT(read_mesh.get_elem_integer_index("elemset_code") == elemset_index);
+
+    // Make sure the elemset_codes match what we are expecting.
+    // The Exodus reader assigns the codes based on operator<
+    // for std::sets, which gives us the ordering {1}, {1,2}, {2}
+    CPPUNIT_ASSERT(read_mesh.get_elemset_code({1}) == 0);
+    CPPUNIT_ASSERT(read_mesh.get_elemset_code({1,2}) == 1);
+    CPPUNIT_ASSERT(read_mesh.get_elemset_code({2}) == 2);
+
+    // Assert that the elemset_codes for particular elements are set as expected
+
+    // Elements 8, 14 are in set1 which has code 0
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(8)->get_extra_integer(elemset_index) == 0);
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(14)->get_extra_integer(elemset_index) == 0);
+
+    // Elements 3, 24 are in both set1 and set2, which has code 1
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(3)->get_extra_integer(elemset_index) == 1);
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(24)->get_extra_integer(elemset_index) == 1);
+
+    // Elements 9, 15 are in set2 which has code 2
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(9)->get_extra_integer(elemset_index) == 2);
+    CPPUNIT_ASSERT(read_mesh.elem_ptr(15)->get_extra_integer(elemset_index) == 2);
   }
 
   void testWriteExodus()
