5f5281017bb37c89ccba20ded9a5472ed49a21a8
vikramvgarg <vikram.v.garg@gmail.com>
2022-04-19 13:41:50 -0700

    Fixup for 85e81b6 to support compilation with complex number support.

--- include/systems/system.h
+++ include/systems/system.h
@@ -1603,10 +1603,10 @@ public:
    */
   void init_qois(unsigned int n_qois);
 
-  void fill_qoi(unsigned int qoi_index, Real qoi_value);
+  void fill_qoi(unsigned int qoi_index, Number qoi_value);
   Real get_qoi_value(unsigned int qoi_index);
 
-  void fill_qoi_error_estimate(unsigned int qoi_index, Real qoi_error_estimate);
+  void fill_qoi_error_estimate(unsigned int qoi_index, Number qoi_error_estimate);
   Real get_qoi_error_estimate_value(unsigned int qoi_index);
 
   /**
