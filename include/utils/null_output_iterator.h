a5d016250ca9ea338f3c9c4906fe7f30fb9f3d3f
Roy Stogner <roy@stogners.org>
2022-05-17 09:15:06 -0500

    Avoid using deprecated std::iterator

--- include/utils/null_output_iterator.h
+++ include/utils/null_output_iterator.h
@@ -38,8 +38,13 @@ namespace libMesh
  */
 template <typename T>
 struct null_output_iterator
-  : std::iterator<std::output_iterator_tag, T>
 {
+  using iterator_category = std::output_iterator_tag;
+  using value_type = T;
+  using difference_type = std::ptrdiff_t;
+  using pointer = T*;
+  using reference = T&;
+
   template <typename T2>
   void operator=(const T2&) {}
 
