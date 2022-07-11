115192bc4d4366a10f27d78db3eb8e219c15a32b
Roy Stogner <Roy.Stogner@inl.gov>
2022-07-11 17:35:00 -0500

    Unit test for bad edge topology

--- tests/mesh/mesh_triangulation.C
+++ tests/mesh/mesh_triangulation.C
@@ -14,6 +14,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <regex>
 
 
 using namespace libMesh;
@@ -36,6 +37,7 @@ public:
   CPPUNIT_TEST( testPoly2TriHoles );
   CPPUNIT_TEST( testPoly2TriMeshedHoles );
   CPPUNIT_TEST( testPoly2TriEdges );
+  CPPUNIT_TEST( testPoly2TriBadEdges );
   CPPUNIT_TEST( testPoly2TriEdgesRefined );
   CPPUNIT_TEST( testPoly2TriSegments );
   CPPUNIT_TEST( testPoly2TriRefined );
@@ -529,6 +531,54 @@ public:
 
     this->testTriangulatorBase(mesh, triangulator);
   }
+
+
+  void testPoly2TriBadEdges()
+  {
+    LOG_UNIT_TEST;
+
+    Mesh mesh(*TestCommWorld);
+    Poly2TriTriangulator triangulator(mesh);
+
+    // The same quad as testTriangulator, but out of order
+    auto node0 = mesh.add_point(Point(0,0), 0);
+    auto node1 = mesh.add_point(Point(1,2), 1);
+    auto node2 = mesh.add_point(Point(1,0), 2);
+    auto node3 = mesh.add_point(Point(0,1), 3);
+
+    // Edges, but not enough to complete the quad
+    auto edge13 = mesh.add_elem(Elem::build(EDGE2));
+    edge13->set_node(0) = node1;
+    edge13->set_node(1) = node3;
+    auto edge02 = mesh.add_elem(Elem::build(EDGE2));
+    edge02->set_node(0) = node0;
+    edge02->set_node(1) = node2;
+    auto edge30 = mesh.add_elem(Elem::build(EDGE2));
+    edge30->set_node(0) = node3;
+    edge30->set_node(1) = node0;
+
+    mesh.prepare_for_use();
+
+#ifdef LIBMESH_ENABLE_EXCEPTIONS
+    // We can't just CPPUNIT_ASSERT_THROW, because we want to make
+    // sure we were thrown from the right place with the right error
+    // message!
+    bool threw_desired_exception = false;
+    try {
+      this->testTriangulatorBase(mesh, triangulator);
+    }
+    catch (libMesh::LogicError & e) {
+      std::regex msg_regex("Bad edge topology");
+      CPPUNIT_ASSERT(std::regex_search(e.what(), msg_regex));
+      threw_desired_exception = true;
+    }
+    catch (...) {
+      CPPUNIT_ASSERT_MESSAGE("Unexpected exception type thrown", false);
+    }
+    CPPUNIT_ASSERT(threw_desired_exception);
+#endif
+  }
+
 
 
   void testPoly2TriEdgesRefined()
