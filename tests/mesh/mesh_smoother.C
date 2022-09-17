cf0677178d69e16ffffefe8f2ee02fb53955ccdf
Daniel Schwen <daniel@schwen.de>
2022-09-17 10:47:31 -0600

    Skip partitioning for the test (#3385)

--- tests/mesh/mesh_smoother.C
+++ tests/mesh/mesh_smoother.C
@@ -25,22 +25,27 @@ public:
   CPPUNIT_TEST_SUITE_END();
 
 private:
+  template <typename T>
   void test(const std::string in_file, const std::string gold_file, bool move_nodes)
   {
     // load input mesh
-    ReplicatedMesh in_mesh(*TestCommWorld);
-    ExodusII_IO exii(in_mesh);
+    T in_mesh(*TestCommWorld);
+    ExodusII_IO in_exii(in_mesh);
     if (in_mesh.processor_id() == 0)
-      exii.read(in_file);
+      in_exii.read(in_file);
     MeshCommunication().broadcast(in_mesh);
+    in_mesh.skip_partitioning(true);
+    in_mesh.allow_renumbering(false);
     in_mesh.prepare_for_use();
 
     // load gold file
-    ReplicatedMesh gold_mesh(*TestCommWorld);
-    ExodusII_IO exii(gold_mesh);
+    T gold_mesh(*TestCommWorld);
+    ExodusII_IO gold_exii(gold_mesh);
     if (gold_mesh.processor_id() == 0)
-      exii.read(gold_file);
+      gold_exii.read(gold_file);
     MeshCommunication().broadcast(gold_mesh);
+    gold_mesh.skip_partitioning(true);
+    gold_mesh.allow_renumbering(false);
     gold_mesh.prepare_for_use();
 
     // run mesh smoother for 100 iterations
@@ -48,12 +53,13 @@ private:
     lms.smooth(100);
 
     // compare node positions
-    for (const auto & node : in_mesh.node_ref_range())
-      {
-        Point a = node;
-        Point b = gold_mesh.node_ref(node->id());
-        CPPUNIT_ASSERT((a-b).norm() < 1e-6);
-      }
+    for (const auto &node : in_mesh.node_ptr_range())
+    {
+      Point a = *node;
+      Point b = gold_mesh.node_ref(node->id());
+      // std::cout << node->id() << ": " << a << " == " << b << " (" << (a - b).norm() << ")\n";
+      CPPUNIT_ASSERT((a - b).norm() < 1e-6);
+    }
   }
 
 public:
@@ -61,10 +67,26 @@ public:
 
   void tearDown() {}
 
-  void laplace_smooth_2d_fixed_boundary_nodes() { LOG_UNIT_TEST; test("meshes/smooth2d_in.e", "smooth2d_nomove.e", false); }
-  void laplace_smooth_2d_movable_boundary_nodes() { LOG_UNIT_TEST; test("meshes/smooth2d_in.e", "smooth2d_move.e", true); }
-  void laplace_smooth_3d_fixed_boundary_nodes() { LOG_UNIT_TEST; test("meshes/smooth3d_in.e", "smooth3d_nomove.e", false); }
-  void laplace_smooth_3d_movable_boundary_nodes() { LOG_UNIT_TEST; test("meshes/smooth3d_in.e", "smooth3d_move.e", true); }
+  void laplace_smooth_2d_fixed_boundary_nodes()
+  {
+    LOG_UNIT_TEST;
+    test<DistributedMesh>("meshes/smooth2d_in.e", "meshes/smooth2d_nomove.e", false);
+  }
+  void laplace_smooth_2d_movable_boundary_nodes()
+  {
+    LOG_UNIT_TEST;
+    test<DistributedMesh>("meshes/smooth2d_in.e", "meshes/smooth2d_move.e", true);
+  }
+  void laplace_smooth_3d_fixed_boundary_nodes()
+  {
+    LOG_UNIT_TEST;
+    test<DistributedMesh>("meshes/smooth3d_in.e", "meshes/smooth3d_nomove.e", false);
+  }
+  void laplace_smooth_3d_movable_boundary_nodes()
+  {
+    LOG_UNIT_TEST;
+    test<DistributedMesh>("meshes/smooth3d_in.e", "meshes/smooth3d_move.e", true);
+  }
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(CopyNodesAndElementsTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(SmoothMeshTest);
