6fe24054b9c5b8609e496d270e525c9a95b984e6
Logan Harbour <loganhharbour@gmail.com>
2022-11-14 16:40:44 -0700

    Run within_segment tests over a variety of scales

--- tests/geom/intersection_tools_test.C
+++ tests/geom/intersection_tools_test.C
@@ -28,30 +28,35 @@ public:
   {
     LOG_UNIT_TEST;
 
-    const Point s1(1.0, 2.0, 3.0);
-    const Point s2(2.0, 3.0, 4.0);
-    const auto length_vec = s2 - s1;
-    const auto length = length_vec.norm();
-    const auto s1_to_s2 = length_vec / length;
-
-    int segments = 10;
-    Real dx = (Real)1 / segments * length;
-    for (const auto i : make_range(-1, segments + 1))
+    const std::vector<Real> scales{1.5e-7, 1, 1.5e7};
+    for (const auto scale : scales)
     {
-      const auto p = s1 + Real(i) * dx * s1_to_s2;
-      IntersectionTools::WithinSegmentResult within_result = IntersectionTools::WithinSegmentResult::NOT_WITHIN;
-      if (i == 0)
-        within_result = IntersectionTools::WithinSegmentResult::AT_BEGINNING;
-      else if (i > 0 && i < segments)
-        within_result = IntersectionTools::WithinSegmentResult::BETWEEN;
-      else if (i == segments)
-        within_result = IntersectionTools::WithinSegmentResult::AT_END;
+      const auto s1 = scale * Point(1.0, 2.0, 3.0);
+      const auto s2 = scale * Point(2.0, 3.0, 4.0);
 
-      CPPUNIT_ASSERT_EQUAL(IntersectionTools::within_segment(s1, s2, p), within_result);
+      const auto length_vec = s2 - s1;
+      const auto length = length_vec.norm();
+      const auto s1_to_s2 = length_vec / length;
+
+      int segments = 10;
+      Real dx = (Real)1 / segments * length;
+      for (const auto i : make_range(-1, segments + 1))
+      {
+        const auto p = s1 + (Real)i * dx * s1_to_s2;
+        IntersectionTools::WithinSegmentResult within_result = IntersectionTools::WithinSegmentResult::NOT_WITHIN;
+        if (i == 0)
+          within_result = IntersectionTools::WithinSegmentResult::AT_BEGINNING;
+        else if (i > 0 && i < segments)
+          within_result = IntersectionTools::WithinSegmentResult::BETWEEN;
+        else if (i == segments)
+          within_result = IntersectionTools::WithinSegmentResult::AT_END;
+
+        CPPUNIT_ASSERT_EQUAL(IntersectionTools::within_segment(s1, s2, p), within_result);
+      }
+
+      CPPUNIT_ASSERT_EQUAL(IntersectionTools::within_segment(s1, s2, scale * Point(9.9, 5, 3)),
+                          IntersectionTools::WithinSegmentResult::NOT_WITHIN);
     }
-
-    CPPUNIT_ASSERT_EQUAL(IntersectionTools::within_segment(s1, s2, Point(9.9, 5, 3)),
-                         IntersectionTools::WithinSegmentResult::NOT_WITHIN);
   }
 
   void collinear()
