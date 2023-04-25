70333443856a542ab4e0e84adadb1db41ec2b604
John Peterson <jwpeterson@gmail.com>
2023-04-25 13:46:13 -0500

    Add allSecondOrderMixed() unit test
    This is similar to the existing allSecondOrderRange() unit test, but
    instead of using a separate subdomain to control which elements are
    converted to 2nd-order, we now convert all of them, automatically
    skipping the first element which is already SECOND-order.
    
    Also avoid using "new" and including elem-specific header files in
    this unit test.

--- tests/mesh/all_second_order.C
+++ tests/mesh/all_second_order.C
@@ -3,7 +3,8 @@
 #include <libmesh/distributed_mesh.h>
 #include <libmesh/replicated_mesh.h>
 #include <libmesh/mesh_generation.h>
-#include <libmesh/edge_edge2.h>
+#include <libmesh/point.h>
+#include <libmesh/elem.h>
 
 // cppunit includes
 #include "test_comm.h"
@@ -19,6 +20,7 @@ public:
   CPPUNIT_TEST( allSecondOrder );
   CPPUNIT_TEST( allSecondOrderRange );
   CPPUNIT_TEST( allSecondOrderDoNothing );
+  CPPUNIT_TEST( allSecondOrderMixed );
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -56,7 +58,7 @@ public:
     // and in subdomain 1.
     for (dof_id_type n=0; n<4; ++n)
       {
-        Elem * elem = mesh.add_elem(new Edge2);
+        Elem * elem = mesh.add_elem(Elem::build(EDGE2));
         elem->set_node(0) = mesh.node_ptr(n);
         elem->set_node(1) = mesh.node_ptr(n+4);
         elem->subdomain_id() = 1;
@@ -104,6 +106,53 @@ public:
 
     // Make sure we still have the same number of nodes
     CPPUNIT_ASSERT_EQUAL(static_cast<dof_id_type>(25), mesh.n_nodes());
+  }
+
+  void allSecondOrderMixed()
+  {
+    ReplicatedMesh mesh(*TestCommWorld);
+
+    // Construct a single-element Quad9 mesh
+    MeshTools::Generation::build_square(mesh,
+                                        /*nx=*/1, /*ny=*/1,
+                                        /*xmin=*/0., /*xmax=*/1.,
+                                        /*ymin=*/0., /*ymax=*/1.,
+                                        /*elem_type=*/QUAD9);
+
+    // Pointer to the single Elem
+    const Elem * elem = mesh.elem_ptr(0);
+
+    // Amount to offset the indices of newly-added nodes by
+    const unsigned int node_offset = elem->n_nodes();
+
+    // Add an extra node above each of the original vertices
+    for (dof_id_type n=0; n<elem->n_vertices(); ++n)
+      mesh.add_point(mesh.point(n) + Point(0,0,1), /*id=*/node_offset + n, /*proc_id=*/0);
+
+    // Construct Edge2 elements and attach to each vertex of Quad9
+    for (dof_id_type n=0; n<elem->n_vertices(); ++n)
+      {
+        Elem * elem = mesh.add_elem(Elem::build(EDGE2));
+        elem->set_node(0) = mesh.node_ptr(n);
+        elem->set_node(1) = mesh.node_ptr(node_offset + n);
+      }
+
+    // Convert all elements to SECOND-order, automatically skipping those that are
+    // already SECOND-order.
+    mesh.all_second_order_range(mesh.element_ptr_range(), /*full_ordered=*/true);
+
+    // Make sure we still have the expected total number of elements
+    CPPUNIT_ASSERT_EQUAL(static_cast<dof_id_type>(5), mesh.n_elem());
+
+    // Make sure we have the correct number of nodes, 9+4+4=17
+    CPPUNIT_ASSERT_EQUAL(static_cast<dof_id_type>(17), mesh.n_nodes());
+
+    // Make sure that the type of the original Elem is unchanged
+    CPPUNIT_ASSERT_EQUAL(QUAD9, elem->type());
+
+    // Make sure that the other elements are now Edge3s
+    for (dof_id_type e=1; e<5; ++e)
+      CPPUNIT_ASSERT_EQUAL(EDGE3, mesh.elem_ptr(e)->type());
   }
 };
 
