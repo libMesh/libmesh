319c874c0601f7001767398ac88573d8c5672986
John Peterson <jwpeterson@gmail.com>
2023-05-22 10:08:23 -0500

    OutputAssembly class should use a BoundingBox object

--- examples/reduced_basis/reduced_basis_ex4/assembly.h
+++ examples/reduced_basis/reduced_basis_ex4/assembly.h
@@ -15,6 +15,7 @@
 #include "libmesh/utility.h"
 #include "libmesh/print_trace.h"
 #include "libmesh/int_range.h"
+#include "libmesh/bounding_box.h"
 
 // rbOOmit includes
 #include "libmesh/rb_assembly_expansion.h"
@@ -45,6 +46,7 @@ using libMesh::subdomain_id_type;
 using libMesh::dof_id_type;
 using libMesh::Utility::pow;
 using libMesh::make_range;
+using libMesh::BoundingBox;
 
 struct ShiftedGaussian : public RBParametrizedFunction
 {
@@ -98,8 +100,9 @@ struct ThetaConstant : RBTheta
   /**
    * Evaluate theta for a single scalar-valued RBParameters object.
    * In this case, Theta(mu) does not depend on mu explicitly, except
-   * to determine the number of "steps" (aka n_steps()) which mu
-   * has, so that the output vector is sized appropriately.
+   * to throw an error in case a multi-step RBParameters object has
+   * been provided, since the evaluate() interface does not support
+   * multi-step RBParameters objects.
    */
   virtual Number evaluate(const RBParameters & mu) override
   {
@@ -110,14 +113,17 @@ struct ThetaConstant : RBTheta
   }
 
   /**
-   * Evaluate theta for multiple mu values, each of which may have multiple "steps".
-   * This theta still doesn't depend on mu, but the output vector must be sized appropriately.
+   * Evaluate theta for multiple mu values, each of which may have
+   * multiple "steps".  In this case, Theta(mu) does not depend on mu
+   * explicitly, except to determine the number of "steps" (aka
+   * n_steps()) which mu has, so that the output vector is sized
+   * appropriately.
    */
   virtual std::vector<Number> evaluate_vec(const std::vector<RBParameters> & mus) override
   {
     // Compute the number of values to be returned in the vector. For
-    // scalar-valued RBParameters objects, there would be mus.size()
-    // values returned in the vector. For step-valued RBParameters
+    // single-step RBParameters objects, there would be mus.size()
+    // values returned in the vector. For multi-step RBParameters
     // objects, there are:
     // sum_i mus[i].n_steps()
     // total Thetas, i.e. one Theta per step.
@@ -226,20 +232,13 @@ struct EIM_F : RBEIMAssembly
 
 /**
  * Output assembly object which computes the average value of the
- * solution variable inside a BoundingBox defined by lower corner
- * [min_x_in, min_y_in] and upper corner [max_x_in, max_y_in].
- * OutputAssembly is copied from reduced_basis_ex1 where it is also
- * used.
+ * solution variable inside a user-provided BoundingBox.
+ * OutputAssembly is also used in reduced_basis_ex1.
  */
 struct OutputAssembly : ElemAssembly
 {
-  OutputAssembly(Real min_x_in, Real max_x_in,
-                 Real min_y_in, Real max_y_in)
-    :
-    min_x(min_x_in),
-    max_x(max_x_in),
-    min_y(min_y_in),
-    max_y(max_y_in)
+  OutputAssembly(const BoundingBox & bbox_in) :
+    bbox(bbox_in)
   {}
 
   // Output: Average value over the region [min_x,max_x]x[min_y,max_y]
@@ -259,18 +258,17 @@ struct OutputAssembly : ElemAssembly
     // Now we will build the affine operator
     unsigned int n_qpoints = c.get_element_qrule().n_points();
 
-    Real output_area = (max_x-min_x) * (max_y-min_y);
+    // TODO: BoundingBox should be able to compute and return its area/volume
+    Real output_area = (bbox.max()(0) - bbox.min()(0)) * (bbox.max()(1) - bbox.min()(1));
 
-    Point avg = c.get_elem().vertex_average();
-    if ((min_x <= avg(0)) && (avg(0) <= max_x) &&
-        (min_y <= avg(1)) && (avg(1) <= max_y))
+    if (bbox.contains_point(c.get_elem().vertex_average()))
       for (unsigned int qp=0; qp != n_qpoints; qp++)
         for (unsigned int i=0; i != n_u_dofs; i++)
           c.get_elem_residual()(i) += JxW[qp] * phi[i][qp] / output_area;
   }
 
   // Member variables that define the output region in 2D
-  Real min_x, max_x, min_y, max_y;
+  BoundingBox bbox;
 };
 
 // Define an RBThetaExpansion class for this PDE
@@ -305,8 +303,8 @@ struct EimTestRBAssemblyExpansion : RBAssemblyExpansion
    * Constructor.
    */
   EimTestRBAssemblyExpansion():
-    L0(/*min_x=*/-0.2, /*max_x=*/0.2,
-       /*min_y=*/-0.2, /*max_x=*/0.2)
+    L0(BoundingBox(/*min=*/Point(-0.2, -0.2),
+                   /*max=*/Point(0.2, 0.2)))
   {
     attach_A_assembly(&A0_assembly);
     attach_output_assembly(&L0);
