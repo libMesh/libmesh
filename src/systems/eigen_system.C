d9c91f70d9f57fb8aa0eb04e29b8ef202155ffbc
Alex Lindsay <alexlindsay239@gmail.com>
2023-06-13 13:19:41 -0700

    Error in EigenSystem::solve if we have constraints
    At least with SLEPc's eigen solver and the assembly routines in
    MOOSE, the result of a solve does not honor constraints such as hanging
    nodes. We do have `CondensedEigenSystem` which is a class intended
    for use with constraints. Unrelated to this PR that class will need to be
    updated in order to support shell matrices if MOOSE is to be able to
    use it

--- src/systems/eigen_system.C
+++ src/systems/eigen_system.C
@@ -208,6 +208,10 @@ void EigenSystem::reinit ()
 
 void EigenSystem::solve ()
 {
+  if (get_dof_map().n_constrained_dofs())
+    libmesh_error_msg("EigenSystem does not support constrained degrees of freedom. If you wish to "
+                      "perform a solve on a system with constraints, then please use the "
+                      "CondensedEigenSystem class instead");
 
   // A reference to the EquationSystems
   EquationSystems & es = this->get_equation_systems();
