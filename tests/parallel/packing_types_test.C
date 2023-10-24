fc0b04a62c0c42025177a56490d51928968f6ba9
Daniel Schwen <daniel.schwen@inl.gov>
2023-10-24 15:12:56 -0600

    Add test (#3662)

---
+++ tests/parallel/packing_types_test.C
@@ -0,0 +1,73 @@
+#include <libmesh/parallel.h>
+#include <libmesh/parallel_eigen.h>
+#include <libmesh/int_range.h>
+
+#include "timpi/parallel_sync.h"
+
+#include <vector>
+
+#include "test_comm.h"
+#include "libmesh_cppunit.h"
+
+using namespace libMesh;
+
+class PackingTypesTest : public CppUnit::TestCase {
+public:
+  LIBMESH_CPPUNIT_TEST_SUITE( PackingTypesTest );
+
+  CPPUNIT_TEST( testEigenMatrix );
+
+  CPPUNIT_TEST_SUITE_END();
+
+private:
+
+public:
+  void setUp()
+  {}
+
+  void tearDown()
+  {}
+
+
+
+  void testEigenMatrix()
+  {
+    LOG_UNIT_TEST;
+
+    typedef std::vector<Parallel::DynamicEigenMatrix> DynamicMatrixVector;
+    std::map<processor_id_type, DynamicMatrixVector> send_data;
+
+    // prepare unique data
+    for (const auto i : index_range(*TestCommWorld))
+    {
+      const auto j = TestCommWorld->rank();
+      send_data[i] = DynamicMatrixVector{ Parallel::DynamicEigenMatrix(i+1, i+2) };
+      for (const auto row: make_range(i+1))
+        for (const auto col: make_range(i+2))
+          send_data[i][0](row, col) = row * 100000.0 + col * 1000.0 + 10.0 * i + j;
+    }
+
+    // verification
+    auto verify_data =
+    [this](processor_id_type j,
+            const DynamicMatrixVector & recv_data)
+    {
+      const auto i = TestCommWorld->rank();
+      const std::size_t rows = recv_data[0].rows();
+      const std::size_t cols = recv_data[0].cols();
+
+      CPPUNIT_ASSERT_EQUAL( rows, static_cast<std::size_t>(i+1) );
+      CPPUNIT_ASSERT_EQUAL( cols, static_cast<std::size_t>(i+2) );
+
+      for (const auto row: make_range(rows))
+        for (const auto col: make_range(cols))
+          CPPUNIT_ASSERT_EQUAL( recv_data[0](row, col), static_cast<Real>(row * 100000.0 + col * 1000.0 + j + 10.0 * i) );
+    };
+
+    // communicate
+    Parallel::push_parallel_vector_data(*TestCommWorld, send_data, verify_data);
+  }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( PackingTypesTest );
