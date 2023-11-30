8ef2cde966beb4b895e23cbcc817c32130ddc5ae
Alex Lindsay <alexander.lindsay@inl.gov>
2023-11-29 21:50:46 -0800

    Add ability to not compute Jacobian so we can use FD

--- examples/vector_fe/vector_fe_ex9/vector_fe_ex9.C
+++ examples/vector_fe/vector_fe_ex9/vector_fe_ex9.C
@@ -203,8 +203,9 @@ compute_qp_soln(std::vector<SolnType> & qp_vec,
   }
 }
 
-class HDGProblem : public libMesh::NonlinearImplicitSystem::ComputeResidualandJacobian,
-                   public libMesh::NonlinearImplicitSystem::ComputePostCheck
+class HDGProblem : public NonlinearImplicitSystem::ComputeResidualandJacobian,
+                   public NonlinearImplicitSystem::ComputeResidual,
+                   public NonlinearImplicitSystem::ComputePostCheck
 {
 public:
   HDGProblem() : u_true_soln(mu), v_true_soln(mu) {}
@@ -228,7 +229,7 @@ public:
   // Ghosted version of old solution
   NumericVector<Number> * ghosted_old_solution;
   SparseMatrix<Number> * J;
-  NumericVector<Number> * residual;
+  NumericVector<Number> * nl_residual;
   boundary_id_type left_bnd;
   boundary_id_type top_bnd;
   boundary_id_type right_bnd;
@@ -236,6 +237,7 @@ public:
   const USoln u_true_soln;
   const VSoln v_true_soln;
   const PSoln p_true_soln;
+  bool compute_jacobian;
 
   // Whether we are performing an MMS study
   bool mms;
@@ -283,10 +285,18 @@ public:
                                      SparseMatrix<Number> * J_in,
                                      NonlinearImplicitSystem & /*S*/) override
   {
-    residual = R_in;
+    nl_residual = R_in;
     J = J_in;
     assemble(true);
   }
+
+  virtual void residual (const NumericVector<Number> & /*X*/,
+                         NumericVector<Number> & R_in,
+                         NonlinearImplicitSystem & /*S*/) override
+    {
+      nl_residual = &R_in;
+      assemble(true);
+    }
 
   virtual void postcheck(const NumericVector<Number> & old_soln,
                          NumericVector<Number> & /*search_direction*/,
@@ -1131,8 +1141,9 @@ private:
 
         // We were performing our finite element assembly for the implicit solve step of our
         // example. Add our local element vectors/matrices into the global system
-        J->add_matrix(K_libmesh, lm_dof_indices);
-        residual->add_vector(F_libmesh, lm_dof_indices);
+        if (compute_jacobian)
+          J->add_matrix(K_libmesh, lm_dof_indices);
+        nl_residual->add_vector(F_libmesh, lm_dof_indices);
       }
       else
       {
@@ -1360,8 +1371,12 @@ main(int argc, char ** argv)
   hdg.parallel_increment = &parallel_inc;
   hdg.ghosted_old_solution = &ghosted_old;
   hdg.mms = mms;
+  hdg.compute_jacobian = infile("compute_jacobian", true);
 
-  lm_system.nonlinear_solver->residual_and_jacobian_object = &hdg;
+  if (hdg.compute_jacobian)
+    lm_system.nonlinear_solver->residual_and_jacobian_object = &hdg;
+  else
+    lm_system.nonlinear_solver->residual_object = &hdg;
   lm_system.nonlinear_solver->postcheck_object = &hdg;
 
   hdg.init();
