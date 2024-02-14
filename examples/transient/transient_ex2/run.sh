912d64b2ac580c673db51563363dac6589234e7f
Roy Stogner <Roy.Stogner@inl.gov>
2024-02-14 13:39:23 -0500

    Use -pc_type jacobi in transient_ex2
    EigenSparseLinearSolver ILU is pretty slow in dbg mode, too slow to
    invoke from scratch on hundreds of time steps, but fortunately Jacobi
    alone is fast and more than sufficient here.
    
    This would be an ideal place to just enable preconditioner reuse, but I
    can't see how to implement that via Eigen APIs.

--- examples/transient/transient_ex2/run.sh
+++ examples/transient/transient_ex2/run.sh
@@ -7,7 +7,7 @@ source $LIBMESH_DIR/examples/run_common.sh
 example_name=transient_ex2
 example_dir=examples/transient/$example_name
 
-options="pipe-mesh.unv"
+options="pipe-mesh.unv -pc_type jacobi"
 run_example "$example_name" "$options"
 
 # No benchmark here; "result_node = 274" makes mesh modification iffy
