b4a6d53d6d29c4f91c3d10d31fcfa79187cdd663
Roy Stogner <Roy.Stogner@inl.gov>
2024-03-05 12:30:11 -0600

    Match matrix partitioning before trying add()

--- tests/numerics/petsc_matrix_test.C
+++ tests/numerics/petsc_matrix_test.C
@@ -39,7 +39,16 @@ public:
     CPPUNIT_ASSERT_EQUAL(mat_to_read->m(), dof_id_type(27));
     CPPUNIT_ASSERT_EQUAL(mat_to_read->n(), dof_id_type(27));
 
+    // Our read_matlab partitioning doesn't necessarily match PETSc's
+    // MatLoad partitioning, so we'll partition a matrix to compare
+    // against manually.  These particular files have bandwidth 8,
+    // so we'll ask for sufficient n_nz and n_oz to handle that
+    // regardless of partitioning.
     auto mat_ascii_format = std::make_unique<PetscMatrix<Number>>(*my_comm);
+    mat_ascii_format->init(mat_to_read->m(), mat_to_read->n(),
+                           mat_to_read->local_m(), mat_to_read->local_n(),
+                           8, 7);
+
     mat_ascii_format->read_matlab("matrices/geom_1_extraction_op.m");
 
     mat_ascii_format->add(-1, *mat_to_read);
