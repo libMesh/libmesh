a88bd8d5c614c7bb3ec77d347455f64b53f63fcc
Roy Stogner <Roy.Stogner@inl.gov>
2024-03-28 17:28:46 -0500

    Don't mix var 0 into an _active_vars
    This is giving us a too-large quadrature rule in a MOOSE use case.

--- src/systems/fem_context.C
+++ src/systems/fem_context.C
@@ -86,7 +86,9 @@ FEMContext::FEMContext (const System & sys,
 FEType FEMContext::find_hardest_fe_type()
 {
   const System & sys = this->get_system();
-  FEType hardest_fe_type = sys.variable_type(0);
+  FEType hardest_fe_type =
+    sys.variable_type(_active_vars ?
+                      (*_active_vars)[0] : 0);
 
   auto check_var = [&hardest_fe_type, &sys](unsigned int v)
     {
