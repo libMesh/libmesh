e099695383e594016718b3df28c1de24b4b70f63
Kyriv <vasileios.kyriakopoulos@inl.gov>
2024-06-10 16:17:21 -0600

    Adding an example to the testing suite to see if coverage changes Refs #000

--- examples/miscellaneous/miscellaneous_ex8/miscellaneous_ex8.C
+++ examples/miscellaneous/miscellaneous_ex8/miscellaneous_ex8.C
@@ -15,8 +15,6 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-
-
 // <h1>Miscellaneous Example 8 - Meshfree Interpolation Utilities.</h1>
 // \author Benjamin S. Kirk
 // \date 2012
@@ -25,76 +23,52 @@
 // demonstrated in this example.
 
 // Example include files
+#include "libmesh/equation_systems.h"
+#include "libmesh/getpot.h"
 #include "libmesh/libmesh.h"
-#include "libmesh/meshfree_interpolation.h"
-#include "libmesh/radial_basis_interpolation.h"
 #include "libmesh/mesh.h"
 #include "libmesh/mesh_refinement.h"
-#include "libmesh/equation_systems.h"
+#include "libmesh/meshfree_interpolation.h"
+#include "libmesh/node.h"
 #include "libmesh/numeric_vector.h"
+#include "libmesh/radial_basis_interpolation.h"
 #include "libmesh/tecplot_io.h"
 #include "libmesh/threads.h"
-#include "libmesh/node.h"
-#include "libmesh/getpot.h"
 #include "meshless_interpolation_function.h"
 
 // C++ includes
 #include <cstdlib>
 
-
 // Bring in everything from the libMesh namespace
 using namespace libMesh;
 
-
-void create_random_point_cloud (const unsigned int Npts,
-                                std::vector<Point> & pts,
-                                const Real max_range = 10)
-{
-  libMesh::out << "Generating "<< Npts << " local point cloud...";
+void create_random_point_cloud(const unsigned int Npts, std::vector<Point> &pts,
+                               const Real max_range = 10) {
+  libMesh::out << "Generating " << Npts << " local point cloud...";
   pts.resize(Npts);
 
-  for (size_t i=0;i<Npts;i++)
-    {
-      pts[i](0) = max_range * (std::rand() % 1000) / Real(1000);
-      pts[i](1) = max_range * (std::rand() % 1000) / Real(1000);
-      pts[i](2) = max_range * (std::rand() % 1000) / Real(1000);
-    }
+  for (size_t i = 0; i < Npts; i++) {
+    pts[i](0) = max_range * (std::rand() % 1000) / Real(1000);
+    pts[i](1) = max_range * (std::rand() % 1000) / Real(1000);
+    pts[i](2) = max_range * (std::rand() % 1000) / Real(1000);
+  }
   libMesh::out << "done\n";
 }
 
+Real exact_solution_u(const Point &p) {
+  const Real x = p(0), y = p(1), z = p(2);
 
-
-Real exact_solution_u (const Point & p)
-{
-  const Real
-    x = p(0),
-    y = p(1),
-    z = p(2);
-
-  return (x*x*x   +
-          y*y*y*y +
-          z*z*z*z*z);
+  return (x * x * x + y * y * y * y + z * z * z * z * z);
 }
 
+Real exact_solution_v(const Point &p) {
+  const Real x = p(0), y = p(1), z = p(2);
 
-
-Real exact_solution_v (const Point & p)
-{
-  const Real
-    x = p(0),
-    y = p(1),
-    z = p(2);
-
-  return (x*x +
-          y*y +
-          z*z*z);
+  return (x * x + y * y + z * z * z);
 }
 
-Number exact_value (const Point & p,
-                    const Parameters &,
-                    const std::string &,
-                    const std::string &)
-{
+Number exact_value(const Point &p, const Parameters &, const std::string &,
+                   const std::string &) {
   return exact_solution_v(p);
 }
 
@@ -102,21 +76,14 @@ Number exact_value (const Point & p,
 // initialization routines for the "Convection-Diffusion"
 // system.  This handles things like setting initial
 // conditions and boundary conditions.
-void init_sys(EquationSystems & es,
-              const std::string & system_name)
-{
+void init_sys(EquationSystems &es, const std::string &system_name) {
   // Get a reference to the Convection-Diffusion system object.
-  System & system =
-    es.get_system<System>(system_name);
+  System &system = es.get_system<System>(system_name);
 
   system.project_solution(exact_value, nullptr, es.parameters);
 }
 
-
-
-
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   // Skip this example if we do not meet certain requirements
   libmesh_example_requires(3 <= LIBMESH_DIM, "3D support");
 #ifndef LIBMESH_HAVE_EIGEN
@@ -134,54 +101,52 @@ int main(int argc, char ** argv)
 #endif
 
   // Initialize libMesh.
-  LibMeshInit init (argc, argv);
+  LibMeshInit init(argc, argv);
   {
     GetPot input(argc, argv);
 
     // Demonstration case 1
     {
-      std::vector<Point>       tgt_pts;
-      std::vector<Number>      tgt_data_idi, tgt_data_rbi;
-      std::vector<std::string> field_vars {"u", "v"};
+      std::vector<Point> tgt_pts;
+      std::vector<Number> tgt_data_idi, tgt_data_rbi;
+      std::vector<std::string> field_vars{"u", "v"};
 
-      InverseDistanceInterpolation<3> idi (init.comm(),
-                                           /* n_interp_pts = */ 8,
-                                           /* power =        */ 2);
+      InverseDistanceInterpolation<3> idi(init.comm(),
+                                          /* n_interp_pts = */ 8,
+                                          /* power =        */ 2);
 
-      RadialBasisInterpolation<3> rbi (init.comm());
+      RadialBasisInterpolation<3> rbi(init.comm());
 
-      idi.set_field_variables (field_vars);
-      rbi.set_field_variables (field_vars);
+      idi.set_field_variables(field_vars);
+      rbi.set_field_variables(field_vars);
 
       const int n_source_points = input("n_source_points", 100);
 
       // Source points are independent on each processor
       const int my_n_source_points =
-        n_source_points / init.comm().size() +
-        (init.comm().rank() < n_source_points % init.comm().size());
+          n_source_points / init.comm().size() +
+          (init.comm().rank() < n_source_points % init.comm().size());
 
-      create_random_point_cloud (my_n_source_points,
-                                 idi.get_source_points());
-
+      create_random_point_cloud(my_n_source_points, idi.get_source_points());
 
       // Explicitly set the data values we will interpolate from
       {
-        const std::vector<Point> & src_pts  (idi.get_source_points());
-        std::vector<Number>      & src_vals (idi.get_source_vals());
+        const std::vector<Point> &src_pts(idi.get_source_points());
+        std::vector<Number> &src_vals(idi.get_source_vals());
 
-        src_vals.clear(); src_vals.reserve(2*src_pts.size());
+        src_vals.clear();
+        src_vals.reserve(2 * src_pts.size());
 
-        for (std::vector<Point>::const_iterator pt_it=src_pts.begin();
-             pt_it != src_pts.end(); ++pt_it)
-          {
-            src_vals.push_back (exact_solution_u (*pt_it));
-            src_vals.push_back (exact_solution_v (*pt_it));
-          }
+        for (std::vector<Point>::const_iterator pt_it = src_pts.begin();
+             pt_it != src_pts.end(); ++pt_it) {
+          src_vals.push_back(exact_solution_u(*pt_it));
+          src_vals.push_back(exact_solution_v(*pt_it));
+        }
       }
 
       // give rbi the same info as idi
       rbi.get_source_points() = idi.get_source_points();
-      rbi.get_source_vals()   = idi.get_source_vals();
+      rbi.get_source_vals() = idi.get_source_vals();
 
       idi.prepare_for_use();
       rbi.prepare_for_use();
@@ -192,42 +157,33 @@ int main(int argc, char ** argv)
       {
         const int n_target_points = input("n_target_points", 100);
 
-        create_random_point_cloud (n_target_points,
-                                   tgt_pts);
+        create_random_point_cloud(n_target_points, tgt_pts);
 
-        //tgt_pts = rbi.get_source_points();
+        // tgt_pts = rbi.get_source_points();
 
-        idi.interpolate_field_data (field_vars,
-                                    tgt_pts,
-                                    tgt_data_idi);
+        idi.interpolate_field_data(field_vars, tgt_pts, tgt_data_idi);
 
-        rbi.interpolate_field_data (field_vars,
-                                    tgt_pts,
-                                    tgt_data_rbi);
+        rbi.interpolate_field_data(field_vars, tgt_pts, tgt_data_rbi);
 
-        std::vector<Number>::const_iterator
-          v_idi = tgt_data_idi.begin(),
-          v_rbi = tgt_data_rbi.begin();
+        std::vector<Number>::const_iterator v_idi = tgt_data_idi.begin(),
+                                            v_rbi = tgt_data_rbi.begin();
 
-        for (std::vector<Point>::const_iterator p_it=tgt_pts.begin();
-             p_it!=tgt_pts.end(); ++p_it)
-          {
-            libMesh::out << "\nAt target point " << *p_it
-                         << "\n u_interp_idi="   << *v_idi
-                         << ", u_interp_rbi="    << *v_rbi
-                         << ", u_exact="         << exact_solution_u(*p_it);
-            ++v_idi;
-            ++v_rbi;
-            libMesh::out << "\n v_interp_idi=" << *v_idi
-                         << ", v_interp_rbi="  << *v_rbi
-                         << ", v_exact="       << exact_solution_v(*p_it)
-                         << std::endl;
-            ++v_idi;
-            ++v_rbi;
-          }
+        for (std::vector<Point>::const_iterator p_it = tgt_pts.begin();
+             p_it != tgt_pts.end(); ++p_it) {
+          libMesh::out << "\nAt target point " << *p_it
+                       << "\n u_interp_idi=" << *v_idi
+                       << ", u_interp_rbi=" << *v_rbi
+                       << ", u_exact=" << exact_solution_u(*p_it);
+          ++v_idi;
+          ++v_rbi;
+          libMesh::out << "\n v_interp_idi=" << *v_idi
+                       << ", v_interp_rbi=" << *v_rbi
+                       << ", v_exact=" << exact_solution_v(*p_it) << std::endl;
+          ++v_idi;
+          ++v_rbi;
+        }
       }
     }
-
 
     // Demonstration case 2
     {
@@ -241,7 +197,7 @@ int main(int argc, char ** argv)
 
       // Skip adaptive runs on a non-adaptive libMesh build
 #ifndef LIBMESH_ENABLE_AMR
-      libmesh_example_requires(n_refinements==0, "--enable-amr");
+      libmesh_example_requires(n_refinements == 0, "--enable-amr");
 #else
       MeshRefinement mesh_refinement_a(mesh_a);
       mesh_refinement_a.uniformly_refine(n_refinements);
@@ -250,76 +206,76 @@ int main(int argc, char ** argv)
 #endif
 
       // Create equation systems objects.
-      EquationSystems
-        es_a(mesh_a), es_b(mesh_b);
+      EquationSystems es_a(mesh_a), es_b(mesh_b);
 
-      System & sys_a = es_a.add_system<System>("src_system");
-      System & sys_b = es_b.add_system<System>("dest_system");
+      System &sys_a = es_a.add_system<System>("src_system");
+      System &sys_b = es_b.add_system<System>("dest_system");
 
-      sys_a.add_variable ("Cp", FIRST);
-      sys_b.add_variable ("Cp", FIRST);
+      sys_a.add_variable("Cp", FIRST);
+      sys_b.add_variable("Cp", FIRST);
 
-      sys_a.attach_init_function (init_sys);
+      sys_a.attach_init_function(init_sys);
       es_a.init();
 
       // Write out the initial conditions.
-      TecplotIO(mesh_a).write_equation_systems ("src.dat",
-                                                es_a);
+      TecplotIO(mesh_a).write_equation_systems("src.dat", es_a);
 
-      InverseDistanceInterpolation<3> idi (init.comm(),
-                                           /* n_interp_pts = */ 4,
-                                           /* power =        */ 2);
-      RadialBasisInterpolation<3> rbi (init.comm());
+      InverseDistanceInterpolation<3> idi(init.comm(),
+                                          /* n_interp_pts = */ 4,
+                                          /* power =        */ 2);
+      RadialBasisInterpolation<3> rbi(init.comm());
 
-      std::vector<Point> & src_pts  (idi.get_source_points());
-      std::vector<Number> & src_vals (idi.get_source_vals());
+      std::vector<Point> &src_pts(idi.get_source_points());
+      std::vector<Number> &src_vals(idi.get_source_vals());
       idi.set_field_variables({"Cp"});
 
       // We now will loop over every node in the source mesh
       // and add it to a source point list, along with the solution
-      for (const auto & node : mesh_a.local_node_ptr_range())
-        {
-          src_pts.push_back(*node);
-          src_vals.push_back(sys_a.current_solution(node->dof_number(0, 0, 0)));
-        }
+      for (const auto &node : mesh_a.local_node_ptr_range()) {
+        src_pts.push_back(*node);
+        src_vals.push_back(sys_a.current_solution(node->dof_number(0, 0, 0)));
+      }
 
       rbi.set_field_variables({"Cp"});
       rbi.get_source_points() = idi.get_source_points();
-      rbi.get_source_vals()   = idi.get_source_vals();
+      rbi.get_source_vals() = idi.get_source_vals();
 
-      // We have only set local values - prepare for use by gathering remote data
+      // We have only set local values - prepare for use by gathering remote
+      // data
       idi.prepare_for_use();
       rbi.prepare_for_use();
 
-      // Create a MeshlessInterpolationFunction that uses our InverseDistanceInterpolation
-      // object.  Since each MeshlessInterpolationFunction shares the same InverseDistanceInterpolation
-      // object in a threaded environment we must also provide a locking mechanism.
+      // Create a MeshlessInterpolationFunction that uses our
+      // InverseDistanceInterpolation object.  Since each
+      // MeshlessInterpolationFunction shares the same
+      // InverseDistanceInterpolation object in a threaded environment we must
+      // also provide a locking mechanism.
       {
         Threads::spin_mutex mutex;
         MeshlessInterpolationFunction mif(idi, mutex);
 
         // project the solution onto system b
         es_b.init();
-        sys_b.project_solution (&mif);
+        sys_b.project_solution(&mif);
 
         // Write the result
-        TecplotIO(mesh_b).write_equation_systems ("dest_idi.dat",
-                                                  es_b);
+        TecplotIO(mesh_b).write_equation_systems("dest_idi.dat", es_b);
       }
 
-      // Create a MeshlessInterpolationFunction that uses our RadialBasisInterpolation
-      // object.  Since each MeshlessInterpolationFunction shares the same RadialBasisInterpolation
-      // object in a threaded environment we must also provide a locking mechanism.
+      // Create a MeshlessInterpolationFunction that uses our
+      // RadialBasisInterpolation object.  Since each
+      // MeshlessInterpolationFunction shares the same RadialBasisInterpolation
+      // object in a threaded environment we must also provide a locking
+      // mechanism.
       {
         Threads::spin_mutex mutex;
         MeshlessInterpolationFunction mif(rbi, mutex);
 
         // project the solution onto system b
-        sys_b.project_solution (&mif);
+        sys_b.project_solution(&mif);
 
         // Write the result
-        TecplotIO(mesh_b).write_equation_systems ("dest_rbi.dat",
-                                                  es_b);
+        TecplotIO(mesh_b).write_equation_systems("dest_rbi.dat", es_b);
       }
     }
   }
