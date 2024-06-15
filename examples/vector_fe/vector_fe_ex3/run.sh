f42ed32597b3e43ebc4db5018c732a6a9528079c
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2024-06-15 02:05:59 +0100

    Run Vector FE Ex3 with 2nd order Nédélec FEs

--- examples/vector_fe/vector_fe_ex3/run.sh
+++ examples/vector_fe/vector_fe_ex3/run.sh
@@ -6,17 +6,29 @@ source $LIBMESH_DIR/examples/run_common.sh
 
 example_name=vector_fe_ex3
 
-# Note: these problems are particularly ill-conditioned, and common
-# preconditioners like ILU don't all work very well.  We currently
-# force Jacobi in these examples.
+# Note: these problems are particularly ill-conditioned, so we currently
+# resort to a direct solver.
 
-options="element_type=TRI6 -pc_type jacobi"
+options="element_type=TRI6 -pc_type lu"
 run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=QUAD8 -pc_type jacobi"
+options="element_type=TRI7 -pc_type lu"
 run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=QUAD9 -pc_type jacobi"
+options="element_type=QUAD8 -pc_type lu"
 run_example_no_extra_options "$example_name" "$options"
 
-# No benchmark until we decide what to do about no_extra_options there
+options="element_type=QUAD9 -pc_type lu"
+run_example_no_extra_options "$example_name" "$options"
+
+options="order=2 element_type=TRI6 -pc_type lu"
+run_example_no_extra_options "$example_name" "$options"
+
+options="order=2 element_type=TRI7 -pc_type lu"
+run_example_no_extra_options "$example_name" "$options"
+
+options="order=2 element_type=QUAD8 -pc_type lu"
+run_example_no_extra_options "$example_name" "$options"
+
+options="order=2 element_type=QUAD9 -pc_type lu"
+run_example_no_extra_options "$example_name" "$options"
