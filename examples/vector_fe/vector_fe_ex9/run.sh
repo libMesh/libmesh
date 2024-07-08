cf64d92632aa949c8805e4f3ea3a0e49927b797c
Alex Lindsay <alexander.lindsay@inl.gov>
2024-07-08 12:39:25 -0700

    TRI7 tests for HDG-NS are extraneous

--- examples/vector_fe/vector_fe_ex9/run.sh
+++ examples/vector_fe/vector_fe_ex9/run.sh
@@ -9,23 +9,11 @@ example_name=vector_fe_ex9
 options="cavity=true mms=true element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
 
-options="cavity=true mms=true dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
-run_example_no_extra_options "$example_name" "$options"
-
 options="cavity=false mms=true element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
-run_example_no_extra_options "$example_name" "$options"
-
-options="cavity=false mms=true dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
 
 options="cavity=true mms=false element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
 
-options="cavity=true mms=false dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
-run_example_no_extra_options "$example_name" "$options"
-
 options="cavity=false mms=false element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
-run_example_no_extra_options "$example_name" "$options"
-
-options="cavity=false mms=false dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
