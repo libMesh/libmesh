095582f8751bb36a580933962a6de7c47a7ac842
Alex Lindsay <alexander.lindsay@inl.gov>
2024-07-11 08:28:15 -0700

    Don't run field split test

--- examples/vector_fe/vector_fe_ex9/run.sh
+++ examples/vector_fe/vector_fe_ex9/run.sh
@@ -12,12 +12,12 @@ run_example_no_extra_options "$example_name" "$options"
 options="cavity=false mms=true element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
 
-options="cavity=false mms=true dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type \
-fieldsplit -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic \
--condensed_pc_fieldsplit_type schur -condensed_pc_fieldsplit_schur_fact_type full \
--condensed_pc_fieldsplit_schur_precondition selfp -condensed_pc_fieldsplit_0_fields 4,5 \
--condensed_pc_fieldsplit_1_fields 6 --use_petsc_dm --node-major-dofs"
-run_example_no_extra_options "$example_name" "$options"
+# options="cavity=false mms=true dim=2 element_type=TRI7 -ksp_type preonly -condensed_pc_type \
+# fieldsplit -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic \
+# -condensed_pc_fieldsplit_type schur -condensed_pc_fieldsplit_schur_fact_type full \
+# -condensed_pc_fieldsplit_schur_precondition selfp -condensed_pc_fieldsplit_0_fields 4,5 \
+# -condensed_pc_fieldsplit_1_fields 6 --use_petsc_dm --node-major-dofs"
+# run_example_no_extra_options "$example_name" "$options"
 
 options="cavity=true mms=false element_type=TRI6 -ksp_type preonly -condensed_pc_type lu -condensed_pc_factor_shift_type NONZERO -snes_linesearch_type basic"
 run_example_no_extra_options "$example_name" "$options"
