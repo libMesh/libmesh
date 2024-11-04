c7322197025c85892a63beb1e527116c0e1320a1
John Peterson <jwpeterson@gmail.com>
2024-11-04 12:07:43 -0600

    detab.sh: implement shellcheck linting suggestions

--- contrib/bin/detab.sh
+++ contrib/bin/detab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script detabs (and reindents) a file using emacs-style indentation.
-if [[ $# -lt 1 ]]; then
+if test $# -lt 1; then
     echo "Usage: $0 file1.C file2.C ..."
     exit 1
 fi
@@ -14,13 +14,13 @@ fi
 # very old (circa 2007 22.1.1 on Yosemite in 2015) and there are some
 # bugs in the way that it indents C++ code, so we definitely recommend
 # using something newer if possible.
-if [ -z $EMACS ]; then
+if [ -z "$EMACS" ]; then
   EMACS=/usr/bin/emacs
 fi
 
-for i in $*; do
+for i in "$@"; do
   # Use perl to get a list of line numbers that have <TAB> characters
-  tabbed_lines=(`perl -ne "print \"$. \" if /\t/" $i`)
+  tabbed_lines=($(perl -ne "print \"$. \" if /\t/" $i))
 
   # Now actually use perl to remove those tab characters
   perl -pli -e 's/\t//g' $i
@@ -28,7 +28,7 @@ for i in $*; do
   # How much work is there to be done?
   n=${#tabbed_lines[@]}
 
-  if [ $n -gt 0 ]; then
+  if [ "$n" -gt 0 ]; then
     # Print status message
     echo "Reindenting $n lines in $i."
 
@@ -37,16 +37,16 @@ for i in $*; do
     eval_commands="--eval=\"(setq-default indent-tabs-mode nil)\" --eval=\"(c++-mode)\""
 
     # Make a long string of --eval commands
-    for i in "${tabbed_lines[@]}"
+    for j in "${tabbed_lines[@]}"
     do
-        eval_commands+=" --eval \"(goto-line $i)\" --eval \"(indent-for-tab-command)\""
+        eval_commands+=" --eval \"(goto-line $j)\" --eval \"(indent-for-tab-command)\""
     done
 
     # Construct the command you want to run as a string, and then use
     # 'eval' to run it.  Note that this is *not* the same thing as
     # running, say, `$cmd`.
     cmd="$EMACS -batch $i $eval_commands -f save-buffer &> /dev/null"
-    eval $cmd
+    eval "$cmd"
   else
     echo "Nothing to be done in $i."
   fi
