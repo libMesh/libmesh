a06986ec8416423fd5bf6b038ae81259a0433c33
John Peterson <jwpeterson@gmail.com>
2024-11-04 16:59:13 -0600

    reindent.sh: implement shellcheck linting suggestions

--- contrib/bin/reindent.sh
+++ contrib/bin/reindent.sh
@@ -2,7 +2,7 @@
 
 # This script reindents files according to the libmesh style, which is
 # the standard emacs style with no indentation in namespaces.
-if [[ $# -lt 1 ]]; then
+if [ $# -lt 1 ]; then
     echo "Usage: $0 file1.C file2.C ..."
     exit 1
 fi
@@ -15,11 +15,11 @@ fi
 # very old (circa 2007 22.1.1 on Yosemite in 2015) and there are some
 # bugs in the way that it indents C++ code, so we definitely recommend
 # using something newer if possible.
-if [ -z $EMACS ]; then
+if [ -z "$EMACS" ]; then
   EMACS=/usr/bin/emacs
 fi
 
-for i in $*; do
+for i in "$@"; do
   # Print name of file we are working on
   echo "Indenting $i"
 
@@ -29,7 +29,7 @@ for i in $*; do
   # .) Sets the indentation level within namespaces to 0
   # .) Runs indent-region on the entire file
   # .) Saves the buffer
-  $EMACS -batch $i  \
+  $EMACS -batch "$i"  \
     --eval="(setq-default indent-tabs-mode nil)" \
     --eval="(c++-mode)" \
     --eval="(c-set-offset 'innamespace 0)" \
