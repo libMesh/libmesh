3dd444d7ec13427bb6b5a9252dbb68b0466f6867
John Peterson <jwpeterson@gmail.com>
2024-11-04 12:23:39 -0600

    create_example_docs.sh: implement shellcheck linting suggestions

--- contrib/bin/create_example_docs.sh
+++ contrib/bin/create_example_docs.sh
@@ -4,17 +4,17 @@
 # also located in this directory.  This script calls ex2html.sh
 # for all of the example programs.
 
-if [ "x$LIBMESH_DIR" = x ]; then
+if test "$LIBMESH_DIR" = ""; then
   export LIBMESH_DIR=../..
 fi
 
-cd $LIBMESH_DIR/examples/
-exampleslist=`ls -d */*ex*/ | sed 's#/$##g'`
-echo $exampleslist
-cd ../contrib/bin/
+cd "$LIBMESH_DIR"/examples/ || exit
+exampleslist=$(find . -type d -name "*ex*")
+echo "$exampleslist"
+cd ../contrib/bin/ || exit
 
 for exdir in $exampleslist; do
-    mybasename=`basename $exdir`
+    mybasename=$(basename $exdir)
     echo "Processing example $exdir";
     ./ex2html.sh $LIBMESH_DIR/examples/$exdir/$mybasename.C "make --no-print-directory -C $LIBMESH_DIR/examples/$exdir run"
 done
