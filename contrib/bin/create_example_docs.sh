5f8068e53172d6953ef907c06408836328a87f09
John Peterson <jwpeterson@gmail.com>
2024-11-04 12:37:18 -0600

    create_examples_docs.sh: switch to using /bin/sh
    I don't think this script is actually used any more, but there is
    nothing in it that prevents it from running with a POSIX shell.

--- contrib/bin/create_example_docs.sh
+++ contrib/bin/create_example_docs.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#
+#!/bin/sh
+
 # This shell script is a wrapper for the script ex2html.sh
 # also located in this directory.  This script calls ex2html.sh
 # for all of the example programs.
@@ -15,6 +15,6 @@ cd ../contrib/bin/ || exit
 
 for exdir in $exampleslist; do
     mybasename=$(basename $exdir)
-    echo "Processing example $exdir";
+    echo "Processing example $exdir, file $LIBMESH_DIR/examples/$exdir/$mybasename.C";
     ./ex2html.sh $LIBMESH_DIR/examples/$exdir/$mybasename.C "make --no-print-directory -C $LIBMESH_DIR/examples/$exdir run"
 done
