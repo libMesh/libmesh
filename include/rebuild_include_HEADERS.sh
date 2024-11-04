8be50f6280ad88cbc7cbab5ad9202f5b129bcaca
John Peterson <jwpeterson@gmail.com>
2024-11-04 11:00:29 -0600

    Combine multiple file redirects into one
    As recommended by shellcheck linting:
    https://www.shellcheck.net/wiki/SC2129

--- include/rebuild_include_HEADERS.sh
+++ include/rebuild_include_HEADERS.sh
@@ -14,26 +14,32 @@ partitioning/parmetis_helper.h"
 echo "# Do not edit - automatically generated from $0" > include_HEADERS
 
 
-echo "# These are headers we need to compile the libMesh library but should" >> include_HEADERS
-echo "# not be installed.  These header files may have implementation" >> include_HEADERS
-echo "# details which are not required for the public interface" >> include_HEADERS
+{
+echo "# These are headers we need to compile the libMesh library but should"
+echo "# not be installed.  These header files may have implementation"
+echo "# details which are not required for the public interface"
+} >> include_HEADERS
 
 printf '%s' "noinst_HEADERS = " >> include_HEADERS
 for header_with_path in $noinst_blacklist ; do
     echo " \\" >> include_HEADERS
     printf '%s' "        "$header_with_path >> include_HEADERS
 done
-echo >> include_HEADERS
-echo >> include_HEADERS
 
-
+{
+echo ""
+echo ""
+} >> include_HEADERS
 
 headers=`find base enums error_estimation fe geom ghosting mesh numerics parallel partitioning timpi_shims physics quadrature reduced_basis solution_transfer solvers systems utils -name "*.h" -o -name "*specializations" -type f | LC_COLLATE=POSIX sort`
 headers="libmesh_config.h $headers"
 
-echo "# These are the headers we actually want to install and make available" >> include_HEADERS
-echo "# to a user.  Consider these the public interface of libMesh." >> include_HEADERS
-echo "# will get installed into '\$(prefix)/include/libmesh'" >> include_HEADERS
+{
+echo "# These are the headers we actually want to install and make available"
+echo "# to a user.  Consider these the public interface of libMesh."
+echo "# will get installed into '\$(prefix)/include/libmesh'"
+} >> include_HEADERS
+
 printf '%s' "include_HEADERS = " >> include_HEADERS
 for header_with_path in $headers ; do
     add_header=1
