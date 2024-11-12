86f0aee544014bd6503ab72e77ad0802c6cd2a08
Roy Stogner <Roy.Stogner@inl.gov>
2024-11-12 13:02:30 -0600

    Add FDMGradient class

---
+++ include/numerics/fdm_gradient.h
@@ -0,0 +1,197 @@
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2024 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#ifndef LIBMESH_FDM_GRADIENT_H
+#define LIBMESH_FDM_GRADIENT_H
+
+// libMesh includes
+#include "libmesh/fem_function_base.h"
+#include "libmesh/libmesh_common.h"
+
+namespace libMesh
+{
+
+template <typename GradType>
+class FDMGradient : public FEMFunctionBase<GradType>
+{
+public:
+  typedef typename DecrementRank<GradType>::type ValType;
+
+  FDMGradient (FEMFunctionBase<ValType> & value_func,
+               Real eps) :
+    _val_func(value_func.clone()), _eps(eps)
+  {}
+
+  virtual void init_context (const FEMContext & c) override
+  { _val->init_context(c); }
+
+  virtual std::unique_ptr<FEMFunctionBase<GradType>> clone () const override
+  { return std::make_unique<FDMGradient>(*_val); }
+
+  virtual GradType operator() (const FEMContext & c,
+                               const Point & p,
+                               const Real time = 0.) override
+  {
+    GradType g;
+
+    auto & val = *_val_func;
+
+    g(0) = (val(input_c, xyz[qp]+Point(_eps), time) -
+            val(input_c, xyz[qp]+Point(-_eps), time)) /
+           2 / _eps;
+#if LIBMESH_DIM > 1
+    g(1) = (val(input_c, xyz[qp]+Point(0,_eps), time) -
+            val(input_c, xyz[qp]+Point(0,-_eps), time)) /
+           2 / _eps;
+#endif
+#if LIBMESH_DIM > 2
+    g(2) = (val(input_c, xyz[qp]+Point(0,0,_eps), time) -
+            val(input_c, xyz[qp]+Point(0,0,-_eps), time)) /
+           2 / _eps;
+#endif
+
+    return g;
+  }
+
+  virtual void operator() (const FEMContext & c,
+                           const Point & p,
+                           const DenseVector<GradType> & output) override
+  {
+    auto sz = output.size();
+    DenseVector<ValType> v(sz);
+
+    val(input_c, xyz[qp]+Point(_eps), v);
+    for (auto i : make_range(sz))
+      output(i)(0) = v(i);
+
+    val(input_c, xyz[qp]+Point(-_eps), v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(0) -= v(i);
+        output(i)(0) /= 2;
+      }
+
+#if LIBMESH_DIM > 1
+    val(input_c, xyz[qp]+Point(0,_eps), v);
+    for (auto i : make_range(sz))
+      output(i)(1) = v(i);
+
+    val(input_c, xyz[qp]+Point(0,-_eps), v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(1) -= v(i);
+        output(i)(1) /= 2;
+      }
+#endif
+#if LIBMESH_DIM > 2
+    val(input_c, xyz[qp]+Point(0,0,_eps), v);
+    for (auto i : make_range(sz))
+      output(i)(2) = v(i);
+
+    val(input_c, xyz[qp]+Point(0,0,-_eps), v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(2) -= v(i);
+        output(i)(2) /= 2;
+      }
+#endif
+  }
+
+
+  virtual void operator() (const FEMContext & c,
+                           const Point & p,
+                           const Real time,
+                           const DenseVector<GradType> & output) override
+  {
+    auto sz = output.size();
+    DenseVector<ValType> v(sz);
+
+    val(input_c, xyz[qp]+Point(_eps), time, v);
+    for (auto i : make_range(sz))
+      output(i)(0) = v(i);
+
+    val(input_c, xyz[qp]+Point(-_eps), time, v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(0) -= v(i);
+        output(i)(0) /= 2;
+      }
+
+#if LIBMESH_DIM > 1
+    val(input_c, xyz[qp]+Point(0,_eps), time, v);
+    for (auto i : make_range(sz))
+      output(i)(1) = v(i);
+
+    val(input_c, xyz[qp]+Point(0,-_eps), time, v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(1) -= v(i);
+        output(i)(1) /= 2;
+      }
+#endif
+#if LIBMESH_DIM > 2
+    val(input_c, xyz[qp]+Point(0,0,_eps), time, v);
+    for (auto i : make_range(sz))
+      output(i)(2) = v(i);
+
+    val(input_c, xyz[qp]+Point(0,0,-_eps), time, v);
+    for (auto i : make_range(sz))
+      {
+        output(i)(2) -= v(i);
+        output(i)(2) /= 2;
+      }
+#endif
+  }
+
+
+  virtual GradType component (const FEMContext & c,
+                              unsigned int i,
+                              const Point & p,
+                              Real time) override
+  {
+    GradType g;
+
+    auto & val = *_val_func;
+
+    g(0) = (val(input_c, i, xyz[qp]+Point(_eps), time) -
+            val(input_c, i, xyz[qp]+Point(-_eps), time)) /
+           2 / _eps;
+#if LIBMESH_DIM > 1
+    g(1) = (val(input_c, i, xyz[qp]+Point(0,_eps), time) -
+            val(input_c, i, xyz[qp]+Point(0,-_eps), time)) /
+           2 / _eps;
+#endif
+#if LIBMESH_DIM > 2
+    g(2) = (val(input_c, i, xyz[qp]+Point(0,0,_eps), time) -
+            val(input_c, i, xyz[qp]+Point(0,0,-_eps), time)) /
+           2 / _eps;
+#endif
+
+    return g;
+  }
+
+private:
+
+  std::unique_ptr<FEMFunctionBase<ValType>> _val_func;
+
+  Real _eps;
+};
+
+
+} // namespace libMesh
+
+#endif // LIBMESH_FDM_GRADIENT_H
