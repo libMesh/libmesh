e574fe9148bfe7cf0a0359dc3526beb5346a86f6
Roy Stogner <Roy.Stogner@inl.gov>
2025-01-09 18:08:55 -0600

    Add Polygon1 Elem subclass

---
+++ include/geom/face_polygon.h
@@ -0,0 +1,266 @@
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2024 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef LIBMESH_FACE_POLYGON_H
+#define LIBMESH_FACE_POLYGON_H
+
+
+// Local includes
+#include "libmesh/libmesh_common.h"
+#include "libmesh/face.h"
+
+namespace libMesh
+{
+
+/**
+ * The \p Polygon is an element in 2D with an arbitrary (but fixed)
+ * number of sides.
+ *
+ * \author Roy H. Stogner
+ * \date 2025
+ * \brief A 2D element with an arbitrary number of sides.
+ */
+class Polygon : public Face
+{
+public:
+
+  /**
+   * Arbitrary polygonal element, takes number of nodes and
+   * sides, and a (probably unused) parent link. Derived classes
+   * implement 'true' elements.
+   *
+   * We heap-allocate our element and node links, but we can't do that
+   * until *after* we've intitialized our parent class, which means
+   * we need to do a little initialization of those links manually too.
+   */
+  Polygon (const unsigned int nn,
+           const unsigned int ns,
+           Elem * p) :
+    Face(nn, ns, p, nullptr, nullptr),
+    _elemlinks_data(ns+2), // neighbors + parent + interior_parent
+    _nodelinks_data(nn)
+  {
+    // Do the manual initialization that Elem::Elem couldn't.  No need
+    // to manually set nullptr, though, since std::vector does that.
+    this->_elemlinks = _elemlinks_data.data();
+    this->_nodes = _nodelinks_data.data();
+    this->_elemlinks[0] = p;
+
+    // Is this likely to ever be used?  We may do refinement with
+    // polygons but it's probably not going to have a hierarchy...
+    if (p)
+      {
+        this->subdomain_id() = p->subdomain_id();
+        this->processor_id() = p->processor_id();
+        _map_type = p->mapping_type();
+        _map_data = p->mapping_data();
+
+#ifdef LIBMESH_ENABLE_AMR
+        this->set_p_level(p->p_level());
+#endif
+      }
+
+    // Make sure the interior parent isn't undefined
+    if (LIBMESH_DIM > 2)
+      this->set_interior_parent(nullptr);
+  }
+
+  Polygon (Polygon &&) = delete;
+  Polygon (const Polygon &) = delete;
+  Polygon & operator= (const Polygon &) = delete;
+  Polygon & operator= (Polygon &&) = delete;
+  virtual ~Polygon() = default;
+
+  /**
+   * \returns The \p Point associated with local \p Node \p i,
+   * in master element rather than physical coordinates.
+   *
+   * This implementation returns the master vertices; subclasses with
+   * more points will need to further override.
+   */
+  virtual Point master_point (const unsigned int i) const override;
+
+  static const int num_children = 0; // Refinement not yet supported
+
+  /**
+   * \returns the number of nodes in the polygon.
+   */
+  virtual unsigned int n_nodes() const override { return this->_nodelinks_data.size(); }
+
+  /**
+   * \returns the number of sides, which is the number of links we had
+   * to save minus one parent link and minus one interior_parent link.
+   */
+  virtual unsigned int n_sides() const override final { return _elemlinks_data.size()-2; }
+
+  /**
+   * \returns the number of vertices.  All polygons have 1 vertex per side.
+   */
+  virtual unsigned int n_vertices() const override final { return _elemlinks_data.size()-2; }
+
+  /**
+   * \returns the number of edges.  All polygons have 1 edge per side.
+   */
+  virtual unsigned int n_edges() const override final { return _elemlinks_data.size()-2; }
+
+  /**
+   * \returns num_children.
+   */
+  virtual unsigned int n_children() const override final { return num_children; }
+
+  /**
+   * \returns \p true if the specified child is on the
+   * specified side.
+   *
+   * Not implemented ... indefinitely?  I don't think we'll be doing
+   * hierarchic refinement for general polygons.
+   */
+  virtual bool is_child_on_side(const unsigned int c,
+                                const unsigned int s) const override;
+
+  /**
+   * \returns The side number opposite to \p s (for n_sides() even),
+   * or throws an error otherwise.
+   */
+  virtual unsigned int opposite_side(const unsigned int s) const override final;
+
+  /**
+   * Don't hide Elem::key() defined in the base class.
+   */
+  using Elem::key;
+
+  /**
+   * \returns An id associated with the global node ids of this
+   * element.  The id is not necessarily unique, but should be
+   * close.
+   */
+  virtual dof_id_type key () const override;
+
+  /**
+   * \returns An id associated with the \p s side of this element.
+   * The id is not necessarily unique, but should be close.
+   */
+  virtual dof_id_type key (const unsigned int s) const override;
+
+  /**
+   * \returns An id associated with the \p s side of this element, as
+   * defined solely by element vertices.  The id is not necessarily
+   * unique, but should be close.  This is particularly useful in the
+   * \p MeshBase::find_neighbors() routine.
+   */
+  virtual dof_id_type low_order_key (const unsigned int s) const override;
+
+  /**
+   * \returns The local node id for node \p side_node on side \p side of
+   * this Elem.
+   *
+   * This implementation assumes a particular node numbering "style"
+   * and may be overridden in subclasses.
+   */
+  virtual unsigned int local_side_node(unsigned int side,
+                                       unsigned int side_node) const override;
+
+  /**
+   * Calls local_side_node(edge, edge_node). For 2D elements, there is an implied
+   * equivalence between edges and sides, e.g. n_edges() == n_sides(), so we treat
+   * these two functions the same.
+   */
+  virtual unsigned int local_edge_node(unsigned int edge,
+                                       unsigned int edge_node) const override;
+
+  /**
+   * \returns A primitive (2-noded) edge for edge i.
+   */
+  virtual std::unique_ptr<Elem> side_ptr (const unsigned int i) override final;
+
+  /**
+   * Rebuilds an EDGE2 coincident with face i.
+   */
+  virtual void side_ptr (std::unique_ptr<Elem> & elem,
+                         const unsigned int i) override final;
+
+  /**
+   * \returns The suggested quality bounds for
+   * the hex based on quality measure q.
+   */
+  virtual std::pair<Real, Real> qual_bounds (const ElemQuality q) const override;
+
+  /**
+   * n_sides sides, one orientation each.  Not marked final because
+   * subclasses may have interior nodes with less symmetry
+   */
+  virtual unsigned int n_permutations() const override { return this->n_sides(); }
+
+  virtual bool is_flipped() const override final;
+
+  virtual std::vector<unsigned int> edges_adjacent_to_node(const unsigned int n) const override;
+
+  /**
+   * Create a triangulation from the current node locations.
+   *
+   * If this is not called by the user, it will be called
+   * automatically when needed.
+   *
+   * If the user moves the polygon nodes, the triangulation may need
+   * to be regenerated manually, or the changed mapping may be lower
+   * quality or even inverted.
+   *
+   * Pure virtual because this strongly depends on what mid-edge or
+   * mid-face nodes the subclass might have.
+   */
+  virtual void retriangulate() = 0;
+
+  unsigned int n_subtriangles() const { return cast_int<unsigned int>(this->_triangulation.size()); }
+
+  virtual std::array<Point, 3> master_subtriangle (unsigned int i) const;
+
+protected:
+
+  /**
+   * Data for links to parent/neighbor/interior_parent elements.
+   *
+   * There should be num_sides neighbors, preceded by any parent link
+   * and followed by any interior_parent link.
+   *
+   * We're stuck with a heap allocation here since the number of sides
+   * in a subclass is chosen at runtime.
+   */
+  std::vector<Elem *> _elemlinks_data;
+
+  /**
+   * Data for links to nodes.  We're stuck with a heap allocation here
+   * since the number of nodes in a subclass is chosen at runtime.
+   */
+  std::vector<Node *> _nodelinks_data;
+
+  /**
+   * Data for a triangulation of the polygon.  Our mapping from master
+   * space to physical space will be based on these subelements.
+   *
+   * Positive int values should correspond to local node numbers.
+   *
+   * Negative int values may correspond to "special" points, e.g. a
+    * centroid or skeleton point.
+   */
+  std::vector<std::array<int, 3>> _triangulation;
+};
+
+} // namespace libMesh
+
+#endif // LIBMESH_FACE_POLYGON_H
---
+++ include/geom/face_polygon1.h
@@ -0,0 +1,196 @@
+// The libMesh Finite Element Library.
+// Copyright (C) 2002-2024 Benjamin S. Kirk, John W. Peterson, Roy H. Stogner
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+#ifndef LIBMESH_FACE_POLYGON1_H
+#define LIBMESH_FACE_POLYGON1_H
+
+// Local includes
+#include "libmesh/libmesh_common.h"
+#include "libmesh/face_polygon.h"
+
+namespace libMesh
+{
+
+/**
+ * The \p POLYGON1 is an element in 2D with an arbitrary (but fixed)
+ * number of first-order (EDGE2) sides.
+ *
+ * Sides and vertices are numbered clockwise starting from 0.
+ *
+ * In master space, a POLYGON1 has point 0 at (0,0) and is a regular
+ * polygon of side length 1.
+ *
+ * \author Roy H. Stogner
+ * \date 2025
+ * \brief A 2D element with an arbitrary number of first-order sides.
+ */
+class Polygon1 : public Polygon
+{
+public:
+
+  /**
+   * Constructor.  Takes the number of sides as an input, and
+   * allocates the same number of nodes, one node for the vertex
+   * between each two sides.
+   *
+   * By default this element has no parent.
+   *
+   * We'll set up a simple default triangulation here, but if users
+   * want to make a non-convex polygon they'll want to retriangulate()
+   * after setting up its nodes.
+   */
+  explicit
+  Polygon1 (const unsigned int num_sides, Elem * p=nullptr);
+
+  Polygon1 (Polygon1 &&) = delete;
+  Polygon1 (const Polygon1 &) = delete;
+  Polygon1 & operator= (const Polygon1 &) = delete;
+  Polygon1 & operator= (Polygon1 &&) = delete;
+  virtual ~Polygon1() = default;
+
+  /**
+   * \returns \p POLYGON1.
+   */
+  virtual ElemType type () const override final { return POLYGON1; }
+
+  /**
+   * \returns the number of triangles to break this into for
+   * visualization.
+   */
+  virtual unsigned int n_sub_elem() const override { return this->n_sides(); }
+
+  /**
+   * \returns The local node number for the node opposite to node n on
+   * side \p opposite_side(s) (for n_sides() even), or throws an error
+   * otherwise.
+   */
+  virtual unsigned int opposite_node(const unsigned int n,
+                                     const unsigned int s) const override final;
+
+  /**
+   * \returns \p true if the specified (local) node number is a vertex.
+   */
+  virtual bool is_vertex(const unsigned int i) const override;
+
+  /**
+   * \returns \p true if the specified (local) node number is an edge.
+   */
+  virtual bool is_edge(const unsigned int i) const override;
+
+  /**
+   * \returns \p true if the specified (local) node number is a face.
+   */
+  virtual bool is_face(const unsigned int i) const override;
+
+  /**
+   * \returns \p true if the specified (local) node number is on the
+   * specified side.
+   */
+  virtual bool is_node_on_side(const unsigned int n,
+                               const unsigned int s) const override;
+
+  virtual std::vector<unsigned int> nodes_on_side(const unsigned int s) const override;
+
+  virtual std::vector<unsigned int> nodes_on_edge(const unsigned int e) const override;
+
+  /**
+   * \returns \p true if the specified (local) node number is on the
+   * specified edge (== is_node_on_side in 2D).
+   */
+  virtual bool is_node_on_edge(const unsigned int n,
+                               const unsigned int e) const override
+  { return this->is_node_on_side(n,e); }
+
+  /**
+   * \returns \p true if the element map is definitely affine within
+   * numerical tolerances.
+   */
+  virtual bool has_affine_map () const override;
+
+  /**
+   * \returns SECOND.
+   */
+  virtual Order default_order() const override;
+
+  /**
+   * Don't hide Elem::key() defined in the base class.
+   */
+  using Elem::key;
+
+  virtual std::unique_ptr<Elem> build_side_ptr (const unsigned int i,
+                                                bool proxy=false) override;
+
+  /**
+   * Rebuilds an EDGE2 coincident with side i.
+   */
+  virtual void build_side_ptr (std::unique_ptr<Elem> & elem,
+                               const unsigned int i) override;
+
+  virtual void connectivity(const unsigned int sf,
+                            const IOPackage iop,
+                            std::vector<dof_id_type> & conn) const override;
+
+  /**
+   * \returns 2 for all \p n.
+   */
+  virtual unsigned int n_second_order_adjacent_vertices (const unsigned int) const override
+  { return 2; }
+
+  /**
+   * Element refinement is not implemented for polygons.
+   */
+  virtual std::pair<unsigned short int, unsigned short int>
+  second_order_child_vertex (const unsigned int n) const override;
+
+  /**
+   * An optimized method for calculating the area of a POLYGON.
+   */
+  virtual Real volume () const override;
+
+  virtual void permute(unsigned int perm_num) override final;
+
+  virtual void flip(BoundaryInfo *) override final;
+
+  ElemType side_type (const unsigned int s) const override final;
+
+  /**
+   * Create a triangulation from the current node locations.
+   */
+  virtual void retriangulate() override final;
+
+protected:
+
+#ifdef LIBMESH_ENABLE_AMR
+
+  /**
+   * Matrix used to create the elements children.
+   */
+  virtual Real embedding_matrix (const unsigned int /*i*/,
+                                 const unsigned int /*j*/,
+                                 const unsigned int /*k*/) const override
+  { libmesh_not_implemented(); return 0; }
+
+#endif // LIBMESH_ENABLE_AMR
+
+};
+
+
+} // namespace libMesh
+
+#endif // LIBMESH_FACE_POLYGON_H
