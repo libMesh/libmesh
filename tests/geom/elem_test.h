d1891af8d39ca3f5c3f0074e11aa35c354df85db
Roy Stogner <Roy.Stogner@inl.gov>
2025-04-13 14:19:23 -0500

    Test C0Polyhedron with a cube
    We're going to need a polyhedron with a center point for more general
    use, since it turns out that there there are simple faces that can't be
    tetrahedralized without ending up with degenerate tets

--- tests/geom/elem_test.h
+++ tests/geom/elem_test.h
@@ -5,7 +5,8 @@
 #include <libmesh/mesh.h>
 #include <libmesh/mesh_generation.h>
 
-// Avoiding Elem::build() here because we can't pass an n_sides to it
+// Avoiding Elem::build() when we can't pass sides or n_sides to it
+#include <libmesh/cell_c0polyhedron.h>
 #include <libmesh/face_c0polygon.h>
 
 #include "libmesh_cppunit.h"
@@ -29,11 +30,14 @@ public:
 
     _mesh = std::make_unique<Mesh>(*TestCommWorld);
 
-    std::unique_ptr<Elem> test_elem = Elem::build(elem_type);
+    std::unique_ptr<Elem> test_elem;
+
+    if (elem_type != C0POLYHEDRON)
+      test_elem = Elem::build(elem_type);
 
 #ifdef LIBMESH_ENABLE_INFINITE_ELEMENTS
 #if LIBMESH_DIM > 1
-    if (test_elem->infinite())
+    if (test_elem.get() && test_elem->infinite())
       {
         Elem * elem = _mesh->add_elem(std::move(test_elem));
 
@@ -143,6 +147,111 @@ public:
         polygon->set_id() = 0;
 
         _mesh->add_elem(std::move(polygon));
+        _mesh->prepare_for_use();
+      }
+    else if (elem_type == C0POLYHEDRON)
+      {
+        // There's even less point in having a build_cube for general
+        // polyhedra, so again we'll hand-make one for testing and
+        // we'll plan on making a build_dual_mesh for the future.
+
+#if 0
+        // Try a truncated cube for relatively simple verification.
+
+        // We have some differing orientations on the top sides, to
+        // test handling of that.
+        // Lower octagon points, counterclockwise
+        _mesh->add_point(Point(1/Real(3), 0, 0), 0);
+        _mesh->add_point(Point(2/Real(3), 0, 0), 1);
+        _mesh->add_point(Point(1, 1/Real(3), 0), 2);
+        _mesh->add_point(Point(1, 2/Real(3), 0), 3);
+        _mesh->add_point(Point(2/Real(3), 1, 0), 4);
+        _mesh->add_point(Point(1/Real(3), 1, 0), 5);
+        _mesh->add_point(Point(0, 2/Real(3), 0), 6);
+        _mesh->add_point(Point(0, 1/Real(3), 0), 7);
+
+        // Lower-middle points, counterclockwise
+        _mesh->add_point(Point(0, 0, 1/Real(3)), 8);
+        _mesh->add_point(Point(1, 0, 1/Real(3)), 9);
+        _mesh->add_point(Point(1, 1, 1/Real(3)), 10);
+        _mesh->add_point(Point(0, 1, 1/Real(3)), 11);
+
+        // Upper-middle points, counterclockwise
+        _mesh->add_point(Point(0, 0, 2/Real(3)), 12);
+        _mesh->add_point(Point(1, 0, 2/Real(3)), 13);
+        _mesh->add_point(Point(1, 1, 2/Real(3)), 14);
+        _mesh->add_point(Point(0, 1, 2/Real(3)), 15);
+
+        // Upper octagon points, counterclockwise
+        _mesh->add_point(Point(1/Real(3), 0, 1), 16);
+        _mesh->add_point(Point(2/Real(3), 0, 1), 17);
+        _mesh->add_point(Point(1, 1/Real(3), 1), 18);
+        _mesh->add_point(Point(1, 2/Real(3), 1), 19);
+        _mesh->add_point(Point(2/Real(3), 1, 1), 20);
+        _mesh->add_point(Point(1/Real(3), 1, 1), 21);
+        _mesh->add_point(Point(0, 2/Real(3), 1), 22);
+        _mesh->add_point(Point(0, 1/Real(3), 1), 23);
+
+        const std::vector<std::vector<unsigned int>> nodes_on_side =
+          { {0, 1, 2, 3, 4, 5, 6, 7},         // min z
+            {0, 1, 9, 13, 17, 16, 12, 8},     // min y
+            {2, 3, 10, 14, 19, 18, 13, 9},    // max x
+            {4, 5, 11, 15, 21, 20, 14, 10},   // max y
+            {6, 7, 8, 12, 23, 22, 15, 11},    // min x
+            {16, 17, 18, 19, 20, 21, 22, 23}, // max z
+            {7, 0, 8},                        // max nothing
+            {1, 2, 9},                        // max x
+            {3, 4, 10},                       // max xy
+            {5, 6, 11},                       // max y
+            {23, 16, 12},                     // max z
+            {17, 18, 13},                     // max xz
+            {19, 20, 14},                     // max xyz
+            {21, 22, 15} };                   // max yz
+#endif
+
+#if 1
+        // Or try a plain cube, for simpler debugging
+        _mesh->add_point(Point(0, 0, 0), 0);
+        _mesh->add_point(Point(1, 0, 0), 1);
+        _mesh->add_point(Point(1, 1, 0), 2);
+        _mesh->add_point(Point(0, 1, 0), 3);
+        _mesh->add_point(Point(0, 0, 1), 4);
+        _mesh->add_point(Point(1, 0, 1), 5);
+        _mesh->add_point(Point(1, 1, 1), 6);
+        _mesh->add_point(Point(0, 1, 1), 7);
+
+        // With some combinations of face triangulations, even a
+        // simple cube has no tetrahedralization that doesn't have
+        // either interior discontinuities or a 0-volume pancake tet!
+        //
+        // The initial "natural" way to orient our sides is commented
+        // out here; permutations that fix diagonals for us are used
+        // instead.
+        const std::vector<std::vector<unsigned int>> nodes_on_side =
+          { {0, 1, 2, 3},   // min z
+            {0, 1, 5, 4},   // min y
+        //     {1, 2, 6, 5},   // max x - bad
+            {2, 6, 5, 1},   // max x
+            {2, 3, 7, 6},   // max y
+        //     {3, 0, 4, 7},   // min x - bad
+            {0, 4, 7, 3},   // min x
+        //     {4, 5, 6, 7} }; // max z - bad
+            {5, 6, 7, 4} }; // max z
+#endif
+
+        // Build all the sides.
+        std::vector<std::shared_ptr<Polygon>> sides(nodes_on_side.size());
+
+        for (auto s : index_range(nodes_on_side))
+          {
+            const auto & nodes_on_s = nodes_on_side[s];
+            sides[s] = std::make_shared<C0Polygon>(nodes_on_s.size());
+            for (auto i : index_range(nodes_on_s))
+              sides[s]->set_node(i, _mesh->node_ptr(nodes_on_s[i]));
+          }
+
+        std::unique_ptr<Elem> polyhedron = std::make_unique<C0Polyhedron>(sides);
+        _mesh->add_elem(std::move(polyhedron));
         _mesh->prepare_for_use();
       }
     else
