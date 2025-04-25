f7b9882e9a9be671d52557075b316f51ae4583ba
Roy Stogner <Roy.Stogner@inl.gov>
2025-04-25 16:07:05 -0500

    Add libmesh_imag() shim

--- include/base/libmesh_common.h
+++ include/base/libmesh_common.h
@@ -184,10 +184,14 @@ typedef std::complex<Real> COMPLEX;
 // Helper functions for complex/real numbers
 // to clean up #ifdef LIBMESH_USE_COMPLEX_NUMBERS elsewhere
 template<typename T> inline T libmesh_real(T a) { return a; }
+template<typename T> inline T libmesh_imag(T /*a*/) { return 0; }
 template<typename T> inline T libmesh_conj(T a) { return a; }
 
 template<typename T>
 inline T libmesh_real(std::complex<T> a) { return std::real(a); }
+
+template<typename T>
+inline T libmesh_imag(std::complex<T> a) { return std::imag(a); }
 
 template<typename T>
 inline std::complex<T> libmesh_conj(std::complex<T> a) { return std::conj(a); }
