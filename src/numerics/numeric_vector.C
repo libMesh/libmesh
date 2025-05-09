284c46438b2739cbf4e0328758294ea72a5bf795
Roy Stogner <Roy.Stogner@inl.gov>
2025-05-09 14:02:39 -0500

    NumericVector::print/read_matlab
    We'll leave the PETSc specialization too, but let's let everything do
    "Matlab"-style output and input.

--- src/numerics/numeric_vector.C
+++ src/numerics/numeric_vector.C
@@ -28,12 +28,23 @@
 #include "libmesh/enum_solver_package.h"
 #include "libmesh/int_range.h"
 
+// gzstream for reading/writing compressed files as a stream
+#ifdef LIBMESH_HAVE_GZSTREAM
+# include "libmesh/ignore_warnings.h" // shadowing in gzstream.h
+# include "gzstream.h"
+# include "libmesh/restore_warnings.h"
+#endif
 
 // C++ includes
 #include <cstdlib> // *must* precede <cmath> for proper std:abs() on PGI, Sun Studio CC
 #include <cmath> // for std::abs
+#include <sstream>
 #include <limits>
 #include <memory>
+
+#ifdef LIBMESH_HAVE_CXX11_REGEX
+#include <regex>
+#endif
 
 
 namespace libMesh
@@ -227,6 +238,215 @@ int NumericVector<T>::global_relative_compare (const NumericVector<T> & other_ve
 
   return first_different_i;
 }
+
+
+
+template <typename T>
+void NumericVector<T>::print_matlab(const std::string & filename) const
+{
+  parallel_object_only();
+
+  libmesh_assert (this->initialized());
+
+  const numeric_index_type first_dof = this->first_local_index(),
+                           end_dof   = this->last_local_index();
+
+  // We'll print the vector from processor 0 to make sure
+  // it's serialized properly
+  if (this->processor_id() == 0)
+    {
+      std::unique_ptr<std::ofstream> file;
+
+      if (filename != "")
+        file = std::make_unique<std::ofstream>(filename.c_str());
+
+      std::ostream & os = (filename == "") ? libMesh::out : *file;
+
+      // Print a header similar to PETSC_VIEWER_ASCII_MATLAB
+      os << "%Vec Object: () " << this->n_processors() << " MPI processes\n"
+         << "%  type: " << (this->n_processors() > 1 ? "mpi" : "seq") << "\n"
+         << "Vec = [\n";
+
+      for (numeric_index_type i : make_range(end_dof))
+        os << (*this)(i) << '\n';
+
+      std::vector<T> cbuf;
+      for (auto p : IntRange<processor_id_type>(1, this->n_processors()))
+        {
+          this->comm().receive(p, cbuf);
+
+          for (auto c : cbuf)
+            os << c << '\n';
+        }
+
+      os << "];\n" << std::endl;
+    }
+  else
+    {
+      std::vector<T> cbuf(end_dof - first_dof);
+
+      for (numeric_index_type i : make_range(end_dof - first_dof))
+        cbuf[i] = (*this)(first_dof+i);
+      this->comm().send(0,cbuf);
+    }
+}
+
+
+
+template <typename T>
+void NumericVector<T>::read_matlab(const std::string & filename)
+{
+  LOG_SCOPE("read_matlab()", "NumericVector");
+
+#ifndef LIBMESH_HAVE_CXX11_REGEX
+  libmesh_not_implemented();  // What is your compiler?!?  Email us!
+  libmesh_ignore(filename);
+#else
+  parallel_object_only();
+
+  const bool gzipped_file = (filename.rfind(".gz") == filename.size() - 3);
+
+  // If we don't already have this size, we'll need to reinit, and
+  // determine which entries each processor is in charge of.
+  std::vector<numeric_index_type> first_entries, end_entries;
+
+  numeric_index_type first_entry = 0,
+                     end_entry = 0,
+                     n = 0;
+
+  // We'll use an istream here; it might be an ifstream if we're
+  // opening a raw ASCII file or a gzstream if we're opening a
+  // compressed one.
+  std::unique_ptr<std::istream> file;
+
+  // First read through the file, saving size and entry data
+  std::vector<T> entries;
+
+  {
+  // We'll read the vector on processor 0 rather than try to juggle
+  // parallel I/O.
+  if (this->processor_id() == 0)
+    {
+      // We'll be using regular expressions to make ourselves slightly
+      // more robust to formatting.
+      const std::regex start_regex // assignment like "Vec_0x84000002_1 = ["
+        ("\\s*\\w+\\s*=\\s*\\[");
+      const std::regex end_regex // end of assignment
+        ("^[^%]*\\]");
+
+      if (gzipped_file)
+        {
+#ifdef LIBMESH_HAVE_GZSTREAM
+          auto inf = std::make_unique<igzstream>();
+          libmesh_assert(inf);
+          inf->open(filename.c_str(), std::ios::in);
+          file = std::move(inf);
+#else
+          libmesh_error_msg("ERROR: need gzstream to handle .gz files!!!");
+#endif
+        }
+      else
+        {
+          auto inf = std::make_unique<std::ifstream>();
+          libmesh_assert(inf);
+
+          std::string new_name = Utility::unzip_file(filename);
+
+          inf->open(new_name.c_str(), std::ios::in);
+          file = std::move(inf);
+        }
+
+      const std::string whitespace = " \t";
+
+      bool have_started = false;
+      bool have_ended = false;
+
+      for (std::string line; std::getline(*file, line);)
+        {
+          std::smatch sm;
+
+          // First, try to match an entry.  This is the most common
+          // case so we won't rely on slow std::regex for it.
+          // stringstream is at least an improvement over that.
+          std::istringstream l(line);
+          T value;
+          l >> value;
+
+          if (!l.fail())
+            {
+              libmesh_error_msg_if
+                (!have_started, "Confused by premature entries in vector file " << filename);
+
+              entries.push_back(value);
+              ++n;
+            }
+
+          else if (std::regex_search(line, start_regex))
+            have_started = true;
+
+          else if (std::regex_search(line, end_regex))
+            {
+              have_ended = true;
+              break;
+            }
+        }
+
+      libmesh_error_msg_if
+        (!have_started, "Confused by missing assignment-beginning in vector file " << filename);
+
+      libmesh_error_msg_if
+        (!have_ended, "Confused by missing assignment-ending in vector file " << filename);
+    }
+
+  this->comm().broadcast(n);
+
+  if (this->initialized() &&
+      n == this->size())
+    {
+      first_entry = this->first_local_index(),
+      end_entry = this->last_local_index();
+    }
+  else
+    {
+      // Determine which rows/columns each processor will be in charge of
+      first_entry =     this->processor_id() * n / this->n_processors(),
+      end_entry   = (this->processor_id()+1) * n / this->n_processors();
+    }
+
+  this->comm().gather(0, first_entry, first_entries);
+  this->comm().gather(0, end_entry, end_entries);
+
+  } // Done reading entry data and broadcasting vector size
+
+  // If we're not already initialized compatibly with the file then
+  // we'll initialize here
+  bool need_init = !this->initialized() ||
+                   (this->size() != n) ||
+                   (this->local_size() != end_entry - first_entry);
+
+  this->comm().max(need_init);
+  
+  if (need_init)
+    this->init(n, end_entry - first_entry);
+
+  // Set the vector values last.  The iota call here is inefficient
+  // but it's probably better than calling a single virtual function
+  // per index.
+  if (this->processor_id() == 0)
+    for (auto p : make_range(this->n_processors()))
+      {
+        const numeric_index_type first_entry_p = first_entries[p];
+        const numeric_index_type n_local = end_entries[p] - first_entry_p;
+        std::vector<numeric_index_type> indices(n_local);
+        std::iota(indices.begin(), indices.end(), first_entry_p);
+        this->insert(entries.data() + first_entries[p],
+                     indices);
+      }
+
+  this->close();
+#endif
+}
+
 
 /*
 // Full specialization for float datatypes (DistributedVector wants this)
