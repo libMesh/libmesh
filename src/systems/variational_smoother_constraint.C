c6e37e47ad28fca6e79a120812a8649bf0c53176
Patrick Behne <patrick.behne@inl.gov>
2025-07-17 12:08:12 -0600

    Added fallback to fixed node constraint in event that constrints do not intersect.

--- src/systems/variational_smoother_constraint.C
+++ src/systems/variational_smoother_constraint.C
@@ -362,9 +362,19 @@ void VariationalSmootherConstraint::constrain()
       const auto &subdomain_constraint = it->second;
       // Combine current boundary constraint with previously determined
       // subdomain_constraint
-      const auto combined_constraint =
-          intersect_constraints(subdomain_constraint, boundary_constraint);
-      this->impose_constraint(node, combined_constraint);
+      try
+      {
+        const auto combined_constraint =
+            intersect_constraints(subdomain_constraint, boundary_constraint);
+        this->impose_constraint(node, combined_constraint);
+      }
+      catch (const std::exception & e)
+      {
+        // This will catch cases where constraints have no intersection
+        // Fall back to fixed node constraint
+        this->impose_constraint(node, PointConstraint(node));
+      }
+
     } else
       this->impose_constraint(node, boundary_constraint);
 
@@ -741,7 +751,19 @@ ConstraintVariant VariationalSmootherConstraint::determine_constraint(
   auto it = valid_planes.begin();
   ConstraintVariant current = *it++;
   for (; it != valid_planes.end(); ++it)
-    current = intersect_constraints(current, *it);
+  {
+    try
+    {
+      current = intersect_constraints(current, *it);
+    }
+    catch (const std::exception & e)
+    {
+      // This will catch cases where constraints have no intersection
+      // Fall back to fixed node constraint
+      current = PointConstraint(node);
+      break;
+    }
+  }
 
   return current;
 }
