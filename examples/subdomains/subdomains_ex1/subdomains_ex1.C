ab86a36caebb763da5f2f362ddb61d326efb0d79
Roy Stogner <Roy.Stogner@inl.gov>
2025-12-10 23:19:23 -0600

    More updates of mesh caches

--- examples/subdomains/subdomains_ex1/subdomains_ex1.C
+++ examples/subdomains/subdomains_ex1/subdomains_ex1.C
@@ -243,6 +243,9 @@ int main (int argc, char ** argv)
         elem->subdomain_id() = 1;
     }
 
+  // Make sure the mesh knows we added new subdomains.
+  mesh.cache_elem_data();
+
   // Create an equation systems object.
   EquationSystems equation_systems (mesh);
 
