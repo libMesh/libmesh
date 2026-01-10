7403c378cfec5987e0883a70b05613e9001800d8
Nuno Nobre <nuno.nobre@stfc.ac.uk>
2026-01-10 15:38:28 +0000

    Downgrade some example runs to jacobi to recover coverage

--- examples/vector_fe/vector_fe_ex4/run.sh
+++ examples/vector_fe/vector_fe_ex4/run.sh
@@ -7,15 +7,15 @@
 example_name=vector_fe_ex4
 
 # Note: these problems are particularly ill-conditioned, so we use a robust
-# preconditioner, hypre AMS.
+# preconditioner, hypre AMS. We use jacobi in two configs for coverage only.
 
-options="element_type=TET10 -pc_type hypre -pc_hypre_type ams"
+options="element_type=TET10 -pc_type jacobi"
 run_example_no_extra_options "$example_name" "$options"
 
 options="element_type=TET14 -pc_type hypre -pc_hypre_type ams"
 run_example_no_extra_options "$example_name" "$options"
 
-options="element_type=HEX20 -pc_type hypre -pc_hypre_type ams"
+options="element_type=HEX20 -pc_type jacobi"
 run_example_no_extra_options "$example_name" "$options"
 
 options="element_type=HEX27 -pc_type hypre -pc_hypre_type ams"
