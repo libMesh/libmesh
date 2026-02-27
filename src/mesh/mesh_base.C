bc7aa7c5843de1fa632f76c578e0895673bc9c1b
Roy Stogner <Roy.Stogner@inl.gov>
2026-02-27 09:15:05 -0600

    Assert caches are prepared before allowing queries

--- src/mesh/mesh_base.C
+++ src/mesh/mesh_base.C
@@ -633,13 +633,6 @@ void MeshBase::change_elemset_id(elemset_id_type old_id, elemset_id_type new_id)
   _all_elemset_ids.insert(new_id);
 }
 
-unsigned int MeshBase::spatial_dimension () const
-{
-  libmesh_assert(_preparation.has_cached_elem_data);
-
-  return cast_int<unsigned int>(_spatial_dimension);
-}
-
 
 
 void MeshBase::set_spatial_dimension(unsigned char d)
