ad6101f04bbd94e827794cf2bc292401160a8418
rochi00 <rochishnu.chowdhury@inl.gov>
2026-05-08 15:04:46 -0600

    Add Kokkos numerics storage and operator headers

---
+++ include/gpu/kokkos_storage.h
@@ -0,0 +1,53 @@
+// libMesh Kokkos storage helpers for dimension-aware vector/tensor views.
+
+#ifndef LIBMESH_KOKKOS_STORAGE_H
+#define LIBMESH_KOKKOS_STORAGE_H
+
+#include "libmesh/kokkos_linalg_base.h"
+
+#include "libmesh/libmesh_common.h"
+#include "libmesh/libmesh_device.h"
+#include "libmesh/type_tensor.h"
+#include "libmesh/type_vector.h"
+
+namespace libMesh::Kokkos
+{
+
+template <typename VectorType, typename ViewType>
+LIBMESH_DEVICE_INLINE
+VectorType load_vector(const ViewType & view, const unsigned int i)
+{
+  return materialize_vector<VectorType>(make_vector_ref(view, i));
+}
+
+template <typename ViewType, typename VectorType>
+LIBMESH_DEVICE_INLINE
+void store_vector(const ViewType & view, const unsigned int i, const VectorType & v)
+{
+  auto out = make_vector_ref(view, i);
+
+  for (unsigned int d = 0; d < LIBMESH_DIM; ++d)
+    vector_set_component(out, d, vector_get_component(v, d));
+}
+
+template <typename TensorType, typename ViewType>
+LIBMESH_DEVICE_INLINE
+TensorType load_tensor(const ViewType & view, const unsigned int i)
+{
+  return materialize_tensor<TensorType>(make_tensor_ref(view, i));
+}
+
+template <typename ViewType, typename TensorType>
+LIBMESH_DEVICE_INLINE
+void store_tensor(const ViewType & view, const unsigned int i, const TensorType & T)
+{
+  auto out = make_tensor_ref(view, i);
+
+  for (unsigned int row = 0; row < LIBMESH_DIM; ++row)
+    for (unsigned int col = 0; col < LIBMESH_DIM; ++col)
+      tensor_set_component(out, row, col, tensor_get_component(T, row, col));
+}
+
+} // namespace libMesh::Kokkos
+
+#endif // LIBMESH_KOKKOS_STORAGE_H
