3cb19b43e5615ac2f5150369a6f94b53395080dc
Roy Stogner <Roy.Stogner@inl.gov>
2026-06-11 12:01:15 -0500

    Kokkos device plumbing
    This is a fraction of a commit authored by Rochi; I stripped off the
    m4 bits so we could just use the updated ACSM versions from the start.

--- include/base/libmesh_exceptions.h
+++ include/base/libmesh_exceptions.h
@@ -23,6 +23,7 @@
 #include "libmesh/libmesh_config.h"
 
 #include "libmesh/libmesh_abort.h"
+#include "libmesh/libmesh_device.h"
 
 #include <stdexcept>
 #include <string>
@@ -217,7 +218,13 @@ public:
 #ifdef LIBMESH_ENABLE_EXCEPTIONS
 #define libmesh_noexcept noexcept
 
+#if LIBMESH_IN_DEVICE_CODE
+// Kokkos device code does not support C++ exceptions.
+#define LIBMESH_THROW(e) do { LIBMESH_DEVICE_ERROR_MSG((e).what()); } while (0)
+#else
 #define LIBMESH_THROW(e) do { throw e; } while (0)
+#endif
+
 #define libmesh_rethrow throw
 #define libmesh_try try
 #define libmesh_catch(e) catch(e)
