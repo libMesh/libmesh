2b8fd21b79e58e630b211fad23d356e4bb8a386f
Roy Stogner <Roy.Stogner@inl.gov>
2026-08-04 17:42:05 -0500

    Don't unset_ in subdomain_name()
    Even if *we* know we're doing a read-only access, we might be trying to
    do it with a non-const mesh, and then unset_ will falsely flag us here.
    
    This bit Logan downstream.

--- src/mesh/mesh_base.C
+++ src/mesh/mesh_base.C
@@ -1886,7 +1886,14 @@ bool MeshBase::get_count_lower_dim_elems_in_point_locator() const
 
 std::string & MeshBase::subdomain_name(subdomain_id_type id)
 {
-  this->unset_has_synched_subdomain_name_map();
+  // Use set_subdomain_name() instead
+  libmesh_deprecated();
+
+  // We'd like to do this so our preparation isn't invalidated by a
+  // write, but for all we know the user is just reading from a
+  // non-const mesh!
+  // this->unset_has_synched_subdomain_name_map();
+
   return _block_id_to_name[id];
 }
 
