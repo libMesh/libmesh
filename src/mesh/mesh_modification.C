e70dc137b797563f48b2a4d59c951ac11ffaca20
Roy Stogner <Roy.Stogner@inl.gov>
2026-08-11 09:24:41 -0500

    Add Pyramid5->Tet4 support to all_tri()

--- src/mesh/mesh_modification.C
+++ src/mesh/mesh_modification.C
@@ -1279,6 +1279,48 @@ void MeshTools::Modification::all_tri (MeshBase & mesh)
               break;
             }
 
+          case PYRAMID5:
+            {
+              // Pyramids all split into two tetrahedra
+              subelem[0] = Elem::build(TET4);
+              subelem[1] = Elem::build(TET4);
+
+              // Choose how to split the quad face in a way that will
+              // be consistent from possibly-different-type elements
+              // splitting from the other side
+              //
+              // Split on 0-2 diagonal
+              if (split_first_diagonal(elem, 0,2, 1,3))
+                {
+                  subelem[0]->set_node(0, elem->node_ptr(0));
+                  subelem[0]->set_node(1, elem->node_ptr(1));
+                  subelem[0]->set_node(2, elem->node_ptr(2));
+                  subelem[0]->set_node(3, elem->node_ptr(4));
+
+                  subelem[1]->set_node(0, elem->node_ptr(0));
+                  subelem[1]->set_node(1, elem->node_ptr(2));
+                  subelem[1]->set_node(2, elem->node_ptr(3));
+                  subelem[1]->set_node(3, elem->node_ptr(4));
+                }
+              // Split on 1-3 diagonal
+              else
+                {
+                  libmesh_assert (split_first_diagonal(elem, 1,3, 0,2));
+
+                  subelem[0]->set_node(0, elem->node_ptr(0));
+                  subelem[0]->set_node(1, elem->node_ptr(1));
+                  subelem[0]->set_node(2, elem->node_ptr(3));
+                  subelem[0]->set_node(3, elem->node_ptr(4));
+
+                  subelem[1]->set_node(0, elem->node_ptr(1));
+                  subelem[1]->set_node(1, elem->node_ptr(2));
+                  subelem[1]->set_node(2, elem->node_ptr(3));
+                  subelem[1]->set_node(3, elem->node_ptr(4));
+                }
+
+              break;
+            }
+
           case C0POLYGON:
             {
               // Split a C0Polygon into the triangles defined by its
