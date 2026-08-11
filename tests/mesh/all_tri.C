42af21ce00b611cf09f9ce1c892b83bf64d630cf
Roy Stogner <Roy.Stogner@inl.gov>
2026-08-11 09:31:49 -0500

    Add all_tri()+Pyramid5 unit test

--- tests/mesh/all_tri.C
+++ tests/mesh/all_tri.C
@@ -45,6 +45,7 @@ public:
   CPPUNIT_TEST( testAllTriPrism18 );
   CPPUNIT_TEST( testAllTriPrism20 );
   CPPUNIT_TEST( testAllTriPrism21 );
+  CPPUNIT_TEST( testAllTriPyramid5 );
   CPPUNIT_TEST( testAllTriC0PolyhedronCube );
   CPPUNIT_TEST( testAllTriC0PolyhedronHexagonalPrism );
 #endif
@@ -123,6 +124,9 @@ public:
   void testAllTriPrism18() { LOG_UNIT_TEST; test_helper_3D(PRISM18, /*nelem=*/6, /*nbcs=*/12); }
   void testAllTriPrism20() { LOG_UNIT_TEST; test_helper_3D(PRISM20, /*nelem=*/6, /*nbcs=*/12); }
   void testAllTriPrism21() { LOG_UNIT_TEST; test_helper_3D(PRISM21, /*nelem=*/6, /*nbcs=*/12); }
+
+  // 6 PYRAMIDs split into 12 TETs with 2 boundary faces per side
+  void testAllTriPyramid5() { LOG_UNIT_TEST; test_helper_3D(PYRAMID5, /*nelem=*/12, /*nbcs=*/12); }
 
   // Build a C0Polygon paving (triangles, quads, hexagons) via
   // build_square and split it into a pure TRI3 mesh.
